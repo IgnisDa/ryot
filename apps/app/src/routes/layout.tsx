976ebc9966a24fd6de60610c24b4ef55630f9542
Diptesh Choudhuri <ignisda2001@gmail.com>
2023-04-03 22:50:59 +0530

    feat(app): add basic application

---
+++ apps/app/src/routes/layout.tsx
@@ -0,0 +1,27 @@
+import { component$, Slot } from '@builder.io/qwik';
+import { routeLoader$ } from '@builder.io/qwik-city';
+
+import Header from '~/components/starter/header/header';
+import Footer from '~/components/starter/footer/footer';
+
+export const useServerTimeLoader = routeLoader$(() => {
+  return {
+    date: new Date().toISOString(),
+  };
+});
+
+export default component$(() => {
+  return (
+    <div class="page">
+      <main>
+        <Header />
+        <Slot />
+      </main>
+      <div class="section dark">
+        <div class="container">
+          <Footer />
+        </div>
+      </div>
+    </div>
+  );
+});
