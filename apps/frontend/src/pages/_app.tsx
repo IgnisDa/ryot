f7fabe244404f34b6907214145007b0504ca3821
Diptesh Choudhuri <ignisda2001@gmail.com>
2023-04-10 16:23:21 +0530

    feat(frontend): add basic about page

--- apps/frontend/src/pages/_app.tsx
+++ apps/frontend/src/pages/_app.tsx
@@ -1,6 +1,13 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import { Inter } from "next/font/google";
+
+const inter = Inter({ subsets: ["latin"] });
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+	return (
+		<div className={`${inter.className}`}>
+			<Component {...pageProps} />
+		</div>
+	);
 }
