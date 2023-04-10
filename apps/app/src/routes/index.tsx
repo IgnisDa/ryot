9f8d74772aa9fedde7640219976f7750e20a3d2b
Diptesh Choudhuri <ignisda2001@gmail.com>
2023-04-10 09:06:26 +0530

    feat(app): add basic data fetching

--- apps/app/src/routes/index.tsx
+++ apps/app/src/routes/index.tsx
@@ -1,20 +1,24 @@
-import { component$ } from "@builder.io/qwik";
+import { component$, useSignal, useVisibleTask$ } from "@builder.io/qwik";
 import type { DocumentHead } from "@builder.io/qwik-city";
 
 export default component$(() => {
+	const data = useSignal();
+
+	useVisibleTask$(async (_tc) => {
+		const res = await fetch(`https://api.randomuser.me/?page=${Math.random()}`);
+		const json = await res.json();
+		const b = json.results[0];
+		data.value = b;
+	});
+
 	return (
 		<>
-			<div class="section">This is the index page</div>
+			<div class="text-red-400 bg-yellow-700">This is the index page</div>
+			<div>{JSON.stringify(data)}</div>
 		</>
 	);
 });
 
 export const head: DocumentHead = {
-	title: "Welcome to Qwik",
-	meta: [
-		{
-			name: "description",
-			content: "Qwik site description",
-		},
-	],
+	title: "Trackona | Dashboard",
 };
