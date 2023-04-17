a3a285b9200363b37ae5f9f013ba716dd700141e
Diptesh Choudhuri <ignisda2001@gmail.com>
2023-04-17 16:43:49 +0530

    feat(backend): allow loading default config

--- apps/backend/src/config.rs
+++ apps/backend/src/config.rs
@@ -1,5 +1,5 @@
 use figment::{
-    providers::{Env, Format, Json, Toml, Yaml},
+    providers::{Env, Format, Json, Serialized, Toml, Yaml},
     Figment,
 };
 use serde::{Deserialize, Serialize};
@@ -9,20 +9,18 @@ pub struct DatabaseConfig {
     pub url: String,
 }
 
-#[derive(Deserialize, Debug, Clone, Serialize, Default)]
+#[derive(Deserialize, Debug, Clone, Serialize)]
 pub struct OpenlibraryConfig {
-    #[serde(default = "OpenlibraryConfig::api_url")]
     pub url: String,
-    #[serde(default = "OpenlibraryConfig::cover_image_url")]
     pub cover_image: String,
 }
 
-impl OpenlibraryConfig {
-    pub fn api_url() -> String {
-        "https://openlibrary.org".to_owned()
-    }
-    pub fn cover_image_url() -> String {
-        "https://covers.openlibrary.org/b".to_owned()
+impl Default for OpenlibraryConfig {
+    fn default() -> Self {
+        Self {
+            url: "https://openlibrary.org".to_owned(),
+            cover_image: "https://covers.openlibrary.org/b".to_owned(),
+        }
     }
 }
 
@@ -31,14 +29,16 @@ pub struct BookConfig {
     pub openlibrary: OpenlibraryConfig,
 }
 
-// #[derive(Deserialize, Debug, Clone, Serialize, Serialize)]
-// pub struct SchedulerConfig {}
+#[derive(Deserialize, Debug, Clone, Serialize, Default)]
+pub struct SchedulerConfig {}
 
 #[derive(Deserialize, Debug, Clone, Serialize, Default)]
 pub struct AppConfig {
     pub database: DatabaseConfig,
+    #[serde(default)]
     pub books: BookConfig,
-    // pub scheduler: SchedulerConfig,
+    #[serde(default)]
+    pub scheduler: SchedulerConfig,
 }
 
 /// Get the figment configuration that is used across the apps.
@@ -46,6 +46,7 @@ pub fn get_figment_config() -> Figment {
     let config = "config";
     let app = "trackona";
     Figment::new()
+        .merge(Serialized::defaults(AppConfig::default()))
         .merge(Env::raw().split("_"))
         .merge(Json::file(format!("{config}/{app}.json")))
         .merge(Toml::file(format!("{config}/{app}.toml")))
