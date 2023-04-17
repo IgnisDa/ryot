807b7f18e50d742ed6868609772c87f6bb8b4ca3
Diptesh Choudhuri <ignisda2001@gmail.com>
2023-04-18 00:55:28 +0530

    feat(backend): complete login endpoint

--- apps/backend/src/users/resolver.rs
+++ apps/backend/src/users/resolver.rs
@@ -1,15 +1,18 @@
-use async_graphql::{Context, Error, Object, Result};
+use async_graphql::{Context, Enum, Object, Result, SimpleObject, Union};
 use scrypt::{
-    password_hash::{rand_core::OsRng, PasswordHasher, SaltString},
+    password_hash::{rand_core::OsRng, PasswordHash, PasswordHasher, PasswordVerifier, SaltString},
     Scrypt,
 };
 use sea_orm::{
-    ActiveValue, ColumnTrait, DatabaseConnection, EntityTrait, PaginatorTrait, QueryFilter,
+    ActiveModelTrait, ActiveValue, ColumnTrait, DatabaseConnection, EntityTrait, PaginatorTrait,
+    QueryFilter, Set,
 };
+use uuid::Uuid;
 
 use crate::{
     entities::{prelude::User, user},
-    migrator::UserLot,
+    graphql::IdObject,
+    migrator::{StringVec, UserLot},
 };
 
 #[derive(Default)]
@@ -17,17 +20,30 @@ pub struct UsersMutation;
 
 #[Object]
 impl UsersMutation {
-    /// Create a new user for the service. Also set their status as admin if
+    /// Create a new user for the service. Also set their `lot` as admin if
     /// they are the first user.
     async fn register_user(
         &self,
         gql_ctx: &Context<'_>,
         username: String,
         #[graphql(secret)] password: String,
-    ) -> Result<i32> {
+    ) -> Result<RegisterResult> {
         gql_ctx
             .data_unchecked::<UsersService>()
             .register_user(&username, &password)
+            .await
+    }
+
+    /// Login a user using their username and password and return an API key.
+    async fn login_user(
+        &self,
+        gql_ctx: &Context<'_>,
+        username: String,
+        #[graphql(secret)] password: String,
+    ) -> Result<LoginResult> {
+        gql_ctx
+            .data_unchecked::<UsersService>()
+            .login_user(&username, &password)
             .await
     }
 }
@@ -44,7 +60,7 @@ impl UsersService {
 }
 
 impl UsersService {
-    async fn register_user(&self, username: &str, password: &str) -> Result<i32> {
+    async fn register_user(&self, username: &str, password: &str) -> Result<RegisterResult> {
         if User::find()
             .filter(user::Column::Name.eq(username))
             .count(&self.db)
@@ -52,9 +68,9 @@ impl UsersService {
             .unwrap()
             != 0
         {
-            return Err(Error::new(
-                "A user with this username already exists".to_owned(),
-            ));
+            return Ok(RegisterResult::Error(RegisterError {
+                error: RegisterErrorVariant::UsernameAlreadyExists,
+            }));
         };
         let salt = SaltString::generate(&mut OsRng);
         let password_hash = Scrypt
@@ -72,6 +88,76 @@ impl UsersService {
             ..Default::default()
         };
         let user = User::insert(user).exec(&self.db).await.unwrap();
-        Ok(user.last_insert_id)
+        Ok(RegisterResult::Ok(IdObject {
+            id: user.last_insert_id,
+        }))
     }
+
+    async fn login_user(&self, username: &str, password: &str) -> Result<LoginResult> {
+        let user = User::find()
+            .filter(user::Column::Name.eq(username))
+            .one(&self.db)
+            .await
+            .unwrap();
+        if user.is_none() {
+            return Ok(LoginResult::Error(LoginError {
+                error: LoginErrorVariant::UsernameDoesNotExist,
+            }));
+        };
+        let user = user.unwrap();
+        let parsed_hash = PasswordHash::new(&user.password).unwrap();
+        if Scrypt
+            .verify_password(password.as_bytes(), &parsed_hash)
+            .is_err()
+        {
+            return Ok(LoginResult::Error(LoginError {
+                error: LoginErrorVariant::CredentialsMismatch,
+            }));
+        }
+        let api_key = Uuid::new_v4();
+        let mut user: user::ActiveModel = user.into();
+        let mut all_keys = user.api_keys.clone().unwrap().0;
+        all_keys.push(api_key.to_string());
+        user.api_keys = Set(StringVec(all_keys));
+        user.update(&self.db).await.unwrap();
+        Ok(LoginResult::Ok(LoginResponse { api_key }))
+    }
+}
+
+#[derive(Enum, Clone, Debug, Copy, PartialEq, Eq)]
+enum RegisterErrorVariant {
+    UsernameAlreadyExists,
+}
+
+#[derive(Debug, SimpleObject)]
+struct RegisterError {
+    error: RegisterErrorVariant,
+}
+
+#[derive(Union)]
+enum RegisterResult {
+    Ok(IdObject),
+    Error(RegisterError),
+}
+
+#[derive(Enum, Clone, Debug, Copy, PartialEq, Eq)]
+enum LoginErrorVariant {
+    UsernameDoesNotExist,
+    CredentialsMismatch,
+}
+
+#[derive(Debug, SimpleObject)]
+struct LoginError {
+    error: LoginErrorVariant,
+}
+
+#[derive(Debug, SimpleObject)]
+struct LoginResponse {
+    api_key: Uuid,
+}
+
+#[derive(Union)]
+enum LoginResult {
+    Ok(LoginResponse),
+    Error(LoginError),
 }
