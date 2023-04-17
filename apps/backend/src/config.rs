e78c2147af2305178f8b33c2ad8ff8cd9be28548
Diptesh Choudhuri <ignisda2001@gmail.com>
2023-04-17 07:48:40 +0530

    feat(backend): add openlibrary config vars

--- apps/backend/src/config.rs
+++ apps/backend/src/config.rs
@@ -1,14 +1,32 @@
 use figment::{providers::Env, Figment};
-use serde::Deserialize;
+use serde::{Deserialize, Serialize};
 
-#[derive(Deserialize, Debug, Clone, Default)]
+#[derive(Deserialize, Debug, Clone, Serialize, Default)]
 pub struct DatabaseConfig {
     pub url: String,
 }
 
-#[derive(Deserialize, Debug, Clone, Default)]
+#[derive(Deserialize, Debug, Clone, Serialize, Default)]
+pub struct OpenlibraryConfig {
+    #[serde(default = "OpenlibraryConfig::url")]
+    pub url: String,
+}
+
+impl OpenlibraryConfig {
+    pub fn url() -> String {
+        "https://openlibrary.org".to_owned()
+    }
+}
+
+#[derive(Deserialize, Debug, Clone, Serialize, Default)]
+pub struct BookConfig {
+    pub openlibrary: OpenlibraryConfig,
+}
+
+#[derive(Deserialize, Debug, Clone, Serialize, Default)]
 pub struct AppConfig {
     pub db: DatabaseConfig,
+    pub book: BookConfig,
 }
 
 /// Get the figment configuration that is used across the apps.
