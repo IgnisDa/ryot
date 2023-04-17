ab221567da3e1ecd372bb57a186fe9b7c16f5856
Diptesh Choudhuri <ignisda2001@gmail.com>
2023-04-18 00:12:52 +0530

    feat(backend): complete register user service

--- apps/backend/src/users/resolver.rs
+++ apps/backend/src/users/resolver.rs
@@ -1,7 +1,16 @@
-use std::sync::Arc;
+use async_graphql::{Context, Error, Object, Result};
+use scrypt::{
+    password_hash::{rand_core::OsRng, PasswordHasher, SaltString},
+    Scrypt,
+};
+use sea_orm::{
+    ActiveValue, ColumnTrait, DatabaseConnection, EntityTrait, PaginatorTrait, QueryFilter,
+};
 
-use async_graphql::{Context, Object, Result};
-use sea_orm::DatabaseConnection;
+use crate::{
+    entities::{prelude::User, user},
+    migrator::UserLot,
+};
 
 #[derive(Default)]
 pub struct UsersMutation;
@@ -14,8 +23,8 @@ impl UsersMutation {
         &self,
         gql_ctx: &Context<'_>,
         username: String,
-        password: String,
-    ) -> Result<bool> {
+        #[graphql(secret)] password: String,
+    ) -> Result<i32> {
         gql_ctx
             .data_unchecked::<UsersService>()
             .register_user(&username, &password)
@@ -25,19 +34,44 @@ impl UsersMutation {
 
 #[derive(Debug)]
 pub struct UsersService {
-    db: Arc<DatabaseConnection>,
+    db: DatabaseConnection,
 }
 
 impl UsersService {
     pub fn new(db: &DatabaseConnection) -> Self {
-        Self {
-            db: Arc::new(db.clone()),
-        }
+        Self { db: db.clone() }
     }
 }
 
 impl UsersService {
-    async fn register_user(&self, username: &str, password: &str) -> Result<bool> {
-        Ok(true)
+    async fn register_user(&self, username: &str, password: &str) -> Result<i32> {
+        if User::find()
+            .filter(user::Column::Name.eq(username))
+            .count(&self.db)
+            .await
+            .unwrap()
+            != 0
+        {
+            return Err(Error::new(
+                "A user with this username already exists".to_owned(),
+            ));
+        };
+        let salt = SaltString::generate(&mut OsRng);
+        let password_hash = Scrypt
+            .hash_password(password.as_bytes(), &salt)?
+            .to_string();
+        let lot = if User::find().count(&self.db).await.unwrap() == 0 {
+            UserLot::Admin
+        } else {
+            UserLot::Normal
+        };
+        let user = user::ActiveModel {
+            name: ActiveValue::Set(username.to_owned()),
+            password: ActiveValue::Set(password_hash.to_owned()),
+            lot: ActiveValue::Set(lot),
+            ..Default::default()
+        };
+        let user = User::insert(user).exec(&self.db).await.unwrap();
+        Ok(user.last_insert_id)
     }
 }
