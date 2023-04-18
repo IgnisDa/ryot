809ff0ed388f800b28f6633932a72b57c749ed71
Diptesh Choudhuri <ignisda2001@gmail.com>
2023-04-18 06:44:20 +0530

    feat(frontend): add page title

--- apps/frontend/src/pages/_app.tsx
+++ apps/frontend/src/pages/_app.tsx
@@ -1,13 +1,19 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { Inter } from "next/font/google";
+import Head from "next/head";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function App({ Component, pageProps }: AppProps) {
 	return (
-		<div className={`${inter.className}`}>
-			<Component {...pageProps} />
-		</div>
+		<>
+			<Head>
+				<title>Trackona</title>
+			</Head>
+			<div className={`${inter.className}`}>
+				<Component {...pageProps} />
+			</div>
+		</>
 	);
 }
