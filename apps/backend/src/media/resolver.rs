e07e8db5116e54201a3dead5125241248eb018c7
Diptesh Choudhuri <ignisda2001@gmail.com>
2023-04-20 14:25:06 +0530

    style(backend): use generics to reduce code duplication

--- apps/backend/src/media/resolver.rs
+++ apps/backend/src/media/resolver.rs
@@ -1,14 +1,23 @@
-use async_graphql::{Context, Error, Object, Result, SimpleObject};
+use async_graphql::{Context, Error, Object, OutputType, Result, SimpleObject};
 use sea_orm::{DatabaseConnection, EntityTrait, ModelTrait};
 use serde::{Deserialize, Serialize};
 
 use crate::{
-    entities::prelude::{Book, Creator, Metadata, MetadataImage},
+    entities::{
+        metadata::Model as MetadataModel,
+        prelude::{Book, Creator, Metadata, MetadataImage},
+    },
     migrator::MetadataLot,
 };
 
 #[derive(Debug, Serialize, Deserialize, SimpleObject, Clone)]
-pub struct BookDetails {
+pub struct BookSpecifics {
+    pub pages: Option<i32>,
+}
+
+#[derive(Debug, Serialize, Deserialize, SimpleObject, Clone)]
+#[graphql(concrete(name = "BookDetails", params(BookSpecifics)))]
+pub struct MediaDetails<T: OutputType> {
     pub id: i32,
     pub title: String,
     pub description: Option<String>,
@@ -17,7 +26,7 @@ pub struct BookDetails {
     pub creators: Vec<String>,
     pub images: Vec<String>,
     pub publish_year: Option<i32>,
-    pub pages: Option<i32>,
+    pub specifics: T,
 }
 
 #[derive(Default)]
@@ -26,7 +35,11 @@ pub struct MediaQuery;
 #[Object]
 impl MediaQuery {
     // Get details about a book present in the database
-    async fn book_details(&self, gql_ctx: &Context<'_>, metadata_id: i32) -> Result<BookDetails> {
+    async fn book_details(
+        &self,
+        gql_ctx: &Context<'_>,
+        metadata_id: i32,
+    ) -> Result<MediaDetails<BookSpecifics>> {
         gql_ctx
             .data_unchecked::<MediaService>()
             .book_details(metadata_id)
@@ -46,7 +59,10 @@ impl MediaService {
 }
 
 impl MediaService {
-    async fn book_details(&self, metadata_id: i32) -> Result<BookDetails> {
+    async fn generic_metadata(
+        &self,
+        metadata_id: i32,
+    ) -> Result<(MetadataModel, Vec<String>, Vec<String>)> {
         let meta = match Metadata::find_by_id(metadata_id)
             .one(&self.db)
             .await
@@ -55,26 +71,43 @@ impl MediaService {
             Some(m) => m,
             None => return Err(Error::new("The record does not exit".to_owned())),
         };
-        let creators = meta.find_related(Creator).all(&self.db).await.unwrap();
+        let creators = meta
+            .find_related(Creator)
+            .all(&self.db)
+            .await
+            .unwrap()
+            .into_iter()
+            .map(|c| c.name)
+            .collect();
         let images = meta
             .find_related(MetadataImage)
             .all(&self.db)
             .await
-            .unwrap();
+            .unwrap()
+            .into_iter()
+            .map(|i| i.url)
+            .collect();
+        Ok((meta, creators, images))
+    }
+
+    async fn book_details(&self, metadata_id: i32) -> Result<MediaDetails<BookSpecifics>> {
+        let (meta, creators, images) = self.generic_metadata(metadata_id).await?;
         let book = Book::find_by_id(metadata_id)
             .one(&self.db)
             .await
             .unwrap()
             .unwrap();
-        let resp = BookDetails {
+        let resp = MediaDetails {
             id: meta.id,
             title: meta.title,
             description: meta.description,
-            lot: meta.lot,
-            creators: creators.into_iter().map(|c| c.name).collect(),
-            images: images.into_iter().map(|i| i.url).collect(),
             publish_year: meta.publish_year,
-            pages: book.num_pages,
+            lot: meta.lot,
+            creators,
+            images,
+            specifics: BookSpecifics {
+                pages: book.num_pages,
+            },
         };
         Ok(resp)
     }
