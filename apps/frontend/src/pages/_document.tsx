fc067a8514c7cc571df969cc5f9b12e2c08eaeb1
Diptesh Choudhuri <ignisda2001@gmail.com>
2023-05-15 14:46:17 +0530

    feat(frontend): set poppins as default font

--- apps/frontend/src/pages/_document.tsx
+++ apps/frontend/src/pages/_document.tsx
@@ -9,7 +9,12 @@ export default class _Document extends Document {
 	override render() {
 		return (
 			<Html>
-				<Head />
+				<Head>
+					<link
+						href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap"
+						rel="stylesheet"
+					/>
+				</Head>
 				<body>
 					<Main />
 					<NextScript />
