e5ff9021d90a3da4b4f8a4e4449d8a5056b78723
Diptesh Choudhuri <ignisda2001@gmail.com>
2023-05-16 22:19:27 +0530

    feat(frontend): minimal PWA setup

--- apps/frontend/src/pages/_app.tsx
+++ apps/frontend/src/pages/_app.tsx
@@ -64,6 +64,10 @@ export default function App({ Component, pageProps }: AppPropsWithLayout) {
 		<>
 			<Head>
 				<title>Ryot</title>
+				<meta
+					name="viewport"
+					content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, user-scalable=no, viewport-fit=cover"
+				/>
 			</Head>
 			<QueryClientProvider client={queryClient}>
 				<MantineProvider
