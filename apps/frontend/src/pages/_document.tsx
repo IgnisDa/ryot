bc2912f863217cf3cc7a06c809eafb7ed4afc7fc
Diptesh Choudhuri <ignisda2001@gmail.com>
2023-05-17 08:40:38 +0530

    feat(frontend): add favicon

--- apps/frontend/src/pages/_document.tsx
+++ apps/frontend/src/pages/_document.tsx
@@ -14,6 +14,23 @@ export default class _Document extends Document {
 						href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap"
 						rel="stylesheet"
 					/>
+					<link
+						rel="apple-touch-icon"
+						sizes="180x180"
+						href="/apple-touch-icon.png"
+					/>
+					<link
+						rel="icon"
+						type="image/png"
+						sizes="32x32"
+						href="/favicon-32x32.png"
+					/>
+					<link
+						rel="icon"
+						type="image/png"
+						sizes="16x16"
+						href="/favicon-16x16.png"
+					/>
 				</Head>
 				<body>
 					<Main />
