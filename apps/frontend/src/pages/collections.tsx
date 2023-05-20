4c4d8b5bd685088577831f858a361d6efaea9c5d
Diptesh Choudhuri <ignisda2001@gmail.com>
2023-05-20 23:43:01 +0530

    fix(frontend): add title tag to collections page

--- apps/frontend/src/pages/collections.tsx
+++ apps/frontend/src/pages/collections.tsx
@@ -11,6 +11,7 @@ import {
 } from "@ryot/generated/graphql/backend/graphql";
 import { IconAlertCircle } from "@tabler/icons-react";
 import { useMutation, useQuery } from "@tanstack/react-query";
+import Head from "next/head";
 import type { ReactElement } from "react";
 
 const Page: NextPageWithLayout = () => {
@@ -34,52 +35,59 @@ const Page: NextPageWithLayout = () => {
 	});
 
 	return (
-		<Container>
-			<Stack>
-				{collections.data && collections.data.length > 0 ? (
-					collections.data.map((collection) => (
-						<Stack key={collection.collectionDetails.id}>
-							<Title order={3} truncate>
-								{collection.collectionDetails.name}
-							</Title>
-							{collection.mediaDetails.length > 0 ? (
-								<Grid>
-									{collection.mediaDetails.map((mediaItem) => (
-										<MediaItemWithoutUpdateModal
-											key={mediaItem.identifier}
-											item={mediaItem}
-											lot={mediaItem.lot}
-											imageOnClick={async () => parseInt(mediaItem.identifier)}
-										>
-											<Button
-												fullWidth
-												color="red"
-												variant="outline"
-												onClick={() => {
-													removeMediaFromCollection.mutate({
-														collectionName: collection.collectionDetails.name,
-														metadataId: Number(mediaItem.identifier),
-													});
-												}}
+		<>
+			<Head>
+				<title>Collections | Ryot</title>
+			</Head>
+			<Container>
+				<Stack>
+					{collections.data && collections.data.length > 0 ? (
+						collections.data.map((collection) => (
+							<Stack key={collection.collectionDetails.id}>
+								<Title order={3} truncate>
+									{collection.collectionDetails.name}
+								</Title>
+								{collection.mediaDetails.length > 0 ? (
+									<Grid>
+										{collection.mediaDetails.map((mediaItem) => (
+											<MediaItemWithoutUpdateModal
+												key={mediaItem.identifier}
+												item={mediaItem}
+												lot={mediaItem.lot}
+												imageOnClick={async () =>
+													parseInt(mediaItem.identifier)
+												}
 											>
-												Remove
-											</Button>
-										</MediaItemWithoutUpdateModal>
-									))}
-								</Grid>
-							) : (
-								<Text>No items in this collection</Text>
-							)}
-						</Stack>
-					))
-				) : (
-					<Alert color="yellow" icon={<IconAlertCircle size="1rem" />}>
-						You do not have any collections. You can create and add media to
-						collections from a media's details page.
-					</Alert>
-				)}
-			</Stack>
-		</Container>
+												<Button
+													fullWidth
+													color="red"
+													variant="outline"
+													onClick={() => {
+														removeMediaFromCollection.mutate({
+															collectionName: collection.collectionDetails.name,
+															metadataId: Number(mediaItem.identifier),
+														});
+													}}
+												>
+													Remove
+												</Button>
+											</MediaItemWithoutUpdateModal>
+										))}
+									</Grid>
+								) : (
+									<Text>No items in this collection</Text>
+								)}
+							</Stack>
+						))
+					) : (
+						<Alert color="yellow" icon={<IconAlertCircle size="1rem" />}>
+							You do not have any collections. You can create and add media to
+							collections from a media's details page.
+						</Alert>
+					)}
+				</Stack>
+			</Container>
+		</>
 	);
 };
 
