c9524a1174d26274cec7dafcd1ceceb6ea5999f8
Diptesh Choudhuri <ignisda2001@gmail.com>
2023-06-02 18:48:02 +0530

    feat(frontend): allow reseting filter

--- apps/frontend/src/pages/list.tsx
+++ apps/frontend/src/pages/list.tsx
@@ -13,6 +13,7 @@ import {
 	Center,
 	Container,
 	Flex,
+	Group,
 	Grid as MantineGrid,
 	Modal,
 	Pagination,
@@ -43,6 +44,7 @@ import {
 } from "@ryot/generated/graphql/backend/graphql";
 import {
 	IconFilter,
+	IconFilterOff,
 	IconListCheck,
 	IconRefresh,
 	IconSearch,
@@ -60,6 +62,12 @@ import { match } from "ts-pattern";
 
 const LIMIT = 20;
 
+const defaultFilters = {
+	mineFilter: MediaFilter.All,
+	mineSortOrder: MediaSortOrder.Asc,
+	mineSortBy: MediaSortBy.ReleaseDate,
+};
+
 const Page: NextPageWithLayout = () => {
 	const [
 		filtersModalOpened,
@@ -67,17 +75,17 @@ const Page: NextPageWithLayout = () => {
 	] = useDisclosure(false);
 	const [mineSortOrder, setMineSortOrder] = useLocalStorage({
 		key: "mineSortOrder",
-		defaultValue: MediaSortOrder.Asc,
+		defaultValue: defaultFilters.mineSortOrder,
 		getInitialValueInEffect: false,
 	});
 	const [mineSortBy, setMineSortBy] = useLocalStorage({
 		key: "mineSortBy",
-		defaultValue: MediaSortBy.ReleaseDate,
+		defaultValue: defaultFilters.mineSortBy,
 		getInitialValueInEffect: false,
 	});
 	const [mineFilter, setMineFilter] = useLocalStorage({
 		key: "mineFilter",
-		defaultValue: MediaFilter.All,
+		defaultValue: defaultFilters.mineFilter,
 		getInitialValueInEffect: false,
 	});
 	const [activeSearchPage, setSearchPage] = useLocalStorage({
@@ -203,9 +211,15 @@ const Page: NextPageWithLayout = () => {
 		) : null;
 
 	const isFilterChanged =
-		mineFilter !== MediaFilter.All ||
-		mineSortOrder !== MediaSortOrder.Asc ||
-		mineSortBy !== MediaSortBy.ReleaseDate;
+		mineFilter !== defaultFilters.mineFilter ||
+		mineSortOrder !== defaultFilters.mineSortOrder ||
+		mineSortBy !== defaultFilters.mineSortBy;
+
+	const resetFilters = () => {
+		setMineFilter(defaultFilters.mineFilter);
+		setMineSortOrder(defaultFilters.mineSortOrder);
+		setMineSortBy(defaultFilters.mineSortBy);
+	};
 
 	return lot ? (
 		<>
@@ -267,14 +281,19 @@ const Page: NextPageWithLayout = () => {
 											withCloseButton={false}
 										>
 											<Stack>
-												<Title order={3}>Filters</Title>
+												<Group>
+													<Title order={3}>Filters</Title>
+													<ActionIcon onClick={resetFilters}>
+														<IconFilterOff size="1.5rem" />
+													</ActionIcon>
+												</Group>
 												<Select
 													withinPortal
+													value={mineFilter.toString()}
 													data={Object.values(MediaFilter).map((o) => ({
 														value: o.toString(),
 														label: startCase(lowerCase(o)),
 													}))}
-													defaultValue={mineFilter.toString()}
 													onChange={(v) => {
 														const filter = match(v)
 															.with("ALL", () => MediaFilter.All)
@@ -292,7 +311,7 @@ const Page: NextPageWithLayout = () => {
 															value: o.toString(),
 															label: startCase(lowerCase(o)),
 														}))}
-														defaultValue={mineSortBy.toString()}
+														value={mineSortBy.toString()}
 														onChange={(v) => {
 															const orderBy = match(v)
 																.with(
