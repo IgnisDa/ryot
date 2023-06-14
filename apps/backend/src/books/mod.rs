a6f10bb3549e29f9bc594b6fa70558e1ae3ee54c
Diptesh Choudhuri <ignisda2001@gmail.com>
2023-06-14 19:08:46 +0530

    Revert "refactor(backend): move media code to common resolver"
    This reverts commit 0a785c0785b803cf42e6644dc32a25b1dde86718.

--- apps/backend/src/books/mod.rs
+++ apps/backend/src/books/mod.rs
@@ -2,3 +2,11 @@ use async_graphql::{InputObject, SimpleObject};
 use serde::{Deserialize, Serialize};
 
 pub mod openlibrary;
+
+#[derive(
+    Debug, Serialize, Deserialize, SimpleObject, Clone, InputObject, PartialEq, Eq, Default,
+)]
+#[graphql(input_name = "BookSpecificsInput")]
+pub struct BookSpecifics {
+    pub pages: Option<i32>,
+}
--- apps/backend/src/movies/mod.rs
+++ apps/backend/src/movies/mod.rs
@@ -2,3 +2,11 @@ use async_graphql::{InputObject, SimpleObject};
 use serde::{Deserialize, Serialize};
 
 pub mod tmdb;
+
+#[derive(
+    Debug, Serialize, Deserialize, SimpleObject, Clone, InputObject, Eq, PartialEq, Default,
+)]
+#[graphql(input_name = "MovieSpecificsInput")]
+pub struct MovieSpecifics {
+    pub runtime: Option<i32>,
+}
