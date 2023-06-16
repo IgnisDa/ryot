d155472f99f3b53b229585702be8a07acc9f8b7e
Diptesh Choudhuri <ignisda2001@gmail.com>
2023-06-16 12:47:42 +0530

    feat(backend): complete anime search

--- apps/backend/src/providers/anilist.rs
+++ apps/backend/src/providers/anilist.rs
@@ -40,53 +40,12 @@ impl MediaProvider for AnilistAnimeService {
     }
 
     async fn search(&self, query: &str, page: Option<i32>) -> Result<MediaSearchResults> {
-        let page = page.unwrap_or(1);
-        let variables = search_query::Variables {
-            page: page.into(),
-            search: query.to_owned(),
-            type_: search_query::MediaType::ANIME,
-            per_page: PAGE_LIMIT.into(),
-        };
-        let body = SearchQuery::build_query(variables);
-        let search = self
-            .client
-            .post("")
-            .body_json(&body)
-            .unwrap()
-            .send()
-            .await
-            .map_err(|e| anyhow!(e))?
-            .body_json::<Response<search_query::ResponseData>>()
-            .await
-            .map_err(|e| anyhow!(e))?
-            .data
-            .unwrap()
-            .page
-            .unwrap();
-        let total = search.page_info.unwrap().total.unwrap().try_into().unwrap();
-        let next_page = if total - (page * PAGE_LIMIT) > 0 {
-            Some(page + 1)
-        } else {
-            None
-        };
+        let (items, total, next_page) =
+            utils::search(&self.client, search_query::MediaType::ANIME, query, page).await?;
         Ok(MediaSearchResults {
             total,
             next_page,
-            items: search
-                .media
-                .unwrap()
-                .into_iter()
-                .map(|b| {
-                    let b = b.unwrap();
-                    MediaSearchItem {
-                        identifier: b.id.to_string(),
-                        lot: MetadataLot::Anime,
-                        title: b.title.unwrap().user_preferred.unwrap(),
-                        images: Vec::from_iter(b.banner_image),
-                        publish_year: b.season_year.map(|b| b.try_into().unwrap()),
-                    }
-                })
-                .collect(),
+            items,
         })
     }
 }
@@ -135,5 +94,55 @@ mod utils {
             .try_into()
             .unwrap();
         client
+    }
+
+    pub async fn search(
+        client: &Client,
+        media_type: search_query::MediaType,
+        query: &str,
+        page: Option<i32>,
+    ) -> Result<(Vec<MediaSearchItem>, i32, Option<i32>)> {
+        let page = page.unwrap_or(1);
+        let variables = search_query::Variables {
+            page: page.into(),
+            search: query.to_owned(),
+            type_: media_type,
+            per_page: PAGE_LIMIT.into(),
+        };
+        let body = SearchQuery::build_query(variables);
+        let search = client
+            .post("")
+            .body_json(&body)
+            .unwrap()
+            .send()
+            .await
+            .map_err(|e| anyhow!(e))?
+            .body_json::<Response<search_query::ResponseData>>()
+            .await
+            .map_err(|e| anyhow!(e))?
+            .data
+            .unwrap()
+            .page
+            .unwrap();
+        let total = search.page_info.unwrap().total.unwrap().try_into().unwrap();
+        let next_page = if total - (page * PAGE_LIMIT) > 0 {
+            Some(page + 1)
+        } else {
+            None
+        };
+        let media = search
+            .media
+            .unwrap()
+            .into_iter()
+            .flatten()
+            .map(|b| MediaSearchItem {
+                identifier: b.id.to_string(),
+                lot: MetadataLot::Anime,
+                title: b.title.unwrap().user_preferred.unwrap(),
+                images: Vec::from_iter(b.banner_image),
+                publish_year: b.season_year.map(|b| b.try_into().unwrap()),
+            })
+            .collect();
+        Ok((media, total, next_page))
     }
 }
