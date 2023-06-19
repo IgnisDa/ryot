94583f6907ed14b51910204f47aee9a4c0e4422f
Diptesh Choudhuri <ignisda2001@gmail.com>
2023-06-19 19:44:35 +0530

    chore(graphql): adapt to new gql schema

--- libs/generated/src/graphql/backend/gql.ts
+++ libs/generated/src/graphql/backend/gql.ts
@@ -37,7 +37,7 @@ const documents = {
     "mutation UpdateUserPreferences($input: UpdateUserPreferencesInput!) {\n  updateUserPreferences(input: $input)\n}": types.UpdateUserPreferencesDocument,
     "query Collections {\n  collections {\n    collectionDetails {\n      id\n      createdOn\n      name\n    }\n    mediaDetails {\n      identifier\n      lot\n      title\n      images\n      publishYear\n    }\n  }\n}": types.CollectionsDocument,
     "query CoreDetails {\n  coreDetails {\n    version\n    authorName\n    repositoryLink\n    usernameChangeAllowed\n  }\n}": types.CoreDetailsDocument,
-    "query CoreEnabledFeatures {\n  coreEnabledFeatures {\n    fileStorage {\n      enabled\n    }\n    signupAllowed {\n      enabled\n    }\n  }\n}": types.CoreEnabledFeaturesDocument,
+    "query CoreEnabledFeatures {\n  coreEnabledFeatures {\n    fileStorage\n    signupAllowed\n  }\n}": types.CoreEnabledFeaturesDocument,
     "query GetPresignedUrl($key: String!) {\n  getPresignedUrl(key: $key)\n}": types.GetPresignedUrlDocument,
     "query MediaDetails($metadataId: Identifier!) {\n  mediaDetails(metadataId: $metadataId) {\n    title\n    description\n    identifier\n    lot\n    source\n    sourceUrl\n    creators {\n      name\n      role\n    }\n    posterImages\n    backdropImages\n    publishYear\n    publishDate\n    genres\n    animeSpecifics {\n      episodes\n    }\n    bookSpecifics {\n      pages\n    }\n    movieSpecifics {\n      runtime\n    }\n    mangaSpecifics {\n      volumes\n      chapters\n    }\n    podcastSpecifics {\n      episodes {\n        title\n        overview\n        thumbnail\n        number\n        runtime\n      }\n      totalEpisodes\n    }\n    showSpecifics {\n      seasons {\n        seasonNumber\n        name\n        overview\n        backdropImages\n        posterImages\n        episodes {\n          id\n          name\n          posterImages\n          episodeNumber\n          publishDate\n          name\n          overview\n          runtime\n        }\n      }\n    }\n    videoGameSpecifics {\n      platforms\n    }\n  }\n}": types.MediaDetailsDocument,
     "query MediaImportReports {\n  mediaImportReports {\n    id\n    source\n    startedOn\n    finishedOn\n    details {\n      import {\n        total\n      }\n      failedItems {\n        lot\n        step\n        identifier\n      }\n    }\n  }\n}": types.MediaImportReportsDocument,
@@ -163,7 +163,7 @@ export function graphql(source: "query CoreDetails {\n  coreDetails {\n    versi
 /**
  * The graphql function is used to parse GraphQL queries into a document that can be used by GraphQL clients.
  */
-export function graphql(source: "query CoreEnabledFeatures {\n  coreEnabledFeatures {\n    fileStorage {\n      enabled\n    }\n    signupAllowed {\n      enabled\n    }\n  }\n}"): (typeof documents)["query CoreEnabledFeatures {\n  coreEnabledFeatures {\n    fileStorage {\n      enabled\n    }\n    signupAllowed {\n      enabled\n    }\n  }\n}"];
+export function graphql(source: "query CoreEnabledFeatures {\n  coreEnabledFeatures {\n    fileStorage\n    signupAllowed\n  }\n}"): (typeof documents)["query CoreEnabledFeatures {\n  coreEnabledFeatures {\n    fileStorage\n    signupAllowed\n  }\n}"];
 /**
  * The graphql function is used to parse GraphQL queries into a document that can be used by GraphQL clients.
  */
