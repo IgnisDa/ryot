1367a5b0fe98e6f0ce505facba7614906ee37e63
Diptesh Choudhuri <ignisda2001@gmail.com>
2023-06-30 11:27:17 +0530

    feat(backend): get search google books working

--- apps/backend/src/providers/google_books.rs
+++ apps/backend/src/providers/google_books.rs
@@ -54,6 +54,82 @@ impl MediaProvider for GoogleBooksService {
     }
 
     async fn search(&self, query: &str, page: Option<i32>) -> Result<MediaSearchResults> {
-        todo!();
+        let page = page.unwrap_or(1);
+        let index = (page - 1) * PAGE_LIMIT;
+        #[derive(Serialize, Deserialize, Debug)]
+        #[serde(rename_all = "camelCase")]
+        struct ImageLinks {
+            small_thumbnail: Option<String>,
+            thumbnail: Option<String>,
+        }
+        #[derive(Serialize, Deserialize, Debug)]
+        #[serde(rename_all = "camelCase")]
+        struct ItemVolumeInfo {
+            title: String,
+            published_date: Option<String>,
+            image_links: Option<ImageLinks>,
+        }
+        #[derive(Serialize, Deserialize, Debug)]
+        #[serde(rename_all = "camelCase")]
+        struct ItemResponse {
+            id: String,
+            volume_info: ItemVolumeInfo,
+        }
+        #[derive(Serialize, Deserialize, Debug)]
+        #[serde(rename_all = "camelCase")]
+        struct SearchResponse {
+            total_items: i32,
+            items: Option<Vec<ItemResponse>>,
+        }
+        let mut rsp = self
+            .client
+            .get("")
+            .query(&serde_json::json!({
+                "q": format!("intitle:{}", query),
+                "maxResults": PAGE_LIMIT,
+                "printType": "books",
+                "startIndex": index
+            }))
+            .unwrap()
+            .await
+            .map_err(|e| anyhow!(e))?;
+        let search: SearchResponse = rsp.body_json().await.map_err(|e| anyhow!(e))?;
+        let resp = search
+            .items
+            .unwrap_or_default()
+            .into_iter()
+            .map(|b| {
+                let mut images = vec![];
+                if let Some(il) = b.volume_info.image_links {
+                    if let Some(a) = il.thumbnail {
+                        images.push(a);
+                    }
+                    if let Some(a) = il.small_thumbnail {
+                        images.push(a);
+                    }
+                }
+                MediaSearchItem {
+                    identifier: b.id,
+                    lot: MetadataLot::Book,
+                    title: b.volume_info.title,
+                    images,
+                    publish_year: b
+                        .volume_info
+                        .published_date
+                        .map(|d| convert_date_to_year(&d))
+                        .flatten(),
+                }
+            })
+            .collect();
+        let next_page = if search.total_items - ((page) * PAGE_LIMIT) > 0 {
+            Some(page + 1)
+        } else {
+            None
+        };
+        Ok(MediaSearchResults {
+            total: search.total_items,
+            items: resp,
+            next_page,
+        })
     }
 }
