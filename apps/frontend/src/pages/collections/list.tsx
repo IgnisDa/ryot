a929e7820cac2fcf410350e5c5344b505ad2c2dc
Diptesh Choudhuri <ignisda2001@gmail.com>
2023-07-08 18:55:39 +0530

    feat(frontend): allow creating new collections

--- apps/frontend/src/pages/collections/list.tsx
+++ apps/frontend/src/pages/collections/list.tsx
@@ -5,27 +5,73 @@ import { gqlClient } from "@/lib/services/api";
 import {
 	ActionIcon,
 	Box,
+	Button,
 	Container,
 	Flex,
+	Input,
+	Modal,
+	SegmentedControl,
 	Stack,
 	Text,
+	TextInput,
+	Textarea,
 	Title,
 } from "@mantine/core";
+import { useForm, zodResolver } from "@mantine/form";
+import { useDisclosure } from "@mantine/hooks";
 import { notifications } from "@mantine/notifications";
 import {
+	CreateCollectionDocument,
+	type CreateCollectionMutationVariables,
 	DeleteCollectionDocument,
 	type DeleteCollectionMutationVariables,
 	PartialCollectionsDocument,
+	Visibility,
 } from "@ryot/generated/graphql/backend/graphql";
-import { IconTrashFilled, IconWritingSign } from "@tabler/icons-react";
+import {
+	IconPlus,
+	IconTrashFilled,
+	IconWritingSign,
+} from "@tabler/icons-react";
 import { useMutation, useQuery } from "@tanstack/react-query";
 import Head from "next/head";
 import { type ReactElement } from "react";
+import { z } from "zod";
+
+const formSchema = z.object({
+	name: z.string(),
+	description: z.string().optional(),
+	visibility: z.nativeEnum(Visibility).default(Visibility.Private),
+});
+type FormSchema = z.infer<typeof formSchema>;
 
 const Page: NextPageWithLayout = () => {
+	const [opened, { open, close }] = useDisclosure(false);
+
+	const form = useForm<FormSchema>({
+		validate: zodResolver(formSchema),
+	});
+
 	const collections = useQuery(["collections"], async () => {
 		const { collections } = await gqlClient.request(PartialCollectionsDocument);
 		return collections;
+	});
+
+	const createCollection = useMutation({
+		mutationFn: async (variables: CreateCollectionMutationVariables) => {
+			const { createCollection } = await gqlClient.request(
+				CreateCollectionDocument,
+				variables,
+			);
+			return createCollection;
+		},
+		onSuccess: () => {
+			notifications.show({
+				title: "Success",
+				message: "Collection created successfully",
+				color: "green",
+			});
+		},
 	});
 
 	const deleteCollection = useMutation({
@@ -36,7 +82,7 @@ const Page: NextPageWithLayout = () => {
 			);
 			return deleteCollection;
 		},
-		onSuccess: () => {
+		onSuccess: (_data) => {
 			collections.refetch();
 		},
 		onError: (e: any) => {
@@ -55,6 +101,12 @@ const Page: NextPageWithLayout = () => {
 			</Head>
 			<Container size={"xs"}>
 				<Stack>
+					<Flex align={"center"} justify={"space-between"}>
+						<Title>Collections</Title>
+						<ActionIcon color="green" variant="outline" onClick={open}>
+							<IconPlus size="1.125rem" />
+						</ActionIcon>
+					</Flex>
 					{collections.data.map((c) => (
 						<Flex
 							key={c.collectionDetails.id}
@@ -64,7 +116,7 @@ const Page: NextPageWithLayout = () => {
 						>
 							<Box>
 								<Flex align={"center"} gap="xs">
-									<Title order={3}>{c.collectionDetails.name}</Title>
+									<Title order={4}>{c.collectionDetails.name}</Title>
 									<Text color="dimmed" size={"xs"}>
 										({c.collectionDetails.numItems})
 									</Text>
@@ -95,6 +147,54 @@ const Page: NextPageWithLayout = () => {
 							</Flex>
 						</Flex>
 					))}
+					<Modal
+						opened={opened}
+						onClose={close}
+						withCloseButton={false}
+						centered
+					>
+						<Box
+							component="form"
+							onSubmit={form.onSubmit((values) => {
+								createCollection.mutate({ input: values });
+								form.reset();
+								close();
+							})}
+						>
+							<Stack>
+								<Title order={3}>Create collection</Title>
+								<TextInput
+									label="Name"
+									required
+									{...form.getInputProps("name")}
+								/>
+								<Box>
+									<Input.Label required>Visibility</Input.Label>
+									<SegmentedControl
+										fullWidth
+										data={[
+											{
+												label: Visibility.Private,
+												value: Visibility.Private,
+											},
+											{
+												label: Visibility.Public,
+												value: Visibility.Public,
+											},
+										]}
+										{...form.getInputProps("visibility")}
+									/>
+								</Box>
+								<Textarea
+									label="Description"
+									{...form.getInputProps("description")}
+								/>
+								<Button variant="outline" type="submit">
+									Create
+								</Button>
+							</Stack>
+						</Box>
+					</Modal>
 				</Stack>
 			</Container>
 		</>
