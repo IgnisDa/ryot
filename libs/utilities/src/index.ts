c21ecacde4582faa653bdd6f39bccf36d4aee816
Diptesh Choudhuri <ignisda2001@gmail.com>
2023-07-10 22:52:54 +0530

    feat(utilities): add fn to generate random string

--- libs/utilities/src/index.ts
+++ libs/utilities/src/index.ts
@@ -1,13 +1,13 @@
-import { camelCase, startCase } from "lodash";
 import TimeAgo from "javascript-time-ago";
 import en from "javascript-time-ago/locale/en.json";
+import { camelCase, startCase } from "lodash";
 
 TimeAgo.addDefaultLocale(en);
 
 const timeAgo = new TimeAgo("en-US");
 
 /**
- * Format a `Date` into a human readable format
+ * Format a `Date` into a human readable format.
  */
 export const formatTimeAgo = (time: Date) => {
 	return timeAgo.format(time);
@@ -26,9 +26,25 @@ export const getInitials = (name: string) => {
 };
 
 /**
- * Change case to a presentable format
+ * Change case to a presentable format.
  */
 export const changeCase = (name: string) =>
 	startCase(camelCase(name.toLowerCase()));
+
+/**
+ * Generate a random string of the given length.
+ * Taken from: https://stackoverflow.com/a/1350278/11667450
+ */
+export const randomString = (length: number) => {
+	let s = "";
+	const randomChar = () => {
+		const n = Math.floor(Math.random() * 62);
+		if (n < 10) return n; //1-10
+		if (n < 36) return String.fromCharCode(n + 55); //A-Z
+		return String.fromCharCode(n + 61); //a-z
+	};
+	while (s.length < length) s += randomChar();
+	return s;
+};
 
 export { startCase, camelCase };
