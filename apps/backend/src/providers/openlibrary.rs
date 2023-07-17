604b6c287587788557235df33620688617d54e0c
Diptesh Choudhuri <ignisda2001@gmail.com>
2023-07-17 11:13:58 +0530

    feat(backend): add retry and backoff logic

--- apps/backend/src/providers/openlibrary.rs
+++ apps/backend/src/providers/openlibrary.rs
@@ -7,6 +7,8 @@ use itertools::Itertools;
 use serde::{Deserialize, Serialize};
 use serde_json::json;
 use surf::{middleware::Redirect, Client, Url};
+use surf_governor::GovernorMiddleware;
+use surf_retry::{ExponentialBackoff, RetryMiddleware};
 
 use crate::{
     config::OpenlibraryConfig,
@@ -342,6 +344,12 @@ impl OpenlibraryService {
             .client
             .clone()
             .with(Redirect::new(5))
+            .with(GovernorMiddleware::per_second(1).ok()?)
+            .with(RetryMiddleware::new(
+                3,
+                ExponentialBackoff::builder().build_with_max_retries(3),
+                1,
+            ))
             .get(format!("isbn/{}.json", isbn))
             .await
             .ok()?;
