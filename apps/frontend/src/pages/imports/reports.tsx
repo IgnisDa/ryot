ed427e308608b8db28e4413b60f6a6428eee1c0e
Diptesh Choudhuri <ignisda2001@gmail.com>
2023-07-26 12:45:27 +0530

    feat(frontend): basic import legend

--- apps/frontend/src/pages/imports/reports.tsx
+++ apps/frontend/src/pages/imports/reports.tsx
@@ -1,13 +1,31 @@
 import type { NextPageWithLayout } from "../_app";
+import LoadingPage from "@/lib/layouts/LoadingPage";
 import LoggedIn from "@/lib/layouts/LoggedIn";
 import { gqlClient } from "@/lib/services/api";
-import { Container, Stack, Title } from "@mantine/core";
-import { ImportReportsDocument } from "@ryot/generated/graphql/backend/graphql";
+import { getStringAsciiValue } from "@/lib/utilities";
+import {
+	Box,
+	Card,
+	Container,
+	Flex,
+	Stack,
+	Text,
+	Title,
+	useMantineTheme,
+} from "@mantine/core";
+import {
+	ImportFailStep,
+	ImportReportsDocument,
+} from "@ryot/generated/graphql/backend/graphql";
+import { changeCase } from "@ryot/utilities";
 import { useQuery } from "@tanstack/react-query";
 import Head from "next/head";
 import { type ReactElement } from "react";
 
 const Page: NextPageWithLayout = () => {
+	const theme = useMantineTheme();
+	const colors = Object.keys(theme.colors);
+
 	const importReports = useQuery({
 		queryKey: ["importReports"],
 		queryFn: async () => {
@@ -16,7 +34,7 @@ const Page: NextPageWithLayout = () => {
 		},
 	});
 
-	return (
+	return importReports.data ? (
 		<>
 			<Head>
 				<title>Import Reports | Ryot</title>
@@ -24,10 +42,43 @@ const Page: NextPageWithLayout = () => {
 			<Container>
 				<Stack>
 					<Title>Import Reports</Title>
-					{JSON.stringify(importReports.data)}
+					<Container size="xs" ml={0} pl={0}>
+						<Card shadow="sm" padding={"sm"}>
+							<Card.Section withBorder p="sm">
+								<Text weight={500} size="lg">
+									Legend
+								</Text>
+							</Card.Section>
+							<Flex mt="xs" columnGap="xs" rowGap={3} wrap="wrap">
+								{Object.values(ImportFailStep).map((s) => (
+									<Text
+										color={
+											colors[
+												(getStringAsciiValue(s) + colors.length) % colors.length
+											]
+										}
+										size="xs"
+									>
+										{changeCase(s)}
+									</Text>
+								))}
+							</Flex>
+						</Card>
+					</Container>
+					{importReports.data.length > 0 ? (
+						<Stack>
+							{importReports.data.map((r) => (
+								<Box key={r.id}>{JSON.stringify(r)}</Box>
+							))}
+						</Stack>
+					) : (
+						<Text>You have not performed any imports</Text>
+					)}
 				</Stack>
 			</Container>
 		</>
+	) : (
+		<LoadingPage />
 	);
 };
 
