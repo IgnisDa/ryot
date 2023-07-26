bd3e3aa0821c42e2a4af29d0817c74b88fa0372c
Diptesh Choudhuri <ignisda2001@gmail.com>
2023-07-26 12:46:08 +0530

    chore(frontend): remove import legend

--- apps/frontend/src/pages/imports/reports.tsx
+++ apps/frontend/src/pages/imports/reports.tsx
@@ -2,30 +2,13 @@ import type { NextPageWithLayout } from "../_app";
 import LoadingPage from "@/lib/layouts/LoadingPage";
 import LoggedIn from "@/lib/layouts/LoggedIn";
 import { gqlClient } from "@/lib/services/api";
-import { getStringAsciiValue } from "@/lib/utilities";
-import {
-	Box,
-	Card,
-	Container,
-	Flex,
-	Stack,
-	Text,
-	Title,
-	useMantineTheme,
-} from "@mantine/core";
-import {
-	ImportFailStep,
-	ImportReportsDocument,
-} from "@ryot/generated/graphql/backend/graphql";
-import { changeCase } from "@ryot/utilities";
+import { Box, Container, Stack, Text, Title } from "@mantine/core";
+import { ImportReportsDocument } from "@ryot/generated/graphql/backend/graphql";
 import { useQuery } from "@tanstack/react-query";
 import Head from "next/head";
 import { type ReactElement } from "react";
 
 const Page: NextPageWithLayout = () => {
-	const theme = useMantineTheme();
-	const colors = Object.keys(theme.colors);
-
 	const importReports = useQuery({
 		queryKey: ["importReports"],
 		queryFn: async () => {
@@ -42,29 +25,6 @@ const Page: NextPageWithLayout = () => {
 			<Container>
 				<Stack>
 					<Title>Import Reports</Title>
-					<Container size="xs" ml={0} pl={0}>
-						<Card shadow="sm" padding={"sm"}>
-							<Card.Section withBorder p="sm">
-								<Text weight={500} size="lg">
-									Legend
-								</Text>
-							</Card.Section>
-							<Flex mt="xs" columnGap="xs" rowGap={3} wrap="wrap">
-								{Object.values(ImportFailStep).map((s) => (
-									<Text
-										color={
-											colors[
-												(getStringAsciiValue(s) + colors.length) % colors.length
-											]
-										}
-										size="xs"
-									>
-										{changeCase(s)}
-									</Text>
-								))}
-							</Flex>
-						</Card>
-					</Container>
 					{importReports.data.length > 0 ? (
 						<Stack>
 							{importReports.data.map((r) => (
