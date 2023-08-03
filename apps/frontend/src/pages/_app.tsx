2e2445b1146c06cbf64aabaa833df6a17d481ee6
Diptesh Choudhuri <ignisda2001@gmail.com>
2023-08-03 20:57:49 +0530

    feat(frontend): add color scheme btn

--- apps/frontend/src/pages/_app.tsx
+++ apps/frontend/src/pages/_app.tsx
@@ -1,5 +1,11 @@
 import { queryClient } from "@/lib/services/api";
-import { Flex, MantineProvider } from "@mantine/core";
+import {
+	type ColorScheme,
+	ColorSchemeProvider,
+	Flex,
+	MantineProvider,
+} from "@mantine/core";
+import { useLocalStorage } from "@mantine/hooks";
 import { ModalsProvider } from "@mantine/modals";
 import { Notifications } from "@mantine/notifications";
 import { QueryClientProvider } from "@tanstack/react-query";
@@ -7,7 +13,7 @@ import type { NextPage } from "next";
 import { NextSeo } from "next-seo";
 import type { AppProps } from "next/app";
 import Head from "next/head";
-import type { ReactElement, ReactNode } from "react";
+import { type ReactElement, type ReactNode } from "react";
 
 export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
 	getLayout?: (page: ReactElement) => ReactNode;
@@ -18,6 +24,15 @@ type AppPropsWithLayout = AppProps & {
 };
 
 export default function App({ Component, pageProps }: AppPropsWithLayout) {
+	const [colorScheme, setColorScheme] = useLocalStorage<ColorScheme>({
+		key: "mantine-color-scheme",
+		defaultValue: "dark",
+		getInitialValueInEffect: true,
+	});
+
+	const toggleColorScheme = (value?: ColorScheme) =>
+		setColorScheme(value || (colorScheme === "dark" ? "light" : "dark"));
+
 	const getLayout = Component.getLayout ?? ((page) => page);
 
 	return (
@@ -44,27 +59,32 @@ export default function App({ Component, pageProps }: AppPropsWithLayout) {
 				/>
 			</Head>
 			<QueryClientProvider client={queryClient}>
-				<MantineProvider
-					withGlobalStyles
-					withNormalizeCSS
-					theme={{
-						colorScheme: "dark",
-						fontFamily: "Poppins",
-						breakpoints: { "3xl": "112em" },
-					}}
+				<ColorSchemeProvider
+					colorScheme={colorScheme}
+					toggleColorScheme={toggleColorScheme}
 				>
-					<ModalsProvider
-						labels={{ confirm: "Confirm", cancel: "Cancel" }}
-						modalProps={{ centered: true, title: "Confirmation" }}
+					<MantineProvider
+						withGlobalStyles
+						withNormalizeCSS
+						theme={{
+							colorScheme,
+							fontFamily: "Poppins",
+							breakpoints: { "3xl": "112em" },
+						}}
 					>
-						<Notifications />
-						<Flex direction={"column"} style={{ minHeight: "100vh" }}>
-							<Flex style={{ flexGrow: 1 }}>
-								{getLayout(<Component {...pageProps} />)}
+						<ModalsProvider
+							labels={{ confirm: "Confirm", cancel: "Cancel" }}
+							modalProps={{ centered: true, title: "Confirmation" }}
+						>
+							<Notifications />
+							<Flex direction={"column"} style={{ minHeight: "100vh" }}>
+								<Flex style={{ flexGrow: 1 }}>
+									{getLayout(<Component {...pageProps} />)}
+								</Flex>
 							</Flex>
-						</Flex>
-					</ModalsProvider>
-				</MantineProvider>
+						</ModalsProvider>
+					</MantineProvider>
+				</ColorSchemeProvider>
 			</QueryClientProvider>
 		</>
 	);
