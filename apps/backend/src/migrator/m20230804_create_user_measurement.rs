5d61d2f97dfbd57f300d9cd9a6a6e63bcc8e2846
Diptesh Choudhuri <ignisda2001@gmail.com>
2023-08-04 19:33:38 +0530

    feat(backend): add model for creating user measurement

---
+++ apps/backend/src/migrator/m20230804_create_user_measurement.rs
@@ -0,0 +1,60 @@
+use sea_orm_migration::prelude::*;
+
+use super::m20230417_create_user::User;
+
+pub struct Migration;
+
+pub static USER_MEASUREMENT_PRIMARY_KEY: &str = "pk-user_measurement";
+
+impl MigrationName for Migration {
+    fn name(&self) -> &str {
+        "m20230804_create_user_measurement"
+    }
+}
+
+#[derive(Iden)]
+pub enum UserMeasurement {
+    Table,
+    UserId,
+    Timestamp,
+    Data,
+}
+
+#[async_trait::async_trait]
+impl MigrationTrait for Migration {
+    async fn up(&self, manager: &SchemaManager) -> Result<(), DbErr> {
+        manager
+            .create_table(
+                Table::create()
+                    .table(UserMeasurement::Table)
+                    .col(
+                        ColumnDef::new(UserMeasurement::Timestamp)
+                            .date_time()
+                            .not_null(),
+                    )
+                    .col(ColumnDef::new(UserMeasurement::UserId).integer().not_null())
+                    .primary_key(
+                        Index::create()
+                            .name(USER_MEASUREMENT_PRIMARY_KEY)
+                            .col(UserMeasurement::UserId)
+                            .col(UserMeasurement::Timestamp),
+                    )
+                    .col(ColumnDef::new(UserMeasurement::Data).json().not_null())
+                    .foreign_key(
+                        ForeignKey::create()
+                            .name("fk-user_measurement-user_id")
+                            .from(UserMeasurement::Table, UserMeasurement::UserId)
+                            .to(User::Table, User::Id)
+                            .on_delete(ForeignKeyAction::Cascade)
+                            .on_update(ForeignKeyAction::Cascade),
+                    )
+                    .to_owned(),
+            )
+            .await?;
+        Ok(())
+    }
+
+    async fn down(&self, _manager: &SchemaManager) -> Result<(), DbErr> {
+        Ok(())
+    }
+}
