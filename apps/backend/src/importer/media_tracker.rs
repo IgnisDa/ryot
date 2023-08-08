0e18beb1845eb645a09dc79662382b78c8cfe94a
Diptesh Choudhuri <ignisda2001@gmail.com>
2023-08-08 11:46:15 +0530

    chore(backend): remove complex media tracker review handling

--- apps/backend/src/importer/media_tracker.rs
+++ apps/backend/src/importer/media_tracker.rs
@@ -296,18 +296,14 @@ pub async fn import(input: DeployMediaTrackerImportInput) -> Result<ImportResult
                 true => ImportOrExportItemIdentifier::NeedsDetails(identifier),
             },
             reviews: Vec::from_iter(details.user_rating.map(|r| {
-                let review = if let Some(s) = r
-                    .clone()
-                    .review
-                    .map(|s| utils::extract_review_information(&s))
-                {
-                    s
-                } else {
+                let review = if let Some(s) = r.clone().review {
                     Some(ImportOrExportItemReview {
                         date: None,
                         spoiler: Some(false),
                         text: r.review,
                     })
+                } else {
+                    None
                 };
                 ImportOrExportItemRating {
                     review,
@@ -350,105 +346,4 @@ pub async fn import(input: DeployMediaTrackerImportInput) -> Result<ImportResult
         failed_items,
         collections: all_collections,
     })
-}
-
-pub mod utils {
-    use super::*;
-
-    use chrono::{NaiveDateTime, TimeZone, Utc};
-    use regex::Regex;
-
-    // Written with the help of ChatGPT.
-    pub fn extract_review_information(input: &str) -> Option<ImportOrExportItemReview> {
-        let regex_str =
-            r"(?m)^(?P<date>\d{2}/\d{2}/\d{4}):(?P<spoiler>\s*\[SPOILERS\])?\n\n(?P<text>[\s\S]*)$";
-        let regex = Regex::new(regex_str).unwrap();
-        if let Some(captures) = regex.captures(input) {
-            let date_str = captures.name("date").unwrap().as_str();
-            let date = NaiveDateTime::parse_from_str(
-                &format!("{} 00:00:00", date_str),
-                "%d/%m/%Y %H:%M:%S",
-            )
-            .ok()
-            .and_then(|d| Utc.from_local_datetime(&d).earliest())
-            .unwrap();
-            let spoiler = captures
-                .name("spoiler")
-                .map_or(false, |m| m.as_str().trim() == "[SPOILERS]");
-            let text = captures.name("text").unwrap().as_str().to_owned();
-            Some(ImportOrExportItemReview {
-                date: Some(date),
-                spoiler: Some(spoiler),
-                text: Some(text),
-            })
-        } else {
-            None
-        }
-    }
-
-    #[cfg(test)]
-    mod tests {
-        use super::*;
-        use rstest::rstest;
-        use sea_orm::prelude::DateTimeUtc;
-
-        static TEXT_1: &str = "The movie was fantastic! Highly recommend.";
-        static TEXT_2: &str = "The ending was unexpected.";
-        static TEXT_3: &str =
-            "Short and sweet romance.\n\nDefinitely worth the 7-8hrs I spent reading it.";
-        static TEXT_4: &str =
-            "This is a great book.\nThe characters are well-developed.\n\nI couldn't put it down!";
-
-        #[rstest]
-        #[case(
-            format!("01/05/2023:\n\n{TEXT_1}"),
-            Utc.with_ymd_and_hms(2023, 5, 1, 0, 0, 0).unwrap(),
-            false,
-            TEXT_1
-        )]
-        #[case(
-            format!("01/05/2023: [SPOILERS]\n\n{TEXT_2}"),
-            Utc.with_ymd_and_hms(2023, 5, 1, 0, 0, 0).unwrap(),
-            true,
-            TEXT_2
-        )]
-        #[case(
-            format!("14/04/2023:\n\n{TEXT_3}"),
-            Utc.with_ymd_and_hms(2023, 4, 14, 0, 0, 0).unwrap(),
-            false,
-            TEXT_3
-        )]
-        #[case(
-            format!("12/08/2019:\n\n{TEXT_4}"),
-            Utc.with_ymd_and_hms(2019, 8, 12, 0, 0, 0).unwrap(),
-            false,
-            TEXT_4
-        )]
-        #[case(
-            format!("12/09/2018: [SPOILERS]\n\n{TEXT_4}"),
-            Utc.with_ymd_and_hms(2018, 9, 12, 0, 0, 0).unwrap(),
-            true,
-            TEXT_4
-        )]
-        fn test_extract_review_information(
-            #[case] input: String,
-            #[case] expected_date: DateTimeUtc,
-            #[case] expected_is_spoiler: bool,
-            #[case] expected_text: &str,
-        ) {
-            let info = extract_review_information(input.as_str());
-            assert!(info.is_some());
-
-            let info = info.unwrap();
-            assert_eq!(info.date.unwrap(), expected_date);
-            assert_eq!(info.spoiler, Some(expected_is_spoiler));
-            assert_eq!(info.text.unwrap(), expected_text.to_owned());
-        }
-
-        #[test]
-        fn test_extract_review_information_invalid() {
-            let info = extract_review_information("");
-            assert!(info.is_none());
-        }
-    }
 }
