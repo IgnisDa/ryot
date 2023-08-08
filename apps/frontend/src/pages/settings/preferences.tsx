84f17fcb1a0ef4b07cfe3df7902e4a704a1d4c85
Diptesh Choudhuri <ignisda2001@gmail.com>
2023-08-08 13:08:13 +0530

    feat(frontend): allow changing exercises preferences

--- apps/frontend/src/pages/settings/preferences.tsx
+++ apps/frontend/src/pages/settings/preferences.tsx
@@ -8,6 +8,7 @@ import {
 	Alert,
 	Container,
 	Divider,
+	NumberInput,
 	SimpleGrid,
 	Stack,
 	Switch,
@@ -113,6 +114,36 @@ const Page: NextPageWithLayout = () => {
 												value: String(ev.currentTarget.checked),
 											},
 										});
+									}}
+								/>
+							),
+						)}
+					</SimpleGrid>
+					<Divider />
+					<Title order={3}>Exercises</Title>
+					<SimpleGrid cols={2}>
+						{Object.entries(userPrefs.data.exercises).map(
+							([name, previousValue], idx) => (
+								<NumberInput
+									key={idx}
+									size="xs"
+									label={match(name)
+										.with(
+											"saveHistory",
+											() =>
+												"The number of elements to save in your exercise history",
+										)
+										.otherwise(() => undefined)}
+									defaultValue={previousValue}
+									disabled={!coreDetails.data.preferencesChangeAllowed}
+									onChange={(num) => {
+										if (num)
+											updateUserEnabledFeatures.mutate({
+												input: {
+													property: `exercises.${snakeCase(name)}`,
+													value: String(num),
+												},
+											});
 									}}
 								/>
 							),
