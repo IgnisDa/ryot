819e24ce366983aaf22ff663495734d7ff78b368
Diptesh Choudhuri <ignisda2001@gmail.com>
2023-08-10 18:26:46 +0530

    feat(backend): handle inbuilt metrics

--- apps/backend/src/users.rs
+++ apps/backend/src/users.rs
@@ -69,17 +69,75 @@ impl Default for UserExercisePreferences {
 #[derive(
     Debug, Serialize, Deserialize, SimpleObject, Clone, Eq, PartialEq, FromJsonQueryResult,
 )]
+pub struct UserMeasurementsInBuiltPreferences {
+    pub weight: bool,
+    pub body_mass_index: bool,
+    pub total_body_water: bool,
+    pub muscle: bool,
+    pub lean_body_mass: bool,
+    pub body_fat: bool,
+    pub bone_mass: bool,
+    pub visceral_fat: bool,
+    pub waist_circumference: bool,
+    pub waist_to_height_ratio: bool,
+    pub hip_circumference: bool,
+    pub waist_to_hip_ratio: bool,
+    pub chest_circumference: bool,
+    pub thigh_circumference: bool,
+    pub biceps_circumference: bool,
+    pub neck_circumference: bool,
+    pub body_fat_caliper: bool,
+    pub chest_skinfold: bool,
+    pub abdominal_skinfold: bool,
+    pub thigh_skinfold: bool,
+    pub basal_metabolic_rate: bool,
+    pub total_daily_energy_expenditure: bool,
+    pub calories: bool,
+}
+
+impl Default for UserMeasurementsInBuiltPreferences {
+    fn default() -> Self {
+        Self {
+            weight: true,
+            body_mass_index: true,
+            total_body_water: true,
+            muscle: true,
+            lean_body_mass: false,
+            body_fat: true,
+            bone_mass: false,
+            visceral_fat: false,
+            waist_circumference: false,
+            waist_to_height_ratio: true,
+            hip_circumference: false,
+            waist_to_hip_ratio: true,
+            chest_circumference: false,
+            thigh_circumference: false,
+            biceps_circumference: false,
+            neck_circumference: false,
+            body_fat_caliper: false,
+            chest_skinfold: false,
+            abdominal_skinfold: false,
+            thigh_skinfold: false,
+            basal_metabolic_rate: true,
+            total_daily_energy_expenditure: true,
+            calories: false,
+        }
+    }
+}
+
+#[derive(
+    Debug, Serialize, Deserialize, SimpleObject, Clone, Eq, PartialEq, FromJsonQueryResult,
+)]
 pub struct UserMeasurementsPreferences {
     pub custom: Vec<String>,
-    /// the inbuilt metrics that have been disabled
-    pub inbuilt_disabled: Vec<String>,
+    pub inbuilt: UserMeasurementsInBuiltPreferences,
 }
 
 impl Default for UserMeasurementsPreferences {
     fn default() -> Self {
         Self {
             custom: vec![],
-            inbuilt_disabled: vec![],
+            inbuilt: UserMeasurementsInBuiltPreferences::default(),
         }
     }
 }
