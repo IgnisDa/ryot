e76773c27307438d09541c02c97fce155126b8fd
Diptesh Choudhuri <ignisda2001@gmail.com>
2023-08-11 10:01:25 +0530

    feat(frontend): display exercise-id in html

--- apps/frontend/src/pages/fitness/exercises.tsx
+++ apps/frontend/src/pages/fitness/exercises.tsx
@@ -100,7 +100,12 @@ const Page: NextPageWithLayout = () => {
 								]}
 							>
 								{exercisesList.data.items.map((exercise) => (
-									<Flex key={exercise.id} gap="lg" align={"center"}>
+									<Flex
+										key={exercise.id}
+										gap="lg"
+										align={"center"}
+										data-exercise-id={exercise.id}
+									>
 										<Avatar
 											imageProps={{ loading: "lazy" }}
 											src={exercise.attributes.images.at(0)}
