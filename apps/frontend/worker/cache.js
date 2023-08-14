efb027f23298a15eee1646c2492c4173f7ecb348
Diptesh Choudhuri <ignisda2001@gmail.com>
2023-08-15 00:10:35 +0530

    ci(frontend): i think got caching working

--- apps/frontend/worker/cache.js
+++ apps/frontend/worker/cache.js
@@ -1,20 +1,7 @@
 "use strict";
 
-// copied from https://github.com/shadowwalker/next-pwa/blob/master/cache.js
-
 // Workbox RuntimeCaching config: https://developers.google.com/web/tools/workbox/reference-docs/latest/module-workbox-build#.RuntimeCachingEntry
 module.exports = [
-	{
-		urlPattern: /https:\/\/.*\.(?:jpg|jpeg|gif|png|svg|ico|webp)$/i,
-		handler: "CacheFirst",
-		options: {
-			cacheName: "static-image-assets",
-			expiration: {
-				maxEntries: 64,
-				maxAgeSeconds: 365 * 24 * 60 * 60, // 365 days
-			},
-		},
-	},
 	{
 		urlPattern: /^https:\/\/fonts\.(?:gstatic)\.com\/.*/i,
 		handler: "CacheFirst",
@@ -45,6 +32,17 @@ module.exports = [
 			expiration: {
 				maxEntries: 4,
 				maxAgeSeconds: 7 * 24 * 60 * 60, // 7 days
+			},
+		},
+	},
+	{
+		urlPattern: /\.(?:jpg|jpeg|gif|png|svg|ico|webp)$/i,
+		handler: "StaleWhileRevalidate",
+		options: {
+			cacheName: "static-image-assets",
+			expiration: {
+				maxEntries: 64,
+				maxAgeSeconds: 24 * 60 * 60, // 24 hours
 			},
 		},
 	},
@@ -113,6 +111,72 @@ module.exports = [
 			expiration: {
 				maxEntries: 32,
 				maxAgeSeconds: 24 * 60 * 60, // 24 hours
+			},
+		},
+	},
+	{
+		urlPattern: /\.(?:json|xml|csv)$/i,
+		handler: "NetworkFirst",
+		options: {
+			cacheName: "static-data-assets",
+			expiration: {
+				maxEntries: 32,
+				maxAgeSeconds: 24 * 60 * 60, // 24 hours
+			},
+		},
+	},
+	{
+		urlPattern: ({ url }) => {
+			const isSameOrigin = self.origin === url.origin;
+			if (!isSameOrigin) return false;
+			const pathname = url.pathname;
+			// Exclude /api/auth/callback/* to fix OAuth workflow in Safari without impact other environment
+			// Above route is default for next-auth, you may need to change it if your OAuth workflow has a different callback route
+			// Issue: https://github.com/shadowwalker/next-pwa/issues/131#issuecomment-821894809
+			if (pathname.startsWith("/api/auth/")) return false;
+			if (pathname.startsWith("/api/")) return true;
+			return false;
+		},
+		handler: "NetworkFirst",
+		method: "GET",
+		options: {
+			cacheName: "apis",
+			expiration: {
+				maxEntries: 16,
+				maxAgeSeconds: 24 * 60 * 60, // 24 hours
+			},
+			networkTimeoutSeconds: 10, // fall back to cache if api does not response within 10 seconds
+		},
+	},
+	{
+		urlPattern: ({ url }) => {
+			const isSameOrigin = self.origin === url.origin;
+			if (!isSameOrigin) return false;
+			const pathname = url.pathname;
+			if (pathname.startsWith("/api/")) return false;
+			return true;
+		},
+		handler: "NetworkFirst",
+		options: {
+			cacheName: "others",
+			expiration: {
+				maxEntries: 32,
+				maxAgeSeconds: 24 * 60 * 60, // 24 hours
+			},
+			networkTimeoutSeconds: 10,
+		},
+	},
+	{
+		urlPattern: ({ url }) => {
+			const isSameOrigin = self.origin === url.origin;
+			return !isSameOrigin;
+		},
+		handler: "NetworkFirst",
+		options: {
+			cacheName: "cross-origin",
+			expiration: {
+				maxEntries: 32,
+				maxAgeSeconds: 365 * 24 * 60 * 60, // 365 days
 			},
 		},
 	},
