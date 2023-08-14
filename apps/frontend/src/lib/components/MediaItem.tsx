26c7ed6f582bc5c056ef9784fad1de4719b63849
Diptesh Choudhuri <ignisda2001@gmail.com>
2023-08-14 10:47:52 +0530

    style(frontend): remove useless newline

--- apps/frontend/src/lib/components/MediaItem.tsx
+++ apps/frontend/src/lib/components/MediaItem.tsx
@@ -166,9 +166,7 @@ export const BaseDisplayItem = (props: {
 								transitionProperty: "transform",
 								transitionTimingFunction: "cubic-bezier(0.4, 0, 0.2, 1)",
 								transitionDuration: "150ms",
-								"&:hover": {
-									boxShadow: "0 0 15px black",
-								},
+								"&:hover": { boxShadow: "0 0 15px black" },
 							},
 						})}
 					/>
