10bcdb1212e05cfdf92ed5faff59e4e926b513b8
Diptesh Choudhuri <ignisda2001@gmail.com>
2023-08-16 10:44:46 +0530

    try(frontend): crappy grid

--- apps/frontend/src/pages/fitness/exercises/in-progress.tsx
+++ apps/frontend/src/pages/fitness/exercises/in-progress.tsx
@@ -8,6 +8,7 @@ import {
 	Button,
 	Container,
 	Flex,
+	Grid,
 	Menu,
 	Paper,
 	Skeleton,
@@ -16,6 +17,7 @@ import {
 	TextInput,
 	Textarea,
 } from "@mantine/core";
+import { ExerciseLot } from "@ryot/generated/graphql/backend/graphql";
 import {
 	IconClipboard,
 	IconDotsVertical,
@@ -30,6 +32,7 @@ import Link from "next/link";
 import { useRouter } from "next/router";
 import { type ReactElement } from "react";
 import { useStopwatch } from "react-timer-hook";
+import { match } from "ts-pattern";
 import { withQuery } from "ufo";
 
 const StatDisplay = (props: { name: string; value: string }) => {
@@ -68,83 +71,168 @@ const DurationTimer = ({ startTime }: { startTime: string }) => {
 const ExerciseDisplay = (props: { idx: number; exercise: Exercise }) => {
 	const [currentWorkout, setCurrentWorkout] = useAtom(currentWorkoutAtom);
 
+	const [durationCol, distanceCol, weightCol, repsCol] = match(
+		props.exercise.lot,
+	)
+		.with(ExerciseLot.DistanceAndDuration, () => [true, true, false, false])
+		.with(ExerciseLot.Duration, () => [true, false, false, false])
+		.with(ExerciseLot.RepsAndWeight, () => [false, false, true, true])
+		.exhaustive();
+
 	return currentWorkout ? (
-		<Paper withBorder p="xs">
-			<Menu shadow="md" width={200}>
-				<Stack>
-					<Flex justify={"space-between"}>
-						<Text>{props.exercise.name}</Text>
-						<Menu.Target>
-							<ActionIcon color="blue">
-								<IconDotsVertical />
-							</ActionIcon>
-						</Menu.Target>
-					</Flex>
-					{currentWorkout.exercises[props.idx].notes.map((n, idx) => (
-						<Flex key={idx} align={"center"} gap="xs">
-							<Textarea
-								style={{ flexGrow: 1 }}
-								placeholder="Add a note"
-								size="xs"
-								maxRows={1}
-								autosize
-								value={n}
-								onChange={(e) => {
-									setCurrentWorkout(
-										produce(currentWorkout, (draft) => {
-											draft.exercises[props.idx].notes[idx] =
-												e.currentTarget.value;
-										}),
-									);
-								}}
-							/>
-							<ActionIcon
-								color="red"
-								onClick={() => {
-									setCurrentWorkout(
-										produce(currentWorkout, (draft) => {
-											draft.exercises[props.idx].notes.splice(idx, 1);
-										}),
-									);
-								}}
-							>
-								<IconTrash />
-							</ActionIcon>
+		<Paper withBorder p="md">
+			<Stack>
+				<Menu shadow="md" width={200}>
+					<Stack>
+						<Flex justify={"space-between"}>
+							<Text>{props.exercise.name}</Text>
+							<Menu.Target>
+								<ActionIcon color="blue">
+									<IconDotsVertical />
+								</ActionIcon>
+							</Menu.Target>
 						</Flex>
-					))}
-				</Stack>
-				<Menu.Dropdown>
-					<Menu.Item
-						icon={<IconClipboard size={14} />}
-						onClick={() => {
-							setCurrentWorkout(
-								produce(currentWorkout, (draft) => {
-									draft.exercises[props.idx].notes.push("");
-								}),
-							);
-						}}
-					>
-						Add note
-					</Menu.Item>
-					<Menu.Item
-						color="red"
-						icon={<IconTrash size={14} />}
-						onClick={() => {
-							const yes = confirm(
-								`This removes '${props.exercise.name}' and all its sets from your workout. You can not undo this action. Are you sure you want to continue?`,
-							);
-							if (yes)
+						{currentWorkout.exercises[props.idx].notes.map((n, idx) => (
+							<Flex key={idx} align={"center"} gap="xs">
+								<Textarea
+									style={{ flexGrow: 1 }}
+									placeholder="Add a note"
+									size="xs"
+									maxRows={1}
+									autosize
+									value={n}
+									onChange={(e) => {
+										setCurrentWorkout(
+											produce(currentWorkout, (draft) => {
+												draft.exercises[props.idx].notes[idx] =
+													e.currentTarget.value;
+											}),
+										);
+									}}
+								/>
+								<ActionIcon
+									color="red"
+									onClick={() => {
+										setCurrentWorkout(
+											produce(currentWorkout, (draft) => {
+												draft.exercises[props.idx].notes.splice(idx, 1);
+											}),
+										);
+									}}
+								>
+									<IconTrash />
+								</ActionIcon>
+							</Flex>
+						))}
+					</Stack>
+					<Menu.Dropdown>
+						<Menu.Item
+							icon={<IconClipboard size={14} />}
+							onClick={() => {
 								setCurrentWorkout(
 									produce(currentWorkout, (draft) => {
-										draft.exercises.splice(props.idx, 1);
+										draft.exercises[props.idx].notes.push("");
 									}),
 								);
-						}}
-					>
-						Remove exercise
-					</Menu.Item>
-				</Menu.Dropdown>
-			</Menu>
+							}}
+						>
+							Add note
+						</Menu.Item>
+						<Menu.Item
+							color="red"
+							icon={<IconTrash size={14} />}
+							onClick={() => {
+								const yes = confirm(
+									`This removes '${props.exercise.name}' and all its sets from your workout. You can not undo this action. Are you sure you want to continue?`,
+								);
+								if (yes)
+									setCurrentWorkout(
+										produce(currentWorkout, (draft) => {
+											draft.exercises.splice(props.idx, 1);
+										}),
+									);
+							}}
+						>
+							Remove exercise
+						</Menu.Item>
+					</Menu.Dropdown>
+				</Menu>
+				<Grid grow>
+					<Grid.Col span={4}>
+						<Text size="xs">SET</Text>
+					</Grid.Col>
+					{durationCol ? (
+						<Grid.Col span={4}>
+							<Text size="xs">DURATION</Text>
+						</Grid.Col>
+					) : null}
+					{distanceCol ? (
+						<Grid.Col span={4}>
+							<Text size="xs">DISTANCE</Text>
+						</Grid.Col>
+					) : null}
+					{weightCol ? (
+						<Grid.Col span={4}>
+							<Text size="xs">WEIGHT</Text>
+						</Grid.Col>
+					) : null}
+					{repsCol ? (
+						<Grid.Col span={4}>
+							<Text size="xs">REPS</Text>
+						</Grid.Col>
+					) : null}
+					<Grid.Col span={4}>
+						<Text size="xs">Done</Text>
+					</Grid.Col>
+				</Grid>
+				{props.exercise.sets.map((s) => (
+					<Grid key={s.idx} grow>
+						<Grid.Col span={4}>
+							<Text size="xs" color="blue">
+								{s.idx + 1}
+							</Text>
+						</Grid.Col>
+						{durationCol ? (
+							<Grid.Col span={4}>
+								<Text size="xs">duration</Text>
+							</Grid.Col>
+						) : null}
+						{distanceCol ? (
+							<Grid.Col span={4}>
+								<Text size="xs">distance</Text>
+							</Grid.Col>
+						) : null}
+						{weightCol ? (
+							<Grid.Col span={4}>
+								<Text size="xs">weight</Text>
+							</Grid.Col>
+						) : null}
+						{repsCol ? (
+							<Grid.Col span={4}>
+								<Text size="xs">reps</Text>
+							</Grid.Col>
+						) : null}
+						<Grid.Col span={4}>
+							<Text size="xs">done</Text>
+						</Grid.Col>
+					</Grid>
+				))}
+				<Button
+					variant="subtle"
+					onClick={() => {
+						setCurrentWorkout(
+							produce(currentWorkout, (draft) => {
+								draft.exercises[props.idx].sets.push({
+									idx: props.exercise.sets.length,
+									stats: {},
+								});
+							}),
+						);
+					}}
+				>
+					Add set
+				</Button>
+			</Stack>
 		</Paper>
 	) : (
 		<Skeleton height={20} radius="xl" />
