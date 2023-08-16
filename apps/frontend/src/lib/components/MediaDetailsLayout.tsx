d664c47c84a9760a7dc506638d5b305b0f924a97
Ellis Clayton <ellis@ellis.codes>
2023-08-16 21:04:17 +1000

    perf(frontend): lazy load carousel images
    The Carousel component doesn't have any support for lazy loading inner
    content. This can mean tens of megabytes of data transfer occur for some
    shows due to having a large number of images available. A simple
    workaround for this is to only load the visible image, along with its
    immediate neighbors. Loading the immediate neighbors ensures the user
    experience remains smooth (not preloading those images can cause some
    elements to jump around as the image loads in).

--- apps/frontend/src/lib/components/MediaDetailsLayout.tsx
+++ apps/frontend/src/lib/components/MediaDetailsLayout.tsx
@@ -1,6 +1,25 @@
 import { Carousel } from "@mantine/carousel";
 import { Anchor, Badge, Box, Flex, Image, Stack, Text } from "@mantine/core";
 import { IconExternalLink } from "@tabler/icons-react";
+import { useState } from "react";
+
+function getSurroundingElements<T>(array: T[], element: number): number[] {
+	if (array.length === 1) {
+		return [0];
+	}
+
+	const lastIndex = array.length - 1;
+
+	if (element === 0) {
+		return [lastIndex, element, element + 1];
+	}
+
+	if (element === lastIndex) {
+		return [element - 1, element, 0];
+	}
+
+	return [element - 1, element, element + 1];
+}
 
 export default function ({
 	children,
@@ -13,6 +32,9 @@ export default function ({
 	backdropImages: string[];
 	externalLink?: { source: string; href?: string | null };
 }) {
+	const images = [...posterImages, ...backdropImages];
+	const [activeImageId, setActiveImageId] = useState<number>(0);
+
 	return (
 		<Flex direction={{ base: "column", md: "row" }} gap={"lg"}>
 			<Box
@@ -24,28 +46,30 @@ export default function ({
 					[t.fn.largerThan("md")]: { width: "35%" },
 				})}
 			>
-        {posterImages.length > 1 ? (
-          <Carousel withIndicators={posterImages.length > 1} w={300}>
-            {[...posterImages, ...backdropImages].map((url, idx) => (
-              <Carousel.Slide key={url} data-image-idx={idx}>
-                <Image
-                  src={url}
-                  radius={"lg"}
-                  imageProps={{ loading: "lazy" }}
-                />
-              </Carousel.Slide>
-            ))}
-          </Carousel>
-        ) : (
-          <Box w={300}>
-            <Image
-              src={posterImages[0] || backdropImages[0]}
-              withPlaceholder
-              height={400}
-              radius={"lg"}
-            />
-          </Box>
-        )}
+				{posterImages.length > 1 ? (
+					<Carousel withIndicators={posterImages.length > 1} w={300} onSlideChange={setActiveImageId}>
+						{images.map((url, idx) => (
+							<Carousel.Slide key={url} data-image-idx={idx}>
+								{getSurroundingElements(images, activeImageId).includes(idx) && (
+									<Image
+										src={url}
+										radius={"lg"}
+										imageProps={{ loading: "lazy" }}
+									/>
+								)}
+							</Carousel.Slide>
+						))}
+					</Carousel>
+				) : (
+					<Box w={300}>
+						<Image
+							src={posterImages[0] || backdropImages[0]}
+							withPlaceholder
+							height={400}
+							radius={"lg"}
+						/>
+					</Box>
+				)}
 				{externalLink ? (
 					<Badge
 						id="data-source"
