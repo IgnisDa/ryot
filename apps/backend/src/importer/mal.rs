99541d7f01b60f865f0492d5163bd151ae1efa5e
Diptesh Choudhuri <ignisda2001@gmail.com>
2023-08-28 21:42:51 +0530

    feat(backend): return review and seen history

--- apps/backend/src/importer/mal.rs
+++ apps/backend/src/importer/mal.rs
@@ -5,12 +5,19 @@ use std::{
 
 use async_graphql::Result;
 use flate2::bufread::GzDecoder;
+use rust_decimal::{prelude::FromPrimitive, Decimal};
+use rust_decimal_macros::dec;
+use sea_orm::prelude::DateTimeUtc;
 use serde::{de::DeserializeOwned, Deserialize, Serialize};
 
 use crate::{
     importer::{DeployMalImportInput, ImportResult},
     migrator::{MetadataLot, MetadataSource},
-    models::media::{ImportOrExportItemIdentifier, ImportOrExportMediaItem},
+    models::media::{
+        ImportOrExportItemIdentifier, ImportOrExportItemRating, ImportOrExportMediaItem,
+        ImportOrExportMediaItemSeen,
+    },
+    utils::{convert_naive_to_utc, convert_string_to_date},
 };
 
 fn decode_data<T>(path: &str) -> Result<T>
@@ -25,17 +32,39 @@ where
     Ok(deserialized)
 }
 
+fn get_date(date: String) -> Option<DateTimeUtc> {
+    if date.starts_with("0000") {
+        None
+    } else {
+        convert_string_to_date(&date).map(convert_naive_to_utc)
+    }
+}
+
 fn convert_to_format(
     item: Item,
     lot: MetadataLot,
 ) -> ImportOrExportMediaItem<ImportOrExportItemIdentifier> {
+    let seen_item = ImportOrExportMediaItemSeen {
+        started_on: get_date(item.my_start_date),
+        ended_on: get_date(item.my_finish_date),
+        show_season_number: None,
+        show_episode_number: None,
+        podcast_episode_number: None,
+    };
+    let review_item = ImportOrExportItemRating {
+        review: None,
+        rating: Some(Decimal::from_u32(item.my_score).unwrap() * dec!(10)),
+        show_season_number: None,
+        show_episode_number: None,
+        podcast_episode_number: None,
+    };
     ImportOrExportMediaItem {
         source_id: item.title,
         lot,
         source: MetadataSource::Mal,
         identifier: ImportOrExportItemIdentifier::NeedsDetails(item.identifier.to_string()),
-        seen_history: vec![],
-        reviews: vec![],
+        seen_history: vec![seen_item],
+        reviews: vec![review_item],
         collections: vec![],
     }
 }
@@ -50,11 +79,13 @@ pub async fn import(input: DeployMalImportInput) -> Result<ImportResult> {
     for item in manga_data.items.into_iter() {
         media.push(convert_to_format(item, MetadataLot::Manga));
     }
-    Ok(ImportResult {
-        collections: vec![],
-        failed_items: vec![],
-        media,
-    })
+    dbg!(&media);
+    todo!();
+    // Ok(ImportResult {
+    //     collections: vec![],
+    //     failed_items: vec![],
+    //     media,
+    // })
 }
 
 #[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
