80d48f53117600021eb37b3af709a77194bcbb91
Diptesh Choudhuri <ignisda2001@gmail.com>
2023-08-28 21:19:18 +0530

    feat(backend): deserialize mal export files

--- apps/backend/src/importer/mal.rs
+++ apps/backend/src/importer/mal.rs
@@ -17,17 +17,15 @@ where
     let mut decoder = GzDecoder::new(data);
     let mut string_data = String::new();
     decoder.read_to_string(&mut string_data)?;
-    dbg!(&string_data);
     let deserialized = serde_xml_rs::from_str::<T>(&string_data)?;
     Ok(deserialized)
 }
 
 pub async fn import(input: DeployMalImportInput) -> Result<ImportResult> {
-    dbg!(&input);
-    let anime_data = decode_data::<AnimeRoot>(&input.anime)?;
+    let anime_data = decode_data::<DataRoot>(&input.anime)?;
     dbg!(&anime_data);
-    // let manga_data = decode_data::<Root>(&input.manga)?;
-    // dbg!(&manga_data);
+    let manga_data = decode_data::<DataRoot>(&input.manga)?;
+    dbg!(&manga_data);
     Ok(ImportResult {
         collections: vec![],
         media: vec![],
@@ -36,53 +34,23 @@ pub async fn import(input: DeployMalImportInput) -> Result<ImportResult> {
 }
 
 #[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
-struct AnimeRoot {
-    myanimelist: Myanimelist,
+struct DataRoot {
+    #[serde(alias = "manga", alias = "anime")]
+    items: Vec<Item>,
 }
 
 #[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
-struct Myanimelist {
-    anime: Vec<Anime>,
-}
-
-#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
-struct Anime {
-    series_animedb_id: i64,
-    series_title: SeriesTitle,
-    series_type: String,
-    series_episodes: i64,
-    my_id: i64,
-    my_watched_episodes: i64,
+struct Item {
+    series_animedb_id: Option<u32>,
+    series_mangadb_id: Option<u32>,
+    series_title: Option<String>,
+    manga_title: Option<String>,
+    series_type: Option<String>,
+    series_episodes: Option<u32>,
+    manga_chapters: Option<u32>,
+    my_watched_episodes: Option<u32>,
+    my_read_chapters: Option<u32>,
     my_start_date: String,
     my_finish_date: String,
-    my_rated: String,
-    my_score: i64,
-    my_storage: String,
-    my_storage_value: i64,
-    my_status: String,
-    my_comments: MyComments,
-    my_times_watched: i64,
-    my_rewatch_value: String,
-    my_priority: String,
-    my_tags: MyTags,
-    my_rewatching: i64,
-    my_rewatching_ep: i64,
-    my_discuss: i64,
-    my_sns: String,
-    update_on_import: i64,
-}
-
-#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
-struct SeriesTitle {
-    cdata: String,
-}
-
-#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
-struct MyComments {
-    cdata: String,
-}
-
-#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
-struct MyTags {
-    cdata: String,
+    my_score: u32,
 }
