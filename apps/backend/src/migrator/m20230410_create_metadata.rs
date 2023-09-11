c2d29d8016af67fb8829446e80d8853049be3d5a
Diptesh Choudhuri <ignisda2001@gmail.com>
2023-09-11 10:15:29 +0530

    feat(backend): add `videos` field to metadata

--- apps/backend/src/migrator/m20230410_create_metadata.rs
+++ apps/backend/src/migrator/m20230410_create_metadata.rs
@@ -117,6 +117,8 @@ pub enum Metadata {
     ProductionStatus,
     // all the images for this media item
     Images,
+    // all the videos for this media item
+    Videos,
     // the unique identifier that is returned by the metadata provider
     Identifier,
     // the provider source
@@ -165,6 +167,7 @@ impl MigrationTrait for Migration {
                     .col(ColumnDef::new(Metadata::ProviderRating).decimal().null())
                     .col(ColumnDef::new(Metadata::PublishDate).date())
                     .col(ColumnDef::new(Metadata::Images).json())
+                    .col(ColumnDef::new(Metadata::Videos).json())
                     .col(ColumnDef::new(Metadata::Identifier).string().not_null())
                     .col(ColumnDef::new(Metadata::Source).string_len(2).not_null())
                     .col(ColumnDef::new(Metadata::Specifics).json().not_null())
