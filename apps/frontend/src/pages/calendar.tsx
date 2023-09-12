7546978d580240e952b586ecd34e284d8042ddf0
Diptesh Choudhuri <ignisda2001@gmail.com>
2023-09-12 22:17:48 +0530

    feat(frontend): allow selecting date ranges

--- apps/frontend/src/pages/calendar.tsx
+++ apps/frontend/src/pages/calendar.tsx
@@ -1,13 +1,44 @@
 import { useCoreDetails } from "@/lib/hooks/graphql";
 import LoadingPage from "@/lib/layouts/LoadingPage";
 import LoggedIn from "@/lib/layouts/LoggedIn";
+import { gqlClient } from "@/lib/services/api";
 import { Box, Container, Stack } from "@mantine/core";
+import { MonthPicker } from "@mantine/dates";
+import { useLocalStorage } from "@mantine/hooks";
+import { UserCalendarEventsDocument } from "@ryot/generated/graphql/backend/graphql";
+import { useQuery } from "@tanstack/react-query";
+import { DateTime } from "luxon";
 import Head from "next/head";
 import { type ReactElement } from "react";
 import type { NextPageWithLayout } from "./_app";
 
 const Page: NextPageWithLayout = () => {
 	const coreDetails = useCoreDetails();
+	const [dateRange, setDateRange] = useLocalStorage<[Date | null, Date | null]>(
+		{
+			defaultValue: [
+				DateTime.now().minus({ months: 2 }).toJSDate(),
+				new Date(),
+			], // [end, start]
+			key: "savedCalendarDate",
+			getInitialValueInEffect: false,
+		},
+	);
+
+	const calendarEvents = useQuery(["calendarEvents", dateRange], async () => {
+		const { userCalendarEvents } = await gqlClient.request(
+			UserCalendarEventsDocument,
+			{
+				endTime: dateRange[0]
+					? DateTime.fromJSDate(dateRange[0]).toISODate()
+					: undefined,
+				startTime: dateRange[1]
+					? DateTime.fromJSDate(dateRange[1]).toISODate()
+					: undefined,
+			},
+		);
+		return userCalendarEvents;
+	});
 
 	return coreDetails.data ? (
 		<>
@@ -16,7 +47,13 @@ const Page: NextPageWithLayout = () => {
 			</Head>
 			<Container>
 				<Stack>
-					<Box>Hello</Box>
+					<MonthPicker
+						size="xs"
+						type="range"
+						value={dateRange}
+						onChange={setDateRange}
+					/>
+					<Box>{JSON.stringify(calendarEvents.data)}</Box>
 				</Stack>
 			</Container>
 		</>
