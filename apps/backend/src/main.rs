dfdaccb2b55f0ca038ef40343f08e66c8d4e27cc
Diptesh Choudhuri <ignisda2001@gmail.com>
2023-09-25 18:38:49 +0530

    feat(backend): configure background jobs interval

--- apps/backend/src/main.rs
+++ apps/backend/src/main.rs
@@ -93,6 +93,7 @@ async fn main() -> Result<()> {
     let user_cleanup_every = config.scheduler.user_cleanup_every;
     let pull_every = config.integration.pull_every;
     let max_file_size = config.server.max_file_size;
+    let new_background_job_check_seconds = config.server.application_job_check_seconds;
     fs::write(
         &config.server.config_dump_path,
         serde_json::to_string_pretty(&config)?,
@@ -288,7 +289,9 @@ async fn main() -> Result<()> {
                     .layer(ApalisExtension(importer_service_1.clone()))
                     .layer(ApalisExtension(media_service_4.clone()))
                     .layer(ApalisExtension(exercise_service_1.clone()))
-                    .with_storage(perform_application_job_storage.clone())
+                    .with_storage_config(perform_application_job_storage.clone(), |cfg| {
+                        cfg.fetch_interval(Duration::from_secs(new_background_job_check_seconds))
+                    })
                     .build_fn(perform_application_job)
             })
             .run()
