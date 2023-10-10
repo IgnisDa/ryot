b13b343f5d7d5561a77d596d996e2a4fa6b63519
Diptesh Choudhuri <ignisda2001@gmail.com>
2023-10-10 21:19:27 +0530

    feat(frontend): add pagination for workout list

--- apps/frontend/src/pages/fitness/exercises/workouts.tsx
+++ apps/frontend/src/pages/fitness/exercises/workouts.tsx
@@ -1,7 +1,18 @@
+import { useCoreDetails } from "@/lib/hooks/graphql";
 import LoadingPage from "@/lib/layouts/LoadingPage";
 import LoggedIn from "@/lib/layouts/LoggedIn";
 import { gqlClient } from "@/lib/services/api";
-import { Container, Flex, SimpleGrid, Stack, Title } from "@mantine/core";
+import {
+	Center,
+	Container,
+	Flex,
+	Pagination,
+	SimpleGrid,
+	Stack,
+	Text,
+	Title,
+} from "@mantine/core";
+import { useLocalStorage } from "@mantine/hooks";
 import { UserWorkoutListDocument } from "@ryot/generated/graphql/backend/graphql";
 import { useQuery } from "@tanstack/react-query";
 import Head from "next/head";
@@ -9,15 +20,22 @@ import { type ReactElement } from "react";
 import type { NextPageWithLayout } from "../../_app";
 
 const Page: NextPageWithLayout = () => {
+	const [activePage, setPage] = useLocalStorage({
+		defaultValue: "1",
+		key: "savedWorkoutListPage",
+		getInitialValueInEffect: false,
+	});
+	const coreDetails = useCoreDetails();
+
 	const userWorkoutList = useQuery(["userWorkoutList"], async () => {
 		const { userWorkoutList } = await gqlClient.request(
 			UserWorkoutListDocument,
-			{ page: 1 },
+			{ page: parseInt(activePage || "1") },
 		);
 		return userWorkoutList;
 	});
 
-	return userWorkoutList.data ? (
+	return coreDetails.data && userWorkoutList.data ? (
 		<>
 			<Head>
 				<title>Workouts | Ryot</title>
@@ -25,19 +43,38 @@ const Page: NextPageWithLayout = () => {
 			<Container>
 				<Stack>
 					<Title>Workouts</Title>
-					<SimpleGrid cols={{ base: 1, md: 2 }}>
-						{userWorkoutList.data.items.map((workout) => (
-							<Flex
-								key={workout.id}
-								align="center"
-								justify="space-between"
-								gap="md"
-								mr="lg"
-							>
-								{JSON.stringify(workout)}
-							</Flex>
-						))}
-					</SimpleGrid>
+					{userWorkoutList.data.items.length > 0 ? (
+						<SimpleGrid cols={{ base: 1, md: 2 }}>
+							{userWorkoutList.data.items.map((workout) => (
+								<Flex
+									key={workout.id}
+									align="center"
+									justify="space-between"
+									gap="md"
+									mr="lg"
+								>
+									{JSON.stringify(workout, null, 2)}
+								</Flex>
+							))}
+						</SimpleGrid>
+					) : (
+						<Text>No workouts found</Text>
+					)}
+					{userWorkoutList.data ? (
+						<Center>
+							<Pagination
+								size="sm"
+								value={parseInt(activePage || "1")}
+								onChange={(v) => setPage(v.toString())}
+								total={Math.ceil(
+									userWorkoutList.data.details.total /
+										coreDetails.data.pageLimit,
+								)}
+								boundaries={1}
+								siblings={0}
+							/>
+						</Center>
+					) : undefined}
 				</Stack>
 			</Container>
 		</>
