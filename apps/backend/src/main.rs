06b80d5257aa3836d1ea8feb85f5f61541afeeb1
Diptesh Choudhuri <ignisda2001@gmail.com>
2023-10-11 18:40:25 +0530

    chore(backend): add info when necessary

--- apps/backend/src/main.rs
+++ apps/backend/src/main.rs
@@ -168,6 +168,7 @@ async fn main() -> Result<()> {
     .await;
 
     if Exercise::find().count(&db).await? == 0 {
+        tracing::info!("Instance does not have exercises data. Deploying job to download them.");
         app_services
             .exercise_service
             .deploy_update_exercise_library_job()
