518bbb14b2332955f1e7dd8d60c3ecb9ca3f9d50
Diptesh Choudhuri <ignisda2001@gmail.com>
2023-10-17 21:18:24 +0530

    feat(frontend): UI to add metadata group to collection

--- apps/frontend/src/pages/media/groups/index.tsx
+++ apps/frontend/src/pages/media/groups/index.tsx
@@ -1,13 +1,31 @@
 import {
+	AddEntityToCollectionModal,
+	DisplayCollection,
 	MediaScrollArea,
 	PartialMetadataDisplay,
 } from "@/lib/components/MediaComponents";
 import MediaDetailsLayout from "@/lib/components/MediaDetailsLayout";
+import { LOCAL_STORAGE_KEYS } from "@/lib/constants";
 import LoadingPage from "@/lib/layouts/LoadingPage";
 import LoggedIn from "@/lib/layouts/LoggedIn";
 import { gqlClient } from "@/lib/services/api";
-import { Container, Flex, SimpleGrid, Text, Title } from "@mantine/core";
-import { MetadataGroupDetailsDocument } from "@ryot/generated/graphql/backend/graphql";
+import {
+	Button,
+	Container,
+	Flex,
+	Group,
+	SimpleGrid,
+	Tabs,
+	Text,
+	Title,
+} from "@mantine/core";
+import { useDisclosure, useLocalStorage } from "@mantine/hooks";
+import {
+	EntityLot,
+	MetadataGroupDetailsDocument,
+	UserMetadataGroupDetailsDocument,
+} from "@ryot/generated/graphql/backend/graphql";
+import { IconDeviceTv, IconUser } from "@tabler/icons-react";
 import { useQuery } from "@tanstack/react-query";
 import Head from "next/head";
 import { useRouter } from "next/router";
@@ -17,6 +35,15 @@ import type { NextPageWithLayout } from "../../_app";
 const Page: NextPageWithLayout = () => {
 	const router = useRouter();
 	const metadataGroupId = parseInt(router.query.id?.toString() || "0");
+	const [
+		collectionModalOpened,
+		{ open: collectionModalOpen, close: collectionModalClose },
+	] = useDisclosure(false);
+	const [activeTab, setActiveTab] = useLocalStorage({
+		key: LOCAL_STORAGE_KEYS.savedActiveMetadataGroupDetailsTab,
+		getInitialValueInEffect: false,
+		defaultValue: "media",
+	});
 
 	const groupDetails = useQuery({
 		queryKey: ["groupDetails", metadataGroupId],
@@ -30,8 +57,19 @@ const Page: NextPageWithLayout = () => {
 		staleTime: Infinity,
 		enabled: !!metadataGroupId,
 	});
+	const userMetadataGroupDetails = useQuery({
+		queryKey: ["usermetadataGroupDetails", metadataGroupId],
+		queryFn: async () => {
+			const { userMetadataGroupDetails } = await gqlClient.request(
+				UserMetadataGroupDetailsDocument,
+				{ metadataGroupId },
+			);
+			return userMetadataGroupDetails;
+		},
+		enabled: !!metadataGroupId,
+	});
 
-	return groupDetails.data ? (
+	return groupDetails.data && userMetadataGroupDetails.data ? (
 		<>
 			<Head>
 				<title>{groupDetails.data.details.title} | Ryot</title>
@@ -49,13 +87,64 @@ const Page: NextPageWithLayout = () => {
 					<Flex id="group-details" wrap="wrap" gap={4}>
 						<Text>{groupDetails.data.details.parts} media items</Text>
 					</Flex>
-					<MediaScrollArea>
-						<SimpleGrid cols={{ base: 3, md: 4, lg: 5 }}>
-							{groupDetails.data.contents.map((media) => (
-								<PartialMetadataDisplay key={media.identifier} media={media} />
+					{userMetadataGroupDetails.data &&
+					userMetadataGroupDetails.data.collections.length > 0 ? (
+						<Group id="entity-collections">
+							{userMetadataGroupDetails.data.collections.map((col) => (
+								<DisplayCollection
+									col={col}
+									entityId={metadataGroupId}
+									entityLot={EntityLot.MetadataGroup}
+									refetch={userMetadataGroupDetails.refetch}
+									key={col.id}
+								/>
 							))}
-						</SimpleGrid>
-					</MediaScrollArea>
+						</Group>
+					) : undefined}
+					<Tabs
+						value={activeTab}
+						onChange={(v) => {
+							if (v) setActiveTab(v);
+						}}
+						variant="outline"
+					>
+						<Tabs.List mb="xs">
+							<Tabs.Tab value="media" leftSection={<IconDeviceTv size={16} />}>
+								Media
+							</Tabs.Tab>
+							<Tabs.Tab value="actions" leftSection={<IconUser size={16} />}>
+								Actions
+							</Tabs.Tab>
+						</Tabs.List>
+						<Tabs.Panel value="media">
+							<MediaScrollArea>
+								<SimpleGrid cols={{ base: 3, md: 4, lg: 5 }}>
+									{groupDetails.data.contents.map((media) => (
+										<PartialMetadataDisplay
+											key={media.identifier}
+											media={media}
+										/>
+									))}
+								</SimpleGrid>
+							</MediaScrollArea>
+						</Tabs.Panel>
+						<Tabs.Panel value="actions">
+							<MediaScrollArea>
+								<SimpleGrid cols={{ base: 1, md: 2 }} spacing="lg">
+									<Button variant="outline" onClick={collectionModalOpen}>
+										Add to collection
+									</Button>
+									<AddEntityToCollectionModal
+										onClose={collectionModalClose}
+										opened={collectionModalOpened}
+										entityId={metadataGroupId}
+										refetchUserMedia={userMetadataGroupDetails.refetch}
+										entityLot={EntityLot.MetadataGroup}
+									/>
+								</SimpleGrid>
+							</MediaScrollArea>
+						</Tabs.Panel>
+					</Tabs>
 				</MediaDetailsLayout>
 			</Container>
 		</>
