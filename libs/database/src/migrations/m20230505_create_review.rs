f88dd26a8b38b85866b3400109d5e746b2555ac3
Diptesh Choudhuri <ignisda2001@gmail.com>
2023-10-24 05:51:51 +0530

    fix(backend): create correct column

--- libs/database/src/migrations/m20230505_create_review.rs
+++ libs/database/src/migrations/m20230505_create_review.rs
@@ -107,7 +107,7 @@ impl MigrationTrait for Migration {
                             .on_delete(ForeignKeyAction::Cascade)
                             .on_update(ForeignKeyAction::Cascade),
                     )
-                    .col(ColumnDef::new(Review::MetadataId).integer().null())
+                    .col(ColumnDef::new(Review::MetadataGroupId).integer().null())
                     .foreign_key(
                         ForeignKey::create()
                             .name(METADATA_GROUP_TO_REVIEW_FOREIGN_KEY)
