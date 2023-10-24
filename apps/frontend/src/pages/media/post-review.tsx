22445fd2c899faf82af119540adaa68cefe02ce4
Diptesh Choudhuri <ignisda2001@gmail.com>
2023-10-24 07:58:51 +0530

    fix(frontend): send correct params for review creation

--- apps/frontend/src/pages/media/post-review.tsx
+++ apps/frontend/src/pages/media/post-review.tsx
@@ -235,6 +235,7 @@ const Page: NextPageWithLayout = () => {
 							input: {
 								metadataId,
 								personId,
+								collectionId,
 								metadataGroupId,
 								...values,
 								reviewId,
