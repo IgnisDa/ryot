3388ca83dabe77f282033d39eb91d14dd483bcae
Diptesh Choudhuri <ignisda2001@gmail.com>
2023-11-05 22:57:20 +0530

    fix(database): change structure of calendar event's extra information

--- libs/database/src/migrations/m20231105_change_calendar_event_column_type.rs
+++ libs/database/src/migrations/m20231105_change_calendar_event_column_type.rs
@@ -10,12 +10,13 @@ impl MigrationTrait for Migration {
     async fn up(&self, manager: &SchemaManager) -> Result<(), DbErr> {
         let db = manager.get_connection();
         db.execute_unprepared(
-            r"
+            r#"
 alter table calendar_event add column temp_json jsonb;
 update calendar_event set temp_json = metadata_extra_information::jsonb;
 alter table calendar_event drop column metadata_extra_information;
 alter table calendar_event rename column temp_json to metadata_extra_information;
-",
+update calendar_event set metadata_extra_information = '{"Other":null}' where metadata_extra_information = '"Other"';
+"#,
         )
         .await?;
         manager
