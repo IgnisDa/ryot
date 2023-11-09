36230f01c6be24ebba90791ad776b7941bae5d2b
Diptesh Choudhuri <ignisda2001@gmail.com>
2023-11-09 05:58:08 +0530

    build(app): add mantine deps

--- apps/app/app/root.tsx
+++ apps/app/app/root.tsx
@@ -1,3 +1,6 @@
+import { ColorSchemeScript, MantineProvider } from "@mantine/core";
+import "@mantine/core/styles.css";
+import { LinksFunction, MetaFunction } from "@remix-run/node";
 import {
 	Links,
 	LiveReload,
@@ -7,20 +10,58 @@ import {
 	ScrollRestoration,
 } from "@remix-run/react";
 
+export const meta: MetaFunction = () => {
+	return [
+		{ title: "Ryot" },
+		{
+			name: "description",
+			content: "The only self hosted tracker you will ever need.",
+		},
+		{
+			property: "og:image",
+			content:
+				"https://raw.githubusercontent.com/IgnisDa/ryot/main/apps/frontend/public/icon-512x512.png",
+		},
+	];
+};
+
+export const links: LinksFunction = () => {
+	return [
+		{
+			rel: "icon",
+			type: "image/png",
+			sizes: "32x32",
+			href: "/favicon-32x32.png",
+		},
+		{
+			rel: "icon",
+			type: "image/png",
+			sizes: "16x16",
+			href: "/favicon-16x16.png",
+		},
+	];
+};
+
 export default function App() {
 	return (
 		<html lang="en">
 			<head>
 				<meta charSet="utf-8" />
-				<meta name="viewport" content="width=device-width, initial-scale=1" />
+				<meta
+					name="viewport"
+					content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, user-scalable=no, viewport-fit=cover"
+				/>
 				<Meta />
 				<Links />
+				<ColorSchemeScript />
 			</head>
 			<body>
-				<Outlet />
-				<ScrollRestoration />
-				<LiveReload />
-				<Scripts />
+				<MantineProvider>
+					<Outlet />
+					<ScrollRestoration />
+					<LiveReload />
+					<Scripts />
+				</MantineProvider>
 			</body>
 		</html>
 	);
