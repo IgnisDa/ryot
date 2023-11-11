9358866aa0a243023cc0367abeabacc949fbe501
Diptesh Choudhuri <ignisda2001@gmail.com>
2023-11-11 12:05:12 +0530

    scaffold(app): start basic dashboard page

--- apps/app/app/routes/_dashboard._index.tsx
+++ apps/app/app/routes/_dashboard._index.tsx
@@ -1,3 +1,25 @@
+import { LoaderFunctionArgs } from "@remix-run/node";
+import { getUserPreferences } from "~/lib/graphql.server";
+
+export const loader = async ({ request }: LoaderFunctionArgs) => {
+	const userPreferences = await getUserPreferences(request);
+	const take = userPreferences.general.dashboard.find(
+		(de) => de.section === DashboardElementLot.InProgress,
+	)?.numElements;
+	invariant(take, "No take found for in progress");
+	const { userCollectionsList } = await gqlClient.request(
+		UserCollectionsListDocument,
+		{ name: "In Progress" },
+	);
+	const collectionId = userCollectionsList[0].id;
+	const { collectionContents } = await gqlClient.request(
+		CollectionContentsDocument,
+		{
+			input: { collectionId, take, sort: { order: GraphqlSortOrder.Desc } },
+		},
+	);
+};
+
 export default function Index() {
 	return (
 		<div>
