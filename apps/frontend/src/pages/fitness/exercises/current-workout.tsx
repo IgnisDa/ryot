0922f2f23cb85301c813aad4aa4785d556c1b89b
Diptesh Choudhuri <ignisda2001@gmail.com>
2023-11-19 15:30:46 +0530

    fix(frontend): size of columns of workout actions

--- apps/frontend/src/pages/fitness/exercises/current-workout.tsx
+++ apps/frontend/src/pages/fitness/exercises/current-workout.tsx
@@ -34,6 +34,7 @@ import {
 	Paper,
 	Progress,
 	RingProgress,
+	SimpleGrid,
 	Skeleton,
 	Stack,
 	Switch,
@@ -268,12 +269,13 @@ const ExerciseDisplay = (props: {
 									produce(currentWorkout, (draft) => {
 										const defaultDuration = parseInt(
 											localStorage.getItem(
-												LOCAL_STORAGE_KEYS.defaultExerciseRestTimer
-											)|| "20",
+												LOCAL_STORAGE_KEYS.defaultExerciseRestTimer,
+											) || "20",
 										);
 										draft.exercises[props.exerciseIdx].restTimer = {
 											enabled: v.currentTarget.checked,
-											duration: props.exercise.restTimer?.duration ?? defaultDuration
+											duration:
+												props.exercise.restTimer?.duration ?? defaultDuration,
 										};
 									}),
 								);
@@ -1192,7 +1194,13 @@ const Page: NextPageWithLayout = () => {
 							/>
 						</Group>
 						<Divider />
-						<Group justify="space-around" wrap="nowrap">
+						<SimpleGrid
+							cols={
+								2 +
+								Number(currentWorkout.exercises.length > 0) +
+								Number(currentWorkout.exercises.length > 1)
+							}
+						>
 							<Button
 								color="orange"
 								variant="subtle"
@@ -1262,7 +1270,7 @@ const Page: NextPageWithLayout = () => {
 							>
 								Cancel
 							</Button>
-						</Group>
+						</SimpleGrid>
 						<Divider />
 						{currentWorkout.exercises.map((ex, idx) => (
 							<ExerciseDisplay
