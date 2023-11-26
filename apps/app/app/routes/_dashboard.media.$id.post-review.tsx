106a8bd0907e42811fa596c95391286c5a96a24f
Diptesh Choudhuri <ignisda2001@gmail.com>
2023-11-26 19:00:46 +0530

    feat(app): review page is now being rendered

--- apps/app/app/routes/_dashboard.media.$id.post-review.tsx
+++ apps/app/app/routes/_dashboard.media.$id.post-review.tsx
@@ -1,9 +1,31 @@
-import { Box, Container } from "@mantine/core";
+import {
+	Box,
+	Button,
+	Checkbox,
+	Container,
+	Flex,
+	Input,
+	NumberInput,
+	Rating,
+	SegmentedControl,
+	Stack,
+	Textarea,
+	Title,
+} from "@mantine/core";
 import { LoaderFunctionArgs, json } from "@remix-run/node";
-import { useLoaderData } from "@remix-run/react";
+import { Form, useLoaderData } from "@remix-run/react";
+import {
+	ReviewDocument,
+	UserReviewScale,
+	Visibility,
+} from "@ryot/generated/graphql/backend/graphql";
+import { IconPercentage } from "@tabler/icons-react";
 import invariant from "tiny-invariant";
+import { match } from "ts-pattern";
 import { z } from "zod";
 import { zx } from "zodix";
+import { getAuthorizationHeader, gqlClient } from "~/lib/api.server";
+import { getUserPreferences } from "~/lib/graphql.server";
 import { ShowAndPodcastSchema } from "~/lib/utils";
 
 const searchParamsSchema = z
@@ -28,15 +50,141 @@ export const loader = async ({ request, params }: LoaderFunctionArgs) => {
 	const query = zx.parseQuery(request, searchParamsSchema);
 	const id = params.id;
 	invariant(id, "No ID provided");
-	return json({ query });
+	const [userPreferences] = await Promise.all([getUserPreferences(request)]);
+	let existingReview = undefined;
+	if (query.entityType === "existingReview") {
+		const reviewId = query.existingReviewId;
+		invariant(reviewId, "No existingReviewId provided");
+		const { review } = await gqlClient.request(
+			ReviewDocument,
+			{ reviewId },
+			await getAuthorizationHeader(request),
+		);
+		existingReview = review;
+	}
+	return json({ query, existingReview, userPreferences });
 };
 
 export default function Page() {
 	const loaderData = useLoaderData<typeof loader>();
 
 	return (
-		<Container>
-			<Box>{JSON.stringify(loaderData)}</Box>
+		<Container size="xs">
+			<Box component={Form}>
+				<Stack>
+					<Title order={3}>Reviewing "{loaderData.query.title}"</Title>
+					<Flex align="center" gap="xl">
+						{match(loaderData.userPreferences.general.reviewScale)
+							.with(UserReviewScale.OutOfFive, () => (
+								<Flex gap="sm" mt="lg">
+									<Input.Label>Rating:</Input.Label>
+									<Rating
+										defaultValue={
+											loaderData.existingReview?.rating
+												? Number(loaderData.existingReview.rating)
+												: undefined
+										}
+										fractions={2}
+									/>
+								</Flex>
+							))
+							.with(UserReviewScale.OutOfHundred, () => (
+								<NumberInput
+									label="Rating"
+									min={0}
+									max={100}
+									step={1}
+									w="40%"
+									hideControls
+									rightSection={<IconPercentage size={16} />}
+									defaultValue={
+										loaderData.existingReview?.rating
+											? Number(loaderData.existingReview.rating)
+											: undefined
+									}
+								/>
+							))
+							.exhaustive()}
+						<Checkbox label="This review is a spoiler" mt="lg" />
+					</Flex>
+					{loaderData.query.isShow ? (
+						<Flex gap="md">
+							<NumberInput
+								label="Season"
+								hideControls
+								defaultValue={
+									loaderData.existingReview?.showSeason
+										? Number(loaderData.existingReview.showSeason)
+										: undefined
+								}
+							/>
+							<NumberInput
+								label="Episode"
+								hideControls
+								defaultValue={
+									loaderData.existingReview?.showEpisode
+										? Number(loaderData.existingReview.showEpisode)
+										: undefined
+								}
+							/>
+						</Flex>
+					) : undefined}
+					{loaderData.query.isPodcast ? (
+						<Flex gap="md">
+							<NumberInput
+								label="Episode"
+								hideControls
+								defaultValue={
+									loaderData.existingReview?.podcastEpisode
+										? Number(loaderData.existingReview.podcastEpisode)
+										: undefined
+								}
+							/>
+						</Flex>
+					) : undefined}
+					<Textarea
+						label="Review"
+						description="Markdown is supported"
+						autoFocus
+						minRows={10}
+						autosize
+						defaultValue={loaderData.existingReview?.text ?? undefined}
+					/>
+					<Box>
+						<Input.Label>Visibility</Input.Label>
+						<SegmentedControl
+							fullWidth
+							data={[
+								{
+									label: Visibility.Public,
+									value: Visibility.Public,
+								},
+								{
+									label: Visibility.Private,
+									value: Visibility.Private,
+								},
+							]}
+							defaultValue={
+								loaderData.existingReview?.visibility ?? Visibility.Public
+							}
+						/>
+					</Box>
+					<Button mt="md" type="submit" w="100%">
+						{loaderData.query.existingReviewId ? "Update" : "Submit"}
+					</Button>
+					{loaderData.query.existingReviewId ? (
+						<Button
+							w="100%"
+							color="red"
+							name="reviewId"
+							value={loaderData.query.existingReviewId}
+							type="submit"
+						>
+							Delete
+						</Button>
+					) : undefined}
+				</Stack>
+			</Box>
 		</Container>
 	);
 }
