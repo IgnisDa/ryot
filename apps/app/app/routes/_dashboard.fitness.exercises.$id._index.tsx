aa4cd77036f373749a1aa1646d81ac4636ea26b6
Diptesh Choudhuri <ignisda2001@gmail.com>
2023-11-28 19:34:13 +0530

    feat(app): get loader data for exercise details

--- apps/app/app/routes/_dashboard.fitness.exercises.$id._index.tsx
+++ apps/app/app/routes/_dashboard.fitness.exercises.$id._index.tsx
@@ -1,14 +1,52 @@
 import { Box, Container } from "@mantine/core";
-import { LoaderFunctionArgs, json } from "@remix-run/node";
+import { LoaderFunctionArgs, MetaFunction, json } from "@remix-run/node";
+import { useLoaderData } from "@remix-run/react";
+import {
+	ExerciseDetailsDocument,
+	UserExerciseDetailsDocument,
+} from "@ryot/generated/graphql/backend/graphql";
+import invariant from "tiny-invariant";
+import { getAuthorizationHeader, gqlClient } from "~/lib/api.server";
+import { getUserPreferences } from "~/lib/graphql.server";
 
-export const loader = async (_args: LoaderFunctionArgs) => {
-	return json({});
+export const loader = async ({ params, request }: LoaderFunctionArgs) => {
+	const exerciseId = params.id;
+	invariant(typeof exerciseId === "string", "id must be a string");
+	const [userPreferences, { exerciseDetails }, { userExerciseDetails }] =
+		await Promise.all([
+			getUserPreferences(request),
+			gqlClient.request(ExerciseDetailsDocument, { exerciseId }),
+			gqlClient.request(
+				UserExerciseDetailsDocument,
+				{ input: { exerciseId } },
+				await getAuthorizationHeader(request),
+			),
+		]);
+	return json({
+		exerciseDetails,
+		userExerciseDetails,
+		userPreferences,
+		exerciseId,
+	});
+};
+
+export const meta: MetaFunction = ({ data }) => {
+	return [
+		{
+			title: `${
+				// biome-ignore lint/suspicious/noExplicitAny:
+				(data as any).exerciseDetails.id
+			} | Ryot`,
+		},
+	];
 };
 
 export default function Page() {
+	const loaderData = useLoaderData<typeof loader>();
+
 	return (
 		<Container>
-			<Box>Hello world!</Box>
+			<Box>{JSON.stringify(loaderData)}</Box>
 		</Container>
 	);
 }
