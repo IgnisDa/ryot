509eb404632579f7e586b80f3b137d05d836bdd6
Diptesh Choudhuri <ignisda2001@gmail.com>
2023-11-28 19:51:55 +0530

    chore(app): add title tag to page

--- apps/app/app/routes/_dashboard.settings.imports-and-export.tsx
+++ apps/app/app/routes/_dashboard.settings.imports-and-export.tsx
@@ -1,8 +1,12 @@
 import { Box, Container } from "@mantine/core";
-import { LoaderFunctionArgs, json } from "@remix-run/node";
+import { LoaderFunctionArgs, MetaFunction, json } from "@remix-run/node";
 
 export const loader = async (_args: LoaderFunctionArgs) => {
 	return json({});
+};
+
+export const meta: MetaFunction = () => {
+	return [{ title: "Imports and Exports | Ryot" }];
 };
 
 export default function Page() {
