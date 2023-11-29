9bcb36fa18f82869781228225b813cef746c2acc
Diptesh Choudhuri <ignisda2001@gmail.com>
2023-11-29 13:24:01 +0530

    feat(app): display genre related media

--- apps/app/app/routes/_dashboard.media.genre.$id._index.tsx
+++ apps/app/app/routes/_dashboard.media.genre.$id._index.tsx
@@ -1,12 +1,17 @@
-import { Box, Container } from "@mantine/core";
+import { Box, Center, Container, Stack, Text, Title } from "@mantine/core";
 import { LoaderFunctionArgs, MetaFunction, json } from "@remix-run/node";
 import { useLoaderData } from "@remix-run/react";
 import { GenreDetailsDocument } from "@ryot/generated/graphql/backend/graphql";
+import { changeCase, getInitials, snakeCase } from "@ryot/ts-utils";
+import { $path } from "remix-routes";
 import invariant from "tiny-invariant";
 import { z } from "zod";
 import { zx } from "zodix";
+import { ApplicationGrid, ApplicationPagination } from "~/components/common";
+import { BaseDisplayItem } from "~/components/media";
 import { gqlClient } from "~/lib/api.server";
 import { getCoreDetails } from "~/lib/graphql.server";
+import { useSearchParam } from "~/lib/hooks";
 
 const searchParamsSchema = z.object({
 	page: zx.IntAsString.default("1"),
@@ -24,10 +29,7 @@ export const loader = async ({ request, params }: LoaderFunctionArgs) => {
 			input: { genreId, page: query.page },
 		}),
 	]);
-	return json({
-		coreDetails,
-		genreDetails,
-	});
+	return json({ query, coreDetails, genreDetails });
 };
 
 export const meta: MetaFunction = ({ data }) => {
@@ -43,10 +45,40 @@ export const meta: MetaFunction = ({ data }) => {
 
 export default function Page() {
 	const loaderData = useLoaderData<typeof loader>();
+	const [_, { setP }] = useSearchParam();
 
 	return (
 		<Container>
-			<Box>{JSON.stringify(loaderData)}</Box>
+			<Stack>
+				<Box>
+					<Title id="genre-title">{loaderData.genreDetails.details.name}</Title>
+					<Text>{loaderData.genreDetails.details.numItems} media items</Text>
+				</Box>
+				<ApplicationGrid>
+					{loaderData.genreDetails.contents.items.map((media) => (
+						<BaseDisplayItem
+							key={media.details.identifier}
+							name={media.details.title}
+							bottomLeft={media.details.publishYear}
+							bottomRight={changeCase(snakeCase(media.metadataLot || ""))}
+							imageLink={media.details.image}
+							imagePlaceholder={getInitials(media.details.title)}
+							href={$path("/media/item/:id", { id: media.details.identifier })}
+						/>
+					))}
+				</ApplicationGrid>
+				<Center>
+					<ApplicationPagination
+						size="sm"
+						defaultValue={loaderData.query.page}
+						onChange={(v) => setP("page", v.toString())}
+						total={Math.ceil(
+							loaderData.genreDetails.contents.details.total /
+								loaderData.coreDetails.pageLimit,
+						)}
+					/>
+				</Center>
+			</Stack>
 		</Container>
 	);
 }
