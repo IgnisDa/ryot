e7538dcdf8cf0b9eba1e8261305f36f4a5a96db1
Diptesh Choudhuri <ignisda2001@gmail.com>
2023-11-29 14:27:09 +0530

    feat(app): get loader data for groups

--- apps/app/app/routes/_dashboard.media.groups.list.tsx
+++ apps/app/app/routes/_dashboard.media.groups.list.tsx
@@ -1,14 +1,40 @@
 import { Box, Container } from "@mantine/core";
-import { LoaderFunctionArgs, json } from "@remix-run/node";
+import { LoaderFunctionArgs, MetaFunction, json } from "@remix-run/node";
+import { useLoaderData } from "@remix-run/react";
+import { MetadataGroupsListDocument } from "@ryot/generated/graphql/backend/graphql";
+import { z } from "zod";
+import { zx } from "zodix";
+import { gqlClient } from "~/lib/api.server";
+import { getCoreDetails } from "~/lib/graphql.server";
 
-export const loader = async (_args: LoaderFunctionArgs) => {
-	return json({});
+const searchParamsSchema = z.object({
+	page: zx.IntAsString.default("1"),
+	query: z.string().optional(),
+});
+
+export type SearchParams = z.infer<typeof searchParamsSchema>;
+
+export const loader = async ({ request }: LoaderFunctionArgs) => {
+	const query = zx.parseQuery(request, searchParamsSchema);
+	const [coreDetails, { metadataGroupsList }] = await Promise.all([
+		getCoreDetails(),
+		gqlClient.request(MetadataGroupsListDocument, {
+			input: { page: query.page, query: query.query },
+		}),
+	]);
+	return json({ coreDetails, query, metadataGroupsList });
+};
+
+export const meta: MetaFunction = () => {
+	return [{ title: "Groups | Ryot" }];
 };
 
 export default function Page() {
+	const loaderData = useLoaderData<typeof loader>();
+
 	return (
 		<Container>
-			<Box>Hello world!</Box>
+			<Box>{JSON.stringify(loaderData)}</Box>
 		</Container>
 	);
 }
