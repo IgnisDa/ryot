92871055373c8c4f8b5bc9c698795e6c9a64ad53
Diptesh Choudhuri <ignisda2001@gmail.com>
2023-11-29 12:45:16 +0530

    feat(app): get loader data for genres list

--- apps/app/app/routes/_dashboard.media.genre.list.tsx
+++ apps/app/app/routes/_dashboard.media.genre.list.tsx
@@ -1,14 +1,42 @@
 import { Box, Container } from "@mantine/core";
-import { LoaderFunctionArgs, json } from "@remix-run/node";
+import { LoaderFunctionArgs, MetaFunction, json } from "@remix-run/node";
+import { useLoaderData } from "@remix-run/react";
+import { GenresListDocument } from "@ryot/generated/graphql/backend/graphql";
+import { z } from "zod";
+import { zx } from "zodix";
+import { getAuthorizationHeader, gqlClient } from "~/lib/api.server";
+import { getCoreDetails } from "~/lib/graphql.server";
 
-export const loader = async (_args: LoaderFunctionArgs) => {
-	return json({});
+const searchParamsSchema = z.object({
+	page: zx.IntAsString.default("1"),
+	query: z.string().optional(),
+});
+
+export type SearchParams = z.infer<typeof searchParamsSchema>;
+
+export const loader = async ({ request }: LoaderFunctionArgs) => {
+	const query = zx.parseQuery(request, searchParamsSchema);
+	const [coreDetails, { genresList }] = await Promise.all([
+		getCoreDetails(),
+		gqlClient.request(
+			GenresListDocument,
+			{ input: { page: query.page, query: query.query } },
+			await getAuthorizationHeader(request),
+		),
+	]);
+	return json({ coreDetails, query, genresList });
+};
+
+export const meta: MetaFunction = () => {
+	return [{ title: "Genres | Ryot" }];
 };
 
 export default function Page() {
+	const loaderData = useLoaderData<typeof loader>();
+
 	return (
 		<Container>
-			<Box>Hello world!</Box>
+			<Box>{JSON.stringify(loaderData)}</Box>
 		</Container>
 	);
 }
