207bac3a808f71cb11698847455870208a390e7e
Diptesh Choudhuri <ignisda2001@gmail.com>
2023-11-29 12:49:29 +0530

    feat(app): complete genres list page

--- apps/app/app/routes/_dashboard.media.genre.list.tsx
+++ apps/app/app/routes/_dashboard.media.genre.list.tsx
@@ -1,11 +1,29 @@
-import { Box, Container } from "@mantine/core";
+import {
+	ActionIcon,
+	Anchor,
+	Box,
+	Center,
+	Container,
+	Flex,
+	Group,
+	Paper,
+	Stack,
+	Text,
+	TextInput,
+	Title,
+} from "@mantine/core";
 import { LoaderFunctionArgs, MetaFunction, json } from "@remix-run/node";
-import { useLoaderData } from "@remix-run/react";
+import { Link, useLoaderData } from "@remix-run/react";
 import { GenresListDocument } from "@ryot/generated/graphql/backend/graphql";
+import { IconSearch, IconX } from "@tabler/icons-react";
+import { useEffect, useState } from "react";
+import { $path } from "remix-routes";
 import { z } from "zod";
 import { zx } from "zodix";
+import { ApplicationGrid, ApplicationPagination } from "~/components/common";
 import { getAuthorizationHeader, gqlClient } from "~/lib/api.server";
 import { getCoreDetails } from "~/lib/graphql.server";
+import { useGetMantineColor, useSearchParam } from "~/lib/hooks";
 
 const searchParamsSchema = z.object({
 	page: zx.IntAsString.default("1"),
@@ -24,7 +42,7 @@ export const loader = async ({ request }: LoaderFunctionArgs) => {
 			await getAuthorizationHeader(request),
 		),
 	]);
-	return json({ coreDetails, query, genresList });
+	return json({ coreDetails, query, listGenres: genresList });
 };
 
 export const meta: MetaFunction = () => {
@@ -33,10 +51,87 @@ export const meta: MetaFunction = () => {
 
 export default function Page() {
 	const loaderData = useLoaderData<typeof loader>();
+	const getMantineColor = useGetMantineColor();
+	const [searchParams, { setP }] = useSearchParam();
+	const [query, setQuery] = useState(searchParams.get("query") || "");
+
+	useEffect(() => setP("query", query), [query]);
 
 	return (
 		<Container>
-			<Box>{JSON.stringify(loaderData)}</Box>
+			<Stack>
+				<Flex align="center" gap="md">
+					<Title>Genres</Title>
+				</Flex>
+				<TextInput
+					name="query"
+					placeholder="Search for genres"
+					leftSection={<IconSearch />}
+					onChange={(e) => setQuery(e.currentTarget.value)}
+					value={query}
+					rightSection={
+						query ? (
+							<ActionIcon onClick={() => setQuery("")}>
+								<IconX size={16} />
+							</ActionIcon>
+						) : undefined
+					}
+					style={{ flexGrow: 1 }}
+					autoCapitalize="none"
+					autoComplete="off"
+				/>
+				{loaderData.listGenres.details.total > 0 ? (
+					<>
+						<Box>
+							<Text display="inline" fw="bold">
+								{loaderData.listGenres.details.total}
+							</Text>{" "}
+							items found
+						</Box>
+						<ApplicationGrid>
+							{loaderData.listGenres.items.map((genre) => (
+								<Paper key={genre.id}>
+									<Group>
+										<Box
+											h={11}
+											w={11}
+											style={{ borderRadius: 2 }}
+											bg={getMantineColor(genre.name)}
+										/>
+										<Box>
+											<Anchor
+												component={Link}
+												to={$path("/media/genre/:id", { id: genre.id })}
+											>
+												{genre.name.substring(0, 13).trim()}
+												{genre.name.length > 13 ? "..." : ""}
+											</Anchor>
+											<Text size="sm" c="dimmed">
+												{genre.numItems} items
+											</Text>
+										</Box>
+									</Group>
+								</Paper>
+							))}
+						</ApplicationGrid>
+					</>
+				) : (
+					<Text>No information to display</Text>
+				)}
+				{loaderData.listGenres ? (
+					<Center mt="xl">
+						<ApplicationPagination
+							size="sm"
+							defaultValue={loaderData.query.page}
+							onChange={(v) => setP("page", v.toString())}
+							total={Math.ceil(
+								loaderData.listGenres.details.total /
+									loaderData.coreDetails.pageLimit,
+							)}
+						/>
+					</Center>
+				) : undefined}
+			</Stack>
 		</Container>
 	);
 }
