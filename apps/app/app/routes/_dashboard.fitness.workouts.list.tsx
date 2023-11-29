0134323e548aa9816d857b3c31f74643730bd448
Diptesh Choudhuri <ignisda2001@gmail.com>
2023-11-29 05:35:33 +0530

    feat(app): get loader data for workouts list

--- apps/app/app/routes/_dashboard.fitness.workouts.list.tsx
+++ apps/app/app/routes/_dashboard.fitness.workouts.list.tsx
@@ -1,8 +1,36 @@
 import { Box, Container } from "@mantine/core";
 import { LoaderFunctionArgs, MetaFunction, json } from "@remix-run/node";
+import { useLoaderData } from "@remix-run/react";
+import { UserWorkoutListDocument } from "@ryot/generated/graphql/backend/graphql";
+import { z } from "zod";
+import { zx } from "zodix";
+import { gqlClient, getAuthorizationHeader } from "~/lib/api.server";
+import { getCoreDetails } from "~/lib/graphql.server";
 
-export const loader = async (_args: LoaderFunctionArgs) => {
-	return json({});
+const searchParamsSchema = z.object({
+	page: zx.IntAsString.default("1"),
+	query: z.string().optional(),
+});
+
+export type SearchParams = z.infer<typeof searchParamsSchema>;
+
+export const loader = async ({ request }: LoaderFunctionArgs) => {
+	const query = zx.parseQuery(request, searchParamsSchema);
+	const [coreDetails, { userWorkoutList }] = await Promise.all([
+		getCoreDetails(),
+		gqlClient.request(
+			UserWorkoutListDocument,
+			{
+				input: { page: query.page, query: query.query },
+			},
+			await getAuthorizationHeader(request),
+		),
+	]);
+	return json({
+		coreDetails,
+		query,
+		userWorkoutList,
+	});
 };
 
 export const meta: MetaFunction = () => {
@@ -10,9 +38,11 @@ export const meta: MetaFunction = () => {
 };
 
 export default function Page() {
+	const loaderData = useLoaderData<typeof loader>();
+
 	return (
 		<Container>
-			<Box>Hello world!</Box>
+			<Box>{JSON.stringify(loaderData)}</Box>
 		</Container>
 	);
 }
