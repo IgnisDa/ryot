298be95a17c06296e469b7c7c398a07348c2381f
Diptesh Choudhuri <ignisda2001@gmail.com>
2023-11-29 14:57:50 +0530

    feat(app): get loader data for group details

--- apps/app/app/routes/_dashboard.media.groups.$id._index.tsx
+++ apps/app/app/routes/_dashboard.media.groups.$id._index.tsx
@@ -1,14 +1,45 @@
 import { Box, Container } from "@mantine/core";
-import { LoaderFunctionArgs, json } from "@remix-run/node";
+import { LoaderFunctionArgs, MetaFunction, json } from "@remix-run/node";
+import { useLoaderData } from "@remix-run/react";
+import {
+	MetadataGroupDetailsDocument,
+	UserMetadataGroupDetailsDocument,
+} from "@ryot/generated/graphql/backend/graphql";
+import invariant from "tiny-invariant";
+import { getAuthorizationHeader, gqlClient } from "~/lib/api.server";
 
-export const loader = async (_args: LoaderFunctionArgs) => {
-	return json({});
+export const loader = async ({ request, params }: LoaderFunctionArgs) => {
+	const metadataGroupId = params.id ? Number(params.id) : undefined;
+	invariant(metadataGroupId, "No ID provided");
+	const [{ metadataGroupDetails }, { userMetadataGroupDetails }] =
+		await Promise.all([
+			gqlClient.request(MetadataGroupDetailsDocument, { metadataGroupId }),
+			gqlClient.request(
+				UserMetadataGroupDetailsDocument,
+				{ metadataGroupId },
+				await getAuthorizationHeader(request),
+			),
+		]);
+	return json({ metadataGroupDetails, userMetadataGroupDetails });
+};
+
+export const meta: MetaFunction = ({ data }) => {
+	return [
+		{
+			title: `${
+				// biome-ignore lint/suspicious/noExplicitAny:
+				(data as any).metadataGroupDetails.details.title
+			} | Ryot`,
+		},
+	];
 };
 
 export default function Page() {
+	const loaderData = useLoaderData<typeof loader>();
+
 	return (
 		<Container>
-			<Box>Hello world!</Box>
+			<Box>{JSON.stringify(loaderData)}</Box>
 		</Container>
 	);
 }
