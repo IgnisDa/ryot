66419cbd5ae81a1c29ea460cc584bd3be9500eef
Diptesh Choudhuri <ignisda2001@gmail.com>
2023-12-02 10:47:32 +0530

    feat(app): display the form to update

--- apps/app/app/routes/_dashboard.settings.profile.tsx
+++ apps/app/app/routes/_dashboard.settings.profile.tsx
@@ -1,4 +1,12 @@
-import { Box, Container } from "@mantine/core";
+import {
+	Box,
+	Button,
+	Container,
+	PasswordInput,
+	Stack,
+	TextInput,
+	Title,
+} from "@mantine/core";
 import { LoaderFunctionArgs, MetaFunction, json } from "@remix-run/node";
 import { useLoaderData } from "@remix-run/react";
 import { getCoreDetails, getUserDetails } from "~/lib/graphql.server";
@@ -8,23 +16,50 @@ export const loader = async ({ request }: LoaderFunctionArgs) => {
 		getCoreDetails(),
 		getUserDetails(request),
 	]);
-	return json({
-		coreDetails,
-		userDetails,
-	});
-}
+	return json({ coreDetails, userDetails });
+};
 
 export const meta: MetaFunction = () => {
 	return [{ title: "Profile Settings | Ryot" }];
 };
-;
 
 export default function Page() {
 	const loaderData = useLoaderData<typeof loader>();
 
 	return (
-		<Container>
-			<Box>{JSON.stringify(loaderData)}</Box>
+		<Container size="xs">
+			<Stack>
+				<Title>Profile settings</Title>
+				<Box component="form">
+					<Stack>
+						<TextInput
+							label="Username"
+							disabled={!loaderData.coreDetails.usernameChangeAllowed}
+							description={
+								!loaderData.coreDetails.usernameChangeAllowed &&
+								"Username can not be changed on this instance"
+							}
+							defaultValue={loaderData.userDetails.name}
+						/>
+						<TextInput
+							label="Email"
+							autoFocus
+							defaultValue={loaderData.userDetails.email ?? undefined}
+						/>
+						<PasswordInput
+							label="Password"
+							disabled={!loaderData.coreDetails.passwordChangeAllowed}
+							description={
+								!loaderData.coreDetails.passwordChangeAllowed &&
+								"Password can not be changed on this instance"
+							}
+						/>
+						<Button type="submit" fullWidth>
+							Update
+						</Button>
+					</Stack>
+				</Box>
+			</Stack>
 		</Container>
 	);
 }
