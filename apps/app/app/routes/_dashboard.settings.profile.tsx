66a2d0364fc72a2303e0462872e37ca65f289156
Diptesh Choudhuri <ignisda2001@gmail.com>
2023-12-02 10:41:03 +0530

    feat(app): loader data for profile page

--- apps/app/app/routes/_dashboard.settings.profile.tsx
+++ apps/app/app/routes/_dashboard.settings.profile.tsx
@@ -1,14 +1,30 @@
 import { Box, Container } from "@mantine/core";
-import { LoaderFunctionArgs, json } from "@remix-run/node";
+import { LoaderFunctionArgs, MetaFunction, json } from "@remix-run/node";
+import { useLoaderData } from "@remix-run/react";
+import { getCoreDetails, getUserDetails } from "~/lib/graphql.server";
 
-export const loader = async (_args: LoaderFunctionArgs) => {
-	return json({});
+export const loader = async ({ request }: LoaderFunctionArgs) => {
+	const [coreDetails, userDetails] = await Promise.all([
+		getCoreDetails(),
+		getUserDetails(request),
+	]);
+	return json({
+		coreDetails,
+		userDetails,
+	});
+}
+
+export const meta: MetaFunction = () => {
+	return [{ title: "Profile Settings | Ryot" }];
 };
+;
 
 export default function Page() {
+	const loaderData = useLoaderData<typeof loader>();
+
 	return (
 		<Container>
-			<Box>Hello world!</Box>
+			<Box>{JSON.stringify(loaderData)}</Box>
 		</Container>
 	);
 }
