116295947153fac29e09a1d97ef6db1057642edc
Diptesh Choudhuri <ignisda2001@gmail.com>
2023-12-03 16:36:45 +0530

    feat(app): loader data for integrations page

--- apps/app/app/routes/_dashboard.settings.integrations.tsx
+++ apps/app/app/routes/_dashboard.settings.integrations.tsx
@@ -1,14 +1,30 @@
 import { Box, Container } from "@mantine/core";
-import { LoaderFunctionArgs, json } from "@remix-run/node";
+import { LoaderFunctionArgs, MetaFunction, json } from "@remix-run/node";
+import { useLoaderData } from "@remix-run/react";
+import { UserIntegrationsDocument } from "@ryot/generated/graphql/backend/graphql";
+import { getAuthorizationHeader, gqlClient } from "~/lib/api.server";
 
-export const loader = async (_args: LoaderFunctionArgs) => {
-	return json({});
+export const loader = async ({ request }: LoaderFunctionArgs) => {
+	const [{ userIntegrations }] = await Promise.all([
+		gqlClient.request(
+			UserIntegrationsDocument,
+			undefined,
+			await getAuthorizationHeader(request),
+		),
+	]);
+	return json({ userIntegrations });
+};
+
+export const meta: MetaFunction = () => {
+	return [{ title: "Integration Settings | Ryot" }];
 };
 
 export default function Page() {
+	const loaderData = useLoaderData<typeof loader>();
+
 	return (
 		<Container>
-			<Box>Hello world!</Box>
+			<Box>{JSON.stringify(loaderData)}</Box>
 		</Container>
 	);
 }
