bb2afee95f4306379fba38f1c1c8b6b57a33c6cd
Diptesh Choudhuri <ignisda2001@gmail.com>
2023-12-03 06:44:31 +0530

    feat(app): get loader data for preferences page

--- apps/app/app/routes/_dashboard.settings.preferences.tsx
+++ apps/app/app/routes/_dashboard.settings.preferences.tsx
@@ -1,14 +1,26 @@
 import { Box, Container } from "@mantine/core";
-import { LoaderFunctionArgs, json } from "@remix-run/node";
+import { LoaderFunctionArgs, MetaFunction, json } from "@remix-run/node";
+import { useLoaderData } from "@remix-run/react";
+import { getCoreDetails, getUserPreferences } from "~/lib/graphql.server";
 
-export const loader = async (_args: LoaderFunctionArgs) => {
-	return json({});
+export const loader = async ({ request }: LoaderFunctionArgs) => {
+	const [coreDetails, userPreferences] = await Promise.all([
+		getCoreDetails(),
+		getUserPreferences(request),
+	]);
+	return json({ coreDetails, userPreferences });
+};
+
+export const meta: MetaFunction = () => {
+	return [{ title: "Preference | Ryot" }];
 };
 
 export default function Page() {
+	const loaderData = useLoaderData<typeof loader>();
+
 	return (
 		<Container>
-			<Box>Hello world!</Box>
+			<Box>{JSON.stringify(loaderData)}</Box>
 		</Container>
 	);
 }
