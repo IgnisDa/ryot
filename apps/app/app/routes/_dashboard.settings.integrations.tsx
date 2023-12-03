9d5b4ca493e7e4b24cef420b6e4d3b7d14106ae1
Diptesh Choudhuri <ignisda2001@gmail.com>
2023-12-03 16:48:41 +0530

    feat(app): render integrations page

--- apps/app/app/routes/_dashboard.settings.integrations.tsx
+++ apps/app/app/routes/_dashboard.settings.integrations.tsx
@@ -1,7 +1,31 @@
-import { Box, Container } from "@mantine/core";
+import {
+	ActionIcon,
+	Box,
+	Button,
+	Container,
+	CopyButton,
+	Flex,
+	Group,
+	Modal,
+	Paper,
+	Select,
+	Stack,
+	Text,
+	TextInput,
+	Title,
+} from "@mantine/core";
+import { useDisclosure } from "@mantine/hooks";
+import { notifications } from "@mantine/notifications";
 import { LoaderFunctionArgs, MetaFunction, json } from "@remix-run/node";
 import { useLoaderData } from "@remix-run/react";
-import { UserIntegrationsDocument } from "@ryot/generated/graphql/backend/graphql";
+import {
+	UserIntegrationsDocument,
+	UserSinkIntegrationSettingKind,
+	UserYankIntegrationSettingKind,
+} from "@ryot/generated/graphql/backend/graphql";
+import { IconCopy, IconTrash } from "@tabler/icons-react";
+import { DateTime } from "luxon";
+import { useState } from "react";
 import { getAuthorizationHeader, gqlClient } from "~/lib/api.server";
 
 export const loader = async ({ request }: LoaderFunctionArgs) => {
@@ -21,10 +45,181 @@ export const meta: MetaFunction = () => {
 
 export default function Page() {
 	const loaderData = useLoaderData<typeof loader>();
+	const [
+		createUserYankIntegrationModalOpened,
+		{
+			open: openCreateUserYankIntegrationModal,
+			close: closeCreateUserYankIntegrationModal,
+		},
+	] = useDisclosure(false);
+	const [createUserYankIntegrationLot, setCreateUserYankIntegrationLot] =
+		useState<UserYankIntegrationSettingKind>();
+	const [createUserSinkIntegrationLot, setCreateUserSinkIntegrationLot] =
+		useState<UserSinkIntegrationSettingKind>();
 
 	return (
-		<Container>
-			<Box>{JSON.stringify(loaderData)}</Box>
+		<Container size="xs">
+			<Stack>
+				<Title>Integration settings</Title>
+				{loaderData.userIntegrations.length > 0 ? (
+					loaderData.userIntegrations.map((i, idx) => (
+						<Paper p="xs" withBorder key={`${i.id}-${idx}`}>
+							<Flex align="center" justify="space-between">
+								<Box>
+									<Text size="xs">{i.description}</Text>
+									<Text size="xs">
+										{DateTime.fromISO(i.timestamp).toRelative()}
+									</Text>
+								</Box>
+								<Group>
+									{i.slug ? (
+										<CopyButton
+											value={
+												typeof window !== "undefined"
+													? `${
+															window.location.origin
+													  }/webhooks/integrations/${i.description
+															.toLowerCase()
+															.split(" ")
+															.at(0)}/${i.slug}`
+													: ""
+											}
+										>
+											{({ copy }) => (
+												<ActionIcon
+													color="green"
+													onClick={() => {
+														copy();
+														notifications.show({
+															color: "green",
+															title: "Operation successful",
+															message:
+																"The integration url has been copied to your clipboard.",
+														});
+													}}
+												>
+													<IconCopy />
+												</ActionIcon>
+											)}
+										</CopyButton>
+									) : undefined}
+									<ActionIcon
+										color="red"
+										variant="subtle"
+										size="sm"
+										onClick={() => {
+											const yes = confirm(
+												"Are you sure you want to delete this integration?",
+											);
+											if (yes)
+												deleteUserIntegration.mutate({
+													integrationId: i.id,
+													integrationLot: i.lot,
+												});
+										}}
+									>
+										<IconTrash />
+									</ActionIcon>
+								</Group>
+							</Flex>
+						</Paper>
+					))
+				) : (
+					<Text>No integrations configured</Text>
+				)}
+				<Box ml="auto">
+					<Button
+						size="xs"
+						variant="light"
+						onClick={openCreateUserYankIntegrationModal}
+					>
+						Add new integration
+					</Button>
+					<Modal
+						opened={createUserYankIntegrationModalOpened}
+						onClose={closeCreateUserYankIntegrationModal}
+						centered
+						withCloseButton={false}
+					>
+						<Box
+							component="form"
+							onSubmit={(values) => {
+								if (createUserYankIntegrationLot) {
+									createUserYankIntegration.mutate({
+										input: {
+											// biome-ignore lint/style/noNonNullAssertion: any is required here
+											baseUrl: values.baseUrl!,
+											// biome-ignore lint/style/noNonNullAssertion: any is required here
+											token: values.token!,
+											lot: createUserYankIntegrationLot,
+										},
+									});
+								} else if (createUserSinkIntegrationLot) {
+									createUserSinkIntegration.mutate({
+										input: {
+											lot: createUserSinkIntegrationLot,
+											username: values.username,
+										},
+									});
+								}
+								closeCreateUserYankIntegrationModal();
+								createUserYankIntegrationForm.reset();
+								setCreateUserYankIntegrationLot(undefined);
+								setCreateUserSinkIntegrationLot(undefined);
+							}}
+						>
+							<Stack>
+								<Select
+									label="Select a source"
+									required
+									data={[
+										...Object.values(UserYankIntegrationSettingKind),
+										...Object.values(UserSinkIntegrationSettingKind),
+									]}
+									// biome-ignore lint/suspicious/noExplicitAny: required here
+									onChange={(v: any) => {
+										if (v) {
+											if (
+												Object.values(UserYankIntegrationSettingKind).includes(
+													v,
+												)
+											) {
+												setCreateUserYankIntegrationLot(
+													v as UserYankIntegrationSettingKind,
+												);
+												setCreateUserSinkIntegrationLot(undefined);
+											}
+											if (
+												Object.values(UserSinkIntegrationSettingKind).includes(
+													v,
+												)
+											) {
+												setCreateUserSinkIntegrationLot(
+													v as UserSinkIntegrationSettingKind,
+												);
+												setCreateUserYankIntegrationLot(undefined);
+											}
+										}
+									}}
+								/>
+								{createUserYankIntegrationLot ? (
+									<>
+										<TextInput label="Base Url" required name="baseUrl" />
+										<TextInput label="Token" required name="token" />
+									</>
+								) : undefined}
+								{createUserSinkIntegrationLot ===
+								UserSinkIntegrationSettingKind.Plex ? (
+									<>
+										<TextInput label="Username" name="username" />
+									</>
+								) : undefined}
+								<Button type="submit">Submit</Button>
+							</Stack>
+						</Box>
+					</Modal>
+				</Box>
+			</Stack>
 		</Container>
 	);
 }
