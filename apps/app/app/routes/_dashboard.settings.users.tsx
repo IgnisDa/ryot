f975281e2b607bc33342151de04eb4c43d1d56b1
Diptesh Choudhuri <ignisda2001@gmail.com>
2023-12-03 06:23:38 +0530

    feat(app): get loader data details for page

--- apps/app/app/routes/_dashboard.settings.users.tsx
+++ apps/app/app/routes/_dashboard.settings.users.tsx
@@ -1,14 +1,32 @@
 import { Box, Container } from "@mantine/core";
-import { LoaderFunctionArgs, json } from "@remix-run/node";
+import { LoaderFunctionArgs, MetaFunction, json } from "@remix-run/node";
+import { useLoaderData } from "@remix-run/react";
+import { UsersListDocument } from "@ryot/generated/graphql/backend/graphql";
+import { getAuthorizationHeader, gqlClient } from "~/lib/api.server";
+import { getCoreDetails } from "~/lib/graphql.server";
 
-export const loader = async (_args: LoaderFunctionArgs) => {
-	return json({});
+export const loader = async ({ request }: LoaderFunctionArgs) => {
+	const [coreDetails, usersList] = await Promise.all([
+		getCoreDetails(),
+		gqlClient.request(
+			UsersListDocument,
+			undefined,
+			await getAuthorizationHeader(request),
+		),
+	]);
+	return json({ coreDetails, usersList });
+};
+
+export const meta: MetaFunction = () => {
+	return [{ title: "User Settings | Ryot" }];
 };
 
 export default function Page() {
+	const loaderData = useLoaderData<typeof loader>();
+
 	return (
 		<Container>
-			<Box>Hello world!</Box>
+			<Box>{JSON.stringify(loaderData)}</Box>
 		</Container>
 	);
 }
