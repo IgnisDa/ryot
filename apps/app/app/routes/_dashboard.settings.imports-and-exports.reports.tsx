3a0fd9bed4884e3e6cf2ceb06457009af739e30e
Diptesh Choudhuri <ignisda2001@gmail.com>
2023-12-03 21:05:38 +0530

    feat(app): display all import reports

--- apps/app/app/routes/_dashboard.settings.imports-and-exports.reports.tsx
+++ apps/app/app/routes/_dashboard.settings.imports-and-exports.reports.tsx
@@ -1,7 +1,20 @@
-import { Box, Container } from "@mantine/core";
+import {
+	Accordion,
+	Anchor,
+	Container,
+	Flex,
+	Indicator,
+	JsonInput,
+	Stack,
+	Text,
+	Title,
+} from "@mantine/core";
 import { LoaderFunctionArgs, MetaFunction, json } from "@remix-run/node";
-import { useLoaderData } from "@remix-run/react";
+import { Link, useLoaderData } from "@remix-run/react";
 import { ImportReportsDocument } from "@ryot/generated/graphql/backend/graphql";
+import { changeCase } from "@ryot/ts-utils";
+import { DateTime } from "luxon";
+import { $path } from "remix-routes";
 import { getAuthorizationHeader, gqlClient } from "~/lib/api.server";
 
 export const loader = async ({ request }: LoaderFunctionArgs) => {
@@ -23,8 +36,73 @@ export default function Page() {
 	const loaderData = useLoaderData<typeof loader>();
 
 	return (
-		<Container>
-			<Box>{JSON.stringify(loaderData)}</Box>
+		<Container size="sm">
+			<Stack>
+				<Flex justify="space-between" align="center">
+					<Title>Import Reports</Title>
+					<Anchor
+						size="xs"
+						to={$path("/settings/imports-and-exports")}
+						component={Link}
+					>
+						New
+					</Anchor>
+				</Flex>
+				{loaderData.importReports.length > 0 ? (
+					<Accordion>
+						{loaderData.importReports.map((report) => (
+							<Accordion.Item value={report.id.toString()}>
+								<Accordion.Control
+									disabled={typeof report.success !== "boolean"}
+								>
+									<Indicator
+										inline
+										size={12}
+										offset={-3}
+										processing={typeof report.success !== "boolean"}
+										color={
+											typeof report.success === "boolean"
+												? report.success
+													? "green"
+													: "red"
+												: undefined
+										}
+									>
+										{changeCase(report.source)} on{" "}
+										{DateTime.fromISO(report.startedOn).toLocaleString(
+											DateTime.DATETIME_SHORT,
+										)}
+									</Indicator>
+								</Accordion.Control>
+								<Accordion.Panel>
+									{report.details ? (
+										<>
+											<Text>Total imported: {report.details.import.total}</Text>
+											<Text>Failed: {report.details.failedItems.length}</Text>
+											{report.details.failedItems.length > 0 ? (
+												<JsonInput
+													size="xs"
+													defaultValue={JSON.stringify(
+														report.details.failedItems,
+														null,
+														4,
+													)}
+													disabled
+													autosize
+												/>
+											) : undefined}
+										</>
+									) : (
+										<Text>This import never finished</Text>
+									)}
+								</Accordion.Panel>
+							</Accordion.Item>
+						))}
+					</Accordion>
+				) : (
+					<Text>You have not performed any imports</Text>
+				)}
+			</Stack>
 		</Container>
 	);
 }
