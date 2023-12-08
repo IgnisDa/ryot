00b10864180dea152cc8b18a896733863f29b4a7
Diptesh Choudhuri <ignisda2001@gmail.com>
2023-12-08 23:30:24 +0530

    Revert "Revert "feat(app): remove usage of old colors""
    This reverts commit c1455b3a965d5bdc0fa9e503e0a0685cc518c1b5.

--- apps/frontend/app/root.tsx
+++ apps/frontend/app/root.tsx
@@ -35,20 +35,6 @@ import { combineHeaders } from "~/lib/utilities.server";
 import { colorSchemeCookie } from "./lib/cookies.server";
 
 const theme = createTheme({
-	colors: {
-		dark: [
-			"#C1C2C5",
-			"#A6A7AB",
-			"#909296",
-			"#5c5f66",
-			"#373A40",
-			"#2C2E33",
-			"#25262b",
-			"#1A1B1E",
-			"#141517",
-			"#101113",
-		],
-	},
 	fontFamily: "Poppins",
 	components: {
 		ActionIcon: ActionIcon.extend({
