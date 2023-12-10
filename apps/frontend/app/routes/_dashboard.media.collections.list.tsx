600f4d38e9e71cb4ed10160c2c34fe6fd415c0ea
Diptesh Choudhuri <ignisda2001@gmail.com>
2023-12-10 13:52:29 +0530

    feat(frontend): display basic collection list

--- apps/frontend/app/routes/_dashboard.media.collections.list.tsx
+++ apps/frontend/app/routes/_dashboard.media.collections.list.tsx
@@ -5,7 +5,9 @@ import {
 	Box,
 	Button,
 	Container,
+	Divider,
 	Flex,
+	Group,
 	Input,
 	Modal,
 	SegmentedControl,
@@ -27,6 +29,7 @@ import { Form, Link, useLoaderData, useNavigation } from "@remix-run/react";
 import {
 	CreateOrUpdateCollectionDocument,
 	DeleteCollectionDocument,
+	PublicCollectionsListDocument,
 	UserCollectionsListDocument,
 	Visibility,
 } from "@ryot/generated/graphql/backend/graphql";
@@ -36,9 +39,18 @@ import { useEffect, useRef, useState } from "react";
 import { namedAction } from "remix-utils/named-action";
 import { z } from "zod";
 import { zx } from "zodix";
+import { ApplicationGrid } from "~/components/common";
 import { getAuthorizationHeader, gqlClient } from "~/lib/api.server";
+import { useGetMantineColor } from "~/lib/hooks";
 import { createToastHeaders } from "~/lib/toast.server";
 import { processSubmission } from "~/lib/utilities.server";
+
+const searchParamsSchema = z.object({
+	page: zx.IntAsString.optional(),
+	query: z.string().optional(),
+});
+
+export type SearchParams = z.infer<typeof searchParamsSchema>;
 
 export const loader = async ({ request }: LoaderFunctionArgs) => {
 	const { userCollectionsList } = await gqlClient.request(
@@ -46,7 +58,12 @@ export const loader = async ({ request }: LoaderFunctionArgs) => {
 		{},
 		await getAuthorizationHeader(request),
 	);
-	return json({ collections: userCollectionsList });
+	const query = zx.parseQuery(request, searchParamsSchema);
+	const { publicCollectionsList } = await gqlClient.request(
+		PublicCollectionsListDocument,
+		{ input: query },
+	);
+	return json({ collections: userCollectionsList, publicCollectionsList });
 };
 
 export const meta: MetaFunction = () => {
@@ -117,6 +134,7 @@ const createOrUpdateSchema = z.object({
 export default function Page() {
 	const transition = useNavigation();
 	const loaderData = useLoaderData<typeof loader>();
+	const getMantineColor = useGetMantineColor();
 	const [toUpdateCollection, setToUpdateCollection] = useState<{
 		name: string;
 		id: number;
@@ -134,32 +152,102 @@ export default function Page() {
 	}, [transition.state]);
 
 	return (
-		<Container>
-			<Stack>
-				<Flex align="center" gap="md">
-					<Title>Collections</Title>
-					<ActionIcon
-						color="green"
-						variant="outline"
-						onClick={() => {
-							setToUpdateCollection(undefined);
-							open();
-						}}
-					>
-						<IconPlus size={20} />
-					</ActionIcon>
-				</Flex>
-				<SimpleGrid cols={{ base: 1, md: 2 }}>
-					{loaderData.collections.map((c) => (
-						<Flex
-							key={c.id}
-							align="center"
-							justify="space-between"
-							gap="md"
-							mr="lg"
+		<>
+			<Container>
+				<Stack>
+					<Flex align="center" gap="md">
+						<Title>Your collections</Title>
+						<ActionIcon
+							color="green"
+							variant="outline"
+							onClick={() => {
+								setToUpdateCollection(undefined);
+								open();
+							}}
 						>
-							<Box>
-								<Flex align="center" gap="xs">
+							<IconPlus size={20} />
+						</ActionIcon>
+					</Flex>
+					<SimpleGrid cols={{ base: 1, md: 2 }}>
+						{loaderData.collections.map((c) => (
+							<Flex
+								key={c.id}
+								align="center"
+								justify="space-between"
+								gap="md"
+								mr="lg"
+							>
+								<Box>
+									<Flex align="center" gap="xs">
+										<Anchor
+											component={Link}
+											to={$path("/media/collections/:id", { id: c.id })}
+										>
+											<Title order={4}>{c.name}</Title>
+										</Anchor>
+										<Text c="dimmed" size="xs">
+											{c.numItems} items, {changeCase(c.visibility || "")}
+										</Text>
+									</Flex>
+									{c.description ? <Text>{c.description}</Text> : undefined}
+								</Box>
+								<Flex gap="sm" style={{ flex: 0 }}>
+									<ActionIcon
+										color="blue"
+										variant="outline"
+										onClick={() => {
+											setToUpdateCollection({
+												name: c.name,
+												id: c.id,
+												description: c.description,
+												visibility: c.visibility,
+											});
+											open();
+										}}
+									>
+										<IconEdit size={18} />
+									</ActionIcon>
+									<Form action="?intent=delete" method="post" ref={formRef}>
+										<ActionIcon
+											color="red"
+											variant="outline"
+											type="submit"
+											name="collectionName"
+											value={c.name}
+											onClick={(e) => {
+												if (
+													!confirm(
+														"Are you sure you want to delete this collection?",
+													)
+												) {
+													e.preventDefault();
+												}
+											}}
+										>
+											<IconTrashFilled size={18} />
+										</ActionIcon>
+									</Form>
+								</Flex>
+							</Flex>
+						))}
+					</SimpleGrid>
+					<Divider mt="xl" />
+					<Flex align="center" gap="xs">
+						<Title>Public collections</Title>
+						<Text c="dimmed">
+							{loaderData.publicCollectionsList.details.total} items
+						</Text>
+					</Flex>
+					<ApplicationGrid>
+						{loaderData.publicCollectionsList.items.map((c) => (
+							<Group>
+								<Box
+									h={11}
+									w={11}
+									style={{ borderRadius: 2 }}
+									bg={getMantineColor(c.name)}
+								/>
+								<Box>
 									<Anchor
 										component={Link}
 										to={$path("/media/collections/:id", { id: c.id })}
@@ -167,108 +255,70 @@ export default function Page() {
 										<Title order={4}>{c.name}</Title>
 									</Anchor>
 									<Text c="dimmed" size="xs">
-										{c.numItems} items, {changeCase(c.visibility || "")}
+										by {c.username || "hello"}
 									</Text>
-								</Flex>
-								{c.description ? <Text>{c.description}</Text> : undefined}
-							</Box>
-							<Flex gap="sm" style={{ flex: 0 }}>
-								<ActionIcon
-									color="blue"
-									variant="outline"
-									onClick={() => {
-										setToUpdateCollection({
-											name: c.name,
-											id: c.id,
-											description: c.description,
-											visibility: c.visibility,
-										});
-										open();
-									}}
-								>
-									<IconEdit size={18} />
-								</ActionIcon>
-								<Form action="?intent=delete" method="post" ref={formRef}>
-									<ActionIcon
-										color="red"
-										variant="outline"
-										type="submit"
-										name="collectionName"
-										value={c.name}
-										onClick={(e) => {
-											if (
-												!confirm(
-													"Are you sure you want to delete this collection?",
-												)
-											) {
-												e.preventDefault();
-											}
-										}}
-									>
-										<IconTrashFilled size={18} />
-									</ActionIcon>
-								</Form>
-							</Flex>
-						</Flex>
-					))}
-				</SimpleGrid>
-				<Modal opened={opened} onClose={close} withCloseButton={false} centered>
-					<Box component={Form} method="post" action="?intent=createOrUpdate">
-						<Stack>
-							<Title order={3}>
-								{toUpdateCollection ? "Update" : "Create"} collection
-							</Title>
-							<TextInput
-								label="Name"
-								required
-								name="name"
+								</Box>
+							</Group>
+						))}
+					</ApplicationGrid>
+				</Stack>
+			</Container>
+			<Modal opened={opened} onClose={close} withCloseButton={false} centered>
+				<Box component={Form} method="post" action="?intent=createOrUpdate">
+					<Stack>
+						<Title order={3}>
+							{toUpdateCollection ? "Update" : "Create"} collection
+						</Title>
+						<TextInput
+							label="Name"
+							required
+							name="name"
+							defaultValue={
+								toUpdateCollection ? toUpdateCollection.name : undefined
+							}
+						/>
+						<Box>
+							<Input.Label>Visibility</Input.Label>
+							<SegmentedControl
+								fullWidth
+								data={[
+									{
+										label: Visibility.Private,
+										value: Visibility.Private,
+									},
+									{
+										label: Visibility.Public,
+										value: Visibility.Public,
+									},
+								]}
+								name="visibility"
 								defaultValue={
-									toUpdateCollection ? toUpdateCollection.name : undefined
+									toUpdateCollection
+										? toUpdateCollection.visibility
+										: Visibility.Private
 								}
 							/>
-							<Box>
-								<Input.Label>Visibility</Input.Label>
-								<SegmentedControl
-									fullWidth
-									data={[
-										{
-											label: Visibility.Private,
-											value: Visibility.Private,
-										},
-										{
-											label: Visibility.Public,
-											value: Visibility.Public,
-										},
-									]}
-									name="visibility"
-									defaultValue={
-										toUpdateCollection
-											? toUpdateCollection.visibility
-											: Visibility.Private
-									}
-								/>
-							</Box>
-							<Textarea
-								label="Description"
-								name="description"
-								defaultValue={
-									toUpdateCollection?.description
-										? toUpdateCollection.description
-										: undefined
-								}
-							/>
-							<Button
-								variant="outline"
-								type="submit"
-								name={toUpdateCollection ? "updateId" : undefined}
-								value={toUpdateCollection ? toUpdateCollection.id : undefined}
-							>
-								{toUpdateCollection ? "Update" : "Create"}
-							</Button>
-						</Stack>
-					</Box>
-				</Modal>
-			</Stack>
-		</Container>
+						</Box>
+						<Textarea
+							label="Description"
+							name="description"
+							defaultValue={
+								toUpdateCollection?.description
+									? toUpdateCollection.description
+									: undefined
+							}
+						/>
+						<Button
+							variant="outline"
+							type="submit"
+							name={toUpdateCollection ? "updateId" : undefined}
+							value={toUpdateCollection ? toUpdateCollection.id : undefined}
+						>
+							{toUpdateCollection ? "Update" : "Create"}
+						</Button>
+					</Stack>
+				</Box>
+			</Modal>
+		</>
 	);
 }
