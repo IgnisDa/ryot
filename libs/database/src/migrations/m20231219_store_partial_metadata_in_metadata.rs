8be02f123d3a34135de8cb365ef212c2df2ca0ed
Diptesh Choudhuri <ignisda2001@gmail.com>
2023-12-19 05:05:58 +0530

    fix(database): use unprepared stmts to create metadata

--- libs/database/src/migrations/m20231219_store_partial_metadata_in_metadata.rs
+++ libs/database/src/migrations/m20231219_store_partial_metadata_in_metadata.rs
@@ -1,23 +1,18 @@
 use sea_orm_migration::prelude::*;
 
-use super::m20230410_create_metadata::Metadata;
-
 #[derive(DeriveMigrationName)]
 pub struct Migration;
 
 #[async_trait::async_trait]
 impl MigrationTrait for Migration {
     async fn up(&self, manager: &SchemaManager) -> Result<(), DbErr> {
-        if !manager.has_column("metadata", "is_partial").await? {
-            manager
-                .alter_table(
-                    Table::alter()
-                        .table(Metadata::Table)
-                        .add_column(ColumnDef::new(Metadata::IsPartial).boolean())
-                        .to_owned(),
-                )
-                .await?;
-        }
+        let conn = manager.get_connection();
+        conn.execute_unprepared(
+            "
+alter table metadata add column if not exists is_partial bool;
+",
+        )
+        .await?;
         Ok(())
     }
 
