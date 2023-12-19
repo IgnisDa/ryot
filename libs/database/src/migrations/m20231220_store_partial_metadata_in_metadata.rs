5cb13548e5a81ea105897c21fa9e9ac19afe0819
Diptesh Choudhuri <ignisda2001@gmail.com>
2023-12-19 10:40:42 +0530

    chore(database): add guard when migrating data

--- libs/database/src/migrations/m20231220_store_partial_metadata_in_metadata.rs
+++ libs/database/src/migrations/m20231220_store_partial_metadata_in_metadata.rs
@@ -10,35 +10,42 @@ impl MigrationTrait for Migration {
         conn.execute_unprepared(
             r#"
 alter table metadata add column if not exists is_partial bool;
-
-INSERT INTO metadata (identifier, title, source, lot, images, is_partial)
-SELECT
-  identifier,
-  title,
-  source,
-  lot,
-  CASE
-    WHEN image IS NOT NULL THEN jsonb_build_array(jsonb_build_object('lot', 'Poster', 'url', jsonb_build_object('Url', image)))
-    ELSE NULL
-  END,
-  true
-FROM
-  partial_metadata
-ON CONFLICT DO NOTHING;
-
-INSERT INTO metadata_to_metadata (to_metadata_id, from_metadata_id, relation)
-SELECT
-  mtpm.metadata_id AS from_metadata_id,
-  (select id from metadata where identifier = pm.identifier and source = pm.source and lot = pm.lot) AS to_metadata_id,
-  mtpm.relation AS relation
-FROM
-  metadata_to_partial_metadata mtpm
-JOIN
-  partial_metadata pm ON mtpm.partial_metadata_id = pm.id
-ON CONFLICT DO NOTHING;
 "#,
         )
         .await?;
+        if manager.has_table("partial_metadata").await? {
+            conn.execute_unprepared(
+    r#"
+    INSERT INTO metadata (identifier, title, source, lot, images, is_partial)
+    SELECT
+      identifier,
+      title,
+      source,
+      lot,
+      CASE
+        WHEN image IS NOT NULL THEN jsonb_build_array(jsonb_build_object('lot', 'Poster', 'url', jsonb_build_object('Url', image)))
+        ELSE NULL
+      END,
+      true
+    FROM
+      partial_metadata
+    ON CONFLICT DO NOTHING;
+
+    INSERT INTO metadata_to_metadata (to_metadata_id, from_metadata_id, relation)
+    SELECT
+      mtpm.metadata_id AS from_metadata_id,
+      (select id from metadata where identifier = pm.identifier and source = pm.source and lot = pm.lot) AS to_metadata_id,
+      mtpm.relation AS relation
+    FROM
+      metadata_to_partial_metadata mtpm
+    JOIN
+      partial_metadata pm ON mtpm.partial_metadata_id = pm.id
+    ON CONFLICT DO NOTHING;
+"#,
+  )
+  .await?;
+        }
+
         Ok(())
     }
 
