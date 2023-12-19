96406ea3315d9be7951bdb339dcd636f6427f00c
Diptesh Choudhuri <ignisda2001@gmail.com>
2023-12-19 05:37:57 +0530

    feat(backend): migrate data from partial_metadata to metadata

--- libs/database/src/migrations/m20231219_store_partial_metadata_in_metadata.rs
+++ libs/database/src/migrations/m20231219_store_partial_metadata_in_metadata.rs
@@ -8,9 +8,24 @@ impl MigrationTrait for Migration {
     async fn up(&self, manager: &SchemaManager) -> Result<(), DbErr> {
         let conn = manager.get_connection();
         conn.execute_unprepared(
-            "
+            r#"
 alter table metadata add column if not exists is_partial bool;
-",
+
+INSERT INTO metadata (identifier, title, source, lot, images, is_partial)
+SELECT
+  identifier,
+  title,
+  source,
+  lot,
+  CASE
+    WHEN image IS NOT NULL THEN jsonb_build_array(jsonb_build_object('lot', 'Poster', 'url', jsonb_build_object('Url', image)))
+    ELSE NULL
+  END,
+  true
+FROM
+  partial_metadata
+ON CONFLICT DO NOTHING;
+"#,
         )
         .await?;
         Ok(())
