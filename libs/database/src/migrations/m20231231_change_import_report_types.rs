b10ca0ec3c6f4156451f4ebeb04ba31e5461c70f
Diptesh Choudhuri <ignisda2001@gmail.com>
2023-12-31 17:00:29 +0530

    feat(backend): change import report type

---
+++ libs/database/src/migrations/m20231231_change_import_report_types.rs
@@ -0,0 +1,18 @@
+use sea_orm_migration::prelude::*;
+
+#[derive(DeriveMigrationName)]
+pub struct Migration;
+
+#[async_trait::async_trait]
+impl MigrationTrait for Migration {
+    async fn up(&self, manager: &SchemaManager) -> Result<(), DbErr> {
+        let conn = manager.get_connection();
+        conn.execute_unprepared("update import_report set source = 'GJ' where source = 'MJ'")
+            .await?;
+        Ok(())
+    }
+
+    async fn down(&self, _manager: &SchemaManager) -> Result<(), DbErr> {
+        Ok(())
+    }
+}
