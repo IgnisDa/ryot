157d2c85913c0de9d0e105a3b4d45f8f86c367a9
Diptesh Choudhuri <ignisda2001@gmail.com>
2024-01-02 11:29:26 +0530

    feat(backend): complete goodreads csv imports

--- apps/backend/src/importer/goodreads.rs
+++ apps/backend/src/importer/goodreads.rs
@@ -1,32 +1,42 @@
 use std::fs;
 
 use async_graphql::Result;
+use chrono::{DateTime, NaiveDate, NaiveDateTime, NaiveTime, Utc};
+use convert_case::{Case, Casing};
 use csv::Reader;
 use database::{MetadataLot, MetadataSource};
 use itertools::Itertools;
+use rust_decimal::Decimal;
+use rust_decimal_macros::dec;
 use serde::Deserialize;
 
 use crate::{
     importer::{DeployGoodreadsImportInput, ImportFailStep, ImportFailedItem, ImportResult},
+    models::media::{
+        ImportOrExportItemIdentifier, ImportOrExportItemRating, ImportOrExportItemReview,
+        ImportOrExportMediaItem, ImportOrExportMediaItemSeen,
+    },
     providers::google_books::GoogleBooksService,
 };
 
 #[derive(Debug, Deserialize)]
 struct Book {
+    #[serde(rename = "Book Id")]
+    id: String,
     #[serde(rename = "Title")]
     title: String,
     #[serde(rename = "ISBN13")]
     isbn13: String,
     #[serde(rename = "My Rating")]
-    my_rating: i32,
+    rating: Decimal,
     #[serde(rename = "Date Read")]
     date_read: Option<String>,
     #[serde(rename = "Bookshelves")]
-    bookshelves: String,
+    bookshelf: String,
     #[serde(rename = "My Review")]
-    my_review: Option<String>,
+    review: String,
     #[serde(rename = "Read Count")]
-    read_count: i32,
+    read_count: usize,
 }
 
 pub async fn import(
@@ -70,7 +80,66 @@ pub async fn import(
             continue;
         }
         if let Some(identifier) = isbn_service.id_from_isbn(&isbn).await {
-            dbg!(&identifier);
+            let mut seen_history = vec![
+                ImportOrExportMediaItemSeen {
+                    started_on: None,
+                    ended_on: None,
+                    ..Default::default()
+                };
+                record.read_count
+            ];
+            if let Some(w) = record.date_read {
+                let w = NaiveDate::parse_from_str(&w, "%Y/%m/%d").unwrap();
+                let read_at = Some(DateTime::from_naive_utc_and_offset(
+                    NaiveDateTime::new(w, NaiveTime::from_hms_opt(0, 0, 0).unwrap()),
+                    Utc,
+                ));
+                seen_history.first_mut().unwrap().ended_on = read_at;
+            }
+            let mut collections = vec![];
+            if !record.bookshelf.is_empty() {
+                collections.push(match record.bookshelf.as_str() {
+                    "to-read" => "Watchlist".to_owned(),
+                    "currently-reading" => "In Progress".to_owned(),
+                    s => s.to_case(Case::Title),
+                });
+            }
+            let mut rating = None;
+            if record.rating > dec!(0) {
+                rating = Some(
+                    record
+                        .rating
+                        // DEV: Rates items out of 10
+                        .saturating_mul(dec!(10)),
+                );
+            }
+            let mut review = None;
+            if !record.review.is_empty() {
+                review = Some(ImportOrExportItemReview {
+                    date: None,
+                    spoiler: Some(false),
+                    text: Some(record.review),
+                    visibility: None,
+                });
+            }
+            let mut reviews = vec![];
+            if review.is_some() || rating.is_some() {
+                reviews.push(ImportOrExportItemRating {
+                    review,
+                    rating,
+                    ..Default::default()
+                });
+            }
+            media.push(ImportOrExportMediaItem {
+                source_id: record.title,
+                lot,
+                source,
+                identifier: record.id,
+                internal_identifier: Some(ImportOrExportItemIdentifier::NeedsDetails(identifier)),
+                seen_history,
+                reviews,
+                collections,
+            });
         } else {
             failed_items.push(ImportFailedItem {
                 lot,
