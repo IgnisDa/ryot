75da50f00b770e24da2c9800397eb399a9f7e0f9
Diptesh Choudhuri <ignisda2001@gmail.com>
2024-01-03 05:59:13 +0530

    fix(frontend): make export report readonly

--- apps/frontend/app/routes/_dashboard.settings.imports-and-exports._index.tsx
+++ apps/frontend/app/routes/_dashboard.settings.imports-and-exports._index.tsx
@@ -593,7 +593,7 @@ export default function Page() {
 																		null,
 																		4,
 																	)}
-																	disabled
+																	readOnly
 																	autosize
 																/>
 															) : null}
