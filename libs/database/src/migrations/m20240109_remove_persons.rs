f2e2cdb21dbfb3d57323ae9dae038b1c9c9b7e1f
Diptesh Choudhuri <ignisda2001@gmail.com>
2024-01-12 09:09:25 +0530

    Person fixes (#568)
    * build(backend,database): add tracing deps
    
    * feat(database): delete people in chunks
    
    * fix(database): drop fks before deleting data
    
    * build(backend): bump version
    
    * build(frontend): upgrade deps

--- libs/database/src/migrations/m20240109_remove_persons.rs
+++ libs/database/src/migrations/m20240109_remove_persons.rs
@@ -38,6 +38,20 @@ mod ce {
     impl ActiveModelBehavior for ActiveModel {}
 }
 
+mod pe {
+    use super::*;
+
+    #[derive(Clone, Debug, PartialEq, DeriveEntityModel, Eq, Serialize, Deserialize)]
+    #[sea_orm(table_name = "person")]
+    pub struct Model {
+        #[sea_orm(primary_key)]
+        pub id: i32,
+    }
+    #[derive(Copy, Clone, Debug, EnumIter, DeriveRelation)]
+    pub enum Relation {}
+    impl ActiveModelBehavior for ActiveModel {}
+}
+
 #[async_trait::async_trait]
 impl MigrationTrait for Migration {
     async fn up(&self, manager: &SchemaManager) -> Result<(), DbErr> {
@@ -71,8 +85,28 @@ persons with these IDs: {:?}. Then upgrade to this version again.
                 persons_to_not_delete
             )));
         }
-        db.execute_unprepared(r#"truncate table "person" cascade;"#)
+        let all_persons = pe::Entity::find()
+            .select_only()
+            .column(pe::Column::Id)
+            .into_tuple::<i32>()
+            .all(db)
             .await?;
+        tracing::warn!("Total people to delete: {}", all_persons.len());
+        db.execute_unprepared(
+            r#"
+alter table metadata_to_person drop constraint "fk-person-item_media-person_id";
+alter table collection_to_entity drop constraint "collection_to_entity-fk3";
+alter table review drop constraint "review_to_person_foreign_key";
+
+delete from metadata_to_person;
+truncate table person restart identity;
+
+alter table metadata_to_person add constraint "fk-person-item_media-person_id" foreign key ("person_id") references "person" ("id") on update cascade on delete cascade;
+alter table collection_to_entity add constraint "collection_to_entity-fk3" foreign key ("person_id") references "person" ("id") on update cascade on delete cascade;
+alter table review add constraint "review_to_person_foreign_key" foreign key ("person_id") references "person" ("id") on update cascade on delete cascade;
+        "#,
+        )
+        .await?;
         Ok(())
     }
 
