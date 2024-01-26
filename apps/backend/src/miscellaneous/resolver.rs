8ef8d4588eaa046eafa5833048e649f2490a1692
Diptesh Choudhuri <ignisda2001@gmail.com>
2024-01-26 12:38:10 +0530

    Revert "fix(backend): do not update attribute for seen"
    This reverts commit 45737a9975e7a2c6f565a214dd1ff63936d6bc6c.

--- apps/backend/src/miscellaneous/resolver.rs
+++ apps/backend/src/miscellaneous/resolver.rs
@@ -3163,6 +3163,7 @@ impl MiscellaneousService {
             Some(s) => s,
             None => return Err(Error::new("No seen found for this user and metadata")),
         };
+        let mut updated_at = seen.updated_at.clone();
         if seen.user_id != user_id {
             return Err(Error::new("No seen found for this user and metadata"));
         }
@@ -3173,6 +3174,8 @@ impl MiscellaneousService {
         if let Some(finished_on) = input.finished_on {
             seen.finished_on = ActiveValue::Set(Some(finished_on));
         }
+        updated_at.push(Utc::now());
+        seen.updated_at = ActiveValue::Set(updated_at);
         let seen = seen.update(&self.db).await.unwrap();
         self.after_media_seen_tasks(seen).await?;
         Ok(true)
