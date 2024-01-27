8fb5e0407e9ba37ad2cb50ca9a9cc41332dd73a2
Diptesh Choudhuri <ignisda2001@gmail.com>
2024-01-27 11:40:00 +0530

    chore(frontend): add umami values

--- apps/frontend/app/lib/utilities.server.ts
+++ apps/frontend/app/lib/utilities.server.ts
@@ -9,9 +9,13 @@ export const expectedEnvironmentVariables = z.object({
 		.string()
 		.optional()
 		.transform((v) => v === "true"),
-	FRONTEND_UMAMI_WEBSITE_ID: z.string().optional(),
+	FRONTEND_UMAMI_SCRIPT_URL: z
+		.string()
+		.default("https://umami.diptesh.me/script"),
+	FRONTEND_UMAMI_WEBSITE_ID: z
+		.string()
+		.default("5ecd6915-d542-4fda-aa5f-70f09f04e2e0"),
 	FRONTEND_UMAMI_DOMAINS: z.string().optional(),
-	FRONTEND_UMAMI_SCRIPT_URL: z.string().optional(),
 });
 
 /**
