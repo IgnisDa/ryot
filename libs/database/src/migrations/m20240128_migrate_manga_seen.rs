017d314ce3b8c8b2091fd12e18414a50ae6034fb
Diptesh Choudhuri <ignisda2001@gmail.com>
2024-01-29 06:13:06 +0530

    fix(database): do not migrate seen in progress

--- libs/database/src/migrations/m20240128_migrate_manga_seen.rs
+++ libs/database/src/migrations/m20240128_migrate_manga_seen.rs
@@ -8,7 +8,6 @@ impl MigrationTrait for Migration {
     async fn up(&self, manager: &SchemaManager) -> Result<(), DbErr> {
         let db = manager.get_connection();
         db.execute_unprepared(r#"
--- Step 1: Create a temporary table
 CREATE TEMP TABLE temp_chapter_entries AS
 SELECT s.id AS original_seen_id, s.progress, s.started_on, s.finished_on, s.user_id,
        m.id AS metadata_id, s.state,
@@ -19,6 +18,7 @@ JOIN metadata m ON s.metadata_id = m.id
 CROSS JOIN LATERAL generate_series(1, NULLIF((m.specifics::jsonb #>> '{d,chapters}')::integer, 0)) AS ch(chapter)
 WHERE m.specifics::jsonb ->> 't' = 'Manga'
 AND m.specifics::jsonb #>> '{d,chapters}' IS NOT NULL
+AND s.state != 'IP'
 AND s.id NOT IN (
     SELECT s2.id
     FROM seen s2
@@ -26,19 +26,16 @@ AND s.id NOT IN (
     AND s2.extra_information IS NOT NULL
 );
 
--- Step 2: Insert the new seen records
 INSERT INTO seen (progress, started_on, finished_on, user_id, metadata_id, state, extra_information, updated_at)
 SELECT progress, started_on, finished_on, user_id, metadata_id, state, extra_information, updated_at
 FROM temp_chapter_entries;
 
--- Step 3: Delete the original seen records
 DELETE FROM seen
 WHERE id IN (
     SELECT original_seen_id
     FROM temp_chapter_entries
 );
 
--- Step 4: Drop the temporary table
 DROP TABLE temp_chapter_entries;
 
 UPDATE seen s
