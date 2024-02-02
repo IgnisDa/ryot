bf2ba18a8fe5c9a221e75aeb25f195eb4a2a0461
Diptesh Choudhuri <ignisda2001@gmail.com>
2024-02-02 13:45:57 +0530

    fix(database): change migration to account for fresh schema

--- libs/database/src/migrations/m20240128_change_calendar_event_data.rs
+++ libs/database/src/migrations/m20240128_change_calendar_event_data.rs
@@ -6,11 +6,16 @@ pub struct Migration;
 #[async_trait::async_trait]
 impl MigrationTrait for Migration {
     async fn up(&self, manager: &SchemaManager) -> Result<(), DbErr> {
-        let db = manager.get_connection();
-        db.execute_unprepared(r#"
+        if manager
+            .has_column("calendar_event", "metadata_extra_information")
+            .await?
+        {
+            let db = manager.get_connection();
+            db.execute_unprepared(r#"
 UPDATE "calendar_event" SET metadata_extra_information = NULL WHERE metadata_extra_information = '{"Other": null}'
         "#)
             .await?;
+        }
         Ok(())
     }
 
