6aee7cf804c14746c4b1b6688bdcd185c6cfad82
Diptesh Choudhuri <ignisda2001@gmail.com>
2024-02-24 18:36:19 +0530

    chore(database): do not update `media_reason` in migration

--- libs/database/src/migrations/m20240224_5_migrate_user_to_person_entries.rs
+++ libs/database/src/migrations/m20240224_5_migrate_user_to_person_entries.rs
@@ -9,26 +9,30 @@ impl MigrationTrait for Migration {
         let db = manager.get_connection();
         db.execute_unprepared(
             r#"
-INSERT INTO public.user_to_entity (user_id, person_id, media_reason)
-SELECT DISTINCT r.user_id, r.person_id, ARRAY['Reviewed']::text[]
+INSERT INTO public.user_to_entity (user_id, person_id, created_on, last_updated_on)
+SELECT DISTINCT r.user_id, r.person_id, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP
 FROM public.review r
 WHERE r.person_id IS NOT NULL
-ON CONFLICT (user_id, person_id) DO UPDATE
-SET media_reason = array_append(user_to_entity.media_reason, 'Reviewed')
-WHERE NOT (user_to_entity.media_reason @> ARRAY['Reviewed']);
+  AND NOT EXISTS (
+    SELECT 1
+    FROM public.user_to_entity ute
+    WHERE ute.user_id = r.user_id AND ute.person_id = r.person_id
+  );
 "#,
         )
         .await?;
         db.execute_unprepared(
             r#"
-INSERT INTO public.user_to_entity (user_id, person_id, media_reason)
-SELECT DISTINCT c.user_id, cte.person_id, ARRAY['Collection']::text[]
+INSERT INTO public.user_to_entity (user_id, person_id, created_on, last_updated_on)
+SELECT DISTINCT c.user_id, cte.person_id, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP
 FROM public.collection_to_entity cte
-JOIN public.collection c ON c.id = cte.collection_id
+JOIN public.collection c ON cte.collection_id = c.id
 WHERE cte.person_id IS NOT NULL
-ON CONFLICT (user_id, person_id) DO UPDATE
-SET media_reason = array_append(user_to_entity.media_reason, 'Collection')
-WHERE NOT (user_to_entity.media_reason @> ARRAY['Collection']);
+  AND NOT EXISTS (
+    SELECT 1
+    FROM public.user_to_entity ute
+    WHERE ute.user_id = c.user_id AND ute.person_id = cte.person_id
+  );
 "#,
         )
         .await?;
