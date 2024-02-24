436d127cee956dd293f441d53fd34141351552a7
Diptesh Choudhuri <ignisda2001@gmail.com>
2024-02-24 16:10:46 +0530

    chore(database): remove old migration contents

--- libs/database/src/migrations/m20240202_3_add_metadata_reason.rs
+++ libs/database/src/migrations/m20240202_3_add_metadata_reason.rs
@@ -5,17 +5,17 @@ pub struct Migration;
 
 #[async_trait::async_trait]
 impl MigrationTrait for Migration {
-    async fn up(&self, manager: &SchemaManager) -> Result<(), DbErr> {
-        if !manager
-            .has_column("user_to_entity", "metadata_reason")
-            .await?
-        {
-            let db = manager.get_connection();
-            db.execute_unprepared(
-                r#"ALTER TABLE user_to_entity ADD COLUMN metadata_reason text[]"#,
-            )
-            .await?;
-        }
+    async fn up(&self, _manager: &SchemaManager) -> Result<(), DbErr> {
+        // if !manager
+        //     .has_column("user_to_entity", "metadata_reason")
+        //     .await?
+        // {
+        //     let db = manager.get_connection();
+        //     db.execute_unprepared(
+        //         r#"ALTER TABLE user_to_entity ADD COLUMN metadata_reason text[]"#,
+        //     )
+        //     .await?;
+        // }
         Ok(())
     }
 
