d2a96272ce24b33ede8b7bacde50e5202aa6edfa
Diptesh Choudhuri <ignisda2001@gmail.com>
2024-03-02 11:58:11 +0530

    chore(frontend): change wording for imports

--- apps/frontend/app/components/media.tsx
+++ apps/frontend/app/components/media.tsx
@@ -54,7 +54,7 @@ import {
 	IconArrowBigUp,
 	IconArrowsRight,
 	IconCheck,
-	IconDropletHalf2Filled,
+	IconCloudDownload,
 	IconEdit,
 	IconEyeCheck,
 	IconPercentage,
@@ -920,9 +920,11 @@ export const NewUserGuideAlert = () => {
 
 export const MediaIsPartial = (props: { mediaType: string }) => {
 	return (
-		<Flex align="center" gap={2}>
-			<IconDropletHalf2Filled size={20} />
-			<Text size="xs">This {props.mediaType} is partially downloaded</Text>
+		<Flex align="center" gap={4}>
+			<IconCloudDownload size={20} />
+			<Text size="xs">
+				Details of this {props.mediaType} are being downloaded
+			</Text>
 		</Flex>
 	);
 };
