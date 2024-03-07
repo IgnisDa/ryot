c6510997c8965a02348c232456fa43a2511d1255
Diptesh Choudhuri <ignisda2001@gmail.com>
2024-03-07 06:00:05 +0530

    feat(migrations): add `watch_providers` field to metadata

---
+++ libs/database/src/migrations/m20240307_add_column_to_metadata_for_watch_providers.rs
@@ -0,0 +1,20 @@
+use sea_orm_migration::prelude::*;
+
+#[derive(DeriveMigrationName)]
+pub struct Migration;
+
+#[async_trait::async_trait]
+impl MigrationTrait for Migration {
+    async fn up(&self, manager: &SchemaManager) -> Result<(), DbErr> {
+        if !manager.has_column("metadata", "watch_providers").await? {
+            let db = manager.get_connection();
+            db.execute_unprepared(r#"alter table metadata add column watch_providers jsonb"#)
+                .await?;
+        }
+        Ok(())
+    }
+
+    async fn down(&self, _manager: &SchemaManager) -> Result<(), DbErr> {
+        Ok(())
+    }
+}
