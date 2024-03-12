1d3c103c291cc4575848cc3a4265362805e7012f
Diptesh Choudhuri <ignisda2001@gmail.com>
2024-03-12 07:01:54 +0530

    fix(backend): use a more accurate filter for json column

--- apps/backend/src/miscellaneous/resolver.rs
+++ apps/backend/src/miscellaneous/resolver.rs
@@ -3058,7 +3058,9 @@ impl MiscellaneousService {
         let db_person = if let Some(db_person) = Person::find()
             .filter(person::Column::Identifier.eq(&person.identifier))
             .filter(person::Column::Source.eq(person.source))
-            .filter(person::Column::SourceSpecifics.eq(person.source_specifics.clone()))
+            .apply_if(person.source_specifics.clone(), |query, v| {
+                query.filter(person::Column::SourceSpecifics.eq(v))
+            })
             .one(&self.db)
             .await
             .unwrap()
