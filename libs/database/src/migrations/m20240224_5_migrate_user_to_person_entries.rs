fc38dad879eb6441ada91a1f43757dec69817b62
Diptesh Choudhuri <ignisda2001@gmail.com>
2024-03-12 06:00:23 +0530

    Bugfixes (#693)
    * fix(backend): do not use schema name as `public`
    
    * fix(backend): update all metadata should happen for all media
    
    * fix(backend): handle cases when display nsfw is disabled
    
    * build(backend): bump version

--- libs/database/src/migrations/m20240224_5_migrate_user_to_person_entries.rs
+++ libs/database/src/migrations/m20240224_5_migrate_user_to_person_entries.rs
@@ -9,13 +9,13 @@ impl MigrationTrait for Migration {
         let db = manager.get_connection();
         db.execute_unprepared(
             r#"
-INSERT INTO public.user_to_entity (user_id, person_id, created_on, last_updated_on)
+INSERT INTO user_to_entity (user_id, person_id, created_on, last_updated_on)
 SELECT DISTINCT r.user_id, r.person_id, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP
-FROM public.review r
+FROM review r
 WHERE r.person_id IS NOT NULL
   AND NOT EXISTS (
     SELECT 1
-    FROM public.user_to_entity ute
+    FROM user_to_entity ute
     WHERE ute.user_id = r.user_id AND ute.person_id = r.person_id
   );
 "#,
@@ -23,14 +23,14 @@ WHERE r.person_id IS NOT NULL
         .await?;
         db.execute_unprepared(
             r#"
-INSERT INTO public.user_to_entity (user_id, person_id, created_on, last_updated_on)
+INSERT INTO user_to_entity (user_id, person_id, created_on, last_updated_on)
 SELECT DISTINCT c.user_id, cte.person_id, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP
-FROM public.collection_to_entity cte
-JOIN public.collection c ON cte.collection_id = c.id
+FROM collection_to_entity cte
+JOIN collection c ON cte.collection_id = c.id
 WHERE cte.person_id IS NOT NULL
   AND NOT EXISTS (
     SELECT 1
-    FROM public.user_to_entity ute
+    FROM user_to_entity ute
     WHERE ute.user_id = c.user_id AND ute.person_id = cte.person_id
   );
 "#,
