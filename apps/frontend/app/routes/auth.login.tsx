e315896a88fe0b864776b955e7a75349db23a951
Diptesh Choudhuri <ignisda2001@gmail.com>
2024-03-23 12:46:17 +0530

    feat(frontend): redirect to correct page

--- apps/frontend/app/routes/auth.login.tsx
+++ apps/frontend/app/routes/auth.login.tsx
@@ -10,6 +10,7 @@ import {
 } from "@remix-run/node";
 import { Form, Link, useLoaderData, useSearchParams } from "@remix-run/react";
 import {
+	CoreDetailsDocument,
 	LoginErrorVariant,
 	LoginUserDocument,
 } from "@ryot/generated/graphql/backend/graphql";
@@ -47,17 +48,21 @@ export const action = async ({ request }: ActionFunctionArgs) => {
 			username: submission.username,
 		},
 	});
+	const { coreDetails } = await gqlClient.request(CoreDetailsDocument);
 	if (loginUser.__typename === "LoginResponse") {
 		let redirectUrl = $path("/");
 		if (submission[redirectToQueryParam])
 			redirectUrl = safeRedirect(submission[redirectToQueryParam]);
-		return redirect(redirectUrl, {
-			headers: {
-				"Set-Cookie": await authCookie.serialize(loginUser.apiKey, {
-					maxAge: loginUser.validFor * 24 * 60 * 60,
-				}),
+		return redirect(
+			$path("/actions", { [redirectToQueryParam]: redirectUrl }),
+			{
+				headers: {
+					"Set-Cookie": await authCookie.serialize(loginUser.apiKey, {
+						maxAge: coreDetails.tokenValidForDays * 24 * 60 * 60,
+					}),
+				},
 			},
-		});
+		);
 	}
 	const message = match(loginUser.error)
 		.with(
