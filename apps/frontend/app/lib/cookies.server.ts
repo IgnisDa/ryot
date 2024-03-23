3c5dd03dff0d36e441c7f532fea7c4e7b956fe79
Diptesh Choudhuri <ignisda2001@gmail.com>
2024-03-23 12:04:04 +0530

    feat(frontend): make cookies secure

--- apps/frontend/app/lib/cookies.server.ts
+++ apps/frontend/app/lib/cookies.server.ts
@@ -1,17 +1,23 @@
 import { createCookie, createCookieSessionStorage } from "@remix-run/node";
 import { ApplicationKey } from "./generals";
 
-export const authCookie = createCookie(ApplicationKey.Auth, {});
+const commonCookieOptions = {
+	name: ApplicationKey.Toast,
+	sameSite: "lax",
+	path: "/",
+	httpOnly: true,
+	secrets: (process.env.SESSION_SECRET || "").split(","),
+	secure: process.env.NODE_ENV === "production",
+} as const;
+
+export const authCookie = createCookie(
+	ApplicationKey.Auth,
+	commonCookieOptions,
+);
+export const toastSessionStorage = createCookieSessionStorage({
+	cookie: commonCookieOptions,
+});
+
 export const colorSchemeCookie = createCookie(ApplicationKey.ColorScheme, {
 	maxAge: 60 * 60 * 24 * 365,
-});
-export const toastSessionStorage = createCookieSessionStorage({
-	cookie: {
-		name: ApplicationKey.Toast,
-		sameSite: "lax",
-		path: "/",
-		httpOnly: true,
-		secrets: (process.env.SESSION_SECRET || "").split(","),
-		secure: process.env.NODE_ENV === "production",
-	},
 });
