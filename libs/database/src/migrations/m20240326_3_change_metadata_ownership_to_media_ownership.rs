969721025b287a6032b84afe4a3224296eb3f09a
Diptesh Choudhuri <ignisda2001@gmail.com>
2024-03-31 08:43:02 +0530

    fix(database): add check before rename

--- libs/database/src/migrations/m20240326_3_change_metadata_ownership_to_media_ownership.rs
+++ libs/database/src/migrations/m20240326_3_change_metadata_ownership_to_media_ownership.rs
@@ -6,11 +6,16 @@ pub struct Migration;
 #[async_trait::async_trait]
 impl MigrationTrait for Migration {
     async fn up(&self, manager: &SchemaManager) -> Result<(), DbErr> {
-        let db = manager.get_connection();
-        db.execute_unprepared(
-            r#"ALTER TABLE "user_to_entity" RENAME COLUMN "metadata_ownership" TO "media_ownership";"#,
-        )
-        .await?;
+        if !manager
+            .has_column("user_to_entity", "media_ownership")
+            .await?
+        {
+            let db = manager.get_connection();
+            db.execute_unprepared(
+                r#"ALTER TABLE "user_to_entity" RENAME COLUMN "metadata_ownership" TO "media_ownership";"#,
+            )
+            .await?;
+        }
         Ok(())
     }
 
