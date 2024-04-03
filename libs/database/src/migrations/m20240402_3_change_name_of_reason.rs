2c51a0df4d00b469d8f65bbaf91fec0fe062c4eb
Diptesh Choudhuri <ignisda2001@gmail.com>
2024-04-03 07:02:10 +0530

    feat(database): update media reason

---
+++ libs/database/src/migrations/m20240402_3_change_name_of_reason.rs
@@ -0,0 +1,24 @@
+use sea_orm_migration::prelude::*;
+
+#[derive(DeriveMigrationName)]
+pub struct Migration;
+
+#[async_trait::async_trait]
+impl MigrationTrait for Migration {
+    async fn up(&self, manager: &SchemaManager) -> Result<(), DbErr> {
+        let db = manager.get_connection();
+        db.execute_unprepared(
+            r#"
+UPDATE user_to_entity
+SET media_reason = array_replace(media_reason, 'Monitored', 'Monitoring')
+WHERE 'Monitored' = ANY(media_reason);
+            "#,
+        )
+        .await?;
+        Ok(())
+    }
+
+    async fn down(&self, _manager: &SchemaManager) -> Result<(), DbErr> {
+        Ok(())
+    }
+}
