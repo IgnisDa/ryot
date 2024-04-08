d94e0e00b702c173e42946fe0fa4b96a75546122
Diptesh Choudhuri <ignisda2001@gmail.com>
2024-04-08 19:28:28 +0530

    feat(frontend): basic nonce stuff

--- apps/frontend/app/routes/auth.callback.tsx
+++ apps/frontend/app/routes/auth.callback.tsx
@@ -1,16 +1,16 @@
 import { type LoaderFunctionArgs, json } from "@remix-run/node";
+import { GetOidcTokenDocument } from "@ryot/generated/graphql/backend/graphql";
 import { z } from "zod";
 import { zx } from "zodix";
+import { gqlClient } from "~/lib/utilities.server";
 
-const searchParamsSchema = z.object({
-	code: z.string(),
-	state: z.string(),
-});
+const searchParamsSchema = z.object({ code: z.string() });
 
 export type SearchParams = z.infer<typeof searchParamsSchema>;
 
 export const loader = async ({ request }: LoaderFunctionArgs) => {
-	const query = zx.parseQuery(request, searchParamsSchema);
-	console.log(query);
-	return json({ query });
+	const input = zx.parseQuery(request, searchParamsSchema);
+	const { getOidcToken } = await gqlClient.request(GetOidcTokenDocument, input);
+	console.log(getOidcToken);
+	return json({ input });
 };
