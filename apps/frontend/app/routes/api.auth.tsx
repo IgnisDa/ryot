08495de82362075c2b1891beeba7938ce8e55d8d
Diptesh Choudhuri <ignisda2001@gmail.com>
2024-04-08 22:27:03 +0530

    feat(frontend): allow continuing with openid

--- apps/frontend/app/routes/api.auth.tsx
+++ apps/frontend/app/routes/api.auth.tsx
@@ -1,3 +1,4 @@
+import { $path } from "@ignisda/remix-routes";
 import {
 	type ActionFunctionArgs,
 	type LoaderFunctionArgs,
@@ -5,12 +6,20 @@ import {
 	redirect,
 } from "@remix-run/node";
 import {
+	CoreDetailsDocument,
 	GetOidcRedirectUrlDocument,
 	GetOidcTokenDocument,
+	LoginUserDocument,
+	RegisterUserDocument,
 } from "@ryot/generated/graphql/backend/graphql";
 import { z } from "zod";
 import { zx } from "zodix";
-import { gqlClient } from "~/lib/utilities.server";
+import {
+	authCookie,
+	combineHeaders,
+	getCookiesForApplication,
+	gqlClient,
+} from "~/lib/utilities.server";
 
 const searchParamsSchema = z.object({ code: z.string() });
 
@@ -19,7 +28,25 @@ export type SearchParams = z.infer<typeof searchParamsSchema>;
 export const loader = async ({ request }: LoaderFunctionArgs) => {
 	const input = zx.parseQuery(request, searchParamsSchema);
 	const { getOidcToken } = await gqlClient.request(GetOidcTokenDocument, input);
-	console.log(getOidcToken);
+	const [{ coreDetails }] = await Promise.all([
+		gqlClient.request(CoreDetailsDocument),
+		gqlClient.request(RegisterUserDocument, {
+			input: { oidc: getOidcToken },
+		}),
+	]);
+	const { loginUser } = await gqlClient.request(LoginUserDocument, {
+		input: { oidc: getOidcToken },
+	});
+	if (loginUser.__typename === "LoginResponse") {
+		const cookies = await getCookiesForApplication(loginUser.apiKey);
+		const options = { maxAge: coreDetails.tokenValidForDays * 24 * 60 * 60 };
+		return redirect($path("/"), {
+			headers: combineHeaders(
+				{ "set-cookie": await authCookie.serialize(loginUser.apiKey, options) },
+				cookies,
+			),
+		});
+	}
 	return json({ input });
 };
 
