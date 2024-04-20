a8bedf50ecf753e1ab475b5a68653cdef30c468c
Diptesh Choudhuri <ignisda2001@gmail.com>
2024-04-20 07:11:31 +0530

    fix(backend): remove from monitoring on show completion

--- apps/backend/src/miscellaneous/resolver.rs
+++ apps/backend/src/miscellaneous/resolver.rs
@@ -6273,37 +6273,37 @@ impl MiscellaneousService {
                         });
                     let is_complete = bag.values().all(|&e| e == 1);
                     if is_complete {
-                        self.remove_entity_from_collection(
-                            seen.user_id,
-                            ChangeCollectionToEntityInput {
-                                collection_name: DefaultCollection::InProgress.to_string(),
-                                metadata_id: Some(seen.metadata_id),
-                                ..Default::default()
-                            },
-                        )
-                        .await
-                        .ok();
+                        for col in &[
+                            DefaultCollection::InProgress.to_string(),
+                            DefaultCollection::Monitoring.to_string(),
+                        ] {
+                            self.remove_entity_from_collection(
+                                seen.user_id,
+                                ChangeCollectionToEntityInput {
+                                    collection_name: col.to_string(),
+                                    metadata_id: Some(seen.metadata_id),
+                                    ..Default::default()
+                                },
+                            )
+                            .await
+                            .ok();
+                        }
                     } else {
-                        self.add_entity_to_collection(
-                            seen.user_id,
-                            ChangeCollectionToEntityInput {
-                                collection_name: DefaultCollection::InProgress.to_string(),
-                                metadata_id: Some(seen.metadata_id),
-                                ..Default::default()
-                            },
-                        )
-                        .await
-                        .ok();
-                        self.add_entity_to_collection(
-                            seen.user_id,
-                            ChangeCollectionToEntityInput {
-                                collection_name: DefaultCollection::Monitoring.to_string(),
-                                metadata_id: Some(seen.metadata_id),
-                                ..Default::default()
-                            },
-                        )
-                        .await
-                        .ok();
+                        for col in &[
+                            DefaultCollection::InProgress.to_string(),
+                            DefaultCollection::Monitoring.to_string(),
+                        ] {
+                            self.add_entity_to_collection(
+                                seen.user_id,
+                                ChangeCollectionToEntityInput {
+                                    collection_name: col.to_string(),
+                                    metadata_id: Some(seen.metadata_id),
+                                    ..Default::default()
+                                },
+                            )
+                            .await
+                            .ok();
+                        }
                     }
                 } else {
                     self.remove_entity_from_collection(
