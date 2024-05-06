4e472f5113096aaac72a921043066cf3a7576b8e
Diptesh Choudhuri <ignisda2001@gmail.com>
2024-05-06 19:01:43 +0530

    feat(database): migration to drop visibility from collection

---
+++ libs/database/src/migrations/m20240507_0_remove_visibility_from_collection.rs
@@ -0,0 +1,18 @@
+use sea_orm_migration::prelude::*;
+
+#[derive(DeriveMigrationName)]
+pub struct Migration;
+
+#[async_trait::async_trait]
+impl MigrationTrait for Migration {
+    async fn up(&self, manager: &SchemaManager) -> Result<(), DbErr> {
+        let db = manager.get_connection();
+        db.execute_unprepared(r#"ALTER TABLE collection DROP COLUMN IF EXISTS visibility"#)
+            .await?;
+        Ok(())
+    }
+
+    async fn down(&self, _manager: &SchemaManager) -> Result<(), DbErr> {
+        Ok(())
+    }
+}
