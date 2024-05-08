2df1cea397239acec62bb67df1523ee1727ad99c
Diptesh Choudhuri <ignisda2001@gmail.com>
2024-05-09 05:16:35 +0530

    fix(database): add if guard

--- libs/database/src/migrations/m20240509_z_associate_collections_with_owners.rs
+++ libs/database/src/migrations/m20240509_z_associate_collections_with_owners.rs
@@ -6,14 +6,18 @@ pub struct Migration;
 #[async_trait::async_trait]
 impl MigrationTrait for Migration {
     async fn up(&self, manager: &SchemaManager) -> Result<(), DbErr> {
-        let db = manager.get_connection();
-        db.execute_unprepared(
-            r#"
-DO $$
+        if manager
+            .has_column("collection", "created_by_user_id")
+            .await?
+        {
+            let db = manager.get_connection();
+            db.execute_unprepared(
+                r#"
+                DO $$
 DECLARE
     col RECORD;
 BEGIN
-    FOR col IN SELECT id, created_by_user_id FROM "collection"
+FOR col IN SELECT id, created_by_user_id FROM "collection"
     LOOP
         INSERT INTO "user_to_collection" (user_id, collection_id)
         VALUES (col.created_by_user_id, col.id)
@@ -21,8 +25,9 @@ BEGIN
     END LOOP;
 END $$;
         "#,
-        )
-        .await?;
+            )
+            .await?;
+        }
         Ok(())
     }
 
