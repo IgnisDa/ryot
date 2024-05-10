b44ea68d24164f517a3fbbd3e57330872a217367
Diptesh Choudhuri <ignisda2001@gmail.com>
2024-05-10 22:51:01 +0530

    feat(database): insert stuff into owned collection

--- libs/database/src/migrations/m20240510_1_port_owned_information.rs
+++ libs/database/src/migrations/m20240510_1_port_owned_information.rs
@@ -21,6 +21,33 @@ BEGIN
             '[{"name": "owned_on", "description": "When did you get this media?", "lot": "Date", "required": true}]'
         )
         ON CONFLICT DO NOTHING;
+
+        INSERT INTO user_to_collection (user_id, collection_id)
+        SELECT aUser.id, id FROM collection
+        WHERE user_id = aUser.id AND name = 'Owned' LIMIT 1;
+    END LOOP;
+END $$;
+            "#,
+        )
+        .await?;
+        db.execute_unprepared(
+            r#"
+DO $$
+DECLARE
+    aUser RECORD;
+    ute RECORD;
+    owned_collection_id INTEGER;
+BEGIN
+    FOR aUser IN SELECT id FROM "user"
+    LOOP
+        FOR ute IN SELECT metadata_id FROM "user_to_entity" where media_ownership is not null and user_id = aUser.id
+        LOOP
+            SELECT id INTO owned_collection_id FROM collection
+            WHERE user_id = aUser.id AND name = 'Owned' LIMIT 1;
+
+            INSERT INTO collection_to_entity (collection_id, metadata_id)
+            VALUES (owned_collection_id, ute.metadata_id);
+        END LOOP;
     END LOOP;
 END $$;
             "#,
