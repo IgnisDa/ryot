16bd582b48848d9d440e0ebc63468fc5852f126f
Diptesh Choudhuri <ignisda2001@gmail.com>
2024-05-10 22:05:08 +0530

    fix(database): order of migrations

--- libs/database/src/migrations/m20240510_0_add_information_template_to_collection.rs
+++ libs/database/src/migrations/m20240510_0_add_information_template_to_collection.rs
@@ -8,22 +8,7 @@ impl MigrationTrait for Migration {
     async fn up(&self, manager: &SchemaManager) -> Result<(), DbErr> {
         let db = manager.get_connection();
         db.execute_unprepared(
-            r#"
-DO $$
-DECLARE
-    aUser RECORD;
-BEGIN
-    FOR aUser IN SELECT id FROM "user"
-    LOOP
-        INSERT INTO collection (name, description, user_id, created_on, last_updated_on, information_template)
-        VALUES (
-            'Owned', 'Items that I have in my inventory.', aUser.id, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP,
-            '[{"name": "owned_on", "description": "When did you get this media?", "lot": "Date", "required": true}]'
-        )
-        ON CONFLICT DO NOTHING;
-    END LOOP;
-END $$;
-            "#,
+            r#"alter table "collection" add column if not exists "information_template" jsonb"#,
         )
         .await?;
         Ok(())
