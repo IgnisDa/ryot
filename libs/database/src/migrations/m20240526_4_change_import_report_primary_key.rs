f45817c134ebd1404735f5f1d64908fbf2bf359f
Diptesh Choudhuri <ignisda2001@gmail.com>
2024-05-26 15:26:49 +0530

    feat(database): migrate data of import report

---
+++ libs/database/src/migrations/m20240526_4_change_import_report_primary_key.rs
@@ -0,0 +1,64 @@
+use nanoid::nanoid;
+use sea_orm::{entity::prelude::*, ActiveValue};
+use sea_orm_migration::prelude::*;
+
+#[derive(DeriveMigrationName)]
+pub struct Migration;
+
+#[derive(Clone, Debug, PartialEq, DeriveEntityModel, Eq)]
+#[sea_orm(table_name = "import_report")]
+pub struct Model {
+    #[sea_orm(primary_key)]
+    pub id: i32,
+    pub new_id: String,
+}
+
+#[derive(Copy, Clone, Debug, EnumIter, DeriveRelation)]
+pub enum Relation {}
+
+impl ActiveModelBehavior for ActiveModel {}
+
+#[async_trait::async_trait]
+impl MigrationTrait for Migration {
+    async fn up(&self, manager: &SchemaManager) -> Result<(), DbErr> {
+        let db = manager.get_connection();
+
+        db.execute_unprepared(
+            r#"
+ALTER TABLE "import_report" ADD COLUMN "new_id" text NOT NULL DEFAULT '';
+UPDATE "import_report" SET "new_id" = 'imp_' || "id";
+            "#,
+        )
+        .await?;
+
+        for rev in Entity::find().all(db).await? {
+            let new_id = format!("imp_{}", nanoid!(12));
+            let mut rev: ActiveModel = rev.into();
+            rev.new_id = ActiveValue::Set(new_id);
+            rev.update(db).await?;
+        }
+
+        db.execute_unprepared(
+            r#"
+ALTER TABLE "import_report" DROP CONSTRAINT "import_report_pkey";
+ALTER TABLE "import_report" DROP COLUMN "id";
+ALTER TABLE "import_report" RENAME COLUMN "new_id" TO "id";
+ALTER TABLE "import_report" ADD PRIMARY KEY ("id");
+"#,
+        )
+        .await?;
+
+        db.execute_unprepared(
+            r#"
+ALTER TABLE "import_report" ALTER COLUMN "id" DROP DEFAULT;
+"#,
+        )
+        .await?;
+
+        Ok(())
+    }
+
+    async fn down(&self, _manager: &SchemaManager) -> Result<(), DbErr> {
+        Ok(())
+    }
+}
