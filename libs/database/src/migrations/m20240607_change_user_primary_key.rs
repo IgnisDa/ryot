2e4828576140e56e6fd1444a2528aec28d2ae1a4
Diptesh Choudhuri <ignisda2001@gmail.com>
2024-06-01 15:47:20 +0530

    feat(backend): change type of user primary key

---
+++ libs/database/src/migrations/m20240607_change_user_primary_key.rs
@@ -0,0 +1,255 @@
+use nanoid::nanoid;
+use sea_orm::{entity::prelude::*, ActiveValue, DatabaseBackend, Statement};
+use sea_orm_migration::prelude::*;
+
+async fn get_whether_column_is_text<'a>(
+    table_name: &str,
+    column_name: &str,
+    db: &SchemaManagerConnection<'a>,
+) -> Result<bool, DbErr> {
+    let resp = db.query_one(Statement::from_sql_and_values(
+        DatabaseBackend::Postgres,
+        r#"SELECT data_type = 'text' as is_text FROM information_schema.columns WHERE table_name = $1 AND column_name = $2"#,
+        [table_name.into(), column_name.into()]
+    ))
+    .await?
+    .unwrap();
+    let is_text: bool = resp.try_get("", "is_text")?;
+    Ok(is_text)
+}
+
+#[derive(DeriveMigrationName)]
+pub struct Migration;
+
+#[derive(Clone, Debug, PartialEq, DeriveEntityModel, Eq)]
+#[sea_orm(table_name = "user")]
+pub struct Model {
+    #[sea_orm(primary_key)]
+    pub id: String,
+    pub temp_id: String,
+}
+
+#[derive(Copy, Clone, Debug, EnumIter, DeriveRelation)]
+pub enum Relation {}
+
+impl ActiveModelBehavior for ActiveModel {}
+
+#[async_trait::async_trait]
+impl MigrationTrait for Migration {
+    async fn up(&self, manager: &SchemaManager) -> Result<(), DbErr> {
+        let db = manager.get_connection();
+        if get_whether_column_is_text("user", "id", db).await? {
+            return Ok(());
+        }
+
+        db.execute_unprepared(
+            r#"
+ALTER TABLE "user" ADD COLUMN "new_id" text NOT NULL DEFAULT '';
+UPDATE "user" SET "new_id" = 'usr_' || "id";
+            "#,
+        )
+        .await?;
+
+        db.execute_unprepared(
+            r#"
+ALTER TABLE "collection" ADD COLUMN "new_user_id" text;
+ALTER TABLE "import_report" ADD COLUMN "new_user_id" text;
+ALTER TABLE "queued_notification" ADD COLUMN "new_user_id" text;
+ALTER TABLE "review" ADD COLUMN "new_user_id" text;
+ALTER TABLE "seen" ADD COLUMN "new_user_id" text;
+ALTER TABLE "user_measurement" ADD COLUMN "new_user_id" text;
+ALTER TABLE "user_to_collection" ADD COLUMN "new_user_id" text;
+ALTER TABLE "user_to_entity" ADD COLUMN "new_user_id" text;
+ALTER TABLE "workout" ADD COLUMN "new_user_id" text;
+ALTER TABLE "exercise" ADD COLUMN "new_created_by_user_id" text;
+
+UPDATE "collection" SET "new_user_id" = 'usr_' || "user_id";
+UPDATE "import_report" SET "new_user_id" = 'usr_' || "user_id";
+UPDATE "queued_notification" SET "new_user_id" = 'usr_' || "user_id";
+UPDATE "review" SET "new_user_id" = 'usr_' || "user_id";
+UPDATE "seen" SET "new_user_id" = 'usr_' || "user_id";
+UPDATE "user_measurement" SET "new_user_id" = 'usr_' || "user_id";
+UPDATE "user_to_collection" SET "new_user_id" = 'usr_' || "user_id";
+UPDATE "user_to_entity" SET "new_user_id" = 'usr_' || "user_id";
+UPDATE "workout" SET "new_user_id" = 'usr_' || "user_id";
+UPDATE "exercise" SET "new_created_by_user_id" = 'usr_' || "created_by_user_id";
+
+ALTER TABLE "collection" DROP CONSTRAINT "collection_to_user_foreign_key";
+ALTER TABLE "import_report" DROP CONSTRAINT "media_import_report_to_user_foreign_key";
+ALTER TABLE "queued_notification" DROP CONSTRAINT "queued_notification_to_user_foreign_key";
+ALTER TABLE "review" DROP CONSTRAINT "review_to_user_foreign_key";
+ALTER TABLE "seen" DROP CONSTRAINT "user_to_seen_foreign_key";
+ALTER TABLE "user_measurement" DROP CONSTRAINT "fk-user_measurement-user_id";
+ALTER TABLE "user_to_collection" DROP CONSTRAINT "user_to_collection-fk2";
+ALTER TABLE "user_to_entity" DROP CONSTRAINT "user_to_entity-fk1";
+ALTER TABLE "workout" DROP CONSTRAINT "workout_to_user_foreign_key";
+ALTER TABLE "exercise" DROP CONSTRAINT "workout_to_user_foreign_key";
+            "#,
+        )
+        .await?;
+
+        db.execute_unprepared(
+            r#"
+ALTER TABLE "user" DROP CONSTRAINT "user_pkey";
+ALTER TABLE "user" DROP COLUMN "id";
+ALTER TABLE "user" RENAME COLUMN "new_id" TO "id";
+ALTER TABLE "user" ADD PRIMARY KEY ("id");
+
+ALTER TABLE "user" ADD COLUMN "temp_id" text;
+UPDATE "user" SET "temp_id" = "id";
+
+ALTER TABLE "collection" DROP COLUMN "user_id";
+ALTER TABLE "collection" RENAME COLUMN "new_user_id" TO "user_id";
+
+ALTER TABLE "import_report" DROP COLUMN "user_id";
+ALTER TABLE "import_report" RENAME COLUMN "new_user_id" TO "user_id";
+
+ALTER TABLE "queued_notification" DROP COLUMN "user_id";
+ALTER TABLE "queued_notification" RENAME COLUMN "new_user_id" TO "user_id";
+
+ALTER TABLE "review" DROP COLUMN "user_id";
+ALTER TABLE "review" RENAME COLUMN "new_user_id" TO "user_id";
+
+ALTER TABLE "seen" DROP COLUMN "user_id";
+ALTER TABLE "seen" RENAME COLUMN "new_user_id" TO "user_id";
+
+ALTER TABLE "user_measurement" DROP COLUMN "user_id";
+ALTER TABLE "user_measurement" RENAME COLUMN "new_user_id" TO "user_id";
+
+ALTER TABLE "user_to_collection" DROP COLUMN "user_id";
+ALTER TABLE "user_to_collection" RENAME COLUMN "new_user_id" TO "user_id";
+
+ALTER TABLE "user_to_entity" DROP COLUMN "user_id";
+ALTER TABLE "user_to_entity" RENAME COLUMN "new_user_id" TO "user_id";
+
+ALTER TABLE "workout" DROP COLUMN "user_id";
+ALTER TABLE "workout" RENAME COLUMN "new_user_id" TO "user_id";
+
+ALTER TABLE "exercise" DROP COLUMN "created_by_user_id";
+ALTER TABLE "exercise" RENAME COLUMN "new_created_by_user_id" TO "created_by_user_id";
+
+ALTER TABLE "collection" ADD CONSTRAINT "collection_to_user_foreign_key" FOREIGN KEY ("user_id") REFERENCES "user"("id") ON UPDATE CASCADE ON DELETE CASCADE;
+ALTER TABLE "import_report" ADD CONSTRAINT "media_import_report_to_user_foreign_key" FOREIGN KEY ("user_id") REFERENCES "user"("id") ON UPDATE CASCADE ON DELETE CASCADE;
+ALTER TABLE "queued_notification" ADD CONSTRAINT "queued_notification_to_user_foreign_key" FOREIGN KEY ("user_id") REFERENCES "user"("id") ON UPDATE CASCADE ON DELETE CASCADE;
+ALTER TABLE "review" ADD CONSTRAINT "review_to_user_foreign_key" FOREIGN KEY ("user_id") REFERENCES "user"("id") ON UPDATE CASCADE ON DELETE CASCADE;
+ALTER TABLE "seen" ADD CONSTRAINT "user_to_seen_foreign_key" FOREIGN KEY ("user_id") REFERENCES "user"("id") ON UPDATE CASCADE ON DELETE CASCADE;
+ALTER TABLE "user_measurement" ADD CONSTRAINT "fk-user_measurement-user_id" FOREIGN KEY ("user_id") REFERENCES "user"("id") ON UPDATE CASCADE ON DELETE CASCADE;
+ALTER TABLE "user_to_collection" ADD CONSTRAINT "user_to_collection-fk2" FOREIGN KEY ("user_id") REFERENCES "user"("id") ON UPDATE CASCADE ON DELETE CASCADE;
+ALTER TABLE "user_to_entity" ADD CONSTRAINT "user_to_entity-fk1" FOREIGN KEY ("user_id") REFERENCES "user"("id") ON UPDATE CASCADE ON DELETE CASCADE;
+ALTER TABLE "workout" ADD CONSTRAINT "workout_to_user_foreign_key" FOREIGN KEY ("user_id") REFERENCES "user"("id") ON UPDATE CASCADE ON DELETE CASCADE;
+ALTER TABLE "exercise" ADD CONSTRAINT "workout_to_user_foreign_key" FOREIGN KEY ("created_by_user_id") REFERENCES "user"("id") ON UPDATE CASCADE ON DELETE SET NULL;
+            "#,
+        )
+        .await?;
+
+        for user in Entity::find().all(db).await? {
+            let new_id = format!("usr_{}", nanoid!(12));
+            let mut user: ActiveModel = user.into();
+            user.temp_id = ActiveValue::Set(new_id);
+            user.update(db).await?;
+        }
+        db.execute_unprepared(r#"UPDATE "user" SET "id" = "temp_id""#)
+            .await?;
+
+        db.execute_unprepared(
+            r#"
+ALTER TABLE "collection"
+ALTER COLUMN "user_id" SET NOT NULL;
+ALTER TABLE "import_report"
+ALTER COLUMN "user_id" SET NOT NULL;
+ALTER TABLE "queued_notification"
+ALTER COLUMN "user_id" SET NOT NULL;
+ALTER TABLE "review"
+ALTER COLUMN "user_id" SET NOT NULL;
+ALTER TABLE "seen"
+ALTER COLUMN "user_id" SET NOT NULL;
+ALTER TABLE "user_measurement"
+ALTER COLUMN "user_id" SET NOT NULL;
+ALTER TABLE "user_to_collection"
+ALTER COLUMN "user_id" SET NOT NULL;
+ALTER TABLE "user_to_entity"
+ALTER COLUMN "user_id" SET NOT NULL;
+ALTER TABLE "workout"
+ALTER COLUMN "user_id" SET NOT NULL;
+            "#,
+        )
+        .await?;
+
+        db.execute_unprepared(
+            r#"
+ALTER TABLE "user" ALTER COLUMN "id" DROP DEFAULT;
+ALTER TABLE "user" DROP COLUMN "temp_id";
+            "#,
+        )
+        .await?;
+
+        db.execute_unprepared(
+            r#"
+-- Step 1: Add temporary columns
+ALTER TABLE "user" ADD COLUMN "temp__name" text;
+ALTER TABLE "user" ADD COLUMN "temp__password" text;
+ALTER TABLE "user" ADD COLUMN "temp__lot" text;
+ALTER TABLE "user" ADD COLUMN "temp__preferences" jsonb;
+ALTER TABLE "user" ADD COLUMN "temp__yank_integrations" jsonb;
+ALTER TABLE "user" ADD COLUMN "temp__sink_integrations" jsonb;
+ALTER TABLE "user" ADD COLUMN "temp__notifications" jsonb;
+ALTER TABLE "user" ADD COLUMN "temp__summary" jsonb;
+ALTER TABLE "user" ADD COLUMN "temp__is_demo" boolean;
+ALTER TABLE "user" ADD COLUMN "temp__oidc_issuer_id" text;
+
+-- Step 2: Update temporary columns with the values from original columns
+UPDATE "user" SET
+    "temp__name" = "name",
+    "temp__password" = "password",
+    "temp__lot" = "lot",
+    "temp__preferences" = "preferences",
+    "temp__yank_integrations" = "yank_integrations",
+    "temp__sink_integrations" = "sink_integrations",
+    "temp__notifications" = "notifications",
+    "temp__summary" = "summary",
+    "temp__is_demo" = "is_demo",
+    "temp__oidc_issuer_id" = "oidc_issuer_id";
+
+-- Step 3: Set temporary columns to not null if the original columns were not null
+ALTER TABLE "user" ALTER COLUMN "temp__name" SET NOT NULL;
+ALTER TABLE "user" ALTER COLUMN "temp__lot" SET NOT NULL;
+ALTER TABLE "user" ALTER COLUMN "temp__preferences" SET NOT NULL;
+
+-- Step 4: Drop original columns with CASCADE
+ALTER TABLE "user" DROP COLUMN "name" CASCADE;
+ALTER TABLE "user" DROP COLUMN "password" CASCADE;
+ALTER TABLE "user" DROP COLUMN "lot" CASCADE;
+ALTER TABLE "user" DROP COLUMN "preferences" CASCADE;
+ALTER TABLE "user" DROP COLUMN "yank_integrations" CASCADE;
+ALTER TABLE "user" DROP COLUMN "sink_integrations" CASCADE;
+ALTER TABLE "user" DROP COLUMN "notifications" CASCADE;
+ALTER TABLE "user" DROP COLUMN "summary" CASCADE;
+ALTER TABLE "user" DROP COLUMN "is_demo" CASCADE;
+ALTER TABLE "user" DROP COLUMN "oidc_issuer_id" CASCADE;
+
+-- Step 5: Rename temporary columns back to original column names
+ALTER TABLE "user" RENAME COLUMN "temp__name" TO "name";
+ALTER TABLE "user" RENAME COLUMN "temp__password" TO "password";
+ALTER TABLE "user" RENAME COLUMN "temp__lot" TO "lot";
+ALTER TABLE "user" RENAME COLUMN "temp__preferences" TO "preferences";
+ALTER TABLE "user" RENAME COLUMN "temp__yank_integrations" TO "yank_integrations";
+ALTER TABLE "user" RENAME COLUMN "temp__sink_integrations" TO "sink_integrations";
+ALTER TABLE "user" RENAME COLUMN "temp__notifications" TO "notifications";
+ALTER TABLE "user" RENAME COLUMN "temp__summary" TO "summary";
+ALTER TABLE "user" RENAME COLUMN "temp__is_demo" TO "is_demo";
+ALTER TABLE "user" RENAME COLUMN "temp__oidc_issuer_id" TO "oidc_issuer_id";
+
+-- Step 6: Recreate indexes
+CREATE UNIQUE INDEX "user__name__index" ON "user" ("name");
+CREATE UNIQUE INDEX "user__oidc_issuer_id__index" ON "user" ("oidc_issuer_id");
+            "#,
+        )
+        .await?;
+
+        Ok(())
+    }
+
+    async fn down(&self, _manager: &SchemaManager) -> Result<(), DbErr> {
+        Ok(())
+    }
+}
--- libs/database/src/migrations/mod.rs
+++ libs/database/src/migrations/mod.rs
@@ -21,6 +21,7 @@ mod m20240509_create_user_to_collection;
 mod m20240531_create_queued_notification;
 mod m20240606_is_v6_migration;
 mod m20240607_change_boolean_column_names;
+mod m20240607_change_user_primary_key;
 
 pub use m20230410_create_metadata::Metadata as AliasedMetadata;
 pub use m20230413_create_person::Person as AliasedPerson;
@@ -61,6 +62,7 @@ impl MigratorTrait for Migrator {
             Box::new(m20240531_create_queued_notification::Migration),
             Box::new(m20240606_is_v6_migration::Migration),
             Box::new(m20240607_change_boolean_column_names::Migration),
+            Box::new(m20240607_change_user_primary_key::Migration),
         ]
     }
 }
