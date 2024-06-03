2b59caffb677d579682613a5a3d8cba704b4a8dd
Diptesh Choudhuri <ignisda2001@gmail.com>
2024-06-03 19:43:52 +0530

    feat(database,backend): add enum for integration lot

--- libs/database/src/definitions.rs
+++ libs/database/src/definitions.rs
@@ -368,3 +368,30 @@ pub enum UserToMediaReason {
     Monitoring,
     Watchlist,
 }
+
+#[derive(
+    Copy,
+    Clone,
+    Debug,
+    Enum,
+    PartialEq,
+    Eq,
+    DeriveActiveEnum,
+    EnumIter,
+    Serialize,
+    Deserialize,
+    Hash,
+    Display,
+)]
+#[sea_orm(
+    rs_type = "String",
+    db_type = "String(StringLen::None)",
+    rename_all = "snake_case"
+)]
+#[serde(rename_all = "snake_case")]
+pub enum IntegrationLot {
+    Audiobookshelf,
+    Jellyfin,
+    Plex,
+    Kodi,
+}
