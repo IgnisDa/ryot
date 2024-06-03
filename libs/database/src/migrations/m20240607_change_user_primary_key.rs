0cbdc80bc8dc3490ff26257faaa0f5fc93ced65c
Diptesh Choudhuri <ignisda2001@gmail.com>
2024-06-03 06:59:33 +0530

    feat(database): add index for sink integrations

--- libs/database/src/migrations/m20240607_change_user_primary_key.rs
+++ libs/database/src/migrations/m20240607_change_user_primary_key.rs
@@ -6,6 +6,8 @@ use sea_orm_migration::prelude::*;
 use serde::{Deserialize, Serialize};
 use serde_json::Value;
 
+use super::m20230417_create_user::SINK_INTEGRATIONS_INDEX_SQL;
+
 async fn get_whether_column_is_text<'a>(
     table_name: &str,
     column_name: &str,
@@ -338,6 +340,8 @@ CREATE UNIQUE INDEX "user__oidc_issuer_id__index" ON "user" ("oidc_issuer_id");
             "#,
         )
         .await?;
+
+        db.execute_unprepared(SINK_INTEGRATIONS_INDEX_SQL).await?;
 
         Ok(())
     }
