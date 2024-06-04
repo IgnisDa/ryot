dcf405f80792f56b1d1f2a02eb708592d1f5e8c9
Diptesh Choudhuri <ignisda2001@gmail.com>
2024-06-04 10:09:06 +0000

    feat(backend): add template for monitoring column

--- apps/backend/src/miscellaneous/mod.rs
+++ apps/backend/src/miscellaneous/mod.rs
@@ -32,6 +32,7 @@ pub struct CollectionExtraInformation {
     pub name: String,
     pub description: String,
     pub lot: CollectionExtraInformationLot,
+    pub default_value: Option<String>,
     pub required: Option<bool>,
 }
 
@@ -52,14 +53,25 @@ meta! {
     Watchlist, (None, "Things I want to watch in the future.");
     InProgress, (None, "Media items that I am currently watching.");
     Completed, (None, "Media items that I have completed.");
-    Monitoring, (None, "Items that I am keeping an eye on.");
     Custom, (None, "Items that I have created manually.");
+    Monitoring, (Some(
+        vec![
+            CollectionExtraInformation {
+                name: "Days".to_string(),
+                description: "How many days do you want to monitor for?".to_string(),
+                lot: CollectionExtraInformationLot::Number,
+                default_value: Some("30".to_string()),
+                required: None,
+            },
+        ]
+    ), "Items that I am keeping an eye on.");
     Owned, (Some(
         vec![
             CollectionExtraInformation {
                 name: "Owned on".to_string(),
                 description: "When did you get this media?".to_string(),
                 lot: CollectionExtraInformationLot::Date,
+                default_value: None,
                 required: None,
             }
         ]
@@ -70,12 +82,14 @@ meta! {
                 name: "Reminder".to_string(),
                 description: "When do you want to be reminded?".to_string(),
                 lot: CollectionExtraInformationLot::Date,
+                default_value: None,
                 required: Some(true),
             },
             CollectionExtraInformation {
                 name: "Text".to_string(),
                 description: "What do you want to be reminded about?".to_string(),
                 lot: CollectionExtraInformationLot::String,
+                default_value: None,
                 required: Some(true),
             }
         ]
