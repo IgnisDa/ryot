daf24880f7ca591978a51abdd4c90ca7b50e9d5c
Diptesh Choudhuri <ignisda2001@gmail.com>
2024-06-14 21:41:42 +0530

    feat(database): create collections for each user

--- libs/database/src/migrations/m20240614_02_introduce_recommendations_collection_for_existing_users.rs
+++ libs/database/src/migrations/m20240614_02_introduce_recommendations_collection_for_existing_users.rs
@@ -1,33 +1,46 @@
 use nanoid::nanoid;
+use sea_orm::entity::prelude::*;
 use sea_orm_migration::prelude::*;
 
 #[derive(DeriveMigrationName)]
 pub struct Migration;
 
+mod user {
+    use super::*;
+
+    #[derive(Clone, Debug, PartialEq, DeriveEntityModel, Eq)]
+    #[sea_orm(table_name = "user")]
+    pub struct Model {
+        #[sea_orm(primary_key)]
+        pub id: String,
+    }
+
+    #[derive(Copy, Clone, Debug, EnumIter, DeriveRelation)]
+    pub enum Relation {}
+
+    impl ActiveModelBehavior for ActiveModel {}
+}
+
 #[async_trait::async_trait]
 impl MigrationTrait for Migration {
     async fn up(&self, manager: &SchemaManager) -> Result<(), DbErr> {
         let db = manager.get_connection();
-        db.execute_unprepared(&format!(
-            r#"
-DO $$
-DECLARE
-    user_rec RECORD;
-BEGIN
-    FOR user_rec IN SELECT id FROM "user"
-    LOOP
-        INSERT INTO collection (name, description, user_id, created_on, last_updated_on, id)
-        VALUES (
+        let users = user::Entity::find().all(db).await?;
+        for user in users {
+            db.execute_unprepared(&format!(
+                r#"
+    INSERT INTO collection (name, description, user_id, created_on, last_updated_on, id)
+    VALUES (
             'Recommendations', 'Items that are recommended to me based on my consumption.',
-            user_rec.id, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, 'col_{id}'
+            '{user_id}', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, 'col_{new_id}'
         )
-        ON CONFLICT DO NOTHING;
-    END LOOP;
-END $$;
+    ON CONFLICT DO NOTHING;
             "#,
-            id = nanoid!(12),
-        ))
-        .await?;
+                user_id = user.id,
+                new_id = nanoid!(12),
+            ))
+            .await?;
+        }
         Ok(())
     }
 
