0028b8250bd08b81f2c09a9b5a9e5ad7593ede05
Diptesh Choudhuri <ignisda2001@gmail.com>
2024-06-19 21:50:06 +0530

    feat(database): migration to remove seen from array

---
+++ libs/database/src/migrations/m20240619_remove_seen_from_media_reason_of_user_to_entity.rs
@@ -0,0 +1,25 @@
+use sea_orm_migration::prelude::*;
+
+#[derive(DeriveMigrationName)]
+pub struct Migration;
+
+#[async_trait::async_trait]
+impl MigrationTrait for Migration {
+    async fn up(&self, manager: &SchemaManager) -> Result<(), DbErr> {
+        let db = manager.get_connection();
+        db.execute_unprepared(
+            r#"
+UPDATE user_to_entity
+SET media_reason = array_remove(media_reason, 'Seen'), needs_to_be_updated = true
+WHERE 'Seen' = ANY(media_reason);
+"#,
+        )
+        .await?;
+
+        Ok(())
+    }
+
+    async fn down(&self, _manager: &SchemaManager) -> Result<(), DbErr> {
+        Ok(())
+    }
+}
