f81440c64ad1844cbf2eb6727311e22578a4c53d
Diptesh Choudhuri <ignisda2001@gmail.com>
2024-06-20 10:45:43 +0530

    Revert "ci: extract startup script"
    This reverts commit 9936f2c7ee84f1f88d724cd7710e5b2eb8763d0f.

--- ci/entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-set -euxo pipefail
-
-concurrently --names "frontend,backend,proxy" --kill-others \
-  "PORT=3000 npx remix-serve ./build/server/index.js" \
-  "BACKEND_PORT=5000 /usr/local/bin/ryot" \
-  "caddy run --config /etc/caddy/Caddyfile"
