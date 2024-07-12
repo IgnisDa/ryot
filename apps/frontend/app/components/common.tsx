72e17222a282bdef44bf3f011ba478a1fd69754c
Diptesh Choudhuri <ignisda2001@gmail.com>
2024-07-12 21:08:35 +0530

    feat(frontend): remove old search params usage

--- apps/frontend/app/components/common.tsx
+++ apps/frontend/app/components/common.tsx
@@ -38,7 +38,6 @@ import {
 	useCookieEnhancedSearchParam,
 	useCoreDetails,
 	useFallbackImageUrl,
-	useSearchParam,
 } from "~/lib/hooks";
 import classes from "~/styles/common.module.css";
 
@@ -136,14 +135,12 @@ export const DebouncedSearchInput = (props: {
 }) => {
 	const [query, setQuery] = useState(props.initialValue || "");
 	const [debounced] = useDebouncedValue(query, 1000);
-	const [_p, { setP }] = useSearchParam();
-	const [_e, { setP: setEnhancedP }] = useCookieEnhancedSearchParam(
+	const [_e, { setP }] = useCookieEnhancedSearchParam(
 		props.enhancedQueryParams || "query",
 	);
 
 	useDidUpdate(() => {
-		const fn = props.enhancedQueryParams ? setEnhancedP : setP;
-		fn(props.queryParam || "query", debounced);
+		setP(props.queryParam || "query", debounced);
 	}, [debounced]);
 
 	return (
