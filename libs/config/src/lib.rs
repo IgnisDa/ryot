9f2f9a0586b0b51cd59ec7dfd5ae8ff4c6413171
Diptesh Choudhuri <ignisda2001@gmail.com>
2024-07-13 22:53:58 +0530

    feat(config): add config params for music tracking

--- libs/config/src/lib.rs
+++ libs/config/src/lib.rs
@@ -179,6 +179,27 @@ pub struct MovieAndShowConfig {
 impl IsFeatureEnabled for MovieAndShowConfig {}
 
 #[derive(Debug, Serialize, Deserialize, Clone, Config)]
+#[config(rename_all = "snake_case", env_prefix = "MUSIC_DISCOGS_")]
+pub struct DiscogsConfig {
+    /// The key to use for making requests to Discogs API.
+    #[setting(default = default_discogs_key)]
+    pub key: String,
+    /// The secret to use for making requests to Discogs API.
+    #[setting(default = default_discogs_secret)]
+    pub secret: String,
+}
+
+#[derive(Debug, Serialize, Deserialize, Clone, Config)]
+#[config(rename_all = "snake_case")]
+pub struct MusicConfig {
+    /// Settings related to Discogs.
+    #[setting(nested)]
+    pub discogs: DiscogsConfig,
+}
+
+impl IsFeatureEnabled for MusicConfig {}
+
+#[derive(Debug, Serialize, Deserialize, Clone, Config)]
 #[config(rename_all = "snake_case", env_prefix = "PODCASTS_LISTENNOTES_")]
 pub struct ListenNotesConfig {
     /// The access token for the Listennotes API.
@@ -441,6 +462,9 @@ pub struct AppConfig {
     /// Settings related to movies and shows.
     #[setting(nested)]
     pub movies_and_shows: MovieAndShowConfig,
+    /// Settings related to music.
+    #[setting(nested)]
+    pub music: MusicConfig,
     /// Settings related to podcasts.
     #[setting(nested)]
     pub podcasts: PodcastConfig,
@@ -478,6 +502,8 @@ impl AppConfig {
         cl.file_storage.s3_access_key_id = gt();
         cl.file_storage.s3_secret_access_key = gt();
         cl.file_storage.s3_url = gt();
+        cl.music.discogs.key = gt();
+        cl.music.discogs.secret = gt();
         cl.movies_and_shows.tmdb.access_token = gt();
         cl.podcasts.listennotes.api_token = gt();
         cl.video_games.twitch.client_id = gt();
