33b0cb0f94ce8911d577deeaefd6c07a73808b04
Diptesh Choudhuri <ignisda2001@gmail.com>
2024-07-30 04:57:51 +0530

    feat(website): handle duplicate emails

--- apps/website/app/routes/callback.tsx
+++ apps/website/app/routes/callback.tsx
@@ -1,5 +1,7 @@
 import { redirect, unstable_defineLoader } from "@remix-run/node";
+import { eq } from "drizzle-orm";
 import { $path } from "remix-routes";
+import { match } from "ts-pattern";
 import { customers } from "~/drizzle/schema.server";
 import {
 	OAUTH_CALLBACK_URL,
@@ -15,16 +17,24 @@ export const loader = unstable_defineLoader(async ({ request }) => {
 		state: params.state,
 	});
 	const claims = tokenSet.claims();
-	if (!claims.email || !claims.sub) throw new Error("Invalid claims");
-	const dbCustomer = await db
-		.insert(customers)
-		.values({ email: claims.email, oidcIssuerId: claims.sub })
-		.returning({ id: customers.id })
-		.onConflictDoUpdate({
-			target: customers.oidcIssuerId,
-			set: { oidcIssuerId: claims.sub },
-		});
-	const customerId = dbCustomer.at(0)?.id;
+	const email = claims.email;
+	if (!email || !claims.sub) throw new Error("Invalid claims");
+	const alreadyCustomer = await db.query.customers.findFirst({
+		where: eq(customers.email, email),
+	});
+	const customerId = await match(alreadyCustomer)
+		.with(undefined, async () => {
+			const dbCustomer = await db
+				.insert(customers)
+				.values({ email, oidcIssuerId: claims.sub })
+				.returning({ id: customers.id })
+				.onConflictDoUpdate({
+					target: customers.oidcIssuerId,
+					set: { oidcIssuerId: claims.sub },
+				});
+			return dbCustomer.at(0)?.id;
+		})
+		.otherwise((value) => value.id);
 	if (!customerId) throw new Error("There was an error registering the user.");
 	return redirect($path("/me"), {
 		headers: { "set-cookie": await authCookie.serialize(customerId) },
