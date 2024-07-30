4b12fe58117ebc9fa14ec46672ef4344a72212b9
Diptesh Choudhuri <ignisda2001@gmail.com>
2024-07-30 21:48:14 +0530

    feat(database): add migrations for new stuff

--- libs/database/src/migrations/m20240607_create_integration.rs
+++ libs/database/src/migrations/m20240607_create_integration.rs
@@ -44,16 +44,8 @@ impl MigrationTrait for Migration {
                     .col(ColumnDef::new(Integration::LastTriggeredOn).timestamp_with_time_zone())
                     .col(ColumnDef::new(Integration::SourceSpecifics).json_binary())
                     .col(ColumnDef::new(Integration::UserId).text().not_null())
-                    .col(
-                        ColumnDef::new(Integration::MinimumProgress)
-                            .decimal()
-                            .not_null(),
-                    )
-                    .col(
-                        ColumnDef::new(Integration::MaximumProgress)
-                            .decimal()
-                            .not_null(),
-                    )
+                    .col(ColumnDef::new(Integration::MinimumProgress).decimal())
+                    .col(ColumnDef::new(Integration::MaximumProgress).decimal())
                     .col(ColumnDef::new(Integration::IsDisabled).boolean())
                     .foreign_key(
                         ForeignKey::create()
