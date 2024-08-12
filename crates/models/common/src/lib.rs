1dc4d4e5fb21c52e579f96e486aa216c2882dcc6
Diptesh Choudhuri <ignisda2001@gmail.com>
2024-08-12 17:25:57 +0530

    refactor(common_models): extract more structs

--- crates/models/common/src/lib.rs
+++ crates/models/common/src/lib.rs
@@ -1,7 +1,9 @@
 use async_graphql::{Enum, InputObject, SimpleObject};
+use enum_meta::{meta, Meta};
 use schematic::Schematic;
 use sea_orm::FromJsonQueryResult;
 use serde::{Deserialize, Serialize};
+use strum::{EnumIter,Display};
 
 #[derive(Debug, SimpleObject, Serialize, Deserialize, Clone)]
 pub struct IdObject {
@@ -70,4 +72,87 @@ pub struct CollectionExtraInformation {
     pub lot: CollectionExtraInformationLot,
     pub default_value: Option<String>,
     pub required: Option<bool>,
+}
+
+#[derive(Display, EnumIter)]
+pub enum DefaultCollection {
+    Watchlist,
+    #[strum(serialize = "In Progress")]
+    InProgress,
+    Completed,
+    Monitoring,
+    Custom,
+    Owned,
+    Reminders,
+}
+
+meta! {
+    DefaultCollection, (Option<Vec<CollectionExtraInformation>>, &'static str);
+    Watchlist, (None, "Things I want to watch in the future.");
+    InProgress, (None, "Media items that I am currently watching.");
+    Completed, (None, "Media items that I have completed.");
+    Custom, (None, "Items that I have created manually.");
+    Monitoring, (None, "Items that I am keeping an eye on.");
+    Owned, (Some(
+        vec![
+            CollectionExtraInformation {
+                name: "Owned on".to_string(),
+                description: "When did you get this media?".to_string(),
+                lot: CollectionExtraInformationLot::Date,
+                default_value: None,
+                required: None,
+            }
+        ]
+    ), "Items that I have in my inventory.");
+    Reminders, (Some(
+        vec![
+            CollectionExtraInformation {
+                name: "Reminder".to_string(),
+                description: "When do you want to be reminded?".to_string(),
+                lot: CollectionExtraInformationLot::Date,
+                default_value: None,
+                required: Some(true),
+            },
+            CollectionExtraInformation {
+                name: "Text".to_string(),
+                description: "What do you want to be reminded about?".to_string(),
+                lot: CollectionExtraInformationLot::String,
+                default_value: None,
+                required: Some(true),
+            }
+        ]
+    ), "Items that I want to be reminded about.");
+}
+
+#[derive(Enum, Serialize, Deserialize, Clone, Debug, Copy, PartialEq, Eq)]
+pub enum BackgroundJob {
+    CalculateSummary,
+    EvaluateWorkouts,
+    UpdateAllMetadata,
+    UpdateAllExercises,
+    RecalculateCalendarEvents,
+    SyncIntegrationsData,
+    PerformBackgroundTasks,
+}
+
+#[derive(Enum, Clone, Debug, Copy, PartialEq, Eq, Serialize, Deserialize, EnumIter, Display)]
+#[strum(serialize_all = "SCREAMING_SNAKE_CASE")]
+pub enum BackendError {
+    NoUserId,
+    NoAuthToken,
+    SessionExpired,
+    AdminOnlyAction,
+    MutationNotAllowed,
+}
+
+#[derive(Debug, Serialize, Deserialize, Clone, SimpleObject, InputObject)]
+#[graphql(input_name = "NamedObjectInput")]
+pub struct NamedObject {
+    pub name: String,
+}
+
+#[derive(Serialize, Deserialize, Debug, InputObject, Clone, Default)]
+pub struct SearchInput {
+    pub query: Option<String>,
+    pub page: Option<i32>,
 }
