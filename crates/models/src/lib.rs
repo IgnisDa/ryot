26fd9846231bad4a66d405dd53c2d62100cb067a
Diptesh Choudhuri <ignisda2001@gmail.com>
2024-08-13 07:04:50 +0530

    chore(models): re-export all importer models

--- crates/models/src/lib.rs
+++ crates/models/src/lib.rs
@@ -2,5 +2,6 @@ pub use common_models::*;
 pub use database_models::*;
 pub use dependent_models::*;
 pub use fitness_models::*;
+pub use importer_models::*;
 pub use media_models::*;
 pub use user_models::*;
