67a871835450dc793269c069151d9d72a9ee8c5d
Diptesh Choudhuri <ignisda2001@gmail.com>
2024-08-14 11:00:12 +0530

    feat(backend): support for multiple collection ids

--- apps/backend/src/utils.rs
+++ apps/backend/src/utils.rs
@@ -9,6 +9,7 @@ use axum::{
     Extension, RequestPartsExt,
 };
 use chrono::{NaiveDate, Utc};
+use database::AliasedCollectionToEntity;
 use itertools::Itertools;
 use openidconnect::{
     core::{CoreClient, CoreProviderMetadata},
@@ -24,6 +25,7 @@ use sea_orm::{
     ActiveModelTrait, ActiveValue, ColumnTrait, ConnectionTrait, DatabaseConnection, EntityTrait,
     QueryFilter, QuerySelect, QueryTrait, Select,
 };
+use sea_query::{Expr, PgFunc};
 
 use crate::{
     background::{ApplicationJob, CoreApplicationJob},
@@ -382,11 +384,25 @@ where
     D: ColumnTrait,
 {
     query.apply_if(collection_id, |query, v| {
+        let unique_collections = v.into_iter().unique().collect_vec();
+        let count = unique_collections.len() as i32;
         let subquery = CollectionToEntity::find()
             .select_only()
             .column(id_column)
-            .filter(collection_to_entity::Column::CollectionId.is_in(v))
+            .filter(
+                Expr::col((
+                    AliasedCollectionToEntity::Table,
+                    collection_to_entity::Column::CollectionId,
+                ))
+                .eq(PgFunc::any(unique_collections)),
+            )
             .filter(id_column.is_not_null())
+            .group_by(id_column)
+            .having(
+                collection_to_entity::Column::CollectionId
+                    .count()
+                    .eq(Expr::val(count)),
+            )
             .into_query();
         if invert_collection.unwrap_or_default() {
             query.filter(entity_column.not_in_subquery(subquery))
