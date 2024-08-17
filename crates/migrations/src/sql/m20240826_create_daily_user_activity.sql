4a823250c56ac2d51a97f7a8110a89053984b04e
Diptesh Choudhuri <ignisda2001@gmail.com>
2024-08-18 00:26:03 +0530

    fix(migrations): include hour in query

--- crates/migrations/src/sql/m20240826_create_daily_user_activity.sql
+++ crates/migrations/src/sql/m20240826_create_daily_user_activity.sql
@@ -1,12 +1,7 @@
-CREATE OR REPLACE FUNCTION get_time_of_day(time_input TIMESTAMPTZ)
-RETURNS TEXT AS $$
+CREATE OR REPLACE FUNCTION get_hour_of_day(time_input TIMESTAMPTZ)
+RETURNS INT AS $$
 BEGIN
-    RETURN CASE
-        WHEN EXTRACT(HOUR FROM time_input) BETWEEN 5 AND 11 THEN 'Morning'
-        WHEN EXTRACT(HOUR FROM time_input) BETWEEN 12 AND 16 THEN 'Afternoon'
-        WHEN EXTRACT(HOUR FROM time_input) BETWEEN 17 AND 20 THEN 'Evening'
-        ELSE 'Night'
-    END;
+    RETURN EXTRACT(HOUR FROM time_input AT TIME ZONE 'UTC');
 END;
 $$ LANGUAGE plpgsql;
 
@@ -17,7 +12,7 @@ WITH counted_lots AS (
     SELECT
         CAST(s."finished_on" AS DATE) AS "date",
         s."user_id",
-        get_time_of_day(s."last_updated_on") AS "time_of_day",
+        get_hour_of_day(s."last_updated_on") AS "hour_of_day",
         m."lot",
         CAST(COUNT(DISTINCT s."metadata_id") AS BIGINT) AS "lot_count"
     FROM
@@ -27,46 +22,46 @@ WITH counted_lots AS (
     WHERE
         s."finished_on" IS NOT NULL
     GROUP BY
-        CAST(s."finished_on" AS DATE), s."user_id", m."lot", "time_of_day"
+        CAST(s."finished_on" AS DATE), s."user_id", m."lot", "hour_of_day"
 ),
 reviews_count AS (
     SELECT
         CAST(r."posted_on" AS DATE) AS "review_day",
         r."user_id",
-        get_time_of_day(r."posted_on") AS "time_of_day",
+        get_hour_of_day(r."posted_on") AS "hour_of_day",
         CAST(COUNT(DISTINCT r."id") AS BIGINT) AS "review_counts"
     FROM
         public."review" r
     GROUP BY
-        CAST(r."posted_on" AS DATE), r."user_id", "time_of_day"
+        CAST(r."posted_on" AS DATE), r."user_id", "hour_of_day"
 ),
 measurements_count AS (
     SELECT
         CAST(m."timestamp" AS DATE) AS "measurement_day",
         m."user_id",
-        get_time_of_day(m."timestamp") AS "time_of_day",
+        get_hour_of_day(m."timestamp") AS "hour_of_day",
         CAST(COUNT(DISTINCT m."timestamp") AS BIGINT) AS "measurement_counts"
     FROM
         public."user_measurement" m
     GROUP BY
-        CAST(m."timestamp" AS DATE), m."user_id", "time_of_day"
+        CAST(m."timestamp" AS DATE), m."user_id", "hour_of_day"
 ),
 workouts_count AS (
     SELECT
         CAST(w."end_time" AS DATE) AS "workout_day",
         w."user_id",
-        get_time_of_day(w."end_time") AS "time_of_day",
+        get_hour_of_day(w."end_time") AS "hour_of_day",
         CAST(COUNT(DISTINCT w."id") AS BIGINT) AS "workout_counts"
     FROM
         public."workout" w
     GROUP BY
-        CAST(w."end_time" AS DATE), w."user_id", "time_of_day"
+        CAST(w."end_time" AS DATE), w."user_id", "hour_of_day"
 ),
 aggregated_times AS (
     SELECT
         COALESCE(cl."date", rc."review_day", mc."measurement_day", wc."workout_day") AS "date",
         COALESCE(cl."user_id", rc."user_id", mc."user_id", wc."user_id") AS "user_id",
-        COALESCE(cl."time_of_day", rc."time_of_day", mc."time_of_day", wc."time_of_day") AS "time_of_day",
+        COALESCE(cl."hour_of_day", rc."hour_of_day", mc."hour_of_day", wc."hour_of_day") AS "hour_of_day",
         CAST(
             SUM(
                 COALESCE(cl."lot_count", 0) +
@@ -74,19 +69,19 @@ aggregated_times AS (
                 COALESCE(mc."measurement_counts", 0) +
                 COALESCE(wc."workout_counts", 0)
             ) AS BIGINT
-        ) AS "time_of_day_count"
+        ) AS "hour_of_day_count"
     FROM
         counted_lots cl
     FULL JOIN
-        reviews_count rc ON cl."date" = rc."review_day" AND cl."user_id" = rc."user_id" AND cl."time_of_day" = rc."time_of_day"
+        reviews_count rc ON cl."date" = rc."review_day" AND cl."user_id" = rc."user_id" AND cl."hour_of_day" = rc."hour_of_day"
     FULL JOIN
-        measurements_count mc ON cl."date" = mc."measurement_day" AND cl."user_id" = mc."user_id" AND cl."time_of_day" = mc."time_of_day"
+        measurements_count mc ON cl."date" = mc."measurement_day" AND cl."user_id" = mc."user_id" AND cl."hour_of_day" = mc."hour_of_day"
     FULL JOIN
-        workouts_count wc ON cl."date" = wc."workout_day" AND cl."user_id" = wc."user_id" AND cl."time_of_day" = wc."time_of_day"
+        workouts_count wc ON cl."date" = wc."workout_day" AND cl."user_id" = wc."user_id" AND cl."hour_of_day" = wc."hour_of_day"
     GROUP BY
         COALESCE(cl."date", rc."review_day", mc."measurement_day", wc."workout_day"),
         COALESCE(cl."user_id", rc."user_id", mc."user_id", wc."user_id"),
-        COALESCE(cl."time_of_day", rc."time_of_day", mc."time_of_day", wc."time_of_day")
+        COALESCE(cl."hour_of_day", rc."hour_of_day", mc."hour_of_day", wc."hour_of_day")
 )
 SELECT
     at."date",
@@ -101,10 +96,10 @@ SELECT
     ) AS "metadata_counts",
     jsonb_agg(
         jsonb_build_object(
-            'time', at."time_of_day",
-            'count', at."time_of_day_count"
+            'hour', at."hour_of_day",
+            'count', at."hour_of_day_count"
         )
-    ) AS "time_of_day_counts",
+    ) AS "hour_counts",
     CAST(SUM(COALESCE(rc."review_counts", 0)) AS BIGINT) AS "review_counts",
     CAST(SUM(COALESCE(mc."measurement_counts", 0)) AS BIGINT) AS "measurement_counts",
     CAST(SUM(COALESCE(wc."workout_counts", 0)) AS BIGINT) AS "workout_counts",
