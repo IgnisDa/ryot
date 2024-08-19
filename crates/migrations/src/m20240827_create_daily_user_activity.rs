4da924d5dfd56aac90c77b4ef3c6e878dc563fce
Diptesh Choudhuri <ignisda2001@gmail.com>
2024-08-19 11:43:26 +0530

    chore(database): remove useless column

--- crates/migrations/src/m20240827_create_daily_user_activity.rs
+++ crates/migrations/src/m20240827_create_daily_user_activity.rs
@@ -18,7 +18,6 @@ pub enum DailyUserActivity {
     MeasurementCounts,
     WorkoutCounts,
     TotalCounts,
-    TotalDuration,
 }
 
 #[async_trait::async_trait]
@@ -69,12 +68,6 @@ impl MigrationTrait for Migration {
                     .col(
                         ColumnDef::new(DailyUserActivity::TotalCounts)
                             .integer()
-                            .not_null()
-                            .default(0),
-                    )
-                    .col(
-                        ColumnDef::new(DailyUserActivity::TotalDuration)
-                            .decimal()
                             .not_null()
                             .default(0),
                     )
