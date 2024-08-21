7d1184495ac846b79ed81e568601b629076ac5a4
Diptesh Choudhuri <ignisda2001@gmail.com>
2024-08-21 15:53:07 +0530

    feat(migrations): change structure of daily user activity table

--- crates/migrations/src/m20240827_create_daily_user_activity.rs
+++ crates/migrations/src/m20240827_create_daily_user_activity.rs
@@ -3,6 +3,23 @@ use sea_orm_migration::prelude::*;
 use super::m20230417_create_user::User;
 
 pub static DAILY_USER_ACTIVITY_PRIMARY_KEY: &str = "pk-daily_user_activity";
+pub static DAILY_USER_ACTIVITY_TOTAL_COUNTS_GENERATED_SQL: &str = r#"
+GENERATED ALWAYS AS (
+    "review_count" + "measurement_count" + "workout_count" + "audio_book_count" +
+    "anime_count" + "book_count" + "podcast_count" + "manga_count" + "movie_count" +
+    "show_count" + "video_game_count" + "visual_novel_count"
+) STORED
+"#;
+pub static DAILY_USER_ACTIVITY_TOTAL_DURATION_GENERATED_SQL: &str = r#"
+GENERATED ALWAYS AS (
+    "workout_duration" + "audio_book_duration" + "podcast_duration" +
+    "movie_duration" +"show_duration"
+) STORED
+"#;
+
+fn integer_not_null<T: IntoIden>(col: T) -> ColumnDef {
+    ColumnDef::new(col).integer().not_null().default(0).take()
+}
 
 #[derive(DeriveMigrationName)]
 pub struct Migration;
@@ -12,14 +29,28 @@ pub enum DailyUserActivity {
     Table,
     UserId,
     Date,
-    MetadataCounts,
-    HourCounts,
-    ReviewCounts,
-    MeasurementCounts,
-    WorkoutCounts,
-    TotalCounts,
+    ReviewCount,
+    MeasurementCount,
+    WorkoutCount,
+    WorkoutDuration,
+    AudioBookCount,
+    AudioBookDuration,
+    AnimeCount,
+    BookCount,
+    PodcastCount,
+    PodcastDuration,
+    MangaCount,
+    MovieCount,
+    MovieDuration,
+    ShowCount,
+    ShowDuration,
+    VideoGameCount,
+    VisualNovelCount,
+    TotalCount,
     /// DEV: in minutes
     TotalDuration,
+    MostActiveHour,
+    LeastActiveHour,
 }
 
 #[async_trait::async_trait]
@@ -37,48 +68,37 @@ impl MigrationTrait for Migration {
                             .col(DailyUserActivity::Date)
                             .col(DailyUserActivity::UserId),
                     )
+                    .col(integer_not_null(DailyUserActivity::ReviewCount))
+                    .col(integer_not_null(DailyUserActivity::WorkoutCount))
+                    .col(integer_not_null(DailyUserActivity::WorkoutDuration))
+                    .col(integer_not_null(DailyUserActivity::MeasurementCount))
+                    .col(integer_not_null(DailyUserActivity::AudioBookCount))
+                    .col(integer_not_null(DailyUserActivity::AudioBookDuration))
+                    .col(integer_not_null(DailyUserActivity::AnimeCount))
+                    .col(integer_not_null(DailyUserActivity::BookCount))
+                    .col(integer_not_null(DailyUserActivity::PodcastCount))
+                    .col(integer_not_null(DailyUserActivity::PodcastDuration))
+                    .col(integer_not_null(DailyUserActivity::MangaCount))
+                    .col(integer_not_null(DailyUserActivity::MovieCount))
+                    .col(integer_not_null(DailyUserActivity::MovieDuration))
+                    .col(integer_not_null(DailyUserActivity::ShowCount))
+                    .col(integer_not_null(DailyUserActivity::ShowDuration))
+                    .col(integer_not_null(DailyUserActivity::VideoGameCount))
+                    .col(integer_not_null(DailyUserActivity::VisualNovelCount))
                     .col(
-                        ColumnDef::new(DailyUserActivity::MetadataCounts)
-                            .json_binary()
-                            .not_null()
-                            .default("[]"),
-                    )
-                    .col(
-                        ColumnDef::new(DailyUserActivity::HourCounts)
-                            .json_binary()
-                            .not_null()
-                            .default("[]"),
-                    )
-                    .col(
-                        ColumnDef::new(DailyUserActivity::ReviewCounts)
+                        ColumnDef::new(DailyUserActivity::TotalCount)
                             .integer()
                             .not_null()
-                            .default(0),
-                    )
-                    .col(
-                        ColumnDef::new(DailyUserActivity::WorkoutCounts)
-                            .integer()
-                            .not_null()
-                            .default(0),
-                    )
-                    .col(
-                        ColumnDef::new(DailyUserActivity::MeasurementCounts)
-                            .integer()
-                            .not_null()
-                            .default(0),
-                    )
-                    .col(
-                        ColumnDef::new(DailyUserActivity::TotalCounts)
-                            .integer()
-                            .not_null()
-                            .default(0),
+                            .extra(DAILY_USER_ACTIVITY_TOTAL_COUNTS_GENERATED_SQL),
                     )
                     .col(
                         ColumnDef::new(DailyUserActivity::TotalDuration)
                             .integer()
                             .not_null()
-                            .default(0),
+                            .extra(DAILY_USER_ACTIVITY_TOTAL_DURATION_GENERATED_SQL),
                     )
+                    .col(ColumnDef::new(DailyUserActivity::MostActiveHour).integer())
+                    .col(ColumnDef::new(DailyUserActivity::LeastActiveHour).integer())
                     .foreign_key(
                         ForeignKey::create()
                             .name("daily_user_activity_to_user_foreign_key")
