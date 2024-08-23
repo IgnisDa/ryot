2b64e6e8487521979670a3ec070ea754bc2c9563
Diptesh Choudhuri <ignisda2001@gmail.com>
2024-08-23 22:12:50 +0530

    chore(statistics): remove models

--- crates/services/statistics/src/lib.rs
+++ crates/services/statistics/src/lib.rs
@@ -1,16 +1,11 @@
 use std::{collections::HashMap, fmt::Write, pin::Pin};
 
 use async_graphql::Result;
-use chrono::Utc;
-use common_models::UserSummaryData;
 use common_utils::{convert_naive_to_utc, ryot_log};
 use database_models::{
     daily_user_activity, metadata,
-    prelude::{
-        DailyUserActivity, Metadata, Review, Seen, UserMeasurement, UserSummary, UserToEntity,
-        Workout,
-    },
-    review, seen, user_measurement, user_summary, user_to_entity, workout,
+    prelude::{DailyUserActivity, Metadata, Review, Seen, UserMeasurement, UserSummary, Workout},
+    review, seen, user_measurement, user_summary, workout,
 };
 use dependent_models::DailyUserActivitiesResponse;
 use enums::{EntityLot, MediaLot, SeenState};
@@ -22,12 +17,12 @@ use media_models::{
     SeenPodcastExtraInformation, SeenShowExtraInformation, ShowSpecifics, VideoGameSpecifics,
     VisualNovelSpecifics,
 };
-use rust_decimal::{prelude::ToPrimitive, Decimal};
+use rust_decimal::prelude::ToPrimitive;
 use sea_orm::{
     prelude::{Date, DateTimeUtc, Expr},
-    sea_query::{Alias, Func, OnConflict},
+    sea_query::{Alias, Func},
     ActiveModelTrait, ActiveValue, ColumnTrait, DatabaseConnection, DbErr, EntityTrait,
-    FromQueryResult, Iden, PaginatorTrait, QueryFilter, QueryOrder, QuerySelect, QueryTrait,
+    FromQueryResult, Iden, QueryFilter, QueryOrder, QuerySelect, QueryTrait,
 };
 use serde::{Deserialize, Serialize};
 
@@ -293,321 +288,6 @@ impl StatisticsService {
     }
 
     #[tracing::instrument(skip(self))]
-    async fn calculate_user_summary(
-        &self,
-        user_id: &String,
-        calculate_from_beginning: bool,
-    ) -> Result<()> {
-        let (mut ls, start_from) = match calculate_from_beginning {
-            true => {
-                UserToEntity::update_many()
-                    .filter(user_to_entity::Column::UserId.eq(user_id))
-                    .col_expr(
-                        user_to_entity::Column::MetadataUnitsConsumed,
-                        Expr::value(Some(0)),
-                    )
-                    .exec(&self.db)
-                    .await?;
-                (UserSummaryData::default(), None)
-            }
-            false => {
-                let here = self.latest_user_summary(user_id).await?;
-                let time = here.calculated_on;
-                (here.data, Some(time))
-            }
-        };
-
-        ryot_log!(debug, "Calculating numbers summary for user: {:?}", ls);
-
-        let metadata_num_reviews = Review::find()
-            .filter(review::Column::UserId.eq(user_id))
-            .filter(review::Column::MetadataId.is_not_null())
-            .apply_if(start_from, |query, v| {
-                query.filter(review::Column::PostedOn.gt(v))
-            })
-            .count(&self.db)
-            .await?;
-
-        ryot_log!(
-            debug,
-            "Calculated number of metadata reviews for user: {:?}",
-            metadata_num_reviews
-        );
-
-        let person_num_reviews = Review::find()
-            .filter(review::Column::UserId.eq(user_id))
-            .filter(review::Column::PersonId.is_not_null())
-            .apply_if(start_from, |query, v| {
-                query.filter(review::Column::PostedOn.gt(v))
-            })
-            .count(&self.db)
-            .await?;
-
-        ryot_log!(
-            debug,
-            "Calculated number of person reviews for user: {:?}",
-            person_num_reviews
-        );
-
-        let num_measurements = UserMeasurement::find()
-            .filter(user_measurement::Column::UserId.eq(user_id))
-            .apply_if(start_from, |query, v| {
-                query.filter(user_measurement::Column::Timestamp.gt(v))
-            })
-            .count(&self.db)
-            .await?;
-
-        ryot_log!(
-            debug,
-            "Calculated number measurements for user: {:?}",
-            num_measurements
-        );
-
-        let num_workouts = Workout::find()
-            .filter(workout::Column::UserId.eq(user_id))
-            .apply_if(start_from, |query, v| {
-                query.filter(workout::Column::EndTime.gt(v))
-            })
-            .count(&self.db)
-            .await?;
-
-        ryot_log!(
-            debug,
-            "Calculated number workouts for user: {:?}",
-            num_workouts
-        );
-
-        let num_metadata_interacted_with = UserToEntity::find()
-            .filter(user_to_entity::Column::UserId.eq(user_id))
-            .filter(user_to_entity::Column::MetadataId.is_not_null())
-            .apply_if(start_from, |query, v| {
-                query.filter(user_to_entity::Column::LastUpdatedOn.gt(v))
-            })
-            .count(&self.db)
-            .await?;
-
-        ryot_log!(
-            debug,
-            "Calculated number metadata interacted with for user: {:?}",
-            num_metadata_interacted_with
-        );
-
-        let num_people_interacted_with = UserToEntity::find()
-            .filter(user_to_entity::Column::UserId.eq(user_id))
-            .filter(user_to_entity::Column::PersonId.is_not_null())
-            .apply_if(start_from, |query, v| {
-                query.filter(user_to_entity::Column::LastUpdatedOn.gt(v))
-            })
-            .count(&self.db)
-            .await?;
-
-        ryot_log!(
-            debug,
-            "Calculated number people interacted with for user: {:?}",
-            num_people_interacted_with
-        );
-
-        let num_exercises_interacted_with = UserToEntity::find()
-            .filter(user_to_entity::Column::UserId.eq(user_id))
-            .filter(user_to_entity::Column::ExerciseId.is_not_null())
-            .apply_if(start_from, |query, v| {
-                query.filter(user_to_entity::Column::LastUpdatedOn.gt(v))
-            })
-            .count(&self.db)
-            .await?;
-
-        ryot_log!(
-            debug,
-            "Calculated number exercises interacted with for user: {:?}",
-            num_exercises_interacted_with
-        );
-
-        let (total_workout_time, total_workout_weight) = Workout::find()
-            .filter(workout::Column::UserId.eq(user_id))
-            .select_only()
-            .column_as(
-                Expr::cust("coalesce((sum(duration) / 60)::numeric, 0)"),
-                "minutes",
-            )
-            .column_as(
-                Expr::cust("coalesce(sum((summary -> 'total' ->> 'weight')::numeric), 0)"),
-                "weight",
-            )
-            .apply_if(start_from, |query, v| {
-                query.filter(workout::Column::EndTime.gt(v))
-            })
-            .into_tuple::<(Decimal, Decimal)>()
-            .one(&self.db)
-            .await?
-            .unwrap();
-
-        ryot_log!(
-            debug,
-            "Calculated total workout time for user: {:?}",
-            total_workout_time
-        );
-
-        ls.media.metadata_overall.reviewed += metadata_num_reviews;
-        ls.media.metadata_overall.interacted_with += num_metadata_interacted_with;
-        ls.media.people_overall.reviewed += person_num_reviews;
-        ls.media.people_overall.interacted_with += num_people_interacted_with;
-        ls.fitness.measurements_recorded += num_measurements;
-        ls.fitness.exercises_interacted_with += num_exercises_interacted_with;
-        ls.fitness.workouts.recorded += num_workouts;
-        ls.fitness.workouts.duration += total_workout_time;
-        ls.fitness.workouts.weight += total_workout_weight;
-
-        ryot_log!(debug, "Calculated numbers summary for user: {:?}", ls);
-
-        let mut seen_items = get_seen_items_stream(&self.db, user_id, start_from, false).await?;
-
-        while let Some(seen) = seen_items.try_next().await.unwrap() {
-            ryot_log!(debug, "Processing seen id {:?}", seen.seen_id);
-            let mut units_consumed = None;
-            if let Some(item) = seen.audio_book_specifics {
-                ls.unique_items.audio_books.insert(seen.metadata_id.clone());
-                if let Some(r) = item.runtime {
-                    ls.media.audio_books.runtime += r;
-                    units_consumed = Some(r);
-                }
-            } else if let Some(item) = seen.book_specifics {
-                ls.unique_items.books.insert(seen.metadata_id.clone());
-                if let Some(pg) = item.pages {
-                    ls.media.books.pages += pg;
-                    units_consumed = Some(pg);
-                }
-            } else if let Some(item) = seen.movie_specifics {
-                ls.unique_items.movies.insert(seen.metadata_id.clone());
-                if let Some(r) = item.runtime {
-                    ls.media.movies.runtime += r;
-                    units_consumed = Some(r);
-                }
-            } else if let Some(_item) = seen.anime_specifics {
-                ls.unique_items.anime.insert(seen.metadata_id.clone());
-                if let Some(s) = seen.anime_extra_information.to_owned() {
-                    if let Some(episode) = s.episode {
-                        ls.unique_items
-                            .anime_episodes
-                            .insert((seen.metadata_id.clone(), episode));
-                        units_consumed = Some(1);
-                    }
-                }
-            } else if let Some(_item) = seen.manga_specifics {
-                ls.unique_items.manga.insert(seen.metadata_id.clone());
-                if let Some(s) = seen.manga_extra_information.to_owned() {
-                    units_consumed = Some(1);
-                    if let Some(chapter) = s.chapter {
-                        ls.unique_items
-                            .manga_chapters
-                            .insert((seen.metadata_id.clone(), chapter));
-                    }
-                    if let Some(volume) = s.volume {
-                        ls.unique_items
-                            .manga_volumes
-                            .insert((seen.metadata_id.clone(), volume));
-                    }
-                }
-            } else if let Some(item) = seen.show_specifics {
-                ls.unique_items.shows.insert(seen.metadata_id.clone());
-                if let Some(s) = seen.show_extra_information.to_owned() {
-                    if let Some((season, episode)) = item.get_episode(s.season, s.episode) {
-                        if let Some(r) = episode.runtime {
-                            ls.media.shows.runtime += r;
-                            units_consumed = Some(r);
-                        }
-                        ls.unique_items.show_episodes.insert((
-                            seen.metadata_id.clone(),
-                            season.season_number,
-                            episode.episode_number,
-                        ));
-                        ls.unique_items
-                            .show_seasons
-                            .insert((seen.metadata_id.clone(), season.season_number));
-                    }
-                };
-            } else if let Some(item) = seen.podcast_specifics {
-                ls.unique_items.podcasts.insert(seen.metadata_id.clone());
-                if let Some(s) = seen.podcast_extra_information.to_owned() {
-                    if let Some(episode) = item.episode_by_number(s.episode) {
-                        if let Some(r) = episode.runtime {
-                            ls.media.podcasts.runtime += r;
-                            units_consumed = Some(r);
-                        }
-                        ls.unique_items
-                            .podcast_episodes
-                            .insert((seen.metadata_id.clone(), s.episode));
-                    }
-                }
-            } else if let Some(_item) = seen.video_game_specifics {
-                ls.unique_items.video_games.insert(seen.metadata_id.clone());
-            } else if let Some(item) = seen.visual_novel_specifics {
-                ls.unique_items
-                    .visual_novels
-                    .insert(seen.metadata_id.clone());
-                if let Some(r) = item.length {
-                    ls.media.visual_novels.runtime += r;
-                    units_consumed = Some(r);
-                }
-            };
-
-            if let Some(consumed_update) = units_consumed {
-                UserToEntity::update_many()
-                    .filter(user_to_entity::Column::UserId.eq(user_id))
-                    .filter(user_to_entity::Column::MetadataId.eq(&seen.metadata_id))
-                    .col_expr(
-                        user_to_entity::Column::MetadataUnitsConsumed,
-                        Expr::expr(Func::coalesce([
-                            Expr::col(user_to_entity::Column::MetadataUnitsConsumed).into(),
-                            Expr::val(0).into(),
-                        ]))
-                        .add(consumed_update),
-                    )
-                    .exec(&self.db)
-                    .await?;
-            }
-        }
-
-        ls.media.podcasts.played_episodes = ls.unique_items.podcast_episodes.len();
-        ls.media.podcasts.played = ls.unique_items.podcasts.len();
-
-        ls.media.shows.watched_episodes = ls.unique_items.show_episodes.len();
-        ls.media.shows.watched_seasons = ls.unique_items.show_seasons.len();
-        ls.media.shows.watched = ls.unique_items.shows.len();
-
-        ls.media.anime.episodes = ls.unique_items.anime_episodes.len();
-        ls.media.anime.watched = ls.unique_items.anime.len();
-
-        ls.media.manga.read = ls.unique_items.manga.len();
-        ls.media.manga.chapters = ls.unique_items.manga_chapters.len();
-
-        ls.media.video_games.played = ls.unique_items.video_games.len();
-        ls.media.audio_books.played = ls.unique_items.audio_books.len();
-        ls.media.books.read = ls.unique_items.books.len();
-        ls.media.movies.watched = ls.unique_items.movies.len();
-        ls.media.visual_novels.played = ls.unique_items.visual_novels.len();
-
-        let usr = UserSummary::insert(user_summary::ActiveModel {
-            data: ActiveValue::Set(ls),
-            calculated_on: ActiveValue::Set(Utc::now()),
-            user_id: ActiveValue::Set(user_id.to_owned()),
-            is_fresh: ActiveValue::Set(calculate_from_beginning),
-        })
-        .on_conflict(
-            OnConflict::column(user_summary::Column::UserId)
-                .update_columns([
-                    user_summary::Column::Data,
-                    user_summary::Column::IsFresh,
-                    user_summary::Column::CalculatedOn,
-                ])
-                .to_owned(),
-        )
-        .exec_with_returning(&self.db)
-        .await?;
-        ryot_log!(debug, "Calculated summary for user: {:?}", usr.user_id);
-        Ok(())
-    }
-
-    #[tracing::instrument(skip(self))]
     async fn calculate_user_activities(
         &self,
         user_id: &String,
@@ -789,8 +469,6 @@ impl StatisticsService {
         calculate_from_beginning: bool,
     ) -> Result<()> {
         self.calculate_user_activities(user_id, calculate_from_beginning)
-            .await?;
-        self.calculate_user_summary(user_id, calculate_from_beginning)
             .await?;
         Ok(())
     }
