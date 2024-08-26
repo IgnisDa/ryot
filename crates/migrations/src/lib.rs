66e1ea380fef715a5737f268ba096c4292e2a28b
Diptesh Choudhuri <ignisda2001@gmail.com>
2024-08-26 05:55:52 +0530

    feat(database): migration for changing structure

--- crates/migrations/src/lib.rs
+++ crates/migrations/src/lib.rs
@@ -28,6 +28,7 @@ mod m20240827_create_daily_user_activity;
 mod m20240827_zz_changes_for_daily_user_activity;
 mod m20240828_add_last_login_on_column_to_user;
 mod m20240828_zz_add_columns_to_daily_user_activity;
+mod m20240829_change_structure_for_exercise_extra_information;
 
 pub use m20230410_create_metadata::Metadata as AliasedMetadata;
 pub use m20230413_create_person::Person as AliasedPerson;
@@ -77,6 +78,7 @@ impl MigratorTrait for Migrator {
             Box::new(m20240827_zz_changes_for_daily_user_activity::Migration),
             Box::new(m20240828_add_last_login_on_column_to_user::Migration),
             Box::new(m20240828_zz_add_columns_to_daily_user_activity::Migration),
+            Box::new(m20240829_change_structure_for_exercise_extra_information::Migration),
         ]
     }
 }
---
+++ crates/migrations/src/m20240829_change_structure_for_exercise_extra_information.rs
@@ -0,0 +1,55 @@
+use sea_orm_migration::prelude::*;
+
+#[derive(DeriveMigrationName)]
+pub struct Migration;
+
+#[async_trait::async_trait]
+impl MigrationTrait for Migration {
+    async fn up(&self, manager: &SchemaManager) -> Result<(), DbErr> {
+        let db = manager.get_connection();
+        db.execute_unprepared(
+            r#"
+DO $$
+DECLARE
+    rec RECORD;
+    new_history JSONB;
+    best_set JSONB;
+    history_element JSONB;
+BEGIN
+    -- Loop through each row in the user_to_entity table where exercise_extra_information is not null
+    FOR rec IN
+        SELECT id, exercise_extra_information
+        FROM "user_to_entity"
+        WHERE exercise_extra_information IS NOT NULL
+    LOOP
+        -- Initialize an empty array to store the new history with best_set
+        new_history := '[]'::jsonb;
+
+        -- Loop through each history element in exercise_extra_information
+        FOR history_element IN
+            SELECT * FROM jsonb_array_elements(rec.exercise_extra_information->'history')
+        LOOP
+            -- Build the new history element with the value
+            new_history := new_history || jsonb_build_object('value', history_element);
+        END LOOP;
+
+        -- Update the exercise_extra_information field with the new history format
+        UPDATE "user_to_entity"
+        SET exercise_extra_information = jsonb_set(
+            exercise_extra_information,
+            '{history}',
+            new_history
+        )
+        WHERE id = rec.id;
+    END LOOP;
+END $$;
+"#,
+        )
+        .await?;
+        Ok(())
+    }
+
+    async fn down(&self, _manager: &SchemaManager) -> Result<(), DbErr> {
+        Ok(())
+    }
+}
