aa2d52e23bc85c6ba918327a4208e6fadcfbaf2d
Diptesh Choudhuri <ignisda2001@gmail.com>
2024-09-29 00:33:23 +0530

    feat(frontend): start handling notification clicks

--- apps/frontend/app/entry.worker.ts
+++ apps/frontend/app/entry.worker.ts
@@ -5,13 +5,13 @@ export type {};
 declare let self: ServiceWorkerGlobalScope;
 
 self.addEventListener("install", (event) => {
-	console.log("Service worker installed");
-
 	event.waitUntil(self.skipWaiting());
 });
 
 self.addEventListener("activate", (event) => {
-	console.log("Service worker activated");
-
 	event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener("notificationclick", (event) => {
+	event.notification.close();
 });
