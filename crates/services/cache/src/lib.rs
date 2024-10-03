45a2c9270e75d0b931184eac3e5c482dcf52b1c0
Diptesh Choudhuri <ignisda2001@gmail.com>
2024-10-03 21:43:01 +0530

    feat(services/cache): function to update cache

--- crates/services/cache/src/lib.rs
+++ crates/services/cache/src/lib.rs
@@ -1,4 +1,10 @@
-use sea_orm::DatabaseConnection;
+use async_graphql::Result;
+use chrono::{Duration, Utc};
+use common_models::ApplicationCacheKey;
+use database_models::{application_cache, prelude::ApplicationCache};
+use sea_orm::{ActiveValue, DatabaseConnection, EntityTrait};
+use sea_query::OnConflict;
+use uuid::Uuid;
 
 pub struct CacheService {
     db: DatabaseConnection,
@@ -10,4 +16,28 @@ impl CacheService {
     }
 }
 
-impl CacheService {}
+impl CacheService {
+    pub async fn set_with_expiry(
+        &self,
+        key: ApplicationCacheKey,
+        expiry_seconds: i64,
+    ) -> Result<Uuid> {
+        let to_insert = application_cache::ActiveModel {
+            key: ActiveValue::Set(key),
+            expires_at: ActiveValue::Set(Some(Utc::now() + Duration::seconds(expiry_seconds))),
+            ..Default::default()
+        };
+        let inserted = ApplicationCache::insert(to_insert)
+            .on_conflict(
+                OnConflict::column(application_cache::Column::Key)
+                    .update_columns([
+                        application_cache::Column::ExpiresAt,
+                        application_cache::Column::CreatedAt,
+                    ])
+                    .to_owned(),
+            )
+            .exec(&self.db)
+            .await?;
+        Ok(inserted.last_insert_id)
+    }
+}
