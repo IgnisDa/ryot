16f7955e31d5ff3d4c24c25d1ed5def4ab6b74e6
Diptesh Choudhuri <ignisda2001@gmail.com>
2024-10-06 11:18:59 +0530

    feat(migrations): add empty watch providers for other media lot too

--- crates/migrations/src/m20241006_changes_for_issue_1056.rs
+++ crates/migrations/src/m20241006_changes_for_issue_1056.rs
@@ -1,3 +1,5 @@
+use enums::MediaLot;
+use sea_orm::Iterable;
 use sea_orm_migration::prelude::*;
 
 #[derive(DeriveMigrationName)]
@@ -23,6 +25,27 @@ SET preferences = jsonb_set(
 "#,
         )
         .await?;
+        for lot in MediaLot::iter() {
+            if lot == MediaLot::Movie {
+                continue;
+            }
+            db.execute_unprepared(&format!(
+                r#"
+UPDATE "user"
+SET preferences = jsonb_set(
+    preferences,
+    '{{general,watch_providers}}',
+    preferences -> 'general' -> 'watch_providers' || jsonb_build_array(
+        jsonb_build_object(
+            'lot', '{lot}',
+            'values', '[]'
+        )
+    )
+);
+"#,
+            ))
+            .await?;
+        }
         Ok(())
     }
 
