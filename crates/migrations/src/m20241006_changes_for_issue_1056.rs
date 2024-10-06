1bdeba501df1b8bd91718fff30ab84a858cc2faa
Diptesh Choudhuri <ignisda2001@gmail.com>
2024-10-06 11:56:53 +0530

    chore(migrations): do not insert useless values

--- crates/migrations/src/m20241006_changes_for_issue_1056.rs
+++ crates/migrations/src/m20241006_changes_for_issue_1056.rs
@@ -1,5 +1,4 @@
 use enums::MediaLot;
-use sea_orm::Iterable;
 use sea_orm_migration::prelude::*;
 
 #[derive(DeriveMigrationName)]
@@ -26,24 +25,6 @@ SET preferences = jsonb_set(
             MediaLot::Movie
         ))
         .await?;
-        for lot in MediaLot::iter() {
-            if lot == MediaLot::Movie {
-                continue;
-            }
-            db.execute_unprepared(&format!(
-                r#"
-UPDATE "user"
-SET preferences = jsonb_set(
-    preferences,
-    '{{general,watch_providers}}',
-    preferences -> 'general' -> 'watch_providers' || jsonb_build_array(
-        jsonb_build_object('lot', '{lot}','values', jsonb '[]')
-    )
-);
-"#,
-            ))
-            .await?;
-        }
         Ok(())
     }
 
