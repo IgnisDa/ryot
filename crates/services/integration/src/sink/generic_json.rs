c6c7dc7ca5f612f5653b792d9f80f5c7daffa43a
Diptesh Choudhuri <ignisda2001@gmail.com>
2024-10-09 13:36:56 +0530

    fix(backend): specify correct attribute

--- crates/services/integration/src/sink/generic_json.rs
+++ crates/services/integration/src/sink/generic_json.rs
@@ -17,9 +17,9 @@ impl GenericJsonIntegration {
         Ok(ImportResult {
             media: payload.media.unwrap_or_default(),
             people: payload.people.unwrap_or_default(),
-            workouts: payload.workouts.unwrap_or_default(),
             measurements: payload.measurements.unwrap_or_default(),
             media_groups: payload.media_groups.unwrap_or_default(),
+            application_workouts: payload.workouts.unwrap_or_default(),
             ..Default::default()
         })
     }
