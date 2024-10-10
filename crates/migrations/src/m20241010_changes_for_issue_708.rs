71c44a4147c3c42d71d4add1139cb3c51d4d5665
Diptesh Choudhuri <ignisda2001@gmail.com>
2024-10-10 22:09:43 +0530

    feat(migrations): delete all metadata to person

---
+++ crates/migrations/src/m20241010_changes_for_issue_708.rs
@@ -0,0 +1,18 @@
+use sea_orm_migration::prelude::*;
+
+#[derive(DeriveMigrationName)]
+pub struct Migration;
+
+#[async_trait::async_trait]
+impl MigrationTrait for Migration {
+    async fn up(&self, manager: &SchemaManager) -> Result<(), DbErr> {
+        let db = manager.get_connection();
+        db.execute_unprepared(r#"DELETE FROM "metadata_to_person";"#)
+            .await?;
+        Ok(())
+    }
+
+    async fn down(&self, _manager: &SchemaManager) -> Result<(), DbErr> {
+        Ok(())
+    }
+}
