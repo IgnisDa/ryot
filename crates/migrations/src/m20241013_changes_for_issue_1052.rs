4f85a5f4ebb96a433666df219dbd39dacf712000
Diptesh Choudhuri <ignisda2001@gmail.com>
2024-10-18 07:47:37 +0530

    feat(migrations): add correct values for supersets for existing workouts

--- crates/migrations/src/m20241013_changes_for_issue_1052.rs
+++ crates/migrations/src/m20241013_changes_for_issue_1052.rs
@@ -57,6 +57,72 @@ SET "summary" = jsonb_set(
                     "#,
             ))
             .await?;
+            db.execute_unprepared(&format!(
+            r#"
+DO $$
+DECLARE
+    workout_row RECORD;
+    exercise_row jsonb;
+    superset_with jsonb;
+    superset_array jsonb := '[]'::jsonb;
+    color_array text[] := ARRAY['red', 'pink', 'gray', 'teal', 'orange', 'yellow'];
+    color_index int := 1;
+    superset_exercises text[];
+    exercise_identifier text;
+    new_supersets jsonb;
+BEGIN
+    FOR workout_row IN SELECT id, information FROM "{x}"
+    LOOP
+        superset_array := '[]'::jsonb;
+        color_index := 1;
+        new_supersets := '[]'::jsonb;
+
+        FOR exercise_row IN SELECT * FROM jsonb_array_elements(workout_row.information->'exercises')
+        LOOP
+            IF jsonb_array_length(exercise_row->'superset_with') > 0 THEN
+                superset_exercises := array_append(superset_exercises, exercise_row->>'identifier');
+
+                FOR superset_with IN SELECT * FROM jsonb_array_elements(exercise_row->'superset_with')
+                LOOP
+                    superset_exercises := array_append(superset_exercises, (workout_row.information->'exercises'->(superset_with::int))->>'identifier');
+                END LOOP;
+
+                superset_exercises := array(
+                    SELECT unnest(superset_exercises) ORDER BY 1
+                );
+
+                IF NOT EXISTS (
+                    SELECT 1
+                    FROM jsonb_array_elements(new_supersets) AS existing_superset
+                    WHERE (existing_superset->'exercises')::text = to_jsonb(superset_exercises)::text
+                ) THEN
+                    superset_array := jsonb_build_object(
+                        'color', color_array[color_index],
+                        'exercises', to_jsonb(superset_exercises),
+                        'identifier', gen_random_uuid()
+                    );
+
+                    color_index := (color_index + 1) % array_length(color_array, 1);
+
+                    new_supersets := new_supersets || superset_array;
+                END IF;
+
+                superset_exercises := ARRAY[]::text[];
+            END IF;
+        END LOOP;
+
+        UPDATE "{x}"
+        SET information = jsonb_set(
+            workout_row.information,
+            '{{supersets}}',
+            new_supersets
+        )
+        WHERE id = workout_row.id;
+    END LOOP;
+END $$;
+    "#
+        ))
+        .await?;
         }
         db.execute_unprepared(
             r#"
@@ -102,13 +168,6 @@ BEGIN
 END $$;
         "#,
         )
-        .await?;
-        db.execute_unprepared(&format!(
-            r#"
-UPDATE "workout" SET "information" = jsonb_set("information", '{{supersets}}', '[]'::jsonb);
-UPDATE "workout_template" SET "information" = jsonb_set("information", '{{supersets}}', '[]'::jsonb);
-    "#
-        ))
         .await?;
         Ok(())
     }
