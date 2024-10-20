6ddf71ae8c8e854d9b2b4e6b656c9d87ec539a39
Diptesh Choudhuri <ignisda2001@gmail.com>
2024-10-20 22:09:34 +0530

    refactor(backend): extract authentication stuff to new utils crate

--- crates/services/importer/src/jellyfin.rs
+++ crates/services/importer/src/jellyfin.rs
@@ -1,116 +1,27 @@
 use async_graphql::Result;
-use common_utils::{ryot_log, APPLICATION_JSON_HEADER, USER_AGENT_STR};
+use common_utils::ryot_log;
 use dependent_models::ImportResult;
 use enum_meta::HashMap;
 use enums::{MediaLot, MediaSource};
+use external_utils::jellyfin::{get_authenticated_client, ItemResponse, ItemsResponse, MediaType};
 use media_models::{
     DeployUrlAndKeyAndUsernameImportInput, ImportOrExportMediaItem, ImportOrExportMediaItemSeen,
 };
-use reqwest::{
-    header::{HeaderMap, HeaderValue, ACCEPT, AUTHORIZATION, USER_AGENT},
-    Client, ClientBuilder,
-};
-use sea_orm::prelude::DateTimeUtc;
-use serde::{Deserialize, Serialize};
 use serde_json::json;
 
 use super::{ImportFailStep, ImportFailedItem};
 
-#[derive(Debug, Serialize, Deserialize, Clone, Eq, PartialEq)]
-enum MediaType {
-    Movie,
-    Series,
-    Episode,
-    #[serde(untagged)]
-    Unknown(String),
-}
-
-#[derive(Serialize, Deserialize, Debug, Clone)]
-#[serde(rename_all = "PascalCase")]
-struct ItemProviderIdsPayload {
-    tmdb: Option<String>,
-}
-
-#[derive(Serialize, Deserialize, Debug, Clone)]
-#[serde(rename_all = "PascalCase")]
-struct ItemUserData {
-    last_played_date: Option<DateTimeUtc>,
-    is_favorite: Option<bool>,
-}
-
-#[derive(Serialize, Deserialize, Debug, Clone)]
-#[serde(rename_all = "PascalCase")]
-struct ItemResponse {
-    id: String,
-    name: String,
-    #[serde(rename = "Type")]
-    type_: Option<MediaType>,
-    index_number: Option<i32>,
-    series_id: Option<String>,
-    series_name: Option<String>,
-    user_data: Option<ItemUserData>,
-    parent_index_number: Option<i32>,
-    provider_ids: Option<ItemProviderIdsPayload>,
-}
-
-#[derive(Serialize, Deserialize, Debug, Clone)]
-#[serde(rename_all = "PascalCase")]
-struct ItemsResponse {
-    items: Vec<ItemResponse>,
-}
-
-#[derive(Serialize, Deserialize, Debug, Clone)]
-#[serde(rename_all = "PascalCase")]
-struct AuthenticateResponse {
-    user: ItemResponse,
-    access_token: String,
-}
-
 pub async fn import(input: DeployUrlAndKeyAndUsernameImportInput) -> Result<ImportResult> {
-    let mut emby_header_value =
-        r#"MediaBrowser , Client="other", Device="script", DeviceId="script", Version="0.0.0""#
-            .to_string();
-    let uri = format!("{}/Users/AuthenticateByName", input.api_url);
-    let client = Client::new();
-    let authenticate = client
-        .post(uri)
-        .header(AUTHORIZATION, &emby_header_value)
-        .json(&serde_json::json!({ "Username": input.username, "Pw": input.password }))
-        .send()
-        .await
-        .unwrap()
-        .json::<AuthenticateResponse>()
-        .await
-        .unwrap();
-    ryot_log!(
-        debug,
-        "Authenticated with token: {}",
-        authenticate.access_token
-    );
-
-    emby_header_value.push_str(&format!(r#", Token="{}""#, authenticate.access_token));
-
-    let mut headers = HeaderMap::new();
-    headers.insert(USER_AGENT, HeaderValue::from_static(USER_AGENT_STR));
-    headers.insert(ACCEPT, APPLICATION_JSON_HEADER.clone());
-    headers.insert(
-        AUTHORIZATION,
-        HeaderValue::from_str(&emby_header_value).unwrap(),
-    );
-    let url = input.api_url;
-    let client: Client = ClientBuilder::new()
-        .default_headers(headers)
-        .build()
-        .unwrap();
-    let user_id = authenticate.user.id;
-    ryot_log!(debug, "Authenticated as user id: {}", user_id);
-
     let mut to_handle_media = vec![];
     let mut failed_items = vec![];
 
+    let base_url = input.api_url;
+    let (client, user_id) =
+        get_authenticated_client(&base_url, input.username, input.password).await?;
+
     let query = json!({ "recursive": true, "IsPlayed": true, "fields": "ProviderIds" });
     let library_data = client
-        .get(&format!("{}/Users/{}/Items", url, user_id))
+        .get(&format!("{}/Users/{}/Items", base_url, user_id))
         .query(&query)
         .send()
         .await
@@ -131,7 +42,7 @@ pub async fn import(input: DeployUrlAndKeyAndUsernameImportInput) -> Result<Impo
                     let mut tmdb_id = series_id_to_tmdb_id.get(&series_id).cloned().flatten();
                     if tmdb_id.is_none() {
                         let details = client
-                            .get(&format!("{}/Items/{}", url, series_id))
+                            .get(&format!("{}/Items/{}", base_url, series_id))
                             .send()
                             .await
                             .unwrap()
