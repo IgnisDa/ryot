8a39099aad2cbbacd5b7dfcad94cd95f9acf0d9f
Diptesh Choudhuri <ignisda2001@gmail.com>
2024-10-26 14:51:56 +0530

    feat(website): start endpoint for trigger to invalidate users with expired subscriptions

--- apps/website/app/routes/cron-trigger.tsx
+++ apps/website/app/routes/cron-trigger.tsx
@@ -1,5 +1,15 @@
 import type { ActionFunctionArgs } from "@remix-run/node";
+import { and, isNotNull, ne } from "drizzle-orm";
+import { customers } from "~/drizzle/schema.server";
+import { db } from "~/lib/config.server";
 
-export const action = (_args: ActionFunctionArgs) => {
-	return {};
+export const action = async (_args: ActionFunctionArgs) => {
+	const toCheck = await db.query.customers.findMany({
+		where: and(
+			isNotNull(customers.paddleCustomerId),
+			ne(customers.planType, "lifetime"),
+		),
+	});
+	console.log(toCheck);
+	return Response.json({ totalChecked: toCheck.length });
 };
