8b63da7d462574896bd0c94a72ec9ead1e209e05
Diptesh Choudhuri <ignisda2001@gmail.com>
2024-10-26 15:07:16 +0530

    feat(website): more trigger stuff

--- apps/website/app/routes/cron-trigger.tsx
+++ apps/website/app/routes/cron-trigger.tsx
@@ -1,7 +1,7 @@
 import type { ActionFunctionArgs } from "@remix-run/node";
 import { and, isNotNull, ne } from "drizzle-orm";
 import { customers } from "~/drizzle/schema.server";
-import { db } from "~/lib/config.server";
+import { db, getPaddleServerClient } from "~/lib/config.server";
 
 export const action = async (_args: ActionFunctionArgs) => {
 	const toCheck = await db.query.customers.findMany({
@@ -10,6 +10,16 @@ export const action = async (_args: ActionFunctionArgs) => {
 			ne(customers.planType, "lifetime"),
 		),
 	});
-	console.log(toCheck);
+	const paddleClient = getPaddleServerClient();
+	for (const customer of toCheck) {
+		if (!customer.paddleCustomerId) continue;
+		const details = await paddleClient.subscriptions
+			.list({
+				status: ["active", "trialing"],
+				customerId: [customer.paddleCustomerId],
+			})
+			.next();
+		console.log(details);
+	}
 	return Response.json({ totalChecked: toCheck.length });
 };
