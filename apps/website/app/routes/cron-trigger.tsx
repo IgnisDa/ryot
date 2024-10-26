095de4e8276aa11d5d58bd1eac3edbfcca24ebdf
Diptesh Choudhuri <ignisda2001@gmail.com>
2024-10-26 20:48:14 +0530

    feat(website): add more processing stuff

--- apps/website/app/routes/cron-trigger.tsx
+++ apps/website/app/routes/cron-trigger.tsx
@@ -1,25 +1,23 @@
 import type { ActionFunctionArgs } from "@remix-run/node";
-import { and, isNotNull, ne } from "drizzle-orm";
-import { customers } from "~/drizzle/schema.server";
+import { eq } from "drizzle-orm";
+import { customers, PlanTypes } from "~/drizzle/schema.server";
 import { db, getPaddleServerClient } from "~/lib/config.server";
 
 export const action = async (_args: ActionFunctionArgs) => {
-	const toCheck = await db.query.customers.findMany({
-		where: and(
-			isNotNull(customers.paddleCustomerId),
-			ne(customers.planType, "lifetime"),
-		),
-	});
 	const paddleClient = getPaddleServerClient();
-	for (const customer of toCheck) {
-		if (!customer.paddleCustomerId) continue;
-		const details = await paddleClient.subscriptions
-			.list({
-				status: ["active", "trialing"],
-				customerId: [customer.paddleCustomerId],
-			})
-			.next();
-		console.log(details);
-	}
-	return Response.json({ totalChecked: toCheck.length });
+	const subscriptionsRequest = paddleClient.subscriptions.list({
+		status: ["canceled", "past_due", "paused"],
+	});
+	do {
+		const details = await subscriptionsRequest.next();
+		for (const subscription of details) {
+			const customer = await db.query.customers.findFirst({
+				where: eq(customers.paddleCustomerId, subscription.customerId),
+			});
+			if (!customer) continue;
+			if (customer.planType === PlanTypes.Values.lifetime) continue;
+			console.log(customer);
+		}
+	} while (subscriptionsRequest.hasMore);
+	return Response.json({ totalChecked: 0 });
 };
