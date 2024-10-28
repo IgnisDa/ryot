78b421f04d3bc6a9e3646d3b5cfbac90adf84d7b
Diptesh Choudhuri <ignisda2001@gmail.com>
2024-10-28 19:02:30 +0530

    chore(website): regenerate sql

--- apps/website/app/drizzle/migrations/0003_same_gorilla_man.sql
+++ apps/website/app/drizzle/migrations/0003_same_gorilla_man.sql
@@ -1,2 +1,3 @@
+ALTER TYPE "public"."plan_type" ADD VALUE 'free' BEFORE 'monthly';--> statement-breakpoint
 ALTER TABLE "customer" DROP CONSTRAINT "customer_paddle_first_transaction_id_unique";--> statement-breakpoint
 ALTER TABLE "customer" DROP COLUMN IF EXISTS "paddle_first_transaction_id";
