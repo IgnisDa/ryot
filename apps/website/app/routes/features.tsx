4fd33c48500f0fe19ddaf9650637834ab6112c19
Diptesh Choudhuri <ignisda2001@gmail.com>
2024-10-29 22:21:15 +0530

    feat(website): change colors of features section

--- apps/website/app/routes/features.tsx
+++ apps/website/app/routes/features.tsx
@@ -2,15 +2,21 @@ import { logoUrl } from "~/lib/utils";
 
 export default function Page() {
 	return (
-		<section id="features" className="w-full py-32 bg-muted">
-			<div className="space-y-4 flex flex-col items-center justify-center">
-				<img src={logoUrl} alt="Ryot" className="size-28 lg:size-40" />
-				<h1 className="text-center text-2xl lg:text-3xl">
-					Think of Ryot as your second brain with superpowers ✨
-				</h1>
-				<h2 className="text-center lg:text-xl text-gray-500">
-					What all can Ryot do for you?
-				</h2>
+		<section id="features">
+			<div className="bg-muted py-32">
+				<img
+					alt="Ryot"
+					src={logoUrl}
+					className="size-28 sm:size-40 mx-auto mb-10"
+				/>
+				<div className="space-y-4">
+					<h1 className="text-center text-2xl sm:text-3xl">
+						Think of Ryot as your second brain with superpowers ✨
+					</h1>
+					<h2 className="text-center sm:text-xl text-gray-500">
+						What all can Ryot do for you?
+					</h2>
+				</div>
 			</div>
 		</section>
 	);
