29cfd4a6c4224b8b86809b382b85d8c6ac950621
Diptesh Choudhuri <ignisda2001@gmail.com>
2024-11-02 18:57:40 +0530

    feat(frontend): generate new layout for calendar

--- apps/frontend/app/routes/_dashboard.calendar_new.tsx
+++ apps/frontend/app/routes/_dashboard.calendar_new.tsx
@@ -1,7 +1,9 @@
-import { Container } from "@mantine/core";
+import { Container, Paper, SimpleGrid, Stack, Text } from "@mantine/core";
 import type { LoaderFunctionArgs, MetaArgs } from "@remix-run/node";
 import { useLoaderData } from "@remix-run/react";
 import { UserCalendarEventsDocument } from "@ryot/generated/graphql/backend/graphql";
+import { sum } from "@ryot/ts-utils";
+import { Fragment } from "react/jsx-runtime";
 import { z } from "zod";
 import { zx } from "zodix";
 import { dayjsLib } from "~/lib/generals";
@@ -22,25 +24,86 @@ export const loader = async ({ request }: LoaderFunctionArgs) => {
 	await redirectUsingEnhancedCookieSearchParams(request, cookieName);
 	const query = zx.parseQuery(request, searchParamsSchema);
 	const date = dayjsLib(query.date);
+	const month = date.month() + 1;
+	const year = date.year();
 	const [{ userCalendarEvents }] = await Promise.all([
 		serverGqlService.authenticatedRequest(request, UserCalendarEventsDocument, {
-			input: { month: date.month() + 1, year: date.year() },
+			input: { month, year },
 		}),
 	]);
-	return { query, userCalendarEvents, cookieName };
+	return { userCalendarEvents, cookieName, month, year };
 };
 
 export const meta = (_args: MetaArgs<typeof loader>) => {
 	return [{ title: "Calendar | Ryot" }];
 };
 
+const weekdays = dayjsLib().localeData().weekdays();
+
+const generateCalendar = (year: number, month: number) => {
+	const startDate = dayjsLib(`${year}-${month.toString().padStart(2, "0")}-01`);
+	const daysInMonth = startDate.daysInMonth();
+	const startDayOfWeek = startDate.day();
+
+	const calendar = Array<(number | null)[]>();
+	let week = Array(7).fill(null);
+	let dayCounter = 1;
+
+	for (let i = startDayOfWeek; i < 7; i++)
+		if (dayCounter <= daysInMonth) week[i] = dayCounter++;
+	calendar.push([...week]);
+
+	while (dayCounter <= daysInMonth) {
+		week = Array(7).fill(null);
+		for (let i = 0; i < 7 && dayCounter <= daysInMonth; i++)
+			week[i] = dayCounter++;
+		calendar.push([...week]);
+	}
+
+	return calendar;
+};
+
 export default function Page() {
 	const loaderData = useLoaderData<typeof loader>();
+	const calendar = generateCalendar(loaderData.year, loaderData.month);
 
 	return (
-		<Container size="xs">
-			Hello world to the calendar
-			<div>{JSON.stringify(loaderData, null, 4)}</div>
+		<Container size="sm">
+			<Stack>
+				<SimpleGrid cols={7} spacing={{ base: "xs", md: "md" }}>
+					{weekdays.map((wkd) => (
+						<Paper
+							py={2}
+							key={wkd}
+							radius="lg"
+							withBorder
+							px={{ base: 6, md: "md" }}
+						>
+							<Text fz={{ base: "xs", md: "lg" }} ta="center">
+								{wkd.slice(0, 3)}
+							</Text>
+						</Paper>
+					))}
+				</SimpleGrid>
+				{calendar.map((week) => (
+					<SimpleGrid cols={7} key={sum(week)}>
+						{week.map((day, index) => (
+							<Fragment key={index.toString()}>
+								<Paper
+									withBorder={day !== null}
+									p={{ base: 6, md: "md" }}
+									radius={day === null ? undefined : "lg"}
+									shadow={day === null ? undefined : "xl"}
+								>
+									<Text ta="center" fz={{ base: "xs", md: "lg" }}>
+										{day}
+									</Text>
+								</Paper>
+							</Fragment>
+						))}
+					</SimpleGrid>
+				))}
+			</Stack>
 		</Container>
 	);
 }
