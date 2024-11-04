e75f765dca602f1f7e6e880f5ae6073caf2756e0
Diptesh Choudhuri <ignisda2001@gmail.com>
2024-11-04 09:17:55 +0530

    fix(frontend): use correct schema for creating integrations

--- apps/frontend/app/routes/_dashboard.settings.integrations.tsx
+++ apps/frontend/app/routes/_dashboard.settings.integrations.tsx
@@ -55,6 +55,7 @@ import { confirmWrapper } from "~/components/confirmation";
 import {
 	PRO_REQUIRED_MESSAGE,
 	applicationBaseUrl,
+	commaDelimitedString,
 	dayjsLib,
 } from "~/lib/generals";
 import {
@@ -163,11 +164,34 @@ const MINIMUM_PROGRESS = "2";
 const MAXIMUM_PROGRESS = "95";
 
 const createSchema = z.object({
-	providerSpecifics: z.any().optional(),
 	minimumProgress: z.string().optional(),
 	maximumProgress: z.string().optional(),
 	provider: z.nativeEnum(IntegrationProvider),
 	syncToOwnedCollection: zx.CheckboxAsString.optional(),
+	providerSpecifics: z
+		.object({
+			plexUsername: z.string().optional(),
+			audiobookshelfBaseUrl: z.string().optional(),
+			audiobookshelfToken: z.string().optional(),
+			komgaBaseUrl: z.string().optional(),
+			komgaUsername: z.string().optional(),
+			komgaPassword: z.string().optional(),
+			komgaProvider: z.nativeEnum(MediaSource).optional(),
+			radarrBaseUrl: z.string().optional(),
+			radarrApiKey: z.string().optional(),
+			radarrProfileId: z.number().optional(),
+			radarrRootFolderPath: z.string().optional(),
+			radarrSyncCollectionIds: commaDelimitedString,
+			sonarrBaseUrl: z.string().optional(),
+			sonarrApiKey: z.string().optional(),
+			sonarrProfileId: z.number().optional(),
+			sonarrRootFolderPath: z.string().optional(),
+			sonarrSyncCollectionIds: commaDelimitedString,
+			jellyfinPushBaseUrl: z.string().optional(),
+			jellyfinPushUsername: z.string().optional(),
+			jellyfinPushPassword: z.string().optional(),
+		})
+		.optional(),
 });
 
 const deleteSchema = z.object({
