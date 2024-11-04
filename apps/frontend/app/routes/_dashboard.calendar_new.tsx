b5b7e43ede0df65755b735c331ffc5ef9552f7c0
Diptesh Choudhuri <ignisda2001@gmail.com>
2024-11-04 09:11:07 +0530

    feat(frontend): start with new calendar page

--- apps/frontend/app/routes/_dashboard.calendar_new.tsx
+++ apps/frontend/app/routes/_dashboard.calendar_new.tsx
@@ -1,12 +1,19 @@
-import { Container, Paper, SimpleGrid, Stack, Text } from "@mantine/core";
+import {
+	ActionIcon,
+	Button,
+	Container,
+	Group,
+	Stack,
+	Title,
+} from "@mantine/core";
 import type { LoaderFunctionArgs, MetaArgs } from "@remix-run/node";
 import { useLoaderData } from "@remix-run/react";
 import { UserCalendarEventsDocument } from "@ryot/generated/graphql/backend/graphql";
-import { sum } from "@ryot/ts-utils";
-import { Fragment } from "react/jsx-runtime";
+import { IconChevronLeft, IconChevronRight } from "@tabler/icons-react";
 import { z } from "zod";
 import { zx } from "zodix";
 import { dayjsLib } from "~/lib/generals";
+import { useAppSearchParam } from "~/lib/hooks";
 import {
 	getEnhancedCookieName,
 	redirectUsingEnhancedCookieSearchParams,
@@ -24,85 +31,52 @@ export const loader = async ({ request }: LoaderFunctionArgs) => {
 	await redirectUsingEnhancedCookieSearchParams(request, cookieName);
 	const query = zx.parseQuery(request, searchParamsSchema);
 	const date = dayjsLib(query.date);
-	const month = date.month() + 1;
-	const year = date.year();
 	const [{ userCalendarEvents }] = await Promise.all([
 		serverGqlService.authenticatedRequest(request, UserCalendarEventsDocument, {
-			input: { month, year },
+			input: { month: date.month() + 1, year: date.year() },
 		}),
 	]);
-	return { userCalendarEvents, cookieName, month, year };
+	return { userCalendarEvents, cookieName, query };
 };
 
 export const meta = (_args: MetaArgs<typeof loader>) => {
 	return [{ title: "Calendar | Ryot" }];
 };
 
-const weekdays = dayjsLib().localeData().weekdays();
-
-const generateCalendar = (year: number, month: number) => {
-	const startDate = dayjsLib(`${year}-${month.toString().padStart(2, "0")}-01`);
-	const daysInMonth = startDate.daysInMonth();
-	const startDayOfWeek = startDate.day();
-
-	const calendar = Array<(number | null)[]>();
-	let week = Array(7).fill(null);
-	let dayCounter = 1;
-
-	for (let i = startDayOfWeek; i < 7; i++)
-		if (dayCounter <= daysInMonth) week[i] = dayCounter++;
-	calendar.push([...week]);
-
-	while (dayCounter <= daysInMonth) {
-		week = Array(7).fill(null);
-		for (let i = 0; i < 7 && dayCounter <= daysInMonth; i++)
-			week[i] = dayCounter++;
-		calendar.push([...week]);
-	}
-
-	return calendar;
-};
-
 export default function Page() {
 	const loaderData = useLoaderData<typeof loader>();
-	const calendar = generateCalendar(loaderData.year, loaderData.month);
+	const [_, { setP }] = useAppSearchParam(loaderData.cookieName);
+	const date = dayjsLib(loaderData.query.date);
 
 	return (
 		<Container size="sm" h="100%" mt="auto">
-			<Stack gap="sm">
-				<SimpleGrid cols={7} spacing={{ base: "xs", md: "md" }}>
-					{weekdays.map((wkd) => (
-						<Paper
-							py={2}
-							key={wkd}
-							radius="lg"
-							withBorder
-							px={{ base: 6, md: "md" }}
+			<Stack>
+				<Group justify="space-between">
+					<Title order={3} td="underline">
+						{date.format("MMMM, YYYY")}
+					</Title>
+					<Button.Group>
+						<ActionIcon
+							variant="outline"
+							onClick={() => {
+								const newMonth = date.subtract(1, "month");
+								setP("date", newMonth.toISOString());
+							}}
 						>
-							<Text fz={{ base: "xs", md: "lg" }} ta="center">
-								{wkd.slice(0, 3)}
-							</Text>
-						</Paper>
-					))}
-				</SimpleGrid>
-				{calendar.map((week) => (
-					<SimpleGrid cols={7} key={sum(week)}>
-						{week.map((day, index) => (
-							<Fragment key={index.toString()}>
-								<Paper
-									withBorder={day !== null}
-									p={{ base: 6, md: "xs" }}
-									radius={day === null ? undefined : "lg"}
-									shadow={day === null ? undefined : "xl"}
-								>
-									<Text ta="center" fz={{ base: "xs", md: "lg" }}>
-										{day}
-									</Text>
-								</Paper>
-							</Fragment>
-						))}
-					</SimpleGrid>
-				))}
+							<IconChevronLeft />
+						</ActionIcon>
+						<ActionIcon
+							variant="outline"
+							ml="xs"
+							onClick={() => {
+								const newMonth = date.add(1, "month");
+								setP("date", newMonth.toISOString());
+							}}
+						>
+							<IconChevronRight />
+						</ActionIcon>
+					</Button.Group>
+				</Group>
 			</Stack>
 		</Container>
 	);
