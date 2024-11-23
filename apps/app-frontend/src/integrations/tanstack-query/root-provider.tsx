0d637cfd9b618514c1e3523bf36ff7a6190db1e2
Diptesh Choudhuri <ignisda2001@gmail.com>
2024-11-24 00:39:17 +0530

    chore: app frontend

---
+++ apps/app-frontend/src/integrations/tanstack-query/root-provider.tsx
@@ -0,0 +1,26 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import type { ReactNode } from "react";
+
+let context: { queryClient: QueryClient } | undefined;
+
+export function getContext() {
+	if (context) {
+		return context;
+	}
+
+	const queryClient = new QueryClient();
+
+	context = { queryClient };
+
+	return context;
+}
+
+export default function TanStackQueryProvider(props: { children: ReactNode }) {
+	const { queryClient } = getContext();
+
+	return (
+		<QueryClientProvider client={queryClient}>
+			{props.children}
+		</QueryClientProvider>
+	);
+}
