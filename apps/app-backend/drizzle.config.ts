53db6147bd3746d58658e9f7efae240f65106624
Diptesh Choudhuri <ignisda2001@gmail.com>
2024-11-23 18:26:45 +0530

    feat: add drizzle configuration and update dependencies in package.json and yarn.lock

---
+++ apps/app-backend/drizzle.config.ts
@@ -0,0 +1,8 @@
+import { defineConfig } from "drizzle-kit";
+
+export default defineConfig({
+  out: "./drizzle",
+  dialect: "postgresql",
+  schema: "./src/db/schema.ts",
+  dbCredentials: { url: process.env.DATABASE_URL || "" },
+});
