fddd5a4192dcb8ec3cb8b815b471cb5665d87e97
Diptesh Choudhuri <ignisda2001@gmail.com>
2024-11-23 23:24:17 +0530

    feat: add anonymous plugin to betterAuth and reorder user schema fields

--- apps/app-backend/src/utils/auth.ts
+++ apps/app-backend/src/utils/auth.ts
@@ -1,8 +1,10 @@
 import { betterAuth } from "better-auth";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
+import { anonymous } from "better-auth/plugins";
 import { db } from "./db";
 
 export const auth = betterAuth({
+  plugins: [anonymous()],
   baseURL: process.env.FRONTEND_URL,
   emailAndPassword: { enabled: true },
   secret: process.env.SERVER_ADMIN_ACCESS_TOKEN,
