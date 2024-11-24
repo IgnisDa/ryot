614729e7bd5e2dace858f3b9c32876d7a2c52bf4
Diptesh Choudhuri <ignisda2001@gmail.com>
2024-11-24 19:38:25 +0530

    chore(migrations): remove newlines

--- crates/migrations/src/m20241124_changes_for_issue_1113.rs
+++ crates/migrations/src/m20241124_changes_for_issue_1113.rs
@@ -9,14 +9,12 @@ pub struct Migration;
 impl MigrationTrait for Migration {
     async fn up(&self, manager: &SchemaManager) -> Result<(), DbErr> {
         let db = manager.get_connection();
-        db.execute_unprepared("TRUNCATE daily_user_activity CASCADE")
+        db.execute_unprepared("TRUNCATE daily_user_activity")
             .await?;
         for col in NEW_DUA_COLUMNS {
             if !manager.has_column("daily_user_activity", col).await? {
                 db.execute_unprepared(&format!(
-                    r#"
-ALTER TABLE "daily_user_activity" ADD COLUMN "{}" TEXT[] NOT NULL DEFAULT '{{}}';
-"#,
+                    r#"ALTER TABLE "daily_user_activity" ADD COLUMN "{}" TEXT[] NOT NULL DEFAULT '{{}}'"#,
                     col,
                 ))
                 .await?;
