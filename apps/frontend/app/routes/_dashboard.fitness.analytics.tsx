9189043f4b0e9a4868491fd9afbb79ebe02595e1
Diptesh Choudhuri <ignisda2001@gmail.com>
2024-11-25 16:38:29 +0530

    feat(frontend): allow specifying custom time ranges

--- apps/frontend/app/routes/_dashboard.fitness.analytics.tsx
+++ apps/frontend/app/routes/_dashboard.fitness.analytics.tsx
@@ -2,15 +2,17 @@ import {
 	Button,
 	Container,
 	Menu,
+	Modal,
 	SimpleGrid,
 	Stack,
 	Text,
 } from "@mantine/core";
-import { DatePickerInput } from "@mantine/dates";
+import { DatePicker } from "@mantine/dates";
 import type { LoaderFunctionArgs, MetaArgs } from "@remix-run/node";
 import { useLoaderData } from "@remix-run/react";
 import { formatDateToNaiveDate } from "@ryot/ts-utils";
-import { IconCalendar } from "@tabler/icons-react";
+import { IconCalendar, IconDeviceFloppy } from "@tabler/icons-react";
+import { useState } from "react";
 import { match } from "ts-pattern";
 import { z } from "zod";
 import { zx } from "zodix";
@@ -30,6 +32,7 @@ const TIME_RANGES = [
 	"Past 30 Days",
 	"Past 6 Months",
 	"Past 12 Months",
+	"Custom",
 ] as const;
 
 const searchParamsSchema = z.object({
@@ -50,6 +53,7 @@ const getStartTime = (range: (typeof TIME_RANGES)[number]) =>
 		.with("Past 30 Days", () => dayjsLib().subtract(30, "day"))
 		.with("Past 6 Months", () => dayjsLib().subtract(6, "month"))
 		.with("Past 12 Months", () => dayjsLib().subtract(12, "month"))
+		.with("Custom", () => undefined)
 		.exhaustive();
 
 export const loader = async ({ request }: LoaderFunctionArgs) => {
@@ -58,7 +62,7 @@ export const loader = async ({ request }: LoaderFunctionArgs) => {
 	const cookieName = await getEnhancedCookieName("fitness.analytics", request);
 	await redirectUsingEnhancedCookieSearchParams(request, cookieName);
 	const startDate =
-		query.startDate || formatDateToNaiveDate(getStartTime(range));
+		query.startDate || formatDateToNaiveDate(getStartTime(range) || new Date());
 	const endDate = query.endDate || formatDateToNaiveDate(dayjsLib());
 	return { range, startDate, endDate, cookieName };
 };
@@ -69,62 +73,99 @@ export const meta = (_args: MetaArgs<typeof loader>) => {
 
 export default function Page() {
 	const loaderData = useLoaderData<typeof loader>();
-	const [_, { setP }] = useAppSearchParam(loaderData.cookieName);
+	const [_, { setP, delP }] = useAppSearchParam(loaderData.cookieName);
+	const [customRangeOpened, setCustomRangeOpened] = useState(false);
 
 	return (
-		<Container>
-			<Stack>
-				<SimpleGrid cols={{ base: 2 }} style={{ alignItems: "center" }}>
-					<Text fz={{ base: "lg", md: "h1" }} ta={{ md: "center" }} fw="bold">
-						Fitness Analytics
-					</Text>
-					<Menu position="bottom-end">
-						<Menu.Target>
-							<Button
-								w={{ md: 200 }}
-								variant="default"
-								ml={{ md: "auto" }}
-								leftSection={<IconCalendar />}
-							>
-								{loaderData.range}
-							</Button>
-						</Menu.Target>
-						<Menu.Dropdown>
-							{TIME_RANGES.map((range) => (
-								<Menu.Item
-									ta="right"
-									key={range}
-									onClick={() => setP("range", range)}
-									color={loaderData.range === range ? "blue" : undefined}
+		<>
+			<CustomDateSelectModal
+				opened={customRangeOpened}
+				onClose={() => setCustomRangeOpened(false)}
+			/>
+			<Container>
+				<Stack>
+					<SimpleGrid cols={{ base: 2 }} style={{ alignItems: "center" }}>
+						<Text fz={{ base: "lg", md: "h1" }} ta={{ md: "center" }} fw="bold">
+							Fitness Analytics
+						</Text>
+						<Menu position="bottom-end">
+							<Menu.Target>
+								<Button
+									w={{ md: 200 }}
+									variant="default"
+									ml={{ md: "auto" }}
+									leftSection={<IconCalendar />}
 								>
-									{range}
-								</Menu.Item>
-							))}
-						</Menu.Dropdown>
-					</Menu>
-				</SimpleGrid>
-				<DatePickerInput
-					w={300}
-					size="xs"
-					mx="auto"
-					type="range"
-					label="Select date range"
-					defaultValue={[
-						new Date(loaderData.startDate),
-						new Date(loaderData.endDate),
-					]}
-					onChange={([start, end]) => {
-						setP(
-							"startDate",
-							start ? formatDateToNaiveDate(start) : loaderData.startDate,
-						);
-						setP(
-							"endDate",
-							end ? formatDateToNaiveDate(end) : loaderData.endDate,
-						);
-					}}
-				/>
-			</Stack>
-		</Container>
+									{loaderData.range}
+								</Button>
+							</Menu.Target>
+							<Menu.Dropdown>
+								{TIME_RANGES.map((range) => (
+									<Menu.Item
+										ta="right"
+										key={range}
+										onClick={() => {
+											if (range === "Custom") {
+												setCustomRangeOpened(true);
+												return;
+											}
+											setP("range", range);
+											delP("startDate");
+											delP("endDate");
+										}}
+										color={loaderData.range === range ? "blue" : undefined}
+									>
+										{range}
+									</Menu.Item>
+								))}
+							</Menu.Dropdown>
+						</Menu>
+					</SimpleGrid>
+				</Stack>
+			</Container>
+		</>
 	);
 }
+
+const CustomDateSelectModal = (props: {
+	opened: boolean;
+	onClose: () => void;
+}) => {
+	const loaderData = useLoaderData<typeof loader>();
+	const [_, { setP }] = useAppSearchParam(loaderData.cookieName);
+	const [value, setValue] = useState<[Date | null, Date | null]>([
+		new Date(loaderData.startDate),
+		new Date(loaderData.endDate),
+	]);
+
+	return (
+		<Modal
+			opened={props.opened}
+			title="Select custom date range"
+			onClose={props.onClose}
+		>
+			<Stack>
+				<DatePicker
+					mx="auto"
+					size="md"
+					type="range"
+					value={value}
+					w="fit-content"
+					onChange={setValue}
+				/>
+				<Button
+					variant="default"
+					leftSection={<IconDeviceFloppy />}
+					onClick={() => {
+						setP("startDate", formatDateToNaiveDate(value[0] || new Date()));
+						setP("endDate", formatDateToNaiveDate(value[1] || new Date()));
+						setP("range", TIME_RANGES[8]);
+						props.onClose();
+					}}
+				>
+					Apply
+				</Button>
+			</Stack>
+		</Modal>
+	);
+};
