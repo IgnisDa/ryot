6315b210089b9a0eec2c2481205275f246dc15ff
Diptesh Choudhuri <ignisda2001@gmail.com>
2024-11-25 16:16:11 +0530

    feat(frontend): do most calculations on the server loader

--- apps/frontend/app/routes/_dashboard.fitness.analytics.tsx
+++ apps/frontend/app/routes/_dashboard.fitness.analytics.tsx
@@ -38,11 +38,24 @@ const searchParamsSchema = z.object({
 
 export type SearchParams = z.infer<typeof searchParamsSchema>;
 
+const getStartTime = (range: (typeof TIME_RANGES)[number]) =>
+	match(range)
+		.with("Yesterday", () => dayjsLib().subtract(1, "day"))
+		.with("This Week", () => dayjsLib().startOf("week"))
+		.with("This Month", () => dayjsLib().startOf("month"))
+		.with("This Year", () => dayjsLib().startOf("year"))
+		.with("Past 7 Days", () => dayjsLib().subtract(7, "day"))
+		.with("Past 30 Days", () => dayjsLib().subtract(30, "day"))
+		.with("Past 6 Months", () => dayjsLib().subtract(6, "month"))
+		.with("Past 12 Months", () => dayjsLib().subtract(12, "month"))
+		.exhaustive();
+
 export const loader = async ({ request }: LoaderFunctionArgs) => {
 	const cookieName = await getEnhancedCookieName("fitness.analytics", request);
 	let { range } = zx.parseQuery(request, searchParamsSchema);
+	range = range ?? "Past 30 Days";
 	await redirectUsingEnhancedCookieSearchParams(request, cookieName);
-	const startDate = formatDateToNaiveDate();
+	const startDate = formatDateToNaiveDate(getStartTime(range));
 	const endDate = formatDateToNaiveDate(dayjsLib());
 	return { range, startDate, endDate, cookieName };
 };
@@ -58,14 +71,14 @@ export default function Page() {
 	return (
 		<Container>
 			<Stack>
-				<SimpleGrid cols={{ base: 1, md: 2 }} style={{ alignItems: "center" }}>
-					<Text fz={{ base: "h2", md: "h1" }} ta="center" fw="bold">
+				<SimpleGrid cols={{ base: 2 }} style={{ alignItems: "center" }}>
+					<Text fz={{ base: "lg", md: "h1" }} ta={{ md: "center" }} fw="bold">
 						Fitness Analytics
 					</Text>
 					<Menu position="bottom-end">
 						<Menu.Target>
 							<Button
-								w={200}
+								w={{ md: 200 }}
 								variant="default"
 								ml={{ md: "auto" }}
 								leftSection={<IconCalendar />}
@@ -78,29 +91,8 @@ export default function Page() {
 								<Menu.Item
 									ta="right"
 									key={range}
-									onClick={() => {
-										const startDate = match(range)
-											.with("Yesterday", () => dayjsLib().subtract(1, "day"))
-											.with("This Week", () => dayjsLib().startOf("week"))
-											.with("This Month", () => dayjsLib().startOf("month"))
-											.with("This Year", () => dayjsLib().startOf("year"))
-											.with("Past 7 Days", () => dayjsLib().subtract(7, "day"))
-											.with("Past 30 Days", () =>
-												dayjsLib().subtract(30, "day"),
-											)
-											.with("Past 6 Months", () =>
-												dayjsLib().subtract(6, "month"),
-											)
-											.with("Past 12 Months", () =>
-												dayjsLib().subtract(12, "month"),
-											)
-											.exhaustive();
-										setP("range", range);
-										if (startDate) {
-											setP("startDate", formatDateToNaiveDate(startDate));
-											setP("endDate", formatDateToNaiveDate(dayjsLib()));
-										}
-									}}
+									onClick={() => setP("range", range)}
+									color={loaderData.range === range ? "blue" : undefined}
 								>
 									{range}
 								</Menu.Item>
