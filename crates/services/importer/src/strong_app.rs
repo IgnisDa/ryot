e2a5208fc5ac0022f7bc32822eab53f639e2a336
Diptesh Choudhuri <ignisda2001@gmail.com>
2024-12-03 22:44:54 +0530

    feat(services/importer): new strategy to import strong app exercises

--- crates/services/importer/src/strong_app.rs
+++ crates/services/importer/src/strong_app.rs
@@ -11,6 +11,7 @@ use fitness_models::{
     SetLot, UserExerciseInput, UserWorkoutInput, UserWorkoutSetRecord, WorkoutSetStatistic,
 };
 use importer_models::{ImportFailStep, ImportFailedItem};
+use indexmap::IndexMap;
 use itertools::Itertools;
 use media_models::DeployStrongAppImportInput;
 use nanoid::nanoid;
@@ -80,120 +81,133 @@ async fn import_exercises(
     } else {
         return Err("Could not determine delimiter".into());
     };
-    let mut entries_reader = ReaderBuilder::new()
+
+    let mut unique_exercises: HashMap<String, exercise::Model> = HashMap::new();
+    let entries_reader = ReaderBuilder::new()
         .delimiter(delimiter)
         .from_reader(file_string.as_bytes())
         .deserialize::<Entry>()
         .map(|r| r.unwrap())
         .collect_vec();
-    // DEV: Without this, the last workout does not get appended
-    entries_reader.push(Entry {
-        set_order: "0".to_string(),
-        date: "invalid".to_string(),
-        ..Default::default()
-    });
-    let mut unique_exercises: HashMap<String, exercise::Model> = HashMap::new();
-    let mut exercises = vec![];
-    let mut sets = vec![];
-    let mut notes = vec![];
-    for (entry, next_entry) in entries_reader.into_iter().tuple_windows() {
-        if entry.set_order == "Note" {
-            continue;
+
+    let mut workouts_to_entries = IndexMap::new();
+    for entry in entries_reader.clone() {
+        workouts_to_entries
+            .entry(entry.workout_number.clone())
+            .or_insert(vec![])
+            .push(entry);
+    }
+
+    let mut exercises_to_workouts = IndexMap::new();
+
+    for (workout_number, entries) in workouts_to_entries {
+        let mut exercises = IndexMap::new();
+        for entry in entries {
+            exercises
+                .entry(entry.exercise_name.clone())
+                .or_insert(vec![])
+                .push(entry);
         }
-        let exercise_lot = if entry.seconds.is_some() && entry.distance.is_some() {
-            ExerciseLot::DistanceAndDuration
-        } else if entry.seconds.is_some() {
-            ExerciseLot::Duration
-        } else if entry.reps.is_some() && entry.weight.is_some() {
-            ExerciseLot::RepsAndWeight
-        } else if entry.reps.is_some() {
-            ExerciseLot::Reps
-        } else {
-            failed.push(ImportFailedItem {
-                lot: None,
-                identifier: format!(
-                    "Workout #{}, Set #{}",
-                    entry.workout_number, entry.set_order
-                ),
-                step: ImportFailStep::InputTransformation,
-                error: Some(format!(
-                    "Could not determine exercise lot: {}",
-                    serde_json::to_string(&entry).unwrap()
-                )),
-            });
-            continue;
-        };
-        let existing_exercise = Exercise::find()
-            .filter(exercise::Column::Id.eq(&entry.exercise_name))
-            .filter(exercise::Column::Lot.eq(exercise_lot))
-            .one(&ss.db)
-            .await?;
-        let exercise_id = if let Some(db_ex) = existing_exercise {
-            db_ex.id
-        } else if let Some(mem_ex) = unique_exercises.get(&entry.exercise_name) {
-            mem_ex.id.clone()
-        } else {
-            let id = format!("{} [{}]", entry.exercise_name, nanoid!(5));
-            unique_exercises.insert(
-                entry.exercise_name.clone(),
-                exercise::Model {
-                    id: id.clone(),
-                    lot: exercise_lot,
-                    ..Default::default()
-                },
-            );
-            id
-        };
-        ryot_log!(debug, "Importing exercise with id = {}", exercise_id);
-        let weight = entry.weight.map(|d| if d == dec!(0) { dec!(1) } else { d });
-        sets.push(UserWorkoutSetRecord {
-            statistic: WorkoutSetStatistic {
-                weight,
-                reps: entry.reps,
-                duration: entry.seconds.and_then(|r| r.checked_div(dec!(60))),
-                distance: entry.distance.and_then(|d| d.checked_div(dec!(1000))),
-                ..Default::default()
-            },
-            note: None,
-            rest_time: None,
-            confirmed_at: None,
-            lot: SetLot::Normal,
-        });
-        if let Some(n) = entry.notes {
-            notes.push(n);
-        }
-        if next_entry.set_order <= entry.set_order {
-            exercises.push(UserExerciseInput {
-                sets,
+        exercises_to_workouts.insert(workout_number, exercises);
+    }
+
+    for (_workout_number, workout) in exercises_to_workouts {
+        let first_exercise = workout.first().unwrap().1.first().unwrap();
+        let ndt = NaiveDateTime::parse_from_str(&first_exercise.date, "%Y-%m-%d %H:%M:%S")
+            .expect("Failed to parse input string");
+        let ndt = utils::get_date_time_with_offset(ndt, &ss.timezone);
+        let workout_duration =
+            Duration::try_seconds(first_exercise.workout_duration.parse().unwrap()).unwrap();
+        let mut collected_exercises = vec![];
+        for (_exercise_name, exercises) in workout.clone() {
+            let mut collected_sets = vec![];
+            let mut notes = vec![];
+            let valid_ex = exercises.iter().find(|e| e.set_order != "Note").unwrap();
+            let exercise_lot = if valid_ex.seconds.is_some() && valid_ex.distance.is_some() {
+                ExerciseLot::DistanceAndDuration
+            } else if valid_ex.seconds.is_some() {
+                ExerciseLot::Duration
+            } else if valid_ex.reps.is_some() && valid_ex.weight.is_some() {
+                ExerciseLot::RepsAndWeight
+            } else if valid_ex.reps.is_some() {
+                ExerciseLot::Reps
+            } else {
+                failed.push(ImportFailedItem {
+                    lot: None,
+                    identifier: format!(
+                        "Workout #{}, Set #{}",
+                        valid_ex.workout_number, valid_ex.set_order
+                    ),
+                    step: ImportFailStep::InputTransformation,
+                    error: Some(format!(
+                        "Could not determine exercise lot: {}",
+                        serde_json::to_string(&valid_ex).unwrap()
+                    )),
+                });
+                continue;
+            };
+            let existing_exercise = Exercise::find()
+                .filter(exercise::Column::Id.eq(&valid_ex.exercise_name))
+                .filter(exercise::Column::Lot.eq(exercise_lot))
+                .one(&ss.db)
+                .await?;
+            let exercise_id = if let Some(db_ex) = existing_exercise {
+                db_ex.id
+            } else if let Some(mem_ex) = unique_exercises.get(&valid_ex.exercise_name) {
+                mem_ex.id.clone()
+            } else {
+                let id = format!("{} [{}]", valid_ex.exercise_name, nanoid!(5));
+                unique_exercises.insert(
+                    valid_ex.exercise_name.clone(),
+                    exercise::Model {
+                        id: id.clone(),
+                        lot: exercise_lot,
+                        ..Default::default()
+                    },
+                );
+                id
+            };
+            ryot_log!(debug, "Importing exercise with id = {}", exercise_id);
+            for sets in exercises {
+                if let Some(note) = sets.notes {
+                    notes.push(note);
+                }
+                let weight = sets.weight.map(|d| if d == dec!(0) { dec!(1) } else { d });
+                collected_sets.push(UserWorkoutSetRecord {
+                    statistic: WorkoutSetStatistic {
+                        weight,
+                        reps: sets.reps,
+                        duration: sets.seconds.and_then(|r| r.checked_div(dec!(60))),
+                        distance: sets.distance.and_then(|d| d.checked_div(dec!(1000))),
+                        ..Default::default()
+                    },
+                    note: None,
+                    rest_time: None,
+                    confirmed_at: None,
+                    lot: SetLot::Normal,
+                });
+            }
+            collected_exercises.push(UserExerciseInput {
                 notes,
                 exercise_id,
                 assets: None,
+                sets: collected_sets,
             });
-            sets = vec![];
-            notes = vec![];
         }
-        if next_entry.date != entry.date {
-            let ndt = NaiveDateTime::parse_from_str(&entry.date, "%Y-%m-%d %H:%M:%S")
-                .expect("Failed to parse input string");
-            let ndt = utils::get_date_time_with_offset(ndt, &ss.timezone);
-            let workout_duration =
-                Duration::try_seconds(entry.workout_duration.parse().unwrap()).unwrap();
-            completed.push(ImportCompletedItem::Workout(UserWorkoutInput {
-                exercises,
-                assets: None,
-                start_time: ndt,
-                supersets: vec![],
-                template_id: None,
-                repeated_from: None,
-                create_workout_id: None,
-                update_workout_id: None,
-                name: entry.workout_name,
-                comment: entry.workout_notes,
-                end_time: ndt + workout_duration,
-                update_workout_template_id: None,
-            }));
-            exercises = vec![];
-        }
+        completed.push(ImportCompletedItem::Workout(UserWorkoutInput {
+            assets: None,
+            start_time: ndt,
+            supersets: vec![],
+            template_id: None,
+            repeated_from: None,
+            create_workout_id: None,
+            update_workout_id: None,
+            exercises: collected_exercises,
+            end_time: ndt + workout_duration,
+            update_workout_template_id: None,
+            name: first_exercise.workout_name.clone(),
+            comment: first_exercise.workout_notes.clone(),
+        }));
     }
     completed.extend(
         unique_exercises
