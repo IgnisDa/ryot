81de5553323eb9acd5a2134349561d3d20597147
Diptesh Choudhuri <ignisda2001@gmail.com>
2024-12-04 18:53:07 +0530

    fix(frontend): send old event attribute

--- apps/frontend/app/lib/hooks.ts
+++ apps/frontend/app/lib/hooks.ts
@@ -136,10 +136,10 @@ export const useUserUnitSystem = () =>
 	useUserPreferences().fitness.exercises.unitSystem;
 
 export const useApplicationEvents = () => {
-	const { version, isServerKeyValidated } = useCoreDetails();
+	const { version, isServerKeyValidated: isPro } = useCoreDetails();
 
 	const sendEvent = (eventName: string, data: Record<string, unknown>) => {
-		window.umami?.track(eventName, { isServerKeyValidated, version, ...data });
+		window.umami?.track(eventName, { isPro, version, ...data });
 	};
 
 	const updateProgress = (title: string) => {
