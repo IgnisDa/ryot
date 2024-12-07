14c47734798d1615bdef313f9810b17028e60349
Diptesh Choudhuri <ignisda2001@gmail.com>
2024-12-07 17:10:25 +0530

    chore: update tsup configuration to include sourcemap and noExternal options

--- apps/app-backend/tsup.config.ts
+++ apps/app-backend/tsup.config.ts
@@ -4,8 +4,10 @@ export default defineConfig({
 	clean: true,
 	outDir: "dist",
 	format: ["esm"],
+	sourcemap: true,
 	platform: "node",
 	target: "node22",
 	splitting: false,
+	noExternal: [/.*/],
 	entry: ["src/index.ts"],
 });
