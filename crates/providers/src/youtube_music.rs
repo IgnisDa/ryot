edf062c469c84c45e78eb87ada554f40902a300b
Diptesh Choudhuri <ignisda2001@gmail.com>
2024-12-14 10:21:05 +0530

    feat(providers): get youtube music search working

--- crates/providers/src/youtube_music.rs
+++ crates/providers/src/youtube_music.rs
@@ -1,15 +1,23 @@
-use anyhow::Result;
+use anyhow::{anyhow, Result};
 use async_trait::async_trait;
-use media_models::MetadataDetails;
-use rustypipe::param::{Language, LANGUAGES};
+use common_models::SearchDetails;
+use common_utils::TEMP_DIR;
+use dependent_models::SearchResults;
+use media_models::{MetadataDetails, MetadataSearchItem};
+use rustypipe::{
+    client::RustyPipe,
+    param::{Language, LANGUAGES},
+};
 use traits::{MediaProvider, MediaProviderLanguages};
 
-#[derive(Debug, Clone)]
-pub struct YoutubeMusicService {}
+pub struct YoutubeMusicService {
+    client: RustyPipe,
+}
 
 impl YoutubeMusicService {
     pub async fn new() -> Self {
-        Self {}
+        let client = RustyPipe::builder().storage_dir(TEMP_DIR).build().unwrap();
+        Self { client }
     }
 }
 
@@ -27,5 +35,37 @@ impl MediaProviderLanguages for YoutubeMusicService {
 impl MediaProvider for YoutubeMusicService {
     async fn metadata_details(&self, identifier: &str) -> Result<MetadataDetails> {
         todo!()
+    }
+
+    async fn metadata_search(
+        &self,
+        query: &str,
+        _page: Option<i32>,
+        _display_nsfw: bool,
+    ) -> Result<SearchResults<MetadataSearchItem>> {
+        let results = self
+            .client
+            .query()
+            .music_search_tracks(query)
+            .await
+            .map_err(|e| anyhow!(e))?;
+        let data = SearchResults {
+            details: SearchDetails {
+                total: 1,
+                next_page: None,
+            },
+            items: results
+                .items
+                .items
+                .into_iter()
+                .map(|i| MetadataSearchItem {
+                    title: i.name,
+                    identifier: i.id,
+                    publish_year: None,
+                    image: i.cover.last().map(|t| t.url.to_owned()),
+                })
+                .collect(),
+        };
+        Ok(data)
     }
 }
