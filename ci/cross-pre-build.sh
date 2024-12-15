475cf24d95430a88a74b811379578b5c4e67010a
Diptesh Choudhuri <ignisda2001@gmail.com>
2024-12-15 16:05:32 +0530

    ci: double quote stuff

--- ci/cross-pre-build.sh
+++ ci/cross-pre-build.sh
@@ -2,6 +2,6 @@
 set -euxo pipefail
 
 if [[ "$CROSS_TARGET" == "aarch64-unknown-linux-gnu" ]]; then
-  dpkg --add-architecture $CROSS_DEB_ARCH
-  apt-get update && apt-get --assume-yes install libssl-dev:$CROSS_DEB_ARCH
+  dpkg --add-architecture "$CROSS_DEB_ARCH"
+  apt-get update && apt-get --assume-yes install "libssl-dev:$CROSS_DEB_ARCH"
 fi
