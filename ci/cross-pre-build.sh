ae0d8298903ec20d23aec0fe146542f808067912
Diptesh Choudhuri <ignisda2001@gmail.com>
2024-12-15 16:28:40 +0530

    ci: remove another double quote

--- ci/cross-pre-build.sh
+++ ci/cross-pre-build.sh
@@ -5,6 +5,6 @@ apt-get update
 apt-get --assume-yes install patch
 
 if [[ "$CROSS_TARGET" == "aarch64-unknown-linux-gnu" ]]; then
-  dpkg --add-architecture "$CROSS_DEB_ARCH"
+  dpkg --add-architecture $CROSS_DEB_ARCH
   apt-get --assume-yes install libssl-dev:$CROSS_DEB_ARCH
 fi
