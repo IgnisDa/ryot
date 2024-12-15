337fe1f56cc076367a4778045bc908f5e071dc47
Diptesh Choudhuri <ignisda2001@gmail.com>
2024-12-15 16:18:29 +0530

    ci: install needed deps

--- ci/cross-pre-build.sh
+++ ci/cross-pre-build.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
 set -euxo pipefail
 
+apt-get update
+apt-get --assume-yes install patch
+
 if [[ "$CROSS_TARGET" == "aarch64-unknown-linux-gnu" ]]; then
   dpkg --add-architecture "$CROSS_DEB_ARCH"
-  apt-get update && apt-get --assume-yes install "libssl-dev:$CROSS_DEB_ARCH"
+  apt-get --assume-yes install "libssl-dev:$CROSS_DEB_ARCH"
 fi
