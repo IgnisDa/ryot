1f8bcae7726984ef7165e14e8de92e853a5cdaa0
Diptesh Choudhuri <ignisda2001@gmail.com>
2024-12-22 10:41:17 +0530

    feat(frontend): add hint on notification settings page

--- apps/frontend/app/routes/_dashboard.settings.notifications.tsx
+++ apps/frontend/app/routes/_dashboard.settings.notifications.tsx
@@ -22,7 +22,7 @@ import type {
 	LoaderFunctionArgs,
 	MetaArgs,
 } from "@remix-run/node";
-import { Form, useLoaderData } from "@remix-run/react";
+import { Form, Link, useLoaderData } from "@remix-run/react";
 import {
 	CreateUserNotificationPlatformDocument,
 	DeleteUserNotificationPlatformDocument,
@@ -39,6 +39,7 @@ import {
 	IconTrash,
 } from "@tabler/icons-react";
 import { useState } from "react";
+import { $path } from "remix-routes";
 import { match } from "ts-pattern";
 import { withQuery } from "ufo";
 import { z } from "zod";
@@ -156,20 +157,137 @@ export default function Page() {
 	] = useState<NotificationPlatformLot>();
 
 	return (
-		<Container size="xs">
-			<Stack>
-				<Title>Notification settings</Title>
-				{loaderData.userNotificationPlatforms.length > 0 ? (
-					loaderData.userNotificationPlatforms.map((notification) => (
-						<DisplayNotification
-							key={notification.id}
-							notification={notification}
+		<>
+			<Modal
+				opened={createUserNotificationPlatformModalOpened}
+				onClose={closeCreateUserNotificationPlatformModal}
+				centered
+				withCloseButton={false}
+			>
+				<Box
+					method="POST"
+					component={Form}
+					action={withQuery(".", { intent: "create" })}
+					onSubmit={() => {
+						closeCreateUserNotificationPlatformModal();
+						setCreateUserNotificationPlatformLot(undefined);
+					}}
+				>
+					<input hidden name="lot" value={createUserNotificationPlatformLot} />
+					<Stack>
+						<Select
+							required
+							searchable
+							label="Select a platform"
+							data={Object.values(NotificationPlatformLot).map((v) => ({
+								label: changeCase(v),
+								value: v,
+							}))}
+							onChange={(v) => {
+								if (v)
+									setCreateUserNotificationPlatformLot(
+										v as NotificationPlatformLot,
+									);
+							}}
 						/>
-					))
-				) : (
-					<Text>No notification platforms configured</Text>
-				)}
-				<Box>
+						{createUserNotificationPlatformLot
+							? match(createUserNotificationPlatformLot)
+									.with(NotificationPlatformLot.Apprise, () => (
+										<>
+											<TextInput label="Base Url" required name="baseUrl" />
+											<TextInput label="Key" required name="apiToken" />
+										</>
+									))
+									.with(NotificationPlatformLot.Discord, () => (
+										<>
+											<TextInput label="Webhook Url" required name="baseUrl" />
+										</>
+									))
+									.with(NotificationPlatformLot.Gotify, () => (
+										<>
+											<TextInput label="Server Url" required name="baseUrl" />
+											<TextInput label="Token" required name="apiToken" />
+											<NumberInput label="Priority" name="priority" />
+										</>
+									))
+									.with(NotificationPlatformLot.Ntfy, () => (
+										<>
+											<TextInput label="Topic" required name="apiToken" />
+											<TextInput label="Server Url" name="baseUrl" />
+											<TextInput
+												label="Access token"
+												description={
+													<>
+														If you want to publish to a{" "}
+														<Anchor
+															size="xs"
+															href="https://docs.ntfy.sh/publish/#access-tokens"
+															target="_blank"
+														>
+															protected topic
+														</Anchor>
+													</>
+												}
+												name="authHeader"
+											/>
+											<NumberInput label="Priority" name="priority" />
+										</>
+									))
+									.with(NotificationPlatformLot.PushBullet, () => (
+										<>
+											<TextInput label="Token" required name="apiToken" />
+										</>
+									))
+									.with(NotificationPlatformLot.PushOver, () => (
+										<>
+											<TextInput label="User Key" required name="apiToken" />
+											<TextInput label="App Key" name="authHeader" />
+										</>
+									))
+									.with(NotificationPlatformLot.PushSafer, () => (
+										<>
+											<TextInput label="Key" required name="apiToken" />
+										</>
+									))
+									.with(NotificationPlatformLot.Telegram, () => (
+										<>
+											<TextInput label="Bot Token" required name="apiToken" />
+											<TextInput label="Chat ID" required name="chatId" />
+										</>
+									))
+									.with(NotificationPlatformLot.Email, () => (
+										<>
+											<TextInput
+												type="email"
+												label="Email ID"
+												required
+												name="apiToken"
+												disabled={!coreDetails.smtpEnabled}
+												description={
+													coreDetails.smtpEnabled ? null : "SMTP is not enabled"
+												}
+											/>
+										</>
+									))
+									.exhaustive()
+							: null}
+						<Button type="submit">Submit</Button>
+					</Stack>
+				</Box>
+			</Modal>
+			<Container size="xs">
+				<Stack>
+					<Title>Notification settings</Title>
+					{loaderData.userNotificationPlatforms.length > 0 ? (
+						loaderData.userNotificationPlatforms.map((notification) => (
+							<DisplayNotification
+								key={notification.id}
+								notification={notification}
+							/>
+						))
+					) : (
+						<Text>No notification platforms configured</Text>
+					)}
 					<Flex justify="end">
 						<Group>
 							{loaderData.userNotificationPlatforms.length > 0 ? (
@@ -192,147 +310,21 @@ export default function Page() {
 							</Button>
 						</Group>
 					</Flex>
-					<Modal
-						opened={createUserNotificationPlatformModalOpened}
-						onClose={closeCreateUserNotificationPlatformModal}
-						centered
-						withCloseButton={false}
-					>
-						<Box
-							method="POST"
-							component={Form}
-							action={withQuery(".", { intent: "create" })}
-							onSubmit={() => {
-								closeCreateUserNotificationPlatformModal();
-								setCreateUserNotificationPlatformLot(undefined);
-							}}
+					<Text size="xs" ta="right">
+						For more settings, please visit the{" "}
+						<Anchor
+							component={Link}
+							to={$path("/settings/preferences", {
+								defaultTab: "notifications",
+							})}
 						>
-							<input
-								hidden
-								name="lot"
-								value={createUserNotificationPlatformLot}
-							/>
-							<Stack>
-								<Select
-									label="Select a platform"
-									required
-									data={Object.values(NotificationPlatformLot).map((v) => ({
-										label: changeCase(v),
-										value: v,
-									}))}
-									onChange={(v) => {
-										if (v)
-											setCreateUserNotificationPlatformLot(
-												v as NotificationPlatformLot,
-											);
-									}}
-								/>
-								{createUserNotificationPlatformLot
-									? match(createUserNotificationPlatformLot)
-											.with(NotificationPlatformLot.Apprise, () => (
-												<>
-													<TextInput label="Base Url" required name="baseUrl" />
-													<TextInput label="Key" required name="apiToken" />
-												</>
-											))
-											.with(NotificationPlatformLot.Discord, () => (
-												<>
-													<TextInput
-														label="Webhook Url"
-														required
-														name="baseUrl"
-													/>
-												</>
-											))
-											.with(NotificationPlatformLot.Gotify, () => (
-												<>
-													<TextInput
-														label="Server Url"
-														required
-														name="baseUrl"
-													/>
-													<TextInput label="Token" required name="apiToken" />
-													<NumberInput label="Priority" name="priority" />
-												</>
-											))
-											.with(NotificationPlatformLot.Ntfy, () => (
-												<>
-													<TextInput label="Topic" required name="apiToken" />
-													<TextInput label="Server Url" name="baseUrl" />
-													<TextInput
-														label="Access token"
-														description={
-															<>
-																If you want to publish to a{" "}
-																<Anchor
-																	size="xs"
-																	href="https://docs.ntfy.sh/publish/#access-tokens"
-																	target="_blank"
-																>
-																	protected topic
-																</Anchor>
-															</>
-														}
-														name="authHeader"
-													/>
-													<NumberInput label="Priority" name="priority" />
-												</>
-											))
-											.with(NotificationPlatformLot.PushBullet, () => (
-												<>
-													<TextInput label="Token" required name="apiToken" />
-												</>
-											))
-											.with(NotificationPlatformLot.PushOver, () => (
-												<>
-													<TextInput
-														label="User Key"
-														required
-														name="apiToken"
-													/>
-													<TextInput label="App Key" name="authHeader" />
-												</>
-											))
-											.with(NotificationPlatformLot.PushSafer, () => (
-												<>
-													<TextInput label="Key" required name="apiToken" />
-												</>
-											))
-											.with(NotificationPlatformLot.Telegram, () => (
-												<>
-													<TextInput
-														label="Bot Token"
-														required
-														name="apiToken"
-													/>
-													<TextInput label="Chat ID" required name="chatId" />
-												</>
-											))
-											.with(NotificationPlatformLot.Email, () => (
-												<>
-													<TextInput
-														type="email"
-														label="Email ID"
-														required
-														name="apiToken"
-														disabled={!coreDetails.smtpEnabled}
-														description={
-															coreDetails.smtpEnabled
-																? null
-																: "SMTP is not enabled"
-														}
-													/>
-												</>
-											))
-											.exhaustive()
-									: null}
-								<Button type="submit">Submit</Button>
-							</Stack>
-						</Box>
-					</Modal>
-				</Box>
-			</Stack>
-		</Container>
+							notification preferences
+						</Anchor>
+						.
+					</Text>
+				</Stack>
+			</Container>
+		</>
 	);
 }
 
