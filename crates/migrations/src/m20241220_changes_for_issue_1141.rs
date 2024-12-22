c34db95c229c402af4b4cd67b9d20d730f1c6398
Diptesh Choudhuri <ignisda2001@gmail.com>
2024-12-22 22:30:19 +0530

    chore(migrations): do not add durations to workouts without it

--- crates/migrations/src/m20241220_changes_for_issue_1141.rs
+++ crates/migrations/src/m20241220_changes_for_issue_1141.rs
@@ -27,9 +27,6 @@ ALTER TABLE "workout" ADD COLUMN "duration" INTEGER;
 UPDATE "workout" SET "duration" = extract(epoch FROM "end_time" - "start_time");
 ALTER TABLE "workout" ALTER COLUMN "duration" SET NOT NULL;
 
-UPDATE "workout" SET "information" =
-jsonb_set("information", '{durations}', JSONB_BUILD_ARRAY(JSONB_BUILD_OBJECT('from', "start_time")));
-
 --
 
 DELETE FROM "daily_user_activity";
