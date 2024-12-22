5455bceeec40c186c85b45a4b564ad518a3e56a6
Diptesh Choudhuri <ignisda2001@gmail.com>
2024-12-22 21:39:11 +0530

    feat(backend): put the commit sha in the application cache version

--- crates/services/cache/src/lib.rs
+++ crates/services/cache/src/lib.rs
@@ -6,7 +6,7 @@ use common_models::ApplicationCacheKey;
 use common_utils::ryot_log;
 use database_models::{application_cache, prelude::ApplicationCache};
 use dependent_models::ApplicationCacheValue;
-use env_utils::APP_VERSION;
+use env_utils::COMMIT_SHA;
 use sea_orm::{ActiveValue, ColumnTrait, DatabaseConnection, EntityTrait, QueryFilter};
 use sea_query::OnConflict;
 use serde::de::DeserializeOwned;
@@ -60,8 +60,8 @@ impl CacheService {
         let to_insert = application_cache::ActiveModel {
             key: ActiveValue::Set(key),
             created_at: ActiveValue::Set(now),
+            version: ActiveValue::Set(COMMIT_SHA.to_owned()),
             value: ActiveValue::Set(serde_json::to_value(value)?),
-            version: ActiveValue::Set(APP_VERSION.to_owned()),
             expires_at: ActiveValue::Set(Some(now + Duration::hours(expiry_hours))),
             ..Default::default()
         };
@@ -70,6 +70,7 @@ impl CacheService {
                 OnConflict::column(application_cache::Column::Key)
                     .update_columns([
                         application_cache::Column::Value,
+                        application_cache::Column::Version,
                         application_cache::Column::ExpiresAt,
                         application_cache::Column::CreatedAt,
                     ])
@@ -93,7 +94,7 @@ impl CacheService {
                 cache
                     .expires_at
                     .map_or(true, |expires_at| expires_at > Utc::now())
-                    && cache.version == APP_VERSION
+                    && cache.version == COMMIT_SHA
             })
             .map(|m| m.value)?;
         serde_json::from_value::<T>(db_value).ok()
