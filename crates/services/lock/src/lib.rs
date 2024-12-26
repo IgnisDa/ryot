0a9ec6786722e3501eab38362e87fbfdf0a8a4f1
Diptesh Choudhuri <ignisda2001@gmail.com>
2024-12-26 07:26:35 +0530

    feat(services/lock): add implementation to lock service

--- crates/services/lock/src/lib.rs
+++ crates/services/lock/src/lib.rs
@@ -1,6 +1,8 @@
 use async_graphql::Result;
 use common_models::ApplicationCacheKey;
+use common_utils::ryot_log;
 use sea_orm::DatabaseConnection;
+use sqlx::postgres::PgAdvisoryLock;
 
 pub struct LockService {
     db: DatabaseConnection,
@@ -13,7 +15,12 @@ impl LockService {
 }
 
 impl LockService {
-    pub async fn acquire_lock(&self, key: ApplicationCacheKey) -> Result<bool> {
-        todo!()
+    pub async fn acquire_lock(&self, key: &ApplicationCacheKey) -> Result<()> {
+        let key_string = serde_json::to_string(key)?;
+        let lock = PgAdvisoryLock::new(key_string);
+        ryot_log!(debug, "Acquiring lock for key: {:?}", key);
+        let conn = self.db.get_postgres_connection_pool().acquire().await?;
+        lock.acquire(conn).await?;
+        Ok(())
     }
 }
