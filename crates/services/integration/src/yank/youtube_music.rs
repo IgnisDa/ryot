6ca49fa79130896622dcdb9ad450d06107177ee8
Diptesh Choudhuri <ignisda2001@gmail.com>
2024-12-27 10:39:15 +0530

    feat(backend): get basic history fetching workng

--- crates/services/integration/src/yank/youtube_music.rs
+++ crates/services/integration/src/yank/youtube_music.rs
@@ -1,6 +1,17 @@
 use anyhow::Result;
+use common_utils::TEMP_DIR;
 use dependent_models::ImportResult;
+use rustypipe::client::RustyPipe;
 
 pub async fn yank_progress(auth_cookie: String) -> Result<ImportResult> {
+    let client = RustyPipe::builder().storage_dir(TEMP_DIR).build().unwrap();
+    client.set_auth_cookie(auth_cookie).await;
+    let a = client
+        .query()
+        .authenticated()
+        .music_history()
+        .await
+        .unwrap();
+    dbg!(a);
     Ok(ImportResult::default())
 }
