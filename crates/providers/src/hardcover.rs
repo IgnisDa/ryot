15d2b9b86f1e3d66e104eeafb460a06314f5069f
Diptesh Choudhuri <ignisda2001@gmail.com>
2025-01-05 12:53:43 +0530

    feat(providers): get metadata search working

--- crates/providers/src/hardcover.rs
+++ crates/providers/src/hardcover.rs
@@ -1,34 +1,75 @@
 use anyhow::Result;
 use application_utils::get_base_http_client;
 use async_trait::async_trait;
-use common_models::PersonSourceSpecifics;
+use common_models::{PersonSourceSpecifics, SearchDetails};
 use common_utils::PAGE_SIZE;
 use database_models::metadata_group::MetadataGroupWithoutId;
 use dependent_models::{
     MetadataGroupSearchResponse, PeopleSearchResponse, PersonDetails, SearchResults,
 };
 use media_models::{MetadataDetails, MetadataSearchItem, PartialMetadataWithoutId};
+use nest_struct::nest_struct;
 use reqwest::{
     header::{HeaderValue, AUTHORIZATION},
     Client,
 };
+use serde::{de::DeserializeOwned, Deserialize};
 use traits::MediaProvider;
 
 static URL: &str = "https://api.hardcover.app/v1/graphql";
 
-fn get_search_query(query: &str, page: i32, query_type: &str) -> String {
-    format!(
-        "
+async fn get_search_response<T: DeserializeOwned>(
+    query: &str,
+    page: i32,
+    query_type: &str,
+    client: &Client,
+) -> Result<T> {
+    let body = format!(
+        r#"
 query {{
   search(
     page: {page}, per_page: {PAGE_SIZE},
-    query: {query}, query_type: {query_type}
+    query: "{query}", query_type: "{query_type}"
   ) {{
     results
   }}
 }}
-    "
-    )
+    "#
+    );
+    let body = serde_json::json!({"query": body});
+    let data = client
+        .post(URL)
+        .json(&body)
+        .send()
+        .await?
+        .json::<T>()
+        .await
+        .unwrap();
+    Ok(data)
+}
+
+#[nest_struct]
+#[derive(Debug, Deserialize)]
+struct Response {
+    data: nest! {
+        search: nest! {
+            results: nest! {
+                found: i32,
+                hits: Vec<
+                  nest! {
+                    document: nest! {
+                        id: String,
+                        title: String,
+                        release_year: Option<i32>,
+                        image: Option<nest! {
+                            url: Option<String>,
+                        }>,
+                    },
+                  }
+                >,
+            }
+        }
+    },
 }
 
 pub struct HardcoverService {
@@ -57,17 +98,31 @@ impl MediaProvider for HardcoverService {
         page: Option<i32>,
         _display_nsfw: bool,
     ) -> Result<SearchResults<MetadataSearchItem>> {
-        let body = get_search_query(query, page.unwrap_or(1), "book");
-        let data = self
-            .client
-            .post(URL)
-            .json(&serde_json::json!({"query": body}))
-            .send()
-            .await?
-            .json::<serde_json::Value>()
-            .await?;
-        dbg!(&data);
-        todo!()
+        let page = page.unwrap_or(1);
+        let response = get_search_response::<Response>(query, page, "book", &self.client).await?;
+        let response = response.data.search.results;
+        let items = response
+            .hits
+            .into_iter()
+            .map(|h| MetadataSearchItem {
+                title: h.document.title,
+                identifier: h.document.id,
+                publish_year: h.document.release_year,
+                image: h.document.image.and_then(|i| i.url),
+            })
+            .collect();
+        let resp = SearchResults {
+            items,
+            details: SearchDetails {
+                total: response.found,
+                next_page: if page < response.found / PAGE_SIZE {
+                    Some(page + 1)
+                } else {
+                    None
+                },
+            },
+        };
+        Ok(resp)
     }
 
     async fn metadata_group_details(
