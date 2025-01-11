b6939a1976aa7cdc1ca57e032e26a83445a572aa
Diptesh Choudhuri <ignisda2001@gmail.com>
2025-01-12 00:37:35 +0530

    fix(migrations): set estimated finish time for imports

--- crates/migrations/src/m20250112_changes_for_issue_1174.rs
+++ crates/migrations/src/m20250112_changes_for_issue_1174.rs
@@ -12,7 +12,11 @@ impl MigrationTrait for Migration {
             .await?
         {
             db.execute_unprepared(
-                r#"ALTER TABLE "import_report" ADD COLUMN "estimated_finish_time" timestamp with time zone NOT NULL"#,
+                r#"
+ALTER TABLE "import_report" ADD COLUMN "estimated_finish_time" timestamp with time zone;
+UPDATE "import_report" SET "estimated_finish_time" = ("started_on" + interval '1 hour');
+ALTER TABLE "import_report" ALTER COLUMN "estimated_finish_time" SET NOT NULL;
+                "#,
             )
             .await?;
         }
