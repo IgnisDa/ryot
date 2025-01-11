ee7101550fdafa2f3617bc7835844eb950a1c3d1
Diptesh Choudhuri <ignisda2001@gmail.com>
2025-01-12 00:35:23 +0530

    feat(migrations): add new column to import report

---
+++ crates/migrations/src/m20250112_changes_for_issue_1174.rs
@@ -0,0 +1,25 @@
+use sea_orm_migration::prelude::*;
+
+#[derive(DeriveMigrationName)]
+pub struct Migration;
+
+#[async_trait::async_trait]
+impl MigrationTrait for Migration {
+    async fn up(&self, manager: &SchemaManager) -> Result<(), DbErr> {
+        let db = manager.get_connection();
+        if !manager
+            .has_column("import_report", "estimated_finish_time")
+            .await?
+        {
+            db.execute_unprepared(
+                r#"ALTER TABLE "import_report" ADD COLUMN "estimated_finish_time" timestamp with time zone NOT NULL"#,
+            )
+            .await?;
+        }
+        Ok(())
+    }
+
+    async fn down(&self, _manager: &SchemaManager) -> Result<(), DbErr> {
+        Ok(())
+    }
+}
