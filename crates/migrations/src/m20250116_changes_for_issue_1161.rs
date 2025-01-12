b8cd3a1b3a90ae841b18e6eabbcf9bda1e68fdbe
Diptesh Choudhuri <ignisda2001@gmail.com>
2025-01-12 19:12:32 +0530

    fix(migrations): add new column to workout calories burnt

--- crates/migrations/src/m20250116_changes_for_issue_1161.rs
+++ crates/migrations/src/m20250116_changes_for_issue_1161.rs
@@ -13,7 +13,10 @@ impl MigrationTrait for Migration {
             db.execute_unprepared(r#"ALTER TABLE "workout" ADD COLUMN "calories_burnt" decimal"#)
                 .await?;
         }
-        if !manager.has_column("daily_user_activity", "id").await? {
+        if !manager
+            .has_column("daily_user_activity", "workout_calories_burnt")
+            .await?
+        {
             db.execute_unprepared("DROP TABLE daily_user_activity")
                 .await?;
             create_daily_user_activity_table(manager).await?;
