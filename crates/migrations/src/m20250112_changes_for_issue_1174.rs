4f492fe7dab29399a1735199f19f1d9c65e2fc63
Diptesh Choudhuri <ignisda2001@gmail.com>
2025-01-12 10:43:33 +0530

    feat(migrations): handle old import reports

--- crates/migrations/src/m20250112_changes_for_issue_1174.rs
+++ crates/migrations/src/m20250112_changes_for_issue_1174.rs
@@ -20,6 +20,31 @@ ALTER TABLE "import_report" ALTER COLUMN "estimated_finish_time" SET NOT NULL;
             )
             .await?;
         }
+        for enum_name in ["ReviewConversion", "SeenHistoryConversion"] {
+            db.execute_unprepared(&format!(
+                r#"
+UPDATE import_report
+SET details = jsonb_set(
+    details,
+    '{{failed_items}}',
+    (
+        SELECT jsonb_agg(
+            CASE
+                WHEN (failed_item ->> 'step') = '{enum_name}' THEN
+                    jsonb_set(failed_item, '{{step}}', '"DatabaseCommit"')
+                ELSE
+                    failed_item
+            END
+        )
+        FROM jsonb_array_elements(details -> 'failed_items') AS failed_item
+    )
+)
+WHERE details -> 'failed_items' @> '[{{"step": "{enum_name}"}}]';
+        "#,
+                enum_name = enum_name
+            ))
+            .await?;
+        }
         Ok(())
     }
 
