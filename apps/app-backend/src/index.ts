448b01a1e82c4456295fd9eddb885144c5ac767a
Diptesh Choudhuri <ignisda2001@gmail.com>
2025-01-25 20:17:32 +0530

    chore: add basic drizzle and better auth setup

--- apps/app-backend/src/index.ts
+++ apps/app-backend/src/index.ts
@@ -1,6 +1,10 @@
+import "dotenv/config";
 import { Hono } from "hono";
+import { auth } from "./utils/auth";
 
 const app = new Hono().basePath("/api");
+
+app.on(["POST", "GET"], "/auth/*", (c) => auth.handler(c.req.raw));
 
 app.get("/", (c) => {
   return c.text("Hello Hono!");
