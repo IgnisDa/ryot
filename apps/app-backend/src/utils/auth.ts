db1d53a72ba6f7503f6544e8f7c19a658198a0a0
Diptesh Choudhuri <ignisda2001@gmail.com>
2025-01-26 21:36:54 +0530

    fix: enhance authentication setup by adding schema to drizzleAdapter and implementing anonymous sign-in

--- apps/app-backend/src/utils/auth.ts
+++ apps/app-backend/src/utils/auth.ts
@@ -2,11 +2,12 @@ import { betterAuth } from "better-auth";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
 import { anonymous } from "better-auth/plugins";
 import { db } from "./db";
+import * as schema from "./db/schema";
 
 export const auth = betterAuth({
   plugins: [anonymous()],
   baseURL: process.env.FRONTEND_URL,
   emailAndPassword: { enabled: true },
   secret: process.env.SERVER_ADMIN_ACCESS_TOKEN,
-  database: drizzleAdapter(db, { provider: "pg" }),
+  database: drizzleAdapter(db, { provider: "pg", schema }),
 });
