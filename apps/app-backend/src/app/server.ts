2b5d8a1d9adccb18b6a4ecf8234f087f459b1016
Diptesh Choudhuri <ignisda2001@gmail.com>
2025-02-08 18:07:32 +0530

    feat: add metrics middleware to track all http requests

--- apps/app-backend/src/app/server.ts
+++ apps/app-backend/src/app/server.ts
@@ -1,8 +1,10 @@
 import { serveStatic } from "@hono/node-server/serve-static";
 import { Hono } from "hono";
+import { metricsMiddleware } from "~/modules/health/middleware";
 import { apiApp } from "./api";
 
 export const app = new Hono()
+	.use("*", metricsMiddleware)
 	.route("/api", apiApp)
 	.use("*", serveStatic({ root: "./client" }))
 	.use("*", serveStatic({ path: "./client/_shell.html" }));
