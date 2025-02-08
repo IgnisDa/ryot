cd72e7e333c1396016c0e515b68a83aea0f4a8cd
Diptesh Choudhuri <ignisda2001@gmail.com>
2025-02-08 07:08:46 +0530

    chore(services/integration): write it into file

--- crates/services/integration/src/sink/tautulli.rs
+++ crates/services/integration/src/sink/tautulli.rs
@@ -1,6 +1,9 @@
+use std::fs;
+
 use anyhow::Result;
 use dependent_models::ImportResult;
 
 pub async fn yank_progress(payload: String) -> Result<ImportResult> {
+    fs::write("tautulli.json", payload)?;
     todo!()
 }
