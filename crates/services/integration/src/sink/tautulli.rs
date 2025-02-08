fcb3a39b12fa4fe90a39301e15da6f0cc712101d
Diptesh Choudhuri <ignisda2001@gmail.com>
2025-02-08 07:25:07 +0530

    chore(services/integration): change written file name

--- crates/services/integration/src/sink/tautulli.rs
+++ crates/services/integration/src/sink/tautulli.rs
@@ -1,9 +1,10 @@
 use std::fs;
 
 use anyhow::Result;
+use chrono::Utc;
 use dependent_models::ImportResult;
 
 pub async fn yank_progress(payload: String) -> Result<ImportResult> {
-    fs::write("tautulli.json", payload)?;
+    fs::write(&format!("tautulli-{}.json", Utc::now()), payload)?;
     todo!()
 }
