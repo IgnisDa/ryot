67a5b8eedce81da969128951f21288725bc2a6b7
Diptesh Choudhuri <ignisda2001@gmail.com>
2025-02-08 18:54:59 +0530

    chore: remove frontend app

--- apps/frontend/app/components/routes/fitness.action/types.ts
+++
@@ -1,8 +0,0 @@
-import type { SetIdentifier } from "~/lib/state/fitness";
-
-export type FuncStartTimer = (params: {
-	duration: number;
-	openTimerDrawer?: boolean;
-	triggeredBy?: SetIdentifier;
-	confirmSetOnFinish?: SetIdentifier;
-}) => void;
