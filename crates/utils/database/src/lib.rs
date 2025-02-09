93200b027ea6dbf03d4bbf3c75525c612f1f81b8
Diptesh Choudhuri <ignisda2001@gmail.com>
2025-02-09 09:04:58 +0530

    fix(utils/database): order seen entries by last updated at

--- crates/utils/database/src/lib.rs
+++ crates/utils/database/src/lib.rs
@@ -183,6 +183,7 @@ pub async fn user_workout_details(
         .distinct()
         .filter(Expr::val(details.start_time).lte(PgFunc::any(Expr::col(seen::Column::UpdatedAt))))
         .filter(Expr::val(details.end_time).gte(PgFunc::any(Expr::col(seen::Column::UpdatedAt))))
+        .order_by_asc(seen::Column::LastUpdatedOn)
         .into_tuple::<String>()
         .all(&ss.db)
         .await?;
