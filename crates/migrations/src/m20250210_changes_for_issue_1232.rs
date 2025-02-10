cab878ef5bd2289885c7aa920ef44b2689d1c438
Diptesh Choudhuri <ignisda2001@gmail.com>
2025-02-10 22:12:43 +0530

    feat(migrations): set the last triggered at date correctly for integrations

--- crates/migrations/src/m20250210_changes_for_issue_1232.rs
+++ crates/migrations/src/m20250210_changes_for_issue_1232.rs
@@ -17,21 +17,25 @@ UPDATE "integration" i SET "trigger_result" = JSONB_BUILD_ARRAY(JSONB_BUILD_OBJE
             )
             .await?;
         }
+        if !manager
+            .has_column("integration", "last_finished_at")
+            .await?
+        {
+            db.execute_unprepared(
+                r#"
+ALTER TABLE "integration" ADD COLUMN "last_finished_at" TIMESTAMPTZ;
+
+UPDATE "integration" i SET "last_finished_at" = i."last_triggered_on";
+                "#,
+            )
+            .await?;
+        }
         if manager
             .has_column("integration", "last_triggered_on")
             .await?
         {
             db.execute_unprepared(r#"ALTER TABLE "integration" DROP COLUMN "last_triggered_on";"#)
                 .await?;
-        }
-        if !manager
-            .has_column("integration", "last_finished_at")
-            .await?
-        {
-            db.execute_unprepared(
-                r#"ALTER TABLE "integration" ADD COLUMN "last_finished_at" TIMESTAMPTZ;"#,
-            )
-            .await?;
         }
         Ok(())
     }
