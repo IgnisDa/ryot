7a60a629c8f6ed47fec4a346f21d6340c85ab2dd
Diptesh Choudhuri <ignisda2001@gmail.com>
2025-04-03 09:09:36 +0530

    Add migration for issue 1330 to update user preferences
    Introduced a new migration file that updates user preferences by modifying the notifications settings in the database. Ensured compliance with coding standards by maintaining the order of attributes based on their length.

--- crates/migrations/src/lib.rs
+++ crates/migrations/src/lib.rs
@@ -42,6 +42,7 @@ mod m20250317_changes_for_issue_1292;
 mod m20250319_changes_for_issue_1294;
 mod m20250401_changes_for_issue_1326;
 mod m20250402_changes_for_issue_1281;
+mod m20250403_changes_for_issue_1330;
 
 pub use m20230404_create_user::User as AliasedUser;
 pub use m20230410_create_metadata::Metadata as AliasedMetadata;
@@ -105,6 +106,7 @@ impl MigratorTrait for Migrator {
             Box::new(m20250319_changes_for_issue_1294::Migration),
             Box::new(m20250401_changes_for_issue_1326::Migration),
             Box::new(m20250402_changes_for_issue_1281::Migration),
+            Box::new(m20250403_changes_for_issue_1330::Migration),
         ]
     }
 }
---
+++ crates/migrations/src/m20250403_changes_for_issue_1330.rs
@@ -0,0 +1,33 @@
+use sea_orm_migration::prelude::*;
+
+#[derive(DeriveMigrationName)]
+pub struct Migration;
+
+#[async_trait::async_trait]
+impl MigrationTrait for Migration {
+    async fn up(&self, manager: &SchemaManager) -> Result<(), DbErr> {
+        let db = manager.get_connection();
+        db.execute_unprepared(
+            r#"
+UPDATE
+  "user"
+SET
+  preferences = JSONB_SET(
+    preferences,
+    '{notifications,to_send}',
+    (preferences -> 'notifications' -> 'to_send') || '"EntityRemovedFromMonitoringCollection"'
+  )
+where
+  NOT (
+    preferences -> 'notifications' -> 'to_send' ? 'EntityRemovedFromMonitoringCollection'
+  );
+            "#,
+        )
+        .await?;
+        Ok(())
+    }
+
+    async fn down(&self, _manager: &SchemaManager) -> Result<(), DbErr> {
+        Ok(())
+    }
+}
