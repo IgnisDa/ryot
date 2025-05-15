41518d0b07f6cd5cf77ad755211a5432d724647c
Diptesh Choudhuri <ignisda2001@gmail.com>
2025-05-15 08:03:03 +0530

    Add handling for new metadata progress update scenarios
    - Implemented a new asynchronous function, create_new_without_dates, to manage the creation of metadata progress updates without date constraints.
    - Enhanced the metadata_progress_update function to handle the CreateNew change type, integrating the new function for improved functionality.
    - This change expands the capabilities of the media model's metadata progress update handling, allowing for more flexible update scenarios.

--- crates/utils/dependent/src/metadata_progress_update.rs
+++ crates/utils/dependent/src/metadata_progress_update.rs
@@ -1,14 +1,35 @@
 use std::sync::Arc;
 
 use async_graphql::Result;
-use media_models::MetadataProgressUpdateInput;
+use media_models::{
+    MetadataProgressUpdateChange, MetadataProgressUpdateChangeCreateNewInput,
+    MetadataProgressUpdateCommonInput, MetadataProgressUpdateInput,
+};
 use supporting_service::SupportingService;
+
+async fn create_new_without_dates(
+    user_id: &String,
+    metadata_id: &String,
+    ss: &Arc<SupportingService>,
+    input: MetadataProgressUpdateCommonInput,
+) -> Result<()> {
+    dbg!(user_id, metadata_id, &input);
+    Ok(())
+}
 
 pub async fn metadata_progress_update(
     user_id: &String,
     ss: &Arc<SupportingService>,
     input: MetadataProgressUpdateInput,
 ) -> Result<()> {
-    dbg!(&input);
+    match input.change {
+        MetadataProgressUpdateChange::CreateNew(create_new_input) => match create_new_input {
+            MetadataProgressUpdateChangeCreateNewInput::WithoutDates(inner_input) => {
+                create_new_without_dates(user_id, &input.metadata_id, ss, inner_input).await?;
+            }
+            _ => todo!(),
+        },
+        _ => todo!(),
+    }
     Ok(())
 }
