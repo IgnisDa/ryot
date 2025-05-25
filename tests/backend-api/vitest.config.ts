5f5bc12c367072f75d5c5e9ffbc6d959551032be
Diptesh Choudhuri <ignisda2001@gmail.com>
2025-05-25 11:34:21 +0530

    Remove dotenv dependency from backend API tests and refactor vitest configuration
    - Removed dotenv from devDependencies in tests/backend-api/package.json to streamline dependencies.
    - Refactored vitest configuration to load environment variables dynamically based on the mode, enhancing flexibility in test setups.

--- tests/backend-api/vitest.config.ts
+++ tests/backend-api/vitest.config.ts
@@ -1,11 +1,15 @@
+import { loadEnv } from "vite";
 import { defineConfig } from "vitest/config";
 
-export default defineConfig({
-	test: {
-		globals: true,
-		testTimeout: 60000,
-		hookTimeout: 60000,
-		environment: "node",
-		globalSetup: ["dotenv/config", "./src/setup/globalSetup.ts"],
-	},
+export default defineConfig(({ mode }) => {
+	return {
+		test: {
+			globals: true,
+			testTimeout: 60000,
+			hookTimeout: 60000,
+			environment: "node",
+			globalSetup: ["./src/setup/globalSetup.ts"],
+			env: loadEnv(mode, process.cwd(), ""),
+		},
+	};
 });
