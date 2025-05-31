dbb16a4e56258c2808f40120ca5b152d10358ceb
Diptesh Choudhuri <ignisda2001@gmail.com>
2025-05-31 08:11:56 +0530

    refactor(jellyfin): enhance Jellyfin import input structure and authentication handling
    - Renamed `DeployUrlAndKeyAndUsernameImportInput` to `DeployJellyfinImportInput` for improved clarity.
    - Updated the `password` field in `DeployJellyfinImportInput` to be optional, enhancing user flexibility.
    - Adjusted the import function and related references to utilize the new structure, ensuring consistency across the codebase.
    - Improved the authentication request handling to accommodate cases where the password may not be provided, ensuring robustness in the authentication process.

---
+++ libs/transactional/emails/ContactSubmission.tsx
@@ -0,0 +1,25 @@
+import { Text } from "@react-email/components";
+import Layout from "../components/Layout";
+
+type ContactSubmissionEmailProps = {
+	message: string;
+	ticketNumber: number;
+};
+
+const subject = "New contact submission";
+
+const ContactSubmissionEmail = (props: ContactSubmissionEmailProps) => (
+	<Layout headingText={subject}>
+		<Text>
+			[{props.ticketNumber}] Your contact submission has been received.
+			<br />
+			Message: <strong>{props.message}</strong>
+			<br />
+			Thank you for contacting us. We will get back to you as soon as possible.
+		</Text>
+	</Layout>
+);
+
+ContactSubmissionEmail.subject = subject;
+
+export default ContactSubmissionEmail;
