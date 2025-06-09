f1e46409df4b80f444a7810c8e3b4650fe0f68b3
Diptesh Choudhuri <ignisda2001@gmail.com>
2025-06-09 12:04:46 +0530

    Refactor: Update ExporterService to use JobManager directly
    - Modified the `ExporterService` struct to encapsulate a `JobManager` instance instead of using an `Arc<SupportingService>`. This change enhances the service's design by directly managing job operations, improving code clarity and maintainability. Updated the constructor to initialize the `JobManager` with the provided `SupportingService` instance, ensuring a more cohesive service structure.

--- crates/services/exporter/src/lib.rs
+++ crates/services/exporter/src/lib.rs
@@ -11,21 +11,26 @@ mod media_exports;
 
 use job_management::JobManager;
 
-pub struct ExporterService(pub Arc<SupportingService>);
+pub struct ExporterService {
+    job_manager: JobManager,
+}
 
 impl ExporterService {
+    pub fn new(service: Arc<SupportingService>) -> Self {
+        Self {
+            job_manager: JobManager::new(service),
+        }
+    }
+
     pub async fn deploy_export_job(&self, user_id: String) -> Result<bool> {
-        let job_manager = JobManager::new(self.0.clone());
-        job_manager.deploy_export_job(user_id).await
+        self.job_manager.deploy_export_job(user_id).await
     }
 
     pub async fn user_exports(&self, user_id: String) -> Result<Vec<ExportJob>> {
-        let job_manager = JobManager::new(self.0.clone());
-        job_manager.user_exports(user_id).await
+        self.job_manager.user_exports(user_id).await
     }
 
     pub async fn perform_export(&self, user_id: String) -> Result<()> {
-        let job_manager = JobManager::new(self.0.clone());
-        job_manager.perform_export(user_id).await
+        self.job_manager.perform_export(user_id).await
     }
 }
