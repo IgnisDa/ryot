97cffdbc8035084e3b02b4227c8aafb65500fb35
Diptesh Choudhuri <ignisda2001@gmail.com>
2025-06-24 23:51:14 +0530

    fix: Optimize genre, creator, and suggestion retrieval in generic_metadata function using try_join for improved error handling

--- crates/utils/dependent/src/metadata_operations.rs
+++ crates/utils/dependent/src/metadata_operations.rs
@@ -14,6 +14,7 @@ use database_models::{
 use database_utils::transform_entity_assets;
 use dependent_models::MetadataBaseData;
 use enum_models::{MetadataToMetadataRelation, UserNotificationContent};
+use futures::{TryFutureExt, try_join};
 use itertools::Itertools;
 use markdown::{CompileOptions, Options, to_html_with_options as markdown_to_html_opts};
 use media_models::{
@@ -675,40 +676,42 @@ pub async fn generic_metadata(
         assets: EntityAssets,
         character: Option<String>,
     }
-    transform_entity_assets(&mut meta.assets, ss).await?;
-    let genres = meta
-        .find_related(Genre)
-        .order_by_asc(genre::Column::Name)
-        .into_model::<GenreListItem>()
-        .all(&ss.db)
-        .await
-        .unwrap();
-    let crts = MetadataToPerson::find()
-        .expr(Expr::col(Asterisk))
-        .filter(metadata_to_person::Column::MetadataId.eq(&meta.id))
-        .join(
-            JoinType::Join,
-            metadata_to_person::Relation::Person
-                .def()
-                .on_condition(|left, right| {
-                    Condition::all().add(
-                        Expr::col((left, metadata_to_person::Column::PersonId))
-                            .equals((right, person::Column::Id)),
-                    )
-                }),
-        )
-        .order_by_asc(metadata_to_person::Column::Index)
-        .into_model::<PartialCreator>()
-        .all(&ss.db)
-        .await?;
-    let suggestions = MetadataToMetadata::find()
-        .select_only()
-        .column(metadata_to_metadata::Column::ToMetadataId)
-        .filter(metadata_to_metadata::Column::FromMetadataId.eq(&meta.id))
-        .filter(metadata_to_metadata::Column::Relation.eq(MetadataToMetadataRelation::Suggestion))
-        .into_tuple::<String>()
-        .all(&ss.db)
-        .await?;
+    let (genres, crts, suggestions, _) = try_join!(
+        meta.find_related(Genre)
+            .order_by_asc(genre::Column::Name)
+            .into_model::<GenreListItem>()
+            .all(&ss.db)
+            .map_err(|_| Error::new("Failed to fetch genres".to_owned())),
+        MetadataToPerson::find()
+            .expr(Expr::col(Asterisk))
+            .filter(metadata_to_person::Column::MetadataId.eq(&meta.id))
+            .join(
+                JoinType::Join,
+                metadata_to_person::Relation::Person
+                    .def()
+                    .on_condition(|left, right| {
+                        Condition::all().add(
+                            Expr::col((left, metadata_to_person::Column::PersonId))
+                                .equals((right, person::Column::Id)),
+                        )
+                    }),
+            )
+            .order_by_asc(metadata_to_person::Column::Index)
+            .into_model::<PartialCreator>()
+            .all(&ss.db)
+            .map_err(|_| Error::new("Failed to fetch creators".to_owned())),
+        MetadataToMetadata::find()
+            .select_only()
+            .column(metadata_to_metadata::Column::ToMetadataId)
+            .filter(metadata_to_metadata::Column::FromMetadataId.eq(&meta.id))
+            .filter(
+                metadata_to_metadata::Column::Relation.eq(MetadataToMetadataRelation::Suggestion)
+            )
+            .into_tuple::<String>()
+            .all(&ss.db)
+            .map_err(|_| Error::new("Failed to fetch suggestions".to_owned())),
+        transform_entity_assets(&mut meta.assets, ss),
+    )?;
 
     let mut creators: HashMap<String, Vec<_>> = HashMap::new();
     for cr in crts {
