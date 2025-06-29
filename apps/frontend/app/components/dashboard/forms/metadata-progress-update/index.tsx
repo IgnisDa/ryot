60d53f736fec1adcfcaf5ae439c6293aca0c5df6
Diptesh Choudhuri <ignisda2001@gmail.com>
2025-06-29 21:06:15 +0530

    refactor: restructure MetadataProgressUpdateForm to include segmented control for progress updates

--- apps/frontend/app/components/dashboard/forms/metadata-progress-update/index.tsx
+++ apps/frontend/app/components/dashboard/forms/metadata-progress-update/index.tsx
@@ -1,8 +1,19 @@
-import { Center, Loader } from "@mantine/core";
+import {
+	Center,
+	Divider,
+	Loader,
+	SegmentedControl,
+	Stack,
+} from "@mantine/core";
 import { useMetadataDetails, useUserMetadataDetails } from "~/lib/hooks";
 import { useMetadataProgressUpdate } from "~/lib/state/media";
 import { MetadataInProgressUpdateForm } from "./in-progress-form";
 import { MetadataNewProgressUpdateForm } from "./new-progress-form";
+
+enum Target {
+	Progress = "progress",
+	Collection = "collection",
+}
 
 export const MetadataProgressUpdateForm = ({
 	closeMetadataProgressUpdateModal,
@@ -29,17 +40,30 @@ export const MetadataProgressUpdateForm = ({
 		closeMetadataProgressUpdateModal();
 	};
 
-	return userMetadataDetails.inProgress ? (
-		<MetadataInProgressUpdateForm
-			onSubmit={onSubmit}
-			metadataDetails={metadataDetails}
-			inProgress={userMetadataDetails.inProgress}
-		/>
-	) : (
-		<MetadataNewProgressUpdateForm
-			onSubmit={onSubmit}
-			metadataDetails={metadataDetails}
-			history={userMetadataDetails.history}
-		/>
+	return (
+		<Stack gap="lg">
+			<SegmentedControl
+				fullWidth
+				defaultValue={Target.Progress}
+				data={[
+					{ label: "Update Progress", value: Target.Progress },
+					{ label: "Add to Collection", value: Target.Collection },
+				]}
+			/>
+			<Divider />
+			{userMetadataDetails.inProgress ? (
+				<MetadataInProgressUpdateForm
+					onSubmit={onSubmit}
+					metadataDetails={metadataDetails}
+					inProgress={userMetadataDetails.inProgress}
+				/>
+			) : (
+				<MetadataNewProgressUpdateForm
+					onSubmit={onSubmit}
+					metadataDetails={metadataDetails}
+					history={userMetadataDetails.history}
+				/>
+			)}
+		</Stack>
 	);
 };
