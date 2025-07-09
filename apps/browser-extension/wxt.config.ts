720da7355b77400fb3a63586d08973c1f26a0524
Diptesh Choudhuri <ignisda2001@gmail.com>
2025-07-09 22:33:36 +0530

    fix(browser-extension): remove redundant manifest version from wxt.config.ts

--- apps/browser-extension/wxt.config.ts
+++ apps/browser-extension/wxt.config.ts
@@ -1,9 +1,6 @@
 import { defineConfig } from "wxt";
 
-// See https://wxt.dev/api/config.html
 export default defineConfig({
+	outDir: "dist",
 	modules: ["@wxt-dev/module-react"],
-	manifest: {
-		version: "0.0.1",
-	},
 });
