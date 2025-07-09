f0b36064b2f3f3d310be55a755509f155f2e7a77
Diptesh Choudhuri <ignisda2001@gmail.com>
2025-07-09 22:36:50 +0530

    fix: update header text from "WXT + React" to "WXT + Ryot"

--- apps/browser-extension/entrypoints/popup/App.tsx
+++ apps/browser-extension/entrypoints/popup/App.tsx
@@ -16,7 +16,7 @@ function App() {
 					<img src={reactLogo} className="logo react" alt="React logo" />
 				</a>
 			</div>
-			<h1>WXT + React</h1>
+			<h1>WXT + Ryot</h1>
 			<div className="card">
 				<button onClick={() => setCount((count) => count + 1)} type="button">
 					count is {count}
