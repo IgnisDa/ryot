9d62c20dec08f1eca9adb02eeefd56dab4b99b12
Diptesh Choudhuri <ignisda2001@gmail.com>
2025-07-09 22:54:30 +0530

    refactor: remove unused assets and clean up App component

--- apps/browser-extension/entrypoints/popup/App.tsx
+++ apps/browser-extension/entrypoints/popup/App.tsx
@@ -1,35 +1,7 @@
-import reactLogo from "@/assets/react.svg";
-import { useState } from "react";
-import wxtLogo from "/wxt.svg";
 import "./App.css";
 
 function App() {
-	const [count, setCount] = useState(0);
-
-	return (
-		<>
-			<div>
-				<a href="https://wxt.dev" target="_blank" rel="noreferrer">
-					<img src={wxtLogo} className="logo" alt="WXT logo" />
-				</a>
-				<a href="https://react.dev" target="_blank" rel="noreferrer">
-					<img src={reactLogo} className="logo react" alt="React logo" />
-				</a>
-			</div>
-			<h1>WXT + Ryot</h1>
-			<div className="card">
-				<button onClick={() => setCount((count) => count + 1)} type="button">
-					count is {count}
-				</button>
-				<p>
-					Edit <code>src/App.tsx</code> and save to test HMR
-				</p>
-			</div>
-			<p className="read-the-docs">
-				Click on the WXT and React logos to learn more
-			</p>
-		</>
-	);
+	return <div>Hello world!</div>;
 }
 
 export default App;
