cdeb09f752dbfd37839d3fa7d7249a9d8c7918e8
Diptesh Choudhuri <ignisda2001@gmail.com>
2025-07-31 09:29:16 +0530

    fix: update ALTER TABLE syntax for consistency in migration script

--- crates/migrations/src/m20250801_is_v9_migration.rs
+++ crates/migrations/src/m20250801_is_v9_migration.rs
@@ -9,14 +9,14 @@ impl MigrationTrait for Migration {
         let db = manager.get_connection();
         db.execute_unprepared(
             r#"
-ALTER TABLE calendar_event MODIFY COLUMN "date" SET NOT NULL;
+ALTER TABLE calendar_event ALTER COLUMN "date" SET NOT NULL;
 
-ALTER TABLE collection_to_entity MODIFY COLUMN "entity_id" SET NOT NULL;
-ALTER TABLE collection_to_entity MODIFY COLUMN "entity_lot" SET NOT NULL;
+ALTER TABLE collection_to_entity ALTER COLUMN "entity_id" SET NOT NULL;
+ALTER TABLE collection_to_entity ALTER COLUMN "entity_lot" SET NOT NULL;
 
-ALTER TABLE exercise MODIFY COLUMN "muscles" SET NOT NULL;
+ALTER TABLE exercise ALTER COLUMN "muscles" SET NOT NULL;
 
-ALTER TABLE review MODIFY COLUMN "entity_lot" SET NOT NULL;
+ALTER TABLE review ALTER COLUMN "entity_lot" SET NOT NULL;
         "#,
         )
         .await?;
