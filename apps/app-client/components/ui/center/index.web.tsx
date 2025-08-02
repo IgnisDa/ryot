bd84644aa4adf38c40c0d00748427483a804b6b1
Diptesh Choudhuri <ignisda2001@gmail.com>
2025-08-03 00:08:40 +0530

    feat: add app-client

---
+++ apps/app-client/components/ui/center/index.web.tsx
@@ -0,0 +1,19 @@
+import type { VariantProps } from "@gluestack-ui/utils/nativewind-utils";
+import React from "react";
+import { centerStyle } from "./styles";
+
+type ICenterProps = React.ComponentPropsWithoutRef<"div"> &
+	VariantProps<typeof centerStyle>;
+
+const Center = React.forwardRef<HTMLDivElement, ICenterProps>(function Center(
+	{ className, ...props },
+	ref,
+) {
+	return (
+		<div className={centerStyle({ class: className })} {...props} ref={ref} />
+	);
+});
+
+Center.displayName = "Center";
+
+export { Center };
---
+++ apps/app-client/components/ui/gluestack-ui-provider/index.web.tsx
@@ -0,0 +1,59 @@
+"use client";
+import { OverlayProvider } from "@gluestack-ui/core/overlay/creator";
+import { ToastProvider } from "@gluestack-ui/core/toast/creator";
+import React, { useEffect, useLayoutEffect } from "react";
+import { script } from "./script";
+
+export type ModeType = "light" | "dark" | "system";
+
+export const useSafeLayoutEffect =
+	typeof window !== "undefined" ? useLayoutEffect : useEffect;
+
+export function GluestackUIProvider({
+	mode = "light",
+	...props
+}: {
+	mode?: ModeType;
+	children?: React.ReactNode;
+}) {
+	const handleMediaQuery = React.useCallback((e: MediaQueryListEvent) => {
+		script(e.matches ? "dark" : "light");
+	}, []);
+
+	useSafeLayoutEffect(() => {
+		if (mode !== "system") {
+			const documentElement = document.documentElement;
+			if (documentElement) {
+				documentElement.classList.add(mode);
+				documentElement.classList.remove(mode === "light" ? "dark" : "light");
+				documentElement.style.colorScheme = mode;
+			}
+		}
+	}, [mode]);
+
+	useSafeLayoutEffect(() => {
+		if (mode !== "system") {
+			return;
+		}
+		const media = window.matchMedia("(prefers-color-scheme: dark)");
+
+		media.addListener(handleMediaQuery);
+
+		return () => media.removeListener(handleMediaQuery);
+	}, [handleMediaQuery]);
+
+	return (
+		<>
+			<script
+				suppressHydrationWarning
+				// biome-ignore lint/security/noDangerouslySetInnerHtml: script injection needed for theme hydration
+				dangerouslySetInnerHTML={{
+					__html: `(${script.toString()})('${mode}')`,
+				}}
+			/>
+			<OverlayProvider>
+				<ToastProvider>{props.children}</ToastProvider>
+			</OverlayProvider>
+		</>
+	);
+}
