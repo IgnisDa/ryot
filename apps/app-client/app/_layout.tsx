4e87d901ceb9a4c9a6ca649da678454f01e1e960
Diptesh Choudhuri <ignisda2001@gmail.com>
2025-08-03 11:33:42 +0530

    feat: integrate @tanstack/react-query for improved data fetching in layout, auth, and onboarding components

--- apps/app-client/app/_layout.tsx
+++ apps/app-client/app/_layout.tsx
@@ -1,11 +1,14 @@
 import { GluestackUIProvider } from "@/components/ui/gluestack-ui-provider";
 import "@/global.css";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Stack } from "expo-router";
 import * as SplashScreen from "expo-splash-screen";
 import { StatusBar } from "expo-status-bar";
 import { Provider } from "jotai";
 import { useEffect } from "react";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
+
+const queryClient = new QueryClient();
 
 export { ErrorBoundary } from "expo-router";
 
@@ -17,13 +20,15 @@ export default function RootLayout() {
 	}, []);
 
 	return (
-		<Provider>
-			<GestureHandlerRootView style={{ flex: 1 }}>
-				<GluestackUIProvider mode="system">
-					<StatusBar style="auto" />
-					<Stack screenOptions={{ headerShown: false }} />
-				</GluestackUIProvider>
-			</GestureHandlerRootView>
-		</Provider>
+		<QueryClientProvider client={queryClient}>
+			<Provider>
+				<GestureHandlerRootView style={{ flex: 1 }}>
+					<GluestackUIProvider mode="system">
+						<StatusBar style="auto" />
+						<Stack screenOptions={{ headerShown: false }} />
+					</GluestackUIProvider>
+				</GestureHandlerRootView>
+			</Provider>
+		</QueryClientProvider>
 	);
 }
