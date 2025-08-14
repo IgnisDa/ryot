19b0228b3e5845129af087827bea4b3a1b3f7e87
Diptesh Choudhuri <ignisda2001@gmail.com>
2025-08-14 12:28:17 +0530

    chore: remove entities

--- crates/models/database/src/lib.rs
+++ crates/models/database/src/lib.rs
@@ -20,7 +20,6 @@ pub mod metadata_to_genre;
 pub mod metadata_to_metadata;
 pub mod metadata_to_metadata_group;
 pub mod metadata_to_person;
-pub mod monitored_entity;
 pub mod notification_platform;
 pub mod person;
 pub mod review;
