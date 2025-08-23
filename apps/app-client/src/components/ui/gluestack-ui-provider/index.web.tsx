b467a419b9c2593c5add1da12792d89902312c8f
Diptesh Choudhuri <ignisda2001@gmail.com>
2025-08-23 14:57:38 +0530

    feat: add basic app-client step 5

---
+++ apps/app-client/src/components/ui/gluestack-ui-provider/index.web.tsx
@@ -0,0 +1,57 @@
+"use client";
+import { OverlayProvider } from "@gluestack-ui/core/overlay/creator";
+import { ToastProvider } from "@gluestack-ui/core/toast/creator";
+import React, { useEffect, useLayoutEffect } from "react";
+
+import { script } from "./script";
+
+export type ModeType = "light" | "dark" | "system";
+
+export const useSafeLayoutEffect = typeof window !== "undefined" ? useLayoutEffect : useEffect;
+
+export function GluestackUIProvider({
+	mode = "light",
+	...props
+}: {
+	mode?: ModeType;
+	children?: React.ReactNode;
+}) {
+	const handleMediaQuery = React.useCallback((e: MediaQueryListEvent) => {
+		script(e.matches ? "dark" : "light");
+	}, []);
+
+	useSafeLayoutEffect(() => {
+		if (mode !== "system") {
+			const documentElement = document.documentElement;
+			documentElement.classList.add(mode);
+			documentElement.classList.remove(mode === "light" ? "dark" : "light");
+			documentElement.style.colorScheme = mode;
+		}
+	}, [mode]);
+
+	useSafeLayoutEffect(() => {
+		if (mode !== "system") {
+			return () => {};
+		}
+		const media = window.matchMedia("(prefers-color-scheme: dark)");
+
+		media.addEventListener("change", handleMediaQuery);
+
+		return () => media.removeEventListener("change", handleMediaQuery);
+	}, [handleMediaQuery]);
+
+	return (
+		<>
+			<script
+				suppressHydrationWarning
+				// oxlint-disable-next-line react/no-danger
+				dangerouslySetInnerHTML={{
+					__html: `(${script.toString()})('${mode}')`,
+				}}
+			/>
+			<OverlayProvider>
+				<ToastProvider>{props.children}</ToastProvider>
+			</OverlayProvider>
+		</>
+	);
+}
---
+++ apps/app-client/src/components/ui/text/index.tsx
@@ -0,0 +1,45 @@
+import type { VariantProps } from "@gluestack-ui/utils/nativewind-utils";
+import React from "react";
+import { Text as RNText } from "react-native";
+
+import { textStyle } from "./styles";
+
+type ITextProps = React.ComponentProps<typeof RNText> & VariantProps<typeof textStyle>;
+
+const Text = React.forwardRef<React.ComponentRef<typeof RNText>, ITextProps>(function Text(
+	{
+		className,
+		isTruncated,
+		bold,
+		underline,
+		strikeThrough,
+		size = "md",
+		sub,
+		italic,
+		highlight,
+		...props
+	},
+	ref,
+) {
+	return (
+		<RNText
+			className={textStyle({
+				isTruncated: isTruncated ?? false,
+				bold: bold ?? false,
+				underline: underline ?? false,
+				strikeThrough: strikeThrough ?? false,
+				size,
+				sub: sub ?? false,
+				italic: italic ?? false,
+				highlight: highlight ?? false,
+				class: className,
+			})}
+			{...props}
+			ref={ref}
+		/>
+	);
+});
+
+Text.displayName = "Text";
+
+export { Text };
