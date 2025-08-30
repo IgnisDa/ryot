1e32567c89391890b183d7b6666cf7fe04a17440
Diptesh Choudhuri <ignisda2001@gmail.com>
2025-08-30 12:36:37 +0530

    fix: separate apt-get update and install commands for aarch64 target in cross-pre-build.sh

--- ci/cross-pre-build.sh
+++ ci/cross-pre-build.sh
@@ -12,5 +12,6 @@ fi
 
 if [[ "$CROSS_TARGET" == "aarch64-unknown-linux-gnu" ]]; then
   dpkg --add-architecture $CROSS_DEB_ARCH
-  apt-get update && apt-get --assume-yes install libssl-dev:$CROSS_DEB_ARCH
+  apt-get update
+  apt-get --assume-yes install libssl-dev:$CROSS_DEB_ARCH
 fi
