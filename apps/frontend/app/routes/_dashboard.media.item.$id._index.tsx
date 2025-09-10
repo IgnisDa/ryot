5c68af3512148ad76e144522ea9bf59287dd35f3
Diptesh Choudhuri <ignisda2001@gmail.com>
2025-09-10 12:07:24 +0530

    fix: rename loading state variable for clarity in metadata details

--- apps/frontend/app/routes/_dashboard.media.item.$id._index.tsx
+++ apps/frontend/app/routes/_dashboard.media.item.$id._index.tsx
@@ -222,7 +222,7 @@ export default function Page() {
 	const userDetails = useUserDetails();
 	const submit = useConfirmSubmit();
 
-	const [metadataDetails, isMetadataStillLoading] = useMetadataDetails(
+	const [metadataDetails, isMetadataPartialDetailsLoading] = useMetadataDetails(
 		loaderData.metadataId,
 	);
 	const userMetadataDetails = useUserMetadataDetails(loaderData.metadataId);
@@ -374,7 +374,7 @@ export default function Page() {
 					<MediaDetailsLayout
 						title={metadataDetails.data.title}
 						assets={metadataDetails.data.assets}
-						isMediaStillLoading={isMetadataStillLoading}
+						isMediaStillLoading={isMetadataPartialDetailsLoading}
 						externalLink={{
 							lot: metadataDetails.data.lot,
 							source: metadataDetails.data.source,
