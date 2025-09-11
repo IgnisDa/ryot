d9cf11b0bf14a3d9cbaee133d077c1a5294e42c0
Diptesh Choudhuri <ignisda2001@gmail.com>
2025-09-11 07:42:23 +0530

    fix: add SQL command to update user onboarding status in migration

--- crates/migrations/sql/src/m20250908_changes_for_issue_1551.rs
+++ crates/migrations/sql/src/m20250908_changes_for_issue_1551.rs
@@ -11,6 +11,15 @@ impl MigrationTrait for Migration {
         db.execute_unprepared(r#"DROP INDEX IF EXISTS "collection_to_entity_entity_id_idx""#)
             .await?;
 
+        db.execute_unprepared(
+            r#"
+UPDATE "user" SET "extra_information" =
+JSONB_SET("extra_information", '{is_onboarding_tour_completed}', 'false')
+WHERE "extra_information" IS NOT NULL
+"#,
+        )
+        .await?;
+
         Ok(())
     }
 
