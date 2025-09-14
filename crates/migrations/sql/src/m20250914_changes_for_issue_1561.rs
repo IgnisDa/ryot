497b5d309a1dd6c46953c27170b085cb0dac8c65
Diptesh Choudhuri <ignisda2001@gmail.com>
2025-09-14 19:09:04 +0530

    Add foreign key relationships and created_by_user_id columns to person and metadata_group tables

---
+++ crates/migrations/sql/src/m20250914_changes_for_issue_1561.rs
@@ -0,0 +1,49 @@
+use sea_orm_migration::prelude::*;
+
+use super::{
+    m20230411_create_metadata_group::METADATA_GROUP_TO_USER_FOREIGN_KEY,
+    m20230413_create_person::PERSON_TO_USER_FOREIGN_KEY,
+};
+
+#[derive(DeriveMigrationName)]
+pub struct Migration;
+
+#[async_trait::async_trait]
+impl MigrationTrait for Migration {
+    async fn up(&self, manager: &SchemaManager) -> Result<(), DbErr> {
+        let db = manager.get_connection();
+
+        if !manager.has_column("person", "created_by_user_id").await? {
+            db.execute_unprepared(&format!(
+                r#"
+ALTER TABLE "person" ADD COLUMN "created_by_user_id" TEXT;
+
+ALTER TABLE "person" ADD CONSTRAINT "{PERSON_TO_USER_FOREIGN_KEY}"
+FOREIGN KEY ("created_by_user_id") REFERENCES "user"("id") ON UPDATE CASCADE ON DELETE SET NULL;
+"#
+            ))
+            .await?;
+        }
+
+        if !manager
+            .has_column("metadata_group", "created_by_user_id")
+            .await?
+        {
+            db.execute_unprepared(&format!(
+                r#"
+ALTER TABLE "metadata_group" ADD COLUMN "created_by_user_id" TEXT;
+
+ALTER TABLE "metadata_group" ADD CONSTRAINT "{METADATA_GROUP_TO_USER_FOREIGN_KEY}"
+FOREIGN KEY ("created_by_user_id") REFERENCES "user"("id") ON UPDATE CASCADE ON DELETE SET NULL;
+"#
+            ))
+            .await?;
+        }
+
+        Ok(())
+    }
+
+    async fn down(&self, _manager: &SchemaManager) -> Result<(), DbErr> {
+        Ok(())
+    }
+}
