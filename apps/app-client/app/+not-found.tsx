75b6701e79ea12fa35e0d1e241c415097f7c2757
Diptesh Choudhuri <ignisda2001@gmail.com>
2025-09-21 03:02:25 +0530

    feat: add app-client

---
+++ apps/app-client/app/+not-found.tsx
@@ -0,0 +1,17 @@
+import { Link, Stack } from "expo-router";
+import { Center } from "@/components/ui/center";
+import { Text } from "@/components/ui/text";
+
+export default function NotFoundScreen() {
+	return (
+		<>
+			<Stack.Screen options={{ title: "Oops!" }} />
+			<Center className="flex-1">
+				<Text className="text-secondary-200">This screen doesn't exist.</Text>
+				<Link href="/" style={{ marginTop: 10 }}>
+					<Text className="text-primary-500">Go to home screen!</Text>
+				</Link>
+			</Center>
+		</>
+	);
+}
