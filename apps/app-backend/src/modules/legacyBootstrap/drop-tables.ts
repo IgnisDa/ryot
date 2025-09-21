dba295ae60c1302041c3b27820a6d644b835c54e
Diptesh Choudhuri <ignisda2001@gmail.com>
2025-09-21 08:57:55 +0530

    chore: migrate metadata groups

--- apps/app-backend/src/modules/legacyBootstrap/drop-tables.ts
+++ apps/app-backend/src/modules/legacyBootstrap/drop-tables.ts
@@ -14,6 +14,18 @@ const dropLegacyUserTableSql = sql`
 DROP TABLE IF EXISTS "old_user" CASCADE;
 `;
 
+const dropLegacyMetadataToMetadataGroupTableSql = sql`
+DROP TABLE IF EXISTS "metadata_to_metadata_group" CASCADE;
+`;
+
+const dropLegacyMetadataGroupToPersonTableSql = sql`
+DROP TABLE IF EXISTS "metadata_group_to_person" CASCADE;
+`;
+
+const dropLegacyMetadataGroupTableSql = sql`
+DROP TABLE IF EXISTS "metadata_group" CASCADE;
+`;
+
 const dropLegacyMigrationsTable = async (database: DbClient) => {
 	await database.execute(dropLegacyMigrationsTableSql);
 };
@@ -26,8 +38,15 @@ const dropLegacyUserTable = async (database: DbClient) => {
 	await database.execute(dropLegacyUserTableSql);
 };
 
+const dropLegacyMetadataGroupTables = async (database: DbClient) => {
+	await database.execute(dropLegacyMetadataToMetadataGroupTableSql);
+	await database.execute(dropLegacyMetadataGroupToPersonTableSql);
+	await database.execute(dropLegacyMetadataGroupTableSql);
+};
+
 export const dropLegacyTables = async (database: DbClient) => {
 	await dropLegacyMigrationsTable(database);
 	await dropLegacyMetadataTable(database);
+	await dropLegacyMetadataGroupTables(database);
 	await dropLegacyUserTable(database);
 };
