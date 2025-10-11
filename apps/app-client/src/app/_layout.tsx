04f46b46e6126dd904008968297c77a2079f5ad0
Diptesh Choudhuri <ignisda2001@gmail.com>
2025-10-11 12:28:05 +0530

    feat: add basic app-client step 3

--- apps/app-client/src/app/_layout.tsx
+++ apps/app-client/src/app/_layout.tsx
@@ -1,5 +1,5 @@
 // oxlint-disable-next-line import/no-unassigned-import
-import "../global.css";
+import "@/global.css";
 import { Stack } from "expo-router";
 
 export default function RootLayout() {
