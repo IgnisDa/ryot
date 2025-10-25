d5db70524c17cc55153d4cd1e76ef03e6783a550
Diptesh Choudhuri <ignisda2001@gmail.com>
2025-10-25 16:18:57 +0530

    feat: implement session retrieval and redirect logic in protected route

--- apps/app-frontend/src/routes/_protected/route.tsx
+++ apps/app-frontend/src/routes/_protected/route.tsx
@@ -1,8 +1,26 @@
-import { createFileRoute, Outlet } from "@tanstack/react-router";
+import { createFileRoute, Outlet, redirect } from "@tanstack/react-router";
+import { createClientOnlyFn } from "@tanstack/react-start";
+import type { AuthClient } from "#/hooks/auth";
+
+const getSession = createClientOnlyFn(
+	async (authClientInstance: AuthClient) => {
+		const session = await authClientInstance.getSession();
+		return session;
+	},
+);
 
 export const Route = createFileRoute("/_protected")({
 	component: RouteComponent,
-	beforeLoad: async ({ context }) => {},
+	beforeLoad: async ({ context, location }) => {
+		const { data } = await getSession(context.authClientInstance);
+		if (!data)
+			throw redirect({
+				to: "/start",
+				search: { redirect: location.href },
+			});
+
+		return { user: data.user };
+	},
 });
 
 function RouteComponent() {
