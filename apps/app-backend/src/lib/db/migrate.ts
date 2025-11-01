50e51397119ad58a2da084ac127ac0d693e06d8a
Diptesh Choudhuri <ignisda2001@gmail.com>
2025-11-01 16:58:59 +0530

    feat(migration): implement legacy user migration and table renaming logic

--- apps/app-backend/src/lib/db/migrate.ts
+++ apps/app-backend/src/lib/db/migrate.ts
@@ -1,11 +1,15 @@
 import { migrate } from "drizzle-orm/node-postgres/migrator";
 
+import { migrateLegacyUsers, renameConflictingTables } from "~/modules/_migration";
+
 import { db } from "./index";
 import { seedInitialDatabase } from "./seed";
 
 const migrationsFolder = `${process.cwd()}/src/drizzle`;
 
 export const migrateDB = async () => {
+	await renameConflictingTables(db);
 	await migrate(db, { migrationsFolder });
+	await migrateLegacyUsers(db);
 	await seedInitialDatabase(db);
 };
