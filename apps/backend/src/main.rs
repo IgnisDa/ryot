7ba44e4bbe5e801be3fb052d037b385cd8b9e743
Diptesh Choudhuri <ignisda2001@gmail.com>
2025-11-18 11:38:13 +0530

    fix: update get_cron_schedules to return Result type for better error handling

--- apps/backend/src/main.rs
+++ apps/backend/src/main.rs
@@ -67,7 +67,7 @@ async fn main() -> Result<()> {
     let host = config.server.backend_host.clone();
     let disable_background_jobs = config.server.disable_background_jobs;
 
-    let (infrequent_scheduler, frequent_scheduler) = get_cron_schedules(&config, tz);
+    let (infrequent_scheduler, frequent_scheduler) = get_cron_schedules(&config, tz)?;
 
     if config.server.sleep_before_startup_seconds > 0 {
         let duration = Duration::from_secs(config.server.sleep_before_startup_seconds);
@@ -224,20 +224,20 @@ fn init_tracing() -> Result<()> {
     Ok(())
 }
 
-fn get_cron_schedules(config: &Arc<AppConfig>, tz: chrono_tz::Tz) -> (Schedule, Schedule) {
+fn get_cron_schedules(config: &Arc<AppConfig>, tz: chrono_tz::Tz) -> Result<(Schedule, Schedule)> {
     let frequent_cron_jobs_every_minutes = config.scheduler.frequent_cron_jobs_every_minutes;
     let infrequent_cron_jobs_hours_format =
         config.scheduler.infrequent_cron_jobs_hours_format.clone();
 
     let infrequent_scheduler =
-        Schedule::from_str(&format!("0 0 {infrequent_cron_jobs_hours_format} * * *")).unwrap();
+        Schedule::from_str(&format!("0 0 {infrequent_cron_jobs_hours_format} * * *"))?;
     log_cron_schedule(stringify!(infrequent_scheduler), &infrequent_scheduler, tz);
 
     let frequent_scheduler =
-        Schedule::from_str(&format!("0 */{frequent_cron_jobs_every_minutes} * * * *")).unwrap();
+        Schedule::from_str(&format!("0 */{frequent_cron_jobs_every_minutes} * * * *"))?;
     log_cron_schedule(stringify!(frequent_scheduler), &frequent_scheduler, tz);
 
-    (infrequent_scheduler, frequent_scheduler)
+    Ok((infrequent_scheduler, frequent_scheduler))
 }
 
 fn log_cron_schedule(name: &str, schedule: &Schedule, tz: chrono_tz::Tz) {
