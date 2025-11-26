66763573e97c81232fe563b38cb2a2650cb9b3a2
Diptesh Choudhuri <ignisda2001@gmail.com>
2025-11-26 07:22:36 +0530

    feat: implement useSavedForm hook for managing form state with local storage

---
+++ apps/frontend/app/lib/hooks/use-saved-form.ts
@@ -0,0 +1,40 @@
+import type { UseFormInput, UseFormReturnType } from "@mantine/form";
+import { useForm } from "@mantine/form";
+import { useEffect } from "react";
+import { useLocalStorage } from "usehooks-ts";
+import { useUserDetails } from "../shared/hooks";
+
+interface UseSavedFormConfig<TValues> extends UseFormInput<TValues> {
+	storageKeyPrefix: string;
+}
+
+export const useSavedForm = <TValues extends Record<string, unknown>>(
+	config: UseSavedFormConfig<TValues>,
+): UseFormReturnType<TValues> & {
+	clearSavedState: () => void;
+} => {
+	const userDetails = useUserDetails();
+	const [savedValues, setSavedValues] = useLocalStorage<TValues | null>(
+		`${userDetails.id}-${config.storageKeyPrefix}`,
+		null,
+	);
+
+	const initialValues = savedValues || config.initialValues;
+
+	const form = useForm<TValues>({ ...config, initialValues });
+
+	useEffect(() => {
+		if (form.isDirty()) {
+			setSavedValues(form.values);
+		}
+	}, [form.values, form.isDirty, setSavedValues]);
+
+	const clearSavedState = () => {
+		setSavedValues(null);
+	};
+
+	return {
+		...form,
+		clearSavedState,
+	};
+};
