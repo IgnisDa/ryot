d6a93b1d8ded7e310ef1b3041d80ff77e83796e2
Diptesh Choudhuri <ignisda2001@gmail.com>
2025-11-28 06:36:37 +0530

    refactor: simplify EntityTranslationDetails by removing language-specific fields

--- crates/models/media/src/translation.rs
+++ crates/models/media/src/translation.rs
@@ -2,13 +2,7 @@ use async_graphql::SimpleObject;
 use serde::{Deserialize, Serialize};
 
 #[derive(Debug, Default, PartialEq, Eq, Serialize, Deserialize, SimpleObject, Clone)]
-pub struct EntityLanguageTranslationDetails {
-    pub language: String,
-    pub value: Option<String>,
-}
-
-#[derive(Debug, Default, PartialEq, Eq, Serialize, Deserialize, SimpleObject, Clone)]
 pub struct EntityTranslationDetails {
-    pub titles: Vec<EntityLanguageTranslationDetails>,
-    pub descriptions: Vec<EntityLanguageTranslationDetails>,
+    pub title: Option<String>,
+    pub description: Option<String>,
 }
