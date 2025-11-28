e555720c3cbb41e95973a548a2f7b33427c57ed4
Diptesh Choudhuri <ignisda2001@gmail.com>
2025-11-28 06:32:07 +0530

    refactor: add Default trait to EntityLanguageTranslationDetails and EntityTranslationDetails structs

--- crates/models/media/src/translation.rs
+++ crates/models/media/src/translation.rs
@@ -1,13 +1,13 @@
 use async_graphql::SimpleObject;
 use serde::{Deserialize, Serialize};
 
-#[derive(Debug, PartialEq, Eq, Serialize, Deserialize, SimpleObject, Clone)]
+#[derive(Debug, Default, PartialEq, Eq, Serialize, Deserialize, SimpleObject, Clone)]
 pub struct EntityLanguageTranslationDetails {
     pub value: String,
     pub language: String,
 }
 
-#[derive(Debug, PartialEq, Eq, Serialize, Deserialize, SimpleObject, Clone)]
+#[derive(Debug, Default, PartialEq, Eq, Serialize, Deserialize, SimpleObject, Clone)]
 pub struct EntityTranslationDetails {
     pub titles: Vec<EntityLanguageTranslationDetails>,
     pub descriptions: Vec<EntityLanguageTranslationDetails>,
