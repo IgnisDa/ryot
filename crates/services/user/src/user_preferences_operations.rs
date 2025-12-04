37d567979ac85936e2f892ffc94f52c3ec15abd6
Diptesh Choudhuri <ignisda2001@gmail.com>
2025-12-04 08:36:35 +0530

    feat(user): add cache expiration for user preferences update

--- crates/services/user/src/user_preferences_operations.rs
+++ crates/services/user/src/user_preferences_operations.rs
@@ -2,6 +2,8 @@ use std::sync::Arc;
 
 use anyhow::Result;
 use database_utils::user_by_id;
+use dependent_models::ApplicationCacheKeyDiscriminants;
+use futures::future::try_join_all;
 use sea_orm::{ActiveModelTrait, ActiveValue, IntoActiveModel};
 use supporting_service::SupportingService;
 use user_models::UserPreferences;
@@ -15,5 +17,20 @@ pub async fn update_user_preference(
     let mut user_model = user_model.into_active_model();
     user_model.preferences = ActiveValue::Set(input);
     user_model.update(&ss.db).await?;
+    let cache_keys = vec![
+        ApplicationCacheKeyDiscriminants::UserPersonDetails,
+        ApplicationCacheKeyDiscriminants::UserMetadataDetails,
+        ApplicationCacheKeyDiscriminants::UserMetadataGroupDetails,
+    ];
+    try_join_all(cache_keys.into_iter().map(|key| {
+        cache_service::expire_key(
+            ss,
+            dependent_models::ExpireCacheKeyInput::BySanitizedKey {
+                key,
+                user_id: Some(user_id.to_owned()),
+            },
+        )
+    }))
+    .await?;
     Ok(true)
 }
