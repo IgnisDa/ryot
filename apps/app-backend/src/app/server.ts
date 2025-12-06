c3b6f3a4cf0be3ca25bad37bc30d3414ec3dd0d6
Diptesh Choudhuri <ignisda2001@gmail.com>
2025-12-06 10:01:27 +0530

    refactor: remove Bull Board dependencies and update Docker Compose for backend service

--- apps/app-backend/src/app/server.ts
+++ apps/app-backend/src/app/server.ts
@@ -1,11 +1,5 @@
-import { createBullBoard } from "@bull-board/api";
-import { BullMQAdapter } from "@bull-board/api/bullMQAdapter";
-import { HonoAdapter } from "@bull-board/hono";
 import { serveStatic } from "@hono/node-server/serve-static";
 import { Hono } from "hono";
-import { basicAuth } from "hono/basic-auth";
-import { config } from "~/lib/config";
-import { getQueues } from "~/lib/queue";
 import { metricsMiddleware } from "~/modules/system/middleware";
 import { apiApp } from "./api";
 
@@ -16,20 +10,5 @@ export const getServer = () => {
 		.use("*", serveStatic({ root: "./client" }))
 		.use("*", serveStatic({ path: "./client/_shell.html" }));
 
-	const systemBackgroundEndpoint = "/api/system/background";
-	const serverAdapter = new HonoAdapter(serveStatic);
-	createBullBoard({
-		serverAdapter,
-		queues: [new BullMQAdapter(getQueues().sandboxScriptQueue)],
-	});
-	serverAdapter.setBasePath(systemBackgroundEndpoint);
-	app.use(
-		systemBackgroundEndpoint,
-		basicAuth({
-			username: config.SERVER_ADMIN_ACCESS_TOKEN,
-			password: config.SERVER_ADMIN_ACCESS_TOKEN,
-		}),
-	);
-	app.route(systemBackgroundEndpoint, serverAdapter.registerPlugin());
 	return app;
 };
