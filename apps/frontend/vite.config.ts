84a9a9bb334891e83ff3fb48ffbf13678eda3a02
Diptesh Choudhuri <ignisda2001@gmail.com>
2025-12-14 09:58:11 +0530

    Enable reactRouterDevTools in Vite configuration

--- apps/frontend/vite.config.ts
+++ apps/frontend/vite.config.ts
@@ -1,5 +1,5 @@
 import { reactRouter } from "@react-router/dev/vite";
-// import { reactRouterDevTools } from "react-router-devtools";
+import { reactRouterDevTools } from "react-router-devtools";
 import { safeRoutes } from "safe-routes/vite";
 import { defineConfig } from "vite";
 import { VitePWA } from "vite-plugin-pwa";
@@ -13,7 +13,7 @@ export default defineConfig({
 			: undefined,
 	},
 	plugins: [
-		// reactRouterDevTools(),
+		reactRouterDevTools(),
 		reactRouter(),
 		safeRoutes(),
 		tsconfigPaths({ ignoreConfigErrors: true }),
