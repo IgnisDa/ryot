5b1995c3b6d491061ecbe7abc89d91f9a70c505e
Diptesh Choudhuri <ignisda2001@gmail.com>
2025-12-21 13:00:40 +0530

    refactor: add image field to EntityTranslationDetails struct

--- crates/models/media/src/translation.rs
+++ crates/models/media/src/translation.rs
@@ -3,6 +3,7 @@ use serde::{Deserialize, Serialize};
 
 #[derive(Debug, Default, PartialEq, Eq, Serialize, Deserialize, SimpleObject, Clone)]
 pub struct EntityTranslationDetails {
+    pub image: Option<String>,
     pub title: Option<String>,
     pub description: Option<String>,
 }
