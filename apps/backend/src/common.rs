2ddcc88744a2df8242cc3355c182ab606227a019
Diptesh Choudhuri <ignisda2001@gmail.com>
2026-01-12 14:16:50 +0530

    refactor: simplify GraphQL tracing by removing custom extension implementation

--- apps/backend/src/common.rs
+++ apps/backend/src/common.rs
@@ -2,13 +2,7 @@ use std::{path::PathBuf, sync::Arc, time::Duration};
 
 use apalis::prelude::MemoryStorage;
 use application_utils::{AuthContext, create_oidc_client};
-use async_graphql::{
-    EmptySubscription, MergedObject, Schema,
-    async_trait::async_trait,
-    extensions::{
-        Extension as GraphqlExtension, ExtensionContext, ExtensionFactory, NextExecute, Tracing,
-    },
-};
+use async_graphql::{EmptySubscription, MergedObject, Schema, extensions::Tracing};
 use async_graphql_axum::{GraphQLRequest, GraphQLResponse};
 use axum::{
     Extension,
@@ -61,37 +55,13 @@ use tower_http::{
     catch_panic::CatchPanicLayer as TowerCatchPanicLayer, cors::CorsLayer as TowerCorsLayer,
     trace::TraceLayer as TowerTraceLayer,
 };
-use tracing::{Instrument, Span};
+use tracing::Span;
 use user_authentication_resolver::{
     UserAuthenticationMutationResolver, UserAuthenticationQueryResolver,
 };
 use user_management_resolver::{UserManagementMutationResolver, UserManagementQueryResolver};
 use user_services_resolver::{UserServicesMutationResolver, UserServicesQueryResolver};
 use uuid::Uuid;
-
-struct GraphqlOperationTracing;
-
-impl ExtensionFactory for GraphqlOperationTracing {
-    fn create(&self) -> Arc<dyn GraphqlExtension> {
-        Arc::new(GraphqlOperationTracingExtension)
-    }
-}
-
-struct GraphqlOperationTracingExtension;
-
-#[async_trait]
-impl GraphqlExtension for GraphqlOperationTracingExtension {
-    async fn execute(
-        &self,
-        ctx: &ExtensionContext<'_>,
-        operation_name: Option<&str>,
-        next: NextExecute<'_>,
-    ) -> async_graphql::Response {
-        let operation_name_value = operation_name.unwrap_or("anonymous");
-        let span = tracing::info_span!("graphql.operation", operation_name = %operation_name_value);
-        next.run(ctx, operation_name).instrument(span).await
-    }
-}
 
 #[builder]
 pub async fn create_app_dependencies(
@@ -125,7 +95,6 @@ pub async fn create_app_dependencies(
         EmptySubscription,
     )
     .extension(Tracing)
-    .extension(GraphqlOperationTracing)
     .data(supporting_service.clone())
     .finish();
 
