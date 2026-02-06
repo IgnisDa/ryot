b9f2f9ac8702332dd1e45fc129610f50e4caa99c
Diptesh Choudhuri <ignisda2001@gmail.com>
2026-02-06 07:59:33 +0530

    chore: remove generated file

--- libs/generated/src/graphql/backend/codegen.ts
+++ libs/generated/src/graphql/backend/codegen.ts
@@ -1,36 +1,30 @@
 import { join } from "node:path";
 import type { CodegenConfig } from "@graphql-codegen/cli";
-
 import { definitionsLibraryPath } from "..";
 
 const config: CodegenConfig = {
-	config: {
-		scalars: {
-			UUID: "string",
-			DateTime: "string",
-			NaiveDate: "string",
-			Decimal: "string",
-		},
-	},
+	overwrite: true,
+	ignoreNoDocuments: true,
+	schema: "http://127.0.0.1:5000/graphql",
 	documents: [
 		join(definitionsLibraryPath, "backend/{queries,mutations}/*.gql"),
 	],
-	generates: {
-		"./src/graphql/backend/": {
-			config: { skipTypename: true },
-			plugins: [],
-			preset: "client",
-			presetConfig: {
-				fragmentMasking: false,
-			},
-		},
-		"src/graphql/backend/types.generated.ts": {
-			plugins: ["typescript"],
+	config: {
+		scalars: {
+			UUID: "string",
+			Decimal: "string",
+			DateTime: "string",
+			NaiveDate: "string",
 		},
 	},
-	ignoreNoDocuments: true,
-	overwrite: true,
-	schema: "http://127.0.0.1:5000/graphql",
+	generates: {
+		"./src/graphql/backend/": {
+			plugins: [],
+			preset: "client",
+			config: { skipTypename: true },
+			presetConfig: { fragmentMasking: false },
+		},
+	},
 };
 
 export default config;
