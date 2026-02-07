65827c27de02efe0b85bac37fa2197426bcda015
Diptesh Choudhuri <ignisda2001@gmail.com>
2026-02-07 15:22:48 +0530

    feat: complete phase 1

--- apps/app-client/src/app/_layout.tsx
+++ apps/app-client/src/app/_layout.tsx
@@ -1,15 +1,18 @@
 // oxlint-disable-next-line import/no-unassigned-import
 import "@/global.css";
+import { RegistryProvider } from "@effect-atom/atom-react";
 import { Slot } from "expo-router";
 import { StatusBar } from "expo-status-bar";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 
 export default function RootLayout() {
 	return (
-		<SafeAreaProvider>
-			<Slot />
-			{/* oxlint-disable-next-line react/style-prop-object */}
-			<StatusBar style="auto" />
-		</SafeAreaProvider>
+		<RegistryProvider>
+			<SafeAreaProvider>
+				<Slot />
+				{/* oxlint-disable-next-line react/style-prop-object */}
+				<StatusBar style="auto" />
+			</SafeAreaProvider>
+		</RegistryProvider>
 	);
 }
