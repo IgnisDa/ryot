31124459dbb95630680540e53dae5fe6fb47fbf3
Diptesh Choudhuri <ignisda2001@gmail.com>
2026-02-15 04:57:35 +0530

    chore: (3.4.5) Remove dayjs re-exports and plugin from sandbox-sdk
    The sandbox-sdk package exposed dayjs and a custom-parse-format plugin
    for sandbox scripts.  Sandbox scripts should use Effect DateTime
    instead.  Remove the re-export modules, their package.json export
    entries, the dayjs dependency, and the corresponding import-list and
    test entries.
    
    Sandbox script files that import @ryot/sandbox-sdk/dayjs are left
    untouched and will be migrated separately.

--- libs/sandbox-sdk/src/dayjs.ts
+++
@@ -1,8 +0,0 @@
-import dayjsFactory from "dayjs";
-import customParseFormat from "dayjs/plugin/customParseFormat.js";
-
-dayjsFactory.extend(customParseFormat);
-
-export type { ConfigType, Dayjs, ManipulateType, OpUnitType, QUnitType } from "dayjs";
-export const dayjs = dayjsFactory;
-export default dayjsFactory;
