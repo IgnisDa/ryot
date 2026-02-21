f730eb14b2cb69c4923512932c32b993fcf30b32
Diptesh Choudhuri <ignisda2001@gmail.com>
2026-02-21 21:00:50 +0530

    feat: add anonymous plugin to betterAuth and reorder user schema fields

--- apps/app-backend/src/utils/db/auth-schema.ts
+++ apps/app-backend/src/utils/db/auth-schema.ts
@@ -6,8 +6,9 @@ export const user = pgTable("user", {
   id: text("id").primaryKey(),
   name: text("name").notNull(),
   email: text("email").notNull().unique(),
-  emailVerified: boolean("email_verified").default(false).notNull(),
   createdAt: timestamp("created_at").defaultNow().notNull(),
+  isAnonymous: boolean("is_anonymous").default(false),
+  emailVerified: boolean("email_verified").default(false).notNull(),
   updatedAt: timestamp("updated_at")
     .defaultNow()
     .$onUpdate(() => /* @__PURE__ */ new Date())
