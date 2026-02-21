f9341aa7f070e3133906130db5b7d0da98ce0813
Diptesh Choudhuri <ignisda2001@gmail.com>
2026-02-22 01:43:47 +0530

    fix: add migration setup to db index file

--- apps/app-backend/src/utils/db/index.ts
+++ apps/app-backend/src/utils/db/index.ts
@@ -1,3 +1,9 @@
+import { resolve } from "node:path";
 import { drizzle } from "drizzle-orm/node-postgres";
+import { migrate } from "drizzle-orm/node-postgres/migrator";
+
+const migrationsFolder = resolve(process.cwd(), "drizzle");
 
 export const db = drizzle(process.env.DATABASE_URL);
+
+await migrate(db, { migrationsFolder });
