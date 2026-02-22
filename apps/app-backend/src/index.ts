6d383be5565d0552b7d65dd7c51008b99df5d08e
Diptesh Choudhuri <ignisda2001@gmail.com>
2026-02-22 09:42:15 +0530

    feat: implement authentication middleware and update protected API route

--- apps/app-backend/src/index.ts
+++ apps/app-backend/src/index.ts
@@ -1,14 +1,33 @@
 import "dotenv/config";
 import { Hono } from "hono";
-import { auth } from "./utils/auth";
+import { createMiddleware } from "hono/factory";
+import { type AuthType, auth, type MaybeAuthType } from "./utils/auth";
 
-const app = new Hono().basePath("/api");
+const requireAuth = createMiddleware<{ Variables: AuthType }>(
+	async (c, next) => {
+		const user = c.get("user");
+		if (!user) return c.json({ error: "Unauthorized" }, 401);
+		return next();
+	},
+);
+
+const protectedApi = new Hono().use("*", requireAuth).get("/", (c) => {
+	const user = c.get("user");
+	console.log("Protected API accessed by user:", user);
+	return c.json(user);
+});
+
+const app = new Hono<{ Variables: MaybeAuthType }>().basePath("/api");
 
 const route = app
 	.on(["POST", "GET"], "/auth/*", (c) => auth.handler(c.req.raw))
-	.get("/", (c) => {
-		return c.text("Hello Hono!");
-	});
+	.use("*", async (c, next) => {
+		const session = await auth.api.getSession({ headers: c.req.raw.headers });
+		c.set("user", session?.user || null);
+		c.set("session", session?.session || null);
+		return next();
+	})
+	.route("/protected", protectedApi);
 
 export type AppType = typeof route;
 
