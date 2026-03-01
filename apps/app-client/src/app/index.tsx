cff87cd7448b96e8f7b9dc2b9a7a13fa2e820239
Diptesh Choudhuri <ignisda2001@gmail.com>
2026-03-01 22:30:19 +0530

    feat: add basic app-client

---
+++ apps/app-client/src/app/index.tsx
@@ -0,0 +1,17 @@
+import { Text, View, StyleSheet } from "react-native";
+
+export default function Index() {
+  return (
+    <View style={styles.container}>
+      <Text>Edit src/app/index.tsx to edit this screen.</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
