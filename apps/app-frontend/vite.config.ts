a15c4de5936676bc5e9fe10c25724e56a2cfe0e4
Diptesh Choudhuri <ignisda2001@gmail.com>
2026-03-01 17:08:13 +0530

    chore: add basic app

---
+++ apps/app-frontend/vite.config.ts
@@ -0,0 +1,19 @@
+import tailwindcss from "@tailwindcss/vite";
+import { devtools } from "@tanstack/devtools-vite";
+import { tanstackStart } from "@tanstack/react-start/plugin/vite";
+import viteReact from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+import tsconfigPaths from "vite-tsconfig-paths";
+
+const config = defineConfig({
+	server: { host: true, port: 3005, strictPort: true, allowedHosts: true },
+	plugins: [
+		devtools(),
+		tsconfigPaths({ projects: ["./tsconfig.json"] }),
+		tailwindcss(),
+		tanstackStart(),
+		viteReact(),
+	],
+});
+
+export default config;
