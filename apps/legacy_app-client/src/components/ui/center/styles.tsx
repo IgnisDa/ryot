0f91722eb8408b022e8bed9763b182069e48e910
Diptesh Choudhuri <ignisda2001@gmail.com>
2026-03-07 15:37:01 +0530

    chore: remove legacy application

--- apps/legacy_app-client/src/components/ui/center/styles.tsx
+++
@@ -1,8 +0,0 @@
-import { isWeb, tva } from "@gluestack-ui/utils/nativewind-utils";
-
-// oxlint-disable-next-line typescript-eslint/no-unnecessary-condition
-const baseStyle = isWeb ? "flex flex-col relative z-0" : "";
-
-export const centerStyle = tva({
-	base: `justify-center items-center ${baseStyle}`,
-});
--- apps/legacy_app-client/src/test-setup.ts
+++
@@ -1,6 +0,0 @@
-const noop = () => {};
-
-console.log = noop;
-console.info = noop;
-console.warn = noop;
-console.error = noop;
