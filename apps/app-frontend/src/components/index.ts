91170411a89e86ad762f39e4e449f4385a2b5dc9
Diptesh Choudhuri <ignisda2001@gmail.com>
2026-03-12 00:09:20 +0530

    chore: delete file

--- apps/app-frontend/src/components/index.ts
+++
@@ -1,9 +0,0 @@
-export { EntityCard, type EntityCardProps } from "./EntityCard";
-export { Sidebar } from "./Sidebar";
-export type {
-	SidebarEntitySchema,
-	SidebarFacet,
-	SidebarProps,
-	SidebarView,
-} from "./Sidebar.types";
-export { StatsCard, type StatsCardProps } from "./StatsCard";
