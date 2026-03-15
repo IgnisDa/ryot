d9809e41063ed4ca9fcc320590768b0440e80560
Diptesh Choudhuri <ignisda2001@gmail.com>
2026-03-15 22:59:29 +0530

    chore: add Bull Board dependencies and refactor server initialization

--- apps/app-backend/src/app/server.ts
+++ apps/app-backend/src/app/server.ts
@@ -1,10 +1,26 @@
+import { createBullBoard } from "@bull-board/api";
+import { BullMQAdapter } from "@bull-board/api/bullMQAdapter";
+import { HonoAdapter } from "@bull-board/hono";
 import { serveStatic } from "@hono/node-server/serve-static";
 import { Hono } from "hono";
+import { getQueues } from "~/lib/queue";
 import { metricsMiddleware } from "~/modules/system/middleware";
 import { apiApp } from "./api";
 
-export const app = new Hono()
-	.use("*", metricsMiddleware)
-	.route("/api", apiApp)
-	.use("*", serveStatic({ root: "./client" }))
-	.use("*", serveStatic({ path: "./client/_shell.html" }));
+export const getServer = () => {
+	const app = new Hono()
+		.use("*", metricsMiddleware)
+		.route("/api", apiApp)
+		.use("*", serveStatic({ root: "./client" }))
+		.use("*", serveStatic({ path: "./client/_shell.html" }));
+
+	const systemBackgroundEndpoint = "/api/system/background";
+	const serverAdapter = new HonoAdapter(serveStatic);
+	createBullBoard({
+		serverAdapter,
+		queues: [new BullMQAdapter(getQueues().sandboxScriptQueue)],
+	});
+	serverAdapter.setBasePath(systemBackgroundEndpoint);
+	app.route(systemBackgroundEndpoint, serverAdapter.registerPlugin());
+	return app;
+};
