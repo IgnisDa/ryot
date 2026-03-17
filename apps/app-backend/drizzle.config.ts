c6e7f2658069f19ab2427dcd12b6d62aade7fe3e
Diptesh Choudhuri <ignisda2001@gmail.com>
2026-03-17 22:22:50 +0530

    chore: change file location

--- apps/app-backend/drizzle.config.ts
+++ apps/app-backend/drizzle.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "drizzle-kit";
 import { config } from "~/lib/config";
 
 export default defineConfig({
-	out: "./drizzle",
+	out: "./src/drizzle",
 	casing: "snake_case",
 	dialect: "postgresql",
 	schema: "./src/lib/db/schema/index.ts",
