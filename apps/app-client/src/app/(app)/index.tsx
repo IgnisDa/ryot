b0890588b7bdec802d2ec0ee11277cb7020e971e
Diptesh Choudhuri <ignisda2001@gmail.com>
2026-03-29 01:18:13 +0530

    feat(app-client): use dedicate workspace atom

--- apps/app-client/src/app/(app)/index.tsx
+++ apps/app-client/src/app/(app)/index.tsx
@@ -1,5 +1,8 @@
 import { Redirect } from "expo-router";
 
+import { useWorkspace } from "@/modules/server/state";
+
 export default function AppIndex() {
-	return <Redirect href="/media" />;
+	const workspace = useWorkspace();
+	return <Redirect href={{ pathname: "/[workspace]", params: { workspace } }} />;
 }
