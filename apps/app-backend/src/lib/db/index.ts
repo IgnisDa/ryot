9e5cc2c8c5bef960954259e73d4b5f0501dd2efb
Diptesh Choudhuri <ignisda2001@gmail.com>
2026-03-31 06:33:56 +0530

    fix: cicular import issues

--- apps/app-backend/src/lib/db/index.ts
+++ apps/app-backend/src/lib/db/index.ts
@@ -1,12 +1,8 @@
 export * as schema from "./schema";
 
 import { drizzle } from "drizzle-orm/node-postgres";
-import { migrate } from "drizzle-orm/node-postgres/migrator";
 import { Pool } from "pg";
 import { config } from "~/lib/config";
-import { seedInitialDatabase } from "./seed";
-
-const migrationsFolder = `${process.cwd()}/src/drizzle`;
 
 export const pool = new Pool({ connectionString: config.DATABASE_URL });
 
@@ -14,8 +10,3 @@ export const db = drizzle({ client: pool, casing: "snake_case" });
 
 export type DbTransaction = Parameters<Parameters<typeof db.transaction>[0]>[0];
 export type DbClient = typeof db | DbTransaction;
-
-export const migrateDB = async () => {
-	await migrate(db, { migrationsFolder });
-	await seedInitialDatabase(db);
-};
