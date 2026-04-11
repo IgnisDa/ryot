eb0de24537d46c0208d071ec11730367181ae26b
Diptesh Choudhuri <ignisda2001@gmail.com>
2026-04-12 00:46:45 +0530

    chore: basic stuff

--- apps/website/vite.config.ts
+++ apps/website/vite.config.ts
@@ -2,14 +2,9 @@ import { reactRouter } from "@react-router/dev/vite";
 import tailwindcss from "@tailwindcss/vite";
 import { safeRoutes } from "safe-routes/vite";
 import { defineConfig } from "vite";
-import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
+	resolve: { tsconfigPaths: true },
 	server: { allowedHosts: true },
-	plugins: [
-		reactRouter(),
-		safeRoutes(),
-		tailwindcss(),
-		tsconfigPaths({ ignoreConfigErrors: true }),
-	],
+	plugins: [reactRouter(), safeRoutes(), tailwindcss()],
 });
