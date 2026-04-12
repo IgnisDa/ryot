bd9a4b6f1248d7190111333665ea19a604311127
Diptesh Choudhuri <ignisda2001@gmail.com>
2026-04-12 09:33:59 +0530

    feat: add TMDB support for movies and shows with schema definitions and API integration

---
+++ apps/app-backend/src/lib/media/movie.ts
@@ -0,0 +1,13 @@
+import { z } from "@hono/zod-openapi";
+import { toAppSchemaProperties } from "@ryot/ts-utils";
+import { nullableIntSchema } from "../zod/base";
+import { freeCreatorSchema, mediaPropertiesSchema } from "./common";
+
+export const moviePropertiesSchema = mediaPropertiesSchema.extend({
+	runtime: nullableIntSchema,
+	freeCreators: z.array(freeCreatorSchema),
+});
+
+export const moviePropertiesJsonSchema = toAppSchemaProperties(
+	moviePropertiesSchema,
+);
