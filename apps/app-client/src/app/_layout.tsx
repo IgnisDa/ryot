f09d98c32b529e3e44d20a156902a19a50bc8a13
Diptesh Choudhuri <ignisda2001@gmail.com>
2026-04-26 21:58:09 +0530

    feat(app-client): enhance dev menu with app storage clear functionality

--- apps/app-client/src/app/_layout.tsx
+++ apps/app-client/src/app/_layout.tsx
@@ -3,6 +3,7 @@ import "@/global.css";
 import { RegistryProvider } from "@effect/atom-react";
 import { Lora_400Regular, Lora_500Medium, Lora_600SemiBold } from "@expo-google-fonts/lora";
 import { Outfit_400Regular, Outfit_500Medium, Outfit_600SemiBold } from "@expo-google-fonts/outfit";
+import { reloadAppAsync } from "expo";
 import * as DevClient from "expo-dev-client";
 import { useFonts } from "expo-font";
 import { Stack } from "expo-router";
@@ -19,7 +20,12 @@ void SplashScreen.preventAutoHideAsync();
 
 if (__DEV__ && Platform.OS !== "web") {
 	void DevClient.registerDevMenuItems([
-		{ name: "Clear app storage", callback: () => void clearAppStorage() },
+		{
+			name: "Clear app storage",
+			callback: () => {
+				void clearAppStorage().then(() => reloadAppAsync("Cleared app storage"));
+			},
+		},
 	]);
 }
 
