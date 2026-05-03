2b39f88346234b8a265637f3fc5258887c67b1b6
Diptesh Choudhuri <ignisda2001@gmail.com>
2026-05-03 17:13:51 +0530

    feat: integrate fetch from expo in API client functions

--- apps/app-client/src/lib/api.ts
+++ apps/app-client/src/lib/api.ts
@@ -1,8 +1,10 @@
 import type { paths } from "@ryot/generated/openapi/app-backend";
+import { fetch } from "expo/fetch";
 import createFetchClient from "openapi-fetch";
 
 export function createApiClient(serverUrl: string) {
 	return createFetchClient<paths>({
+		fetch,
 		credentials: "include",
 		baseUrl: `${serverUrl}/api`,
 	});
