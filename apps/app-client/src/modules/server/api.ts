f739e1edd450a8553f0f5983e92290f26fa750be
Diptesh Choudhuri <ignisda2001@gmail.com>
2026-05-10 23:12:12 +0530

    feat(app-client): use new appQueryClient

--- apps/app-client/src/modules/server/api.ts
+++ apps/app-client/src/modules/server/api.ts
@@ -3,16 +3,11 @@ import { Effect } from "effect";
 import { FetchHttpClient } from "effect/unstable/http";
 import { Atom } from "effect/unstable/reactivity";
 
-import { serverUrlAtom } from "@/modules/server/state";
-import { CLOUD_URL } from "@/modules/server/url";
+import { appQueryClient } from "@/api/query-client";
 
 const publicApiRuntime = Atom.runtime(FetchHttpClient.layer);
 
-export const systemConfigAtom = publicApiRuntime.atom((get) =>
-	makeContractClient(`${get(serverUrlAtom) ?? CLOUD_URL}/api`).pipe(
-		Effect.flatMap((client) => client.system.config()),
-	),
-);
+export const systemConfigAtom = appQueryClient.query("system", "config", {});
 
 export const connectToServerAtom = publicApiRuntime.fn((serverUrl: string) =>
 	makeContractClient(`${serverUrl}/api`).pipe(Effect.flatMap((client) => client.system.health())),
