2ba829c748755734104105d8dadb4b6de596036a
Diptesh Choudhuri <ignisda2001@gmail.com>
2026-05-16 09:37:12 +0530

    fix(api): update base URL for fetch client and auth client to localhost fix(vite): change server port from 3005 to 5000

--- apps/app-frontend/src/lib/auth.tsx
+++ apps/app-frontend/src/lib/auth.tsx
@@ -3,6 +3,7 @@ import { createAuthClient } from "better-auth/client";
 import { tanstackStartCookies } from "better-auth/tanstack-start";
 
 export const authClient = createAuthClient({
+	baseURL: "http://localhost:8000/api/auth",
 	plugins: [apiKeyClient(), tanstackStartCookies()],
 });
 
