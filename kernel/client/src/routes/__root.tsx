8d3147228f3118d5540ea8c4fdc5f1807c061c71
Diptesh Choudhuri <ignisda2001@gmail.com>
2026-06-27 17:01:05 +0530

    chore: [clientplugin - pre works] add basic tanstack application
    chore: update port

---
+++ kernel/client/src/routes/__root.tsx
@@ -0,0 +1,28 @@
+import { TanStackDevtools } from "@tanstack/react-devtools";
+import { Outlet, createRootRoute } from "@tanstack/react-router";
+import { TanStackRouterDevtoolsPanel } from "@tanstack/react-router-devtools";
+
+import "../styles.css";
+
+export const Route = createRootRoute({
+	component: RootComponent,
+});
+
+function RootComponent() {
+	return (
+		<>
+			<Outlet />
+			<TanStackDevtools
+				config={{
+					position: "bottom-right",
+				}}
+				plugins={[
+					{
+						name: "TanStack Router",
+						render: <TanStackRouterDevtoolsPanel />,
+					},
+				]}
+			/>
+		</>
+	);
+}
