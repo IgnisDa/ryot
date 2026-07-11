82cfc52f5c581bb4e4f928278eefc0ca1990bcaf
Diptesh Choudhuri <ignisda2001@gmail.com>
2026-07-11 18:46:27 +0530

    feat: add capacitor to project

---
+++ kernel/client/android/app/src/main/java/io/ryot/app/MainActivity.java
@@ -0,0 +1,5 @@
+package io.ryot.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ kernel/client/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from "@capacitor/cli";
+
+const config: CapacitorConfig = {
+	webDir: "dist",
+	appName: "Ryot",
+	appId: "io.ryot.app",
+};
+
+export default config;
